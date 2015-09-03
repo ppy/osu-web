af83c68599f5a2ca4073c9d2efbf6377c8524a9b
nanaya <me@myconan.net>
2015-09-03 13:46:21 +0900

    Immediate send on initial load.

--- resources/views/layout/metadata.blade.php
+++ resources/views/layout/metadata.blade.php
@@ -44,14 +44,17 @@
 	{{--
 		Documentation: https://developers.google.com/analytics/devguides/collection/analyticsjs/
 	--}}
-	<script>
+	<script data-turbolinks-track>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 	ga('create', '{{ config("services.ga.tracking_id") }}', 'auto');
-	$(document).on('ready page:load', function() {
+	ga('send', 'pageview');
+
+	{{-- turbolinks --}}
+	$(document).on('page:load', function() {
 		ga('set', 'location', document.location.href);
 		ga('send', 'pageview');
 	});
