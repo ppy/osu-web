3260ea383212ac3ff88baa06ce59547bec2545e7
nanaya <me@myconan.net>
2015-09-03 13:20:33 +0900

    Add licence and link to documentation.

--- resources/views/layout/_analytics.blade.php
+++ resources/views/layout/_analytics.blade.php
@@ -1,4 +1,25 @@
+{{--
+    Copyright 2015 ppy Pty. Ltd.
+
+    This file is part of osu!web. osu!web is distributed with the hope of
+    attracting more community contributions to the core ecosystem of osu!.
+
+    osu!web is free software: you can redistribute it and/or modify
+    it under the terms of the Affero GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+--}}
 @if(config("services.ga.tracking_id") !== null)
+	{{--
+		Documentation: https://developers.google.com/analytics/devguides/collection/analyticsjs/
+	--}}
 	<script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
