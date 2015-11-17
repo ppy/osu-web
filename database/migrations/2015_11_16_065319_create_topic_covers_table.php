8cccf5044353386da75dc4b06f1f3ebb21aef2db
nanaya <me@myconan.net>
2015-11-17 22:01:36 +0900

    Fix foreign key declaration and add index

--- database/migrations/2015_11_16_065319_create_topic_covers_table.php
+++ database/migrations/2015_11_16_065319_create_topic_covers_table.php
@@ -14,13 +14,24 @@ class CreateTopicCoversTable extends Migration
     {
         Schema::create('forum_topic_covers', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('topic_id')->nullable();
-            $table->integer('user_id')->nullable();
+            $table->mediumInteger('topic_id')->unsigned()->nullable();
+            $table->mediumInteger('user_id')->unsigned()->nullable();
             $table->string('hash');
             $table->string('ext');
             $table->timestamps();
 
-            $table->index(['topic_id']);
+            $table->index('topic_id');
+            $table->index('user_id');
+
+            $table->foreign('topic_id')
+                ->references('topic_id')
+                ->on('phpbb_topics')
+                ->onDelete('set null');
+
+            $table->foreign('user_id')
+                ->references('user_id')
+                ->on('phpbb_users')
+                ->onDelete('set null');
         });
     }
 
