d96f84ad7a5a21890d89f88a6bfe58ddc006ac32
Dean Herbert <pe@ppy.sh>
2015-12-08 18:16:06 +0900

    All actions on OrdersController are safe to be authenticated only.

--- app/Http/Controllers/Store/Admin/OrdersController.php
+++ app/Http/Controllers/Store/Admin/OrdersController.php
@@ -13,11 +13,7 @@ class OrdersController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth', ['only' => [
-            'index',
-            'show',
-            'update',
-        ]]);
+        $this->middleware('auth');
 
         return parent::__construct();
     }
