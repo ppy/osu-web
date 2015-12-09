08cafa40a057ab29033e9c2578fb4646c39b7c16
Jamie Taylor <me@nekodex.net>
2015-12-09 19:00:25 +0900

    Fix non-API beatmap score lookups

--- app/Models/BeatmapSet.php
+++ app/Models/BeatmapSet.php
@@ -266,7 +266,7 @@ class BeatmapSet extends Model
         }
 
         if (!empty($rank)) {
-            $klass = presence($mode != null) ? Score\Model::getClass($mode) : Score\Combined::class;
+            $klass = presence($mode != null) ? Score\Best\Model::getClass($mode) : Score\Best\Combined::class;
             $scores = $klass::forUser(Auth::user())->whereIn('rank', $rank)->lists('beatmapset_id');
             $matchParams[] = ['ids' => ['type' => 'beatmaps', 'values' => $scores]];
         }
