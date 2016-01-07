aa7a42a279b6179b0786940835878f39b7e355ee
nanaya <me@myconan.net>
2016-01-07 16:28:25 +0900

    Initial model for rank history

---
+++ app/Models/RankHistory.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ *    Copyright 2015 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class RankHistory extends Model
+{
+    protected $table = 'osu_user_performance_rank';
+
+    public $timestamps = false;
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'mode' => 'integer',
+    ];
+
+    public function getDataAttribute()
+    {
+        $data = [];
+
+        for ($i = 0; $i < 90; $i++) {
+            $column = "r{$i}";
+
+            $data[] = intval($this->$column);
+        }
+
+        return $data;
+    }
+}
