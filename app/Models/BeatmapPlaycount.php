0c3aeaa83346ce67655789af4cebd7b33910e0a7
nanaya <me@myconan.net>
2016-01-07 20:20:12 +0900

    Add model for playcount

---
+++ app/Models/BeatmapPlaycount.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ *    Copyright 2015 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BeatmapPlaycount extends Model
+{
+    protected $table = 'osu_user_beatmap_playcount';
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'beatmap_id' => 'integer',
+        'playcount' => 'integer',
+    ];
+
+    public $timestamps = false;
+
+    public function beatmap()
+    {
+        return $this->belongsTo(Beatmap::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
