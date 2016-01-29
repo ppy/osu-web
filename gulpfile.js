f2e2ac59f41cf4f7fea7aecf25ca3a71a0e81657
nanaya <me@myconan.net>
2016-01-29 18:20:09 +0900

    A better filename
    Note that the resulting 'vendor' name is still misleading as well.

--- gulpfile.js
+++ gulpfile.js
@@ -40,10 +40,10 @@ elixir(function(mix) {
   .copy('node_modules/font-awesome/fonts', 'public/vendor/fonts/font-awesome')
   .copy('node_modules/photoswipe/dist/default-skin', 'public/vendor/_photoswipe-default-skin')
   .less('app.less')
-  .browserify(['vendor-npm.js'], 'resources/assets/js/build/vendor-npm.js')
+  .browserify(['vendor-modules.js'], 'resources/assets/js/build/vendor-modules.js')
   .scripts([
     path.join(composer_root, 'helthe/turbolinks/Resources/public/js/turbolinks.js'),
-    'build/vendor-npm.js',
+    'build/vendor-modules.js',
     'ga.js',
     'messages.js',
   ], 'public/js/vendor.js')
