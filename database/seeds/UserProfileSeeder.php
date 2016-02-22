a5be7416db533743bac1881d14b9b27c024be04f
Hugh Percival <hugh.percival@digital-results.com>
2016-02-22 16:30:11 +0000

    StyleCI edit

--- database/seeds/UserProfileSeeder.php
+++ database/seeds/UserProfileSeeder.php
@@ -31,48 +31,48 @@ class UserProfileSeeder extends Seeder
 
             $bmcount = 0;
             foreach ($bms as $bm) {
-              if ($bmcount < 5) {
-                // $bm = array_rand($bms, 1);
+                if ($bmcount < 5) {
+                    // $bm = array_rand($bms, 1);
                 // dd($bm);
                 if (DB::table('osu_favouritemaps')->where('user_id', $usr_id)->where('beatmapset_id', $bm['beatmapset_id'])->first()) {
                     DB::table('osu_favouritemaps')->where('user_id', $usr_id)->where('beatmapset_id', $bm['beatmapset_id'])->delete();
                 }
-                $fav = new App\Models\FavouriteBeatmapSet;
-                $fav->beatmapset_id = $bm['beatmapset_id'];
-                $fav->user_id = $usr_id;
-                $fav->save();
+                    $fav = new App\Models\FavouriteBeatmapSet;
+                    $fav->beatmapset_id = $bm['beatmapset_id'];
+                    $fav->user_id = $usr_id;
+                    $fav->save();
 
             // Add a random couple few first place ranks
 
                 $bm = $bms[rand(0, count($bms) - 1)];
-                if (DB::table('osu_user_beatmap_playcount')->where('user_id', $usr_id)->where('beatmap_id', $bm['beatmap_id'])->first()) {
-                    DB::table('osu_user_beatmap_playcount')->where('user_id', $usr_id)->where('beatmap_id', $bm['beatmap_id'])->delete();
-                }
-                $playcount = new App\Models\BeatmapPlaycount;
+                    if (DB::table('osu_user_beatmap_playcount')->where('user_id', $usr_id)->where('beatmap_id', $bm['beatmap_id'])->first()) {
+                        DB::table('osu_user_beatmap_playcount')->where('user_id', $usr_id)->where('beatmap_id', $bm['beatmap_id'])->delete();
+                    }
+                    $playcount = new App\Models\BeatmapPlaycount;
 
-                $playcount->user_id = $usr_id;
-                $playcount->beatmap_id = $bm['beatmap_id'];
-                $playcount->playcount = rand(0, 1500);
-                $playcount->save();
+                    $playcount->user_id = $usr_id;
+                    $playcount->beatmap_id = $bm['beatmap_id'];
+                    $playcount->playcount = rand(0, 1500);
+                    $playcount->save();
 
-                $bm = $bms[rand(0, count($bms) - 1)];
-                if (DB::table('osu_leaders')->where('beatmap_id', $bm['beatmap_id'])->first()) {
                     $bm = $bms[rand(0, count($bms) - 1)];
+                    if (DB::table('osu_leaders')->where('beatmap_id', $bm['beatmap_id'])->first()) {
+                        $bm = $bms[rand(0, count($bms) - 1)];
                     // try once more
                     if (DB::table('osu_leaders')->where('beatmap_id', $bm['beatmap_id'])->first()) {
                         DB::table('osu_leaders')->where('beatmap_id', $bm['beatmap_id'])->delete();
                     }
-                }
-                $leader = new App\Models\BeatmapLeader\Osu;
-                $leader->beatmap_id = $bm['beatmap_id'];
-                $leader->user_id = $usr_id;
-                $leader->score_id = $bm['score_id'];
-                $leader->save();
+                    }
+                    $leader = new App\Models\BeatmapLeader\Osu;
+                    $leader->beatmap_id = $bm['beatmap_id'];
+                    $leader->user_id = $usr_id;
+                    $leader->score_id = $bm['score_id'];
+                    $leader->save();
 
-                ++$bmcount;
-              }
+                    ++$bmcount;
+                }
             }
-          }
+        }
         } catch (\Illuminate\Database\QueryException $e) {
             echo "Error: Unable to save User Profile Data\r\n".$e;
         } catch (Exception $ex) {
