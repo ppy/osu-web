812f4f70aa6fd7120f440aab316e2caf5c980237
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-02-27 12:57:47 +0100

    Added migration and model for osu_slack_users

---
+++ app/Models/SlackUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SlackUser extends Model
+{
+    protected $table = "osu_slack_users";
+    protected $primaryKey = "slack_id";
+
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $dates = ["created_at"];
+}
