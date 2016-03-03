e2f13bbc6964a1ad24c2aeb373bf7b7bf81f806f
Boris-Chengbiao Zhou <bobo1239@web.de>
2016-03-03 16:07:56 +0100

    Remove unneeded migrations table

--- database/db-osu-data.sql
+++ database/db-osu-data.sql
@@ -11,33 +11,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` VALUES ('2015_01_01_133337_base_tables',1);
-INSERT INTO `migrations` VALUES ('2015_01_01_133337_store_base_tables',1);
-INSERT INTO `migrations` VALUES ('2015_01_23_114030_extend_products_table',1);
-INSERT INTO `migrations` VALUES ('2015_01_29_013313_add_last_order_tracking_state',1);
-INSERT INTO `migrations` VALUES ('2015_02_03_051509_record_cost_and_shipping_in_order',1);
-INSERT INTO `migrations` VALUES ('2015_02_04_165733_record_cost_and_shipping_in_order_populate',1);
-INSERT INTO `migrations` VALUES ('2015_05_01_013313_add_item_family',1);
-INSERT INTO `migrations` VALUES ('2015_06_04_083548_infinite_stock_products',1);
-INSERT INTO `migrations` VALUES ('2015_06_04_093340_custom_product_implementations',1);
-INSERT INTO `migrations` VALUES ('2015_06_18_022845_order_transaction_id',1);
-INSERT INTO `migrations` VALUES ('2015_06_19_050528_add_order_paid_at_column',1);
-INSERT INTO `migrations` VALUES ('2015_07_08_042238_create_tournaments_tables',1);
-INSERT INTO `migrations` VALUES ('2015_07_09_084235_create_user_profile_customizations_table',1);
-INSERT INTO `migrations` VALUES ('2015_07_09_091745_add_tournament_description_text',1);
-INSERT INTO `migrations` VALUES ('2015_07_09_105144_create_tournaments_registrations_table',1);
-INSERT INTO `migrations` VALUES ('2015_08_10_083016_add_slug_to_achievements',1);
-INSERT INTO `migrations` VALUES ('2015_11_16_065319_create_topic_covers_table',1);
-INSERT INTO `migrations` VALUES ('2015_11_20_122003_add_unique_index_to_forum_topic_covers_forum_id',1);
-INSERT INTO `migrations` VALUES ('2015_12_07_061307_create_forum_covers_table',1);
-INSERT INTO `migrations` VALUES ('2016_01_12_074035_unique_user_customization_user_id',1);
-INSERT INTO `migrations` VALUES ('2016_01_26_194005_disable_products',1);
-INSERT INTO `migrations` VALUES ('2016_01_29_090810_add_description_to_achievements',1);
-
---
 -- Dumping data for table `osu_genres`
 --
 
@@ -75,4 +48,4 @@ INSERT INTO `osu_languages` VALUES (11,'Italian',5);
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-19 18:41:04
+-- Dump completed on 2016-03-03 15:06:03
