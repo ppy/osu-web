73080b1366ba5010549c5afe26ef33205efd6f63
Jamie Taylor <me@nekodex.net>
2016-03-03 13:23:38 +0900

    Still throw the exception when sentry isn’t enabled

--- app/Jobs/RegenerateBeatmapSetCover.php
+++ app/Jobs/RegenerateBeatmapSetCover.php
@@ -7,6 +7,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Raven_Client;
+use App\Exceptions\SilencedException;
 
 class RegenerateBeatmapSetCover extends Job implements SelfHandling, ShouldQueue
 {
@@ -34,14 +36,17 @@ class RegenerateBeatmapSetCover extends Job implements SelfHandling, ShouldQueue
             echo "Processing {$this->beatmapset->beatmapset_id}... ";
             $this->beatmapset->regenerateCovers();
             echo "ok.\n";
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
+            echo "errored.\n";
             if (config('osu.beatmap_processor.sentry')) {
                 $tags = [
                     'beatmapset_id' => $this->beatmapset->beatmapset_id,
                 ];
                 $client = new Raven_Client(config('osu.beatmap_processor.sentry'), ['tags' => $tags]);
                 $client->captureException($e);
-                throw new SilencedException("Silenced Exception: [{get_class($e)}] " . $e->getMessage());
+                throw new SilencedException("Silenced Exception: [".get_class($e)."] ".$e->getMessage());
+            } else {
+                throw $e;
             }
         }
     }
