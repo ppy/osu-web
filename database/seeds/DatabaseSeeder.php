dbdd79e4b3f7d52273f220a13a4918fd062b0803
Hugh Percival <hugh.percival@digital-results.com>
2016-03-04 15:28:10 +0000

    Indent one last file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,31 +13,31 @@ class DatabaseSeeder extends Seeder
     {
         try {
             // Beatmaps andsets
-          $this->command->info('Seeding Beatmaps...');
+            $this->command->info('Seeding Beatmaps...');
             $this->call(BeatmapSeeder::class);
 
-      // Users, Stats, Ranks
-          $this->command->info('Seeding Users and Stats...');
+            // Users, Stats, Ranks
+            $this->command->info('Seeding Users and Stats...');
             $this->call(UserSeeder::class);
 
-      // Events
-          $this->command->info('Seeding Events...');
+            // Events
+            $this->command->info('Seeding Events...');
             $this->call(EventSeeder::class);
 
-      // Scores
-          $this->command->info('Seeding Scores...');
+            // Scores
+            $this->command->info('Seeding Scores...');
             $this->call(ScoreSeeder::class);
 
-     // Forums, topics, posts etc
-          $this->command->info('Seeding Forum Data...');
+            // Forums, topics, posts etc
+            $this->command->info('Seeding Forum Data...');
             $this->call(ForumSeeder::class);
 
-      // Users Profile Data (Favourite maps, First place ranks, Playcounts)
-          $this->command->info('Seeding Users Profile Data (e.g. favourite maps, first place ranks, playcounts)');
+            // Users Profile Data (Favourite maps, First place ranks, Playcounts)
+            $this->command->info('Seeding Users Profile Data (e.g. favourite maps, first place ranks, playcounts)');
             $this->call(UserProfileSeeder::class);
 
-      // Miscellaneous Data (e.g. counts)
-          $this->command->info('Seeding Miscellaneous Data');
+            // Miscellaneous Data (e.g. counts)
+            $this->command->info('Seeding Miscellaneous Data');
             $this->call(MiscSeeder::class);
         } catch (ErrorException $er) {
             $this->command->error($er->getMessage());
