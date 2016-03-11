d8db50e60cfaedd6c777666d812eccde5b610f3b
Jamie Taylor <me@nekodex.net>
2016-03-11 17:52:16 +0900

    Remove unused coverUrl

--- app/Models/BeatmapSet.php
+++ app/Models/BeatmapSet.php
@@ -680,9 +680,4 @@ class BeatmapSet extends Model
     {
         return $this->belongsTo("App\Models\User", 'user_id', 'approvedby_id');
     }
-
-    public function coverUrl()
-    {
-        return "https://b.ppy.sh/thumb/{$this->beatmapset_id}l.jpg";
-    }
 }
