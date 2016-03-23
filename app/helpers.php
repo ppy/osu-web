276b4c1e3097aab33f271367fa662abd924275f3
Jamie Taylor <me@nekodex.net>
2016-03-23 17:06:05 +0900

    TIL: php function names aren’t case sensitive... THE MORE YOU KNOW ミ☆

--- app/helpers.php
+++ app/helpers.php
@@ -449,7 +449,7 @@ function deltree($dir)
 {
     $files = array_diff(scandir($dir), ['.', '..']);
     foreach ($files as $file) {
-        (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+        (is_dir("$dir/$file")) ? deltree("$dir/$file") : unlink("$dir/$file");
     }
 
     return rmdir($dir);
