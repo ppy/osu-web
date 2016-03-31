3ed45de83252f3675ff537810d293b887e1ac2c8
Jamie Taylor <me@nekodex.net>
2016-03-31 14:59:30 +0900

    Enable refresh tokens

--- config/oauth2.php
+++ config/oauth2.php
@@ -37,6 +37,11 @@ return [
             'class' => '\League\OAuth2\Server\Grant\PasswordGrant',
             'callback' => '\App\Libraries\OAuth2PasswordGrantVerifier',
             'access_token_ttl' => 3600
+        ],
+        'refresh_token' => [
+            'class' => '\League\OAuth2\Server\Grant\RefreshTokenGrant',
+            'access_token_ttl' => 3600,
+            'refresh_token_ttl' => 7.884e+6 // ~3 months
         ]
     ],
 
