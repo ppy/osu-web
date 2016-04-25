567cdc5d19435cf82bfbca58f5f70f58861f3218
nanaya <me@myconan.net>
2016-04-25 21:08:48 +0900

    Refactor store header to use bem

--- resources/views/store/header.blade.php
+++ resources/views/store/header.blade.php
@@ -15,36 +15,44 @@
     You should have received a copy of the GNU Affero General Public License
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
-<div class="osu-layout__row osu-layout__row--page header-row no-print" id="store-header">
-    {{--
-    * setting `display: flex` on main row div breaks width on
-    * several browsers (IE11, Firefox 36)
-    --}}
-    <div>
-        @if ((new Carbon\Carbon("2015-07-15"))->isFuture())
-            <a href="{{ action("StoreController@getListing") }}" class="store-logo--late"></a>
-        @else
-            <a href="{{ action("StoreController@getListing") }}" id="store-logo">
-                @include("store._logo")
-            </a>
-        @endif
-
-        @if(!isset($skip_back_link))
-            <div class="float float-left">
-                <a href="javascript:history.back()" class="float-link">
-                    <i class="fa fa-chevron-left"></i>
-                    <span>back</span>
+<div class="osu-layout__row osu-layout__row--page-compact header-row no-print">
+    <div class="store-header">
+        <div class="store-header__main">
+            @if ((new Carbon\Carbon("2015-07-15"))->isFuture())
+                <a href="{{ action("StoreController@getListing") }}" class="store-logo--late"></a>
+            @else
+                <a href="{{ action("StoreController@getListing") }}" class="store-logo">
+                    @include("store._logo")
                 </a>
-            </div>
-        @endif
+            @endif
 
-        @if(isset($cart) && $cart && $cart->items()->exists())
-            <div class="float float-right">
-                <a href="/store/cart" class="float-link">
-                    <span>{{{$cart->getItemCount()}}} item(s) in cart (${{{$cart->getSubtotal()}}})</span>
-                    <i class="fa fa-shopping-cart"></i>
-                </a>
-            </div>
-        @endif
+            @if(!isset($skip_back_link))
+                <div class="store-header__float store-header__float--left">
+                    <a href="javascript:history.back()" class="store-header__float-link">
+                        <span class="store-header__float-link-text store-header__float-link-text--icon">
+                            <i class="fa fa-chevron-left"></i>
+                        </span>
+
+                        <span class="store-header__float-link-text">
+                            back
+                        </span>
+                    </a>
+                </div>
+            @endif
+
+            @if(isset($cart) && $cart && $cart->items()->exists())
+                <div class="store-header__float store-header__float--right">
+                    <a href="/store/cart" class="store-header__float-link">
+                        <span class="store-header__float-link-text">
+                            {{ $cart->getItemCount() }} item(s) in cart (${{ $cart->getSubtotal() }})
+                        </span>
+
+                        <span class="store-header__float-link-text store-header__float-link-text--icon">
+                            <i class="fa fa-shopping-cart"></i>
+                        </span>
+                    </a>
+                </div>
+            @endif
+        </div>
     </div>
 </div>
