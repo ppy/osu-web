b1d4c9a219e8bb7064f4b59b358e31c777c2f3d1
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-05-02 10:08:20 +0200

    apparently i forgot to move some routes

--- app/helpers.php
+++ app/helpers.php
@@ -238,7 +238,7 @@ function nav_links()
         ];
     } else {
         $links['beatmaps'] = [
-            'getListing' => route('beatmaps'),
+            'index' => route('beatmapsets.index'),
         ];
     }
 
