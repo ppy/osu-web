deb3ce869a629e1d8d47e778ed64a13212a13f4c
nanaya <me@myconan.net>
2016-05-13 20:51:53 +0900

    Less colons

--- resources/lang/en/beatmaps.php
+++ resources/lang/en/beatmaps.php
@@ -150,11 +150,11 @@ return [
                     'country' => 'Country Ranking',
                     'friend' => 'Friend Ranking',
                     'first' => [
-                        'accuracy' => 'Accuracy:',
-                        'score' => 'Score:',
-                        'count300' => '300:',
-                        'count100' => '100:',
-                        'count50' => '50:',
+                        'accuracy' => 'Accuracy',
+                        'score' => 'Score',
+                        'count300' => '300',
+                        'count100' => '100',
+                        'count50' => '50',
                     ],
                     'list' => [
                         'rank-header' => 'Rank',
