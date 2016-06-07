9ad4cea053a0f5a4f0af949b90195e4a191f83a2
Reese <reesebiz@users.noreply.github.com>
2016-06-06 22:10:11 -0500

    Fixed login modal positioning

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -34,11 +34,40 @@
                 @endforeach
             </div>
             <a href="#" class="landing-nav__logo"><h1>osu!</h1></a>
-            <div class="landing-nav__section landing-nav__section--right">
-                <a href="#" class="landing-nav__section__link" data-toggle="modal" data-target="#user-dropdown-modal">{{ trans("users.login._") }}</a>
+            <div class="landing-nav__section landing-nav__section--right js-nav-avatar">
+                <a href="#" class="landing-nav__section__link" title="{{ trans("users.anonymous.login_link") }}" data-toggle="modal" data-target="#user-dropdown-modal">{{ trans("users.login._") }}</a>
                 <a href="{{ route("users.register") }}" class="landing-nav__section__link">{{ trans("users.signup._") }}</a>
             </div>
         </div>
+        @section('user-dropdown-modal')
+    <div id="user-dropdown-modal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog modal__dialog js-user-dropdown-modal__dialog">
+            @if (Auth::check())
+                <div class="js-react--user-card"></div>
+            @else
+                <div class="modal-content modal-content--no-shadow">
+                    <div class="modal-header modal-header--login"><h1 class="modal-header__title">{{ trans("users.login._") }}</h1></div>
+                    <div class="modal-body modal-body--user-dropdown modal-body--no-rounding">
+                        <h2 class="modal-body__title modal-body__title">{{ trans("users.login.title") }}</h2>
+
+                        {!! Form::open(["url" => route("users.login"), "id" => "login-form", "class" => "modal-body__form form", "data-remote" => true]) !!}
+                            <div class="form__input-group form-group form-group--compact">
+                                <input class="modal-af form-group__control form-control form-group__control--compact" name="username" type="text" placeholder="{{ trans("users.login.username") }}" required>
+                                <input class="form-group__control form-control form-group__control--compact" name="password" type="password" placeholder="{{ trans("users.login.password") }}" required>
+                            </div>
+
+                            <button class="btn-osu btn-osu-default form__button" type="submit"><i class="fa fa-sign-in"></i></button>
+                        {!! Form::close() !!}
+
+                        <p class="modal-body__paragraph"><a href="{{ route("users.forgot-password") }}" target="_blank">{{ trans("users.login.forgot") }}</a></p>
+                        <p class="modal-body__paragraph"><a href="{{ route("users.register") }}" target="_blank">{{ trans("users.login.register") }}</a></p>
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+@stop
+
     </nav>
     <header class="osu-layout__section osu-layout__section--full">
         <div class="osu-layout__row osu-layout__row--page-compact landing-hero">
@@ -74,9 +103,6 @@
     <footer>
         
     </footer>
-    <div class="flex-none nav-user-bar-container">
-        @include("objects.user-dropdown")
-    </div>
 @endsection
 
 @section ("script")
