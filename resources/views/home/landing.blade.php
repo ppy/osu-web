01bd5e402f45145f9f0f101659405e4f84840b64
Reese <reesebiz@users.noreply.github.com>
2016-06-09 08:00:04 -0500

    Added graph circle styling and download button responsiveness

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -20,97 +20,97 @@
     'title' => 'osu!',
     'blank' => 'true',
     'body_additional_classes' => 'osu-layout--body-landing'
-])
+    ])
 
-@section("content")
-<nav class="osu-layout__section osu-layout__section--minimum">
-    <!-- Mobile Navigation -->
-    @include('objects.smartphone-header', ['navLinks' => landing_nav_links(), 'subLinks' => false])
+    @section("content")
+    <nav class="osu-layout__section osu-layout__section--minimum">
+        <!-- Mobile Navigation -->
+        @include('objects.smartphone-header', ['navLinks' => landing_nav_links(), 'subLinks' => false])
 
-    <!-- Desktop Navigation -->
-    <div class="osu-layout__row landing-nav hidden-xs">
-        <div class="landing-nav__section landing-nav__section--left">
-            @foreach (landing_nav_links() as $section => $links)
-            <a href="{{ array_values($links)[0] }}" class="landing-nav__section__link {{ ($section == "home") ? "landing-nav__section__link--bold" : "" }}">{{ trans("layout.menu.$section._") }}</a>
-            @endforeach
-        </div>
-        <a href="#" class="landing-nav__logo"><h1>osu!</h1></a>
-        <div class="landing-nav__section landing-nav__section--right js-nav-avatar">
-            <a href="#" class="landing-nav__section__link" title="{{ trans("users.anonymous.login_link") }}" data-toggle="modal" data-target="#user-dropdown-modal">{{ trans("users.login._") }}</a>
-            <a href="{{ route("users.register") }}" class="landing-nav__section__link">{{ trans("users.signup._") }}</a>
-        </div>
-    </div>
-    <div id="user-dropdown-modal" class="modal fade" tabindex="-1">
-        <div class="modal-dialog modal__dialog js-user-dropdown-modal__dialog">
-            @if (Auth::check())
-            <div class="js-react--user-card"></div>
-            @else
-            <div class="modal-content modal-content--no-shadow">
-                <div class="modal-header modal-header--login"><h1 class="modal-header__title">{{ trans("users.login._") }}</h1></div>
-                <div class="modal-body modal-body--user-dropdown modal-body--no-rounding">
-                    <h2 class="modal-body__title modal-body__title">{{ trans("users.login.title") }}</h2>
-
-                    {!! Form::open(["url" => route("users.login"), "id" => "login-form", "class" => "modal-body__form form", "data-remote" => true]) !!}
-                    <div class="form__input-group form-group form-group--compact">
-                        <input class="modal-af form-group__control form-control form-group__control--compact" name="username" type="text" placeholder="{{ trans("users.login.username") }}" required>
-                        <input class="form-group__control form-control form-group__control--compact" name="password" type="password" placeholder="{{ trans("users.login.password") }}" required>
-                    </div>
-
-                    <button class="btn-osu btn-osu-default form__button" type="submit"><i class="fa fa-sign-in"></i></button>
-                    {!! Form::close() !!}
-
-                    <p class="modal-body__paragraph"><a href="{{ route("users.forgot-password") }}" target="_blank">{{ trans("users.login.forgot") }}</a></p>
-                    <p class="modal-body__paragraph"><a href="{{ route("users.register") }}" target="_blank">{{ trans("users.login.register") }}</a></p>
-                </div>
+        <!-- Desktop Navigation -->
+        <div class="osu-layout__row landing-nav hidden-xs">
+            <div class="landing-nav__section landing-nav__section--left">
+                @foreach (landing_nav_links() as $section => $links)
+                <a href="{{ array_values($links)[0] }}" class="landing-nav__section__link {{ ($section == "home") ? "landing-nav__section__link--bold" : "" }}">{{ trans("layout.menu.$section._") }}</a>
+                @endforeach
             </div>
-            @endif
+            <a href="#" class="landing-nav__logo"><h1>osu!</h1></a>
+            <div class="landing-nav__section landing-nav__section--right js-nav-avatar">
+                <a href="#" class="landing-nav__section__link" title="{{ trans("users.anonymous.login_link") }}" data-toggle="modal" data-target="#user-dropdown-modal">{{ trans("users.login._") }}</a>
+                <a href="{{ route("users.register") }}" class="landing-nav__section__link">{{ trans("users.signup._") }}</a>
+            </div>
+        </div>
+        <div id="user-dropdown-modal" class="modal fade" tabindex="-1">
+            <div class="modal-dialog modal__dialog js-user-dropdown-modal__dialog">
+                @if (Auth::check())
+                <div class="js-react--user-card"></div>
+                @else
+                <div class="modal-content modal-content--no-shadow">
+                    <div class="modal-header modal-header--login"><h1 class="modal-header__title">{{ trans("users.login._") }}</h1></div>
+                    <div class="modal-body modal-body--user-dropdown modal-body--no-rounding">
+                        <h2 class="modal-body__title modal-body__title">{{ trans("users.login.title") }}</h2>
+
+                        {!! Form::open(["url" => route("users.login"), "id" => "login-form", "class" => "modal-body__form form", "data-remote" => true]) !!}
+                        <div class="form__input-group form-group form-group--compact">
+                            <input class="modal-af form-group__control form-control form-group__control--compact" name="username" type="text" placeholder="{{ trans("users.login.username") }}" required>
+                            <input class="form-group__control form-control form-group__control--compact" name="password" type="password" placeholder="{{ trans("users.login.password") }}" required>
+                        </div>
+
+                        <button class="btn-osu btn-osu-default form__button" type="submit"><i class="fa fa-sign-in"></i></button>
+                        {!! Form::close() !!}
+
+                        <p class="modal-body__paragraph"><a href="{{ route("users.forgot-password") }}" target="_blank">{{ trans("users.login.forgot") }}</a></p>
+                        <p class="modal-body__paragraph"><a href="{{ route("users.register") }}" target="_blank">{{ trans("users.login.register") }}</a></p>
+                    </div>
+                </div>
+                @endif
+            </div>
+        </div>
+    </nav>
+    <div id="popup-container">
+        <div class="alert alert-dismissable popup-clone col-md-6 col-md-offset-3 text-center" style="display: none">
+            <button type="button" data-dismiss="alert" class="close"><i class="fa fa-close"></i></button>
+            <span class="popup-text"></span>
         </div>
     </div>
-</nav>
-<div id="popup-container">
-    <div class="alert alert-dismissable popup-clone col-md-6 col-md-offset-3 text-center" style="display: none">
-        <button type="button" data-dismiss="alert" class="close"><i class="fa fa-close"></i></button>
-        <span class="popup-text"></span>
-    </div>
-</div>
-<header class="osu-layout__section osu-layout__section--full">
-    <div class="osu-layout__row osu-layout__row--page-compact landing-hero">
-        <div class="landing-hero-slider">
-            @for($i = 1; $i <= 2; $i++)
-            <a href="#" class="landing-slide">
-                <span class="landing-slide__bg">
-                    <img class="landing-slide__bg--image" src="/images/layout/landing-page/home-slider-{{$i}}.jpg" alt="pippi">
-                </span>
-                <span class="landing-slide__cta">
-                    <span class="landing-slide__cta__content">{!! trans("home.landing.slogans.$i") !!}</span>
-                </span>
-            </a>
-            @endfor
-        </div>
-        <div class="landing-hero-download">
-            <div class="landing-hero-download__inner">
-                <a href="http://m1.ppy.sh/r/osu!install.exe" class="landing-download-button shadow-hover">
-                    <span class="fa fa-2x fa-cloud-download landing-download-button__icon hidden-xs"></span>
-                    <span class="landing-download-button__content">
-                        <span class="landing-download-button__content--top">{{ trans("home.landing.download._") }}</span>
-                        <span class="landing-download-button__content--bottom js-download-platform"></span>
+    <header class="osu-layout__section osu-layout__section--full">
+        <div class="osu-layout__row osu-layout__row--page-compact landing-hero">
+            <div class="landing-hero-slider">
+                @for($i = 1; $i <= 2; $i++)
+                <a href="#" class="landing-slide">
+                    <span class="landing-slide__bg">
+                        <img class="landing-slide__bg--image" src="/images/layout/landing-page/home-slider-{{$i}}.jpg" alt="pippi">
+                    </span>
+                    <span class="landing-slide__cta">
+                        <span class="landing-slide__cta__content">{!! trans("home.landing.slogans.$i") !!}</span>
                     </span>
                 </a>
-                <a href="{{ route('download') }}" class="landing-download-other js-download-other"></a>
+                @endfor
             </div>
+            <div class="landing-hero-download">
+                <div class="landing-hero-download__inner">
+                    <a href="http://m1.ppy.sh/r/osu!install.exe" class="landing-download-button shadow-hover">
+                        <span class="fa fa-2x fa-cloud-download landing-download-button__icon"></span>
+                        <span class="landing-download-button__content">
+                            <span class="landing-download-button__content--top">{{ trans("home.landing.download._") }}</span>
+                            <span class="landing-download-button__content--bottom js-download-platform"></span>
+                        </span>
+                    </a>
+                    <a href="{{ route('download') }}" class="landing-download-other js-download-other"></a>
+                </div>
+            </div>
+            <div class="js-landing-graph landing-graph">
+                <div class="landing-graph__info">
+                    <b>6,249,738</b> registered players, <b>2,845</b> online players now
+                </div>
+            </div>        
         </div>
-        <div class="js-landing-graph landing-graph">
-            <div class="landing-graph__info">
-                <b>6,249,738</b> registered players, <b>2,845</b> online players now
-            </div>
-        </div>        
-    </div>
-</header>
-<main>
-</main>
-<footer>
+    </header>
+    <main>
+    </main>
+    <footer>
 
-</footer>
+    </footer>
 @endsection
 
 @section ("script")
@@ -134,20 +134,20 @@
 
     // Define area
     var area = d3.svg.area().interpolate("basis")
-        .x(function(d) { return xScale(d.date); })
-        .y0(height)
-        .y1(function(d) { return yScale(d.users_osu); });
+    .x(function(d) { return xScale(d.date); })
+    .y0(height)
+    .y1(function(d) { return yScale(d.users_osu); });
 
     // Define svg canvas
     var svg = d3.select(".js-landing-graph")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Read in data
-    d3.csv("/landing-csv.csv", function(error, data){
+    d3.csv("/landing-csv.csv", function(error, data) {
         if (error) throw error;
 
         // Parsing data
@@ -162,12 +162,13 @@
 
         // Appending groups
         svg.append("path")
-            .datum(data)
-            .attr("class", "landing-graph__area")
-            .attr("d", area);
+        .datum(data)
+        .attr("class", "landing-graph__area")
+        .attr("d", area);
 
         // Append peak text
         var maxElem = null;
+
         //Find the date for the max
         for(var i = data.length - 1; i >= 0; i--)
         {
@@ -177,16 +178,16 @@
 
         //Append text at 45 degrees to circle
         var text = svg.append("text")
-            .attr('class', 'landing-graph__peak-text')
-            .text(Lang.get('home.landing.peak', {"count": maxElem.users_osu.toLocaleString()}))
-            .attr('y', -peakR)
-            .attr('x', xScale(maxElem.date) + peakR);
+        .attr('class', 'landing-graph__peak--text')
+        .text(Lang.get('home.landing.peak', {"count": maxElem.users_osu.toLocaleString()}))
+        .attr('y', -peakR * 2)
+        .attr('x', xScale(maxElem.date) + peakR * 2);
 
         var peak = svg.append("circle")
-            .attr('class', 'landing-graph__peak-circle')
-            .attr('cy', 0)
-            .attr('cx', xScale(maxElem.date))
-            .attr('r', peakR);
+        .attr('class', 'landing-graph__peak--circle')
+        .attr('cy', 0)
+        .attr('cx', xScale(maxElem.date))
+        .attr('r', peakR);
     });
 </script>
 @endsection
