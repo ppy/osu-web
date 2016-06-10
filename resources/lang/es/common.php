a36315aa831672c4b544ea9ed16d09ed0df40e19
Uniterno <pablopabo1@hotmail.com>
2016-06-10 00:21:29 -0500

    Create common.php

---
+++ resources/lang/es/common.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *    Copyright 2015 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed in the hopes of
+ *    attracting more community contributions to the core ecosystem of osu!
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+return [
+        'buttons' => [
+            'cancel' => 'Cancelar',
+            'post' => 'Postear',
+            'reply' => 'Responder',
+            'reset' => 'Resetear',
+            'save' => 'Guardar',
+            'show_more' => 'mostrar más',
+        ],
+        'count' => [
+            'item' => ':count unidad|:count unidades',
+        ],
+        'dropzone' => [
+            'target' => 'soltar aquí para cargar',
+        ],
+        'pagination' => [
+            'previous' => 'anterior',
+            'next' => 'siguiente',
+        ],
+        'time' => [
+            'days_ago' => 'hace :count día|hace :count días',
+            'hours_ago' => 'hace :count hora|hace :count horas',
+            'now' => 'ahora',
+        ],
+        'title' => [
+            'notice' => 'Aviso',
+        ],
+];
