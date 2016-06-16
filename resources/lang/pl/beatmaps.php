2b7d2c638a328427639b45900bbebb03c3f5eff5
Cezary Gozdalski <cezary2000@gmail.com>
2016-06-16 19:05:00 +0200

    Polish translation

---
+++ resources/lang/pl/beatmaps.php
@@ -0,0 +1,231 @@
+<?php
+
+/**
+ *    Copyright 2015 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed in the hopes of
+ *    attracting more community contributions to the core ecosystem of osu!
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'discussion-posts' => [
+        'store' => [
+            'error' => 'Zapisywanie posta nie powiodło się.',
+        ],
+    ],
+
+    'discussion-votes' => [
+        'update' => [
+            'error' => 'Aktualizacja oceny nie powiodła się.',
+        ],
+    ],
+
+    'discussions' => [
+        'collapse' => [
+            'all-collapse' => 'Zwiń wszystkie',
+            'all-expand' => 'Rozwiń wszystkie',
+        ],
+
+        'edit' => 'edytuj',
+        'edited' => 'Ostatnio edytowane przez :editor :update_time',
+        'empty' => [
+            'empty' => 'Brak dyskusji!',
+            'filtered' => 'Nie znaleziono żadnych dyskusji zgodnych z wybranym filtrem.',
+        ],
+
+        'message_hint' => [
+            'in_general' => 'Ten post znajdzie się w generalnej dyskusji tego mapsetu. Aby zmodować tę mapę, zacznij post od konkretnego momentu (np. 00:12:345).',
+            'in_timeline' => 'Aby zgłosić uwagi dotyczące kilku różnych momentów piosenki, utwórz dla nich odrębne komentarze (po jednym dla każdego momentu utworu).',
+        ],
+
+        'message_placeholder' => 'Pisz tutaj',
+
+        'message_type' => [
+            'praise' => 'Pochwała',
+            'problem' => 'Problem',
+            'suggestion' => 'Sugestia',
+        ],
+
+        'message_type_select' => 'Wybierz typ komentarza',
+
+        'mode' => [
+            'general' => 'Główne',
+            'timeline' => 'Oś czasu',
+        ],
+
+        'require-login' => 'Zaloguj się, aby móc postować i odpowiadać.',
+        'resolved' => 'Rozwiązane',
+
+        'show' => [
+            'title' => 'Dyskusja na temat mapy',
+        ],
+
+        'stats' => [
+            'mine' => 'Moje',
+            'pending' => 'Oczekujące',
+            'praises' => 'Pochwały',
+            'resolved' => 'Rozwiązane',
+            'total' => 'Łącznie',
+        ],
+    ],
+
+    'listing' => [
+        'search' => [
+            'prompt' => 'wpisz poszukiwane wyrażenie...',
+            'options' => 'Więcej opcji wyszukiwarki',
+            'not-found' => 'brak wyników',
+            'not-found-quote' => '... nic nie znaleziono.',
+        ],
+        'mode' => 'Tryb gry',
+        'status' => 'Status mapy',
+        'mapped-by' => 'zmapowana przez :mapper',
+        'source' => 'pochodzi z :source',
+        'load-more' => 'Załaduj więcej...',
+    ],
+    'beatmapset' => [
+        'show' => [
+            'details' => [
+                'made-by' => 'stworzony przez :user',
+                'submitted' => 'dodana ',
+                'ranked' => 'rankingowa od ',
+                'logged-out' => 'Musisz się zalogować, aby pobierać beatmapy!',
+                'download' => [
+                    'normal' => 'pobierz',
+                    'direct' => 'osu!direct',
+                    'no-video' => 'wersja bez wideo',
+                ],
+            ],
+            'stats' => [
+              //this is left intentionally in english, you can't translate these so it sounds normal
+                'cs' => 'Circle Size',
+                'hp' => 'HP Drain',
+                'od' => 'Accuracy',
+                'ar' => 'Approach Rate',
+                'stars' => 'Trudność',
+                'length' => 'Długość',
+                'bpm' => 'BPM',
+
+                'chart' => [
+                    'cs' => 'CS',
+                    'hp' => 'HP',
+                    'od' => 'OD',
+                    'ar' => 'AR',
+                    'sd' => 'SD',
+                ],
+
+                'source' => 'Źródło',
+                'tags' => 'Tagi',
+            ],
+            'extra' => [
+                'description' => [
+                    'title' => 'Opis',
+                ],
+                'success-rate' => [
+                    'title' => 'Wskaźnik sukcesu',
+                    'rate' => 'Wskaźnik sukcesu: :percentage%',
+                    'points' => 'Wykres',
+                    'retry' => 'Ponowne próby',
+                    //this is left intentionally as well
+                    'fail' => 'Fail',
+                ],
+                'scoreboard' => [
+                    'title' => 'Tablica wyników',
+                    'no-scores' => [
+                        'global' => 'Brak wyników. Może powinieneś jakieś zdobyć?',
+                        'loading' => 'Ładowanie wyników...',
+                        'country' => 'Nikt z twojego kraju nie ustanowił tutaj wyniku!',
+                        'friend' => 'Żaden z twoich znajomych nie ma tutaj wyniku!',
+                    ],
+                    'supporter-only' => 'Musisz być supporterem, aby uzyskać dostęp do rankingu krajowego i znajomych!',
+                    'supporter-link' => 'Kliknij <a href=":link">tutaj</a>, aby zobaczyć, co jeszcze otrzymujesz w zamian za bycie supporterem!',
+                    'global' => 'Ranking globalny',
+                    'country' => 'Ranking krajowy',
+                    'friend' => 'Ranking znajomych',
+                    'first' => [
+                        'accuracy' => 'Celność',
+                        'score' => 'Wynik',
+                        'count300' => '300',
+                        'count100' => '100',
+                        'count50' => '50',
+                    ],
+                    'list' => [
+                        'rank-header' => 'Miejsce',
+                        'player-header' => 'Gracz',
+                        'score' => 'Wynik',
+                        'accuracy' => 'Celność',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'mode' => [
+        'any' => 'Jakikolwiek',
+        'osu' => 'osu!',
+        'taiko' => 'osu!taiko',
+        'fruits' => 'osu!catch',
+        'mania' => 'osu!mania',
+    ],
+    'status' => [
+        'any' => 'Jakikolwiek',
+        'ranked-approved' => 'Rankingowe & Zatwierdzone',
+        'approved' => 'Zatwierdzone',
+        'faves' => 'Ulubione',
+        'modreqs' => 'Wymagające modów',
+        'pending' => 'Oczekujące',
+        'graveyard' => 'Cmentarz',
+        'my-maps' => 'Moje mapy',
+    ],
+    'genre' => [
+        'any' => 'Jakikolwiek',
+        'unspecified' => 'Jakikolwiek',
+        'video-game' => 'Gra',
+        'anime' => 'Anime',
+        'rock' => 'Rock',
+        'pop' => 'Pop',
+        'other' => 'Inne',
+        'novelty' => 'Nowość',
+        'hip-hop' => 'Hip Hop',
+        'electronic' => 'Electro',
+    ],
+    'language' => [
+    'any' => 'Jakikolwiek',
+    'english' => 'Angielski',
+    'chinese' => 'Chiński',
+    'french' => 'Francuski',
+    'german' => 'Niemiecki',
+    'italian' => 'Włoski',
+    'japanese' => 'Japoński',
+    'korean' => 'Koreański',
+    'spanish' => 'Hiszpański',
+    'swedish' => 'Szwedzki',
+    //left over untranslated on purpose
+    'instrumental' => 'Instrumental',
+    'other' => 'Inne',
+    ],
+    'extra' => [
+        'video' => 'Posiada wideo',
+        'storyboard' => 'Posiada storyboard',
+    ],
+    'rank' => [
+        'any' => 'Jakikolwiek',
+        'XH' => 'Srebrne SS',
+        'X' => 'SS',
+        'SH' => 'Srebrne S',
+        'S' => 'S',
+        'A' => 'A',
+        'B' => 'B',
+        'C' => 'C',
+        'D' => 'D',
+    ],
+];
---
+++ resources/lang/pl/layout.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ *    Copyright 2015 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed in the hopes of
+ *    attracting more community contributions to the core ecosystem of osu!
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'defaults' => [
+        'page_description' => 'osu! - Rytm jest tylko *kliknięcie* stąd!  Z Ouendanem/EBA, Taiko i oryginalnymi trybami gry, a także w pełni funkcjonalnym edytorem!',
+    ],
+
+    'menu' => [
+        'home' => [
+            '_' => 'strona główna',
+            'getNews' => 'aktualności',
+            'getChangelog' => 'zmiany',
+            'getDownload' => 'pobieranie',
+            'getIcons' => 'ikony',
+            'supportTheGame' => 'wspomóż grę',
+        ],
+        'help' => [
+            '_' => 'pomoc',
+            'getWiki' => 'wiki',
+            'getFaq' => 'faq',
+            'getSupport' => 'pomoc techniczna',
+        ],
+        'beatmaps' => [
+            '_' => 'beatmapy',
+            'show' => 'info',
+            'index' => 'lista',
+            // 'getPacks' => 'paczki',
+            // 'getCharts' => 'charty', //not so sure if that's the right translation
+        ],
+        'beatmapsets' => [
+            '_' => 'mapsety',
+            'discussion' => 'modowanie',
+        ],
+        'ranking' => [
+            '_' => 'ranking',
+            'getOverall' => 'ogólny',
+            'getCountry' => 'krajowy',
+            'getCharts' => 'charty',
+            'getMapper' => 'mapper',
+            'index' => 'ogólne',
+        ],
+        'community' => [
+            '_' => 'społeczność',
+            'getForum' => 'forum',
+            'getChat' => 'czat',
+            'getSupport' => 'pomoc techniczna',
+            'getLive' => 'na żywo',
+            'getSlack' => 'osu!dev',
+            'profile' => 'profil',
+            'tournaments' => 'turnieje',
+            'tournaments-index' => 'turnieje',
+            'tournaments-show' => 'informacje o turniejach',
+            'forum-topics-create' => 'forum',
+            'forum-topics-show' => 'forum',
+            'forum-forums-index' => 'forum',
+            'forum-forums-show' => 'forum',
+        ],
+        'error' => [
+            '_' => 'błąd',
+            '404' => 'nie znaleziono strony',
+            '403' => 'brak dostępu',
+            '401' => 'brak dostępu',
+            '405' => 'nie znaleziono strony',
+            '500' => 'coś się popsuło',
+            '503' => 'konserwacja',
+        ],
+        'user' => [
+            '_' => 'użytkownik',
+            'getLogin' => 'zaloguj się',
+            'disabled' => 'wyłączono',
+
+            'register' => 'zarejestruj się',
+            'reset' => 'odzyskaj dostęp',
+            'new' => 'nowy',
+
+            'messages' => 'Wiadomości',
+            'settings' => 'Ustawienia',
+            'logout' => 'Wyloguj się',
+            'help' => 'Pomoc',
+        ],
+        'store' => [
+            '_' => 'sklep',
+            'getListing' => 'przedmioty',
+            'getCart' => 'koszyk',
+
+            'getCheckout' => 'zapłać',
+            'getInvoice' => 'paragon',
+            'getProduct' => 'produkt',
+
+            'new' => 'nowy',
+            'home' => 'strona główna',
+            'index' => 'strona główna',
+            'thanks' => 'dzięki',
+        ],
+        'admin-forum' => [
+            '_' => 'admin::forum',
+            'forum-covers-index' => 'nagłówki na forum',
+        ],
+        'admin-store' => [
+            '_' => 'admin::store',
+            'orders-index' => 'zamówienia',
+            'orders-show' => 'zamówienie',
+        ],
+        'admin' => [
+            '_' => 'admin',
+            'logs-index' => 'logi',
+            'beatmapsets' => [
+                '_' => 'mapsety',
+                'covers' => 'nagłówki',
+                'show' => 'szczegóły',
+            ],
+        ],
+    ],
+    'errors' => [
+        '404' => [
+            'error' => 'Nie znaleziono strony',
+            'description' => "Przepraszamy, ale strona, jaką chciałeś ujrzeć, nie istnieje!",
+            'link' => false,
+        ],
+        '403' => [
+            'error' => "Nie powinieneś tu być.",
+            'description' => 'Jednakże, możesz spróbować się wrócić.',
+            'link' => false,
+        ],
+        '401' => [
+            'error' => "Nie powinieneś tu być.",
+            'description' => 'Jednakże, możesz spróbować się wrócić. Albo się zalogować',
+            'link' => false,
+        ],
+        '405' => [
+            'error' => 'Nie znaleziono strony',
+            'description' => "Przepraszamy, ale strona, jaką chciałeś ujrzeć, nie istnieje!",
+            'link' => false,
+        ],
+        '500' => [
+            'error' => 'O nie! Coś się popsuło! ;_;',
+            'description' => "Jesteśmy automatycznie powiadamieni o tym problemie.",
+            'link' => false,
+        ],
+        'fatal' => [
+            'error' => 'O nie! Coś się popsuło (bardzo)! ;_;',
+            'description' => "Jesteśmy automatycznie powiadamieni o tym problemie.",
+            'link' => false,
+        ],
+        '503' => [
+            'error' => 'Konserwacja!',
+            'description' => "Konserwacja zajmuje od 5 sekund do 10 minut. Jeżeli jesteśmy niedostępni na dłużej, spojrzyj <a>:link tutaj</a>, aby dowiedzieć się więcej.",
+            'link' => [
+                'text' => '@osustatus',
+                'href' => 'https://twitter.com/osustatus',
+            ],
+        ],
+        // used by sentry if it returns an error
+        'reference' => "Na wszelki wypadek, tutaj jest kod który możesz dać osobom z pomocy technicznej!",
+    ],
+];
