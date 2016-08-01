2d088edc07553a19ce82f6d4ccda21fb9ebe22af
nanaya <me@myconan.net>
2016-08-01 15:30:15 +0900

    Avoid checking out empty order

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -231,7 +231,7 @@ class StoreController extends Controller
     {
         $order = $this->userCart();
 
-        if (!$order) {
+        if ($order->items()->count() === 0) {
             return response(['message' => 'cart is empty'], 422);
         }
 
