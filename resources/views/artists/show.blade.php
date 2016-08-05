7f110cc1c3839c7415d3e3e6e6f8903cef835db7
Jamie Taylor <me@nekodex.net>
2016-08-05 19:05:40 +0900

    Add retina image support for artist page (except album art)

--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -24,8 +24,42 @@
 ])
 
 @section("content")
+    <style type="text/css">
+        @if($images['header_url'] != '')
+        .osu-page-header-v2--artist {
+          background-image: url('{{$images['header_url']}}');
+        }
+        @endif
+        @if($images['cover_url'] != '')
+        .artist__portrait {
+            background-image: url('{{$images['cover_url']}}');
+        }
+        @endif
+        @if($artist->label !== null)
+        .artist__label-overlay {
+            background-image: url('{{$artist->label->icon_url}}');
+        }
+        @endif
+        @media (-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx) {
+            @if($images['header_url'] != '')
+            .osu-page-header-v2--artist {
+                background-image: url('{{retinaify($images['header_url'])}}');
+            }
+            @endif
+            @if($images['cover_url'] != '')
+            .artist__portrait {
+                background-image: url('{{retinaify($images['cover_url'])}}');
+            }
+            @endif
+            @if($artist->label !== null)
+            .artist__label-overlay {
+                background-image: url('{{retinaify($artist->label->icon_url)}}');
+            }
+            @endif
+        }
+    </style>
     <div class="osu-layout__row">
-        <div class="osu-page-header-v2 osu-page-header-v2--artist" style="{{ $images['header_url'] != '' ? "background-image: url('{$images['header_url']}');" : ''}}">
+        <div class="osu-page-header-v2 osu-page-header-v2--artist">
             <div class="osu-page-header-v2__overlay"></div>
             <div class="osu-page-header-v2__subtitle osu-page-header-v2__subtitle--artist">Artist &raquo;</div>
             <div class="osu-page-header-v2__title">{{$artist->name}}</div>
@@ -38,9 +72,9 @@
                 <div class="js-react--artistTrackplayer"></div>
             </div>
             <div class="page-contents__content--sidebar">
-                <div class="artist__portrait" style="{{ $images['cover_url'] != '' ? "background-image: url('{$images['cover_url']}');" : ''}}"">
+                <div class="artist__portrait">
                     @if($artist->label !== null)
-                        <a class="artist__label-overlay" style="background-image: url('{{$artist->label->icon_url}}');" href="{{$artist->label->website}}"></a>
+                        <a class="artist__label-overlay" href="{{$artist->label->website}}"></a>
                     @endif
                 </div>
                 <div class="artist__links-area">
