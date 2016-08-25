07cb2cce619432ca6e6d394c64e0678da72de9ed
Jamie Taylor <me@nekodex.net>
2016-08-25 16:33:32 +0900

    Create a view for aggregate contest votes

---
+++ database/migrations/2016_08_25_055700_create_contest_vote_aggregates_view.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContestVotesAggregateView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('CREATE VIEW contest_vote_aggregates AS SELECT contest_id, contest_entry_id, count(*) as votes FROM contest_votes GROUP BY contest_entry_id');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW contest_vote_aggregates');
+    }
+}
