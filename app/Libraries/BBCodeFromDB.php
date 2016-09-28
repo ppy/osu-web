77e5bda938da4e5851633ad492278e62e7fd3108
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-09-29 00:00:56 +0200

    Properly scale youtube embeds

--- app/Libraries/BBCodeFromDB.php
+++ app/Libraries/BBCodeFromDB.php
@@ -274,8 +274,8 @@ class BBCodeFromDB
 
     public function parseYoutube($text)
     {
-        $text = str_replace("[youtube:{$this->uid}]", "<div class='bbcode__video'><iframe src='https://www.youtube.com/embed/", $text);
-        $text = str_replace("[/youtube:{$this->uid}]", "?rel=0' frameborder='0' allowfullscreen></iframe></div>", $text);
+        $text = str_replace("[youtube:{$this->uid}]", "<div class='bbcode__video-box'><div class='bbcode__video'><iframe src='https://www.youtube.com/embed/", $text);
+        $text = str_replace("[/youtube:{$this->uid}]", "?rel=0' frameborder='0' allowfullscreen></iframe></div></div>", $text);
 
         return $text;
     }
