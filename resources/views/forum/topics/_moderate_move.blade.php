b60b627f00f875497e35957825c3e7a6c83004e9
nanaya <me@myconan.net>
2016-10-06 18:20:59 +0900

    Button and their not-quite-dom-ness
    Flexbug #9.

--- resources/views/forum/topics/_moderate_move.blade.php
+++ resources/views/forum/topics/_moderate_move.blade.php
@@ -16,7 +16,7 @@
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
 <button
-    class="btn-circle btn-circle--topic-nav"
+    class="btn-circle btn-circle--button btn-circle--topic-nav"
     data-target="#forum-topic-move-modal"
     data-toggle="modal"
     type="button"
