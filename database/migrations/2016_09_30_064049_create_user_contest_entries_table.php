1c80943f3a3a3d70de755b6ef09b176758f757ac
Jamie Taylor <me@nekodex.net>
2016-10-07 22:42:34 +0900

    UserContestEntry model

---
+++ database/migrations/2016_09_30_064049_create_user_contest_entries_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserContestEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_contest_entries', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+
+            $table->increments('id');
+
+            $table->string('original_filename');
+            $table->string('hash');
+            $table->string('ext');
+
+            $table->integer('filesize');
+
+            $table->mediumInteger('user_id')->unsigned();
+
+            $table->integer('contest_id')->unsigned();
+
+            $table->timestamps();
+
+            $table->index(['user_id']);
+            $table->index(['contest_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_contest_entries');
+    }
+}
