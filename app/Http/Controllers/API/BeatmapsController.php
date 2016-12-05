74e6d9f849583dad67fcf0e2c6da17a04c418ebd
Edho Arief <me@myconan.net>
2016-12-05 07:44:48 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/API/BeatmapsController.php
+++ app/Http/Controllers/API/BeatmapsController.php
@@ -17,13 +17,14 @@
  *    You should have received a copy of the GNU Affero General Public License
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace App\Http\Controllers\API;
 
-use Response;
-use Request;
-use App\Transformers\API\ScoreTransformer;
-use App\Transformers\API\BeatmapTransformer;
 use App\Models\Beatmap;
+use App\Transformers\API\BeatmapTransformer;
+use App\Transformers\API\ScoreTransformer;
+use Request;
+use Response;
 
 class BeatmapsController extends Controller
 {
