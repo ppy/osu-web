a9737c9d74e5d2f7166e703d9474065de51284f5
nanaya <me@myconan.net>
2017-01-12 02:37:47 +0900

    Now with darker background
    So dark it almost blends with the page itself =D

--- resources/views/wiki/show.blade.php
+++ resources/views/wiki/show.blade.php
@@ -1,5 +1,5 @@
 {{--
-    Copyright 2016 ppy Pty. Ltd.
+    Copyright 2015-2017 ppy Pty. Ltd.
 
     This file is part of osu!web. osu!web is distributed with the hope of
     attracting more community contributions to the core ecosystem of osu!.
@@ -17,8 +17,9 @@
 --}}
 
 @extends('master', [
-    'titleAppend' => $path,
+    'body_additional_classes' => 'osu-layout--body-333',
     'title' => null,
+    'titleAppend' => $path,
 ])
 
 @section('content')
