24194a0150f6521b0ee2f85c8288f61cc149bb4e
kj415j45 <919815238@qq.com>
2017-01-27 05:54:33 +0800

    finish event

---
+++ resources/lang/zh-cn/events.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'achievement' => '<strong><em>:user</em></strong> 解锁了 "<strong>:achievement</strong>" 成就!',
+    'beatmapset_delete' => ':beatmapset 已经被删除.',
+    'beatmapset_revive' => ':beatmapset 已经被 <strong>:user</strong> 恢复.',
+    'beatmapset_update' => '<strong><em>:user</em></strong> 更新了谱面 "<em>:beatmapset</em>"',
+    'beatmapset_upload' => '<strong><em>:user</em></strong> 提交了一个新谱面 ":beatmapset"',
+    'empty' => "该用户最近没有活动",
+    'rank' => '<strong><em>:user</em></strong> 在 <em>:beatmap</em> (:mode) 中取得第 :rank 名',
+    'rank_lost' => '<strong><em>:user</em></strong> 失去了 <em>:beatmap</em> (:mode) 的第一名',
+    'user_support_again' => '<strong>:user</strong> 又一次支持了osu! - 感谢您的慷慨捐赠!',
+    'user_support_first' => '<strong>:user</strong> 成为osu!支持者 - 感谢您的慷慨捐赠!',
+    'user_support_gift' => '<strong>:user</strong> 收到了给osu!支持者的礼物!',
+    'username_change' => '<strong>:previousUsername</strong> 将他的名字改为 <strong><em>:user</strong></em>!',
+];
