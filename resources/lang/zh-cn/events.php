b220cc6886676a951a55467d586a2fc998ad1375
kj415j45 <919815238@qq.com>
2017-01-27 06:09:32 +0800

    fix to observe StyleCI

--- resources/lang/zh-cn/events.php
+++ resources/lang/zh-cn/events.php
@@ -24,7 +24,7 @@ return [
     'beatmapset_revive' => ':beatmapset 已经被 <strong>:user</strong> 恢复.',
     'beatmapset_update' => '<strong><em>:user</em></strong> 更新了谱面 "<em>:beatmapset</em>"',
     'beatmapset_upload' => '<strong><em>:user</em></strong> 提交了一个新谱面 ":beatmapset"',
-    'empty' => "该用户最近没有活动",
+    'empty' => '该用户最近没有活动',
     'rank' => '<strong><em>:user</em></strong> 在 <em>:beatmap</em> (:mode) 中取得第 :rank 名',
     'rank_lost' => '<strong><em>:user</em></strong> 失去了 <em>:beatmap</em> (:mode) 的第一名',
     'user_support_again' => '<strong>:user</strong> 又一次支持了osu! - 感谢您的慷慨捐赠!',
