903ae803320c0b1e2ec22f63e3b34c6b38f29705
kj415j45 <919815238@qq.com>
2017-01-27 08:46:13 +0800

    finish common

---
+++ resources/lang/zh-cn/common.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'confirmation' => '您确定要这样做吗?',
+    'saved' => '已保存',
+
+    'buttons' => [
+        'cancel' => '取消',
+        'delete' => '删除',
+        'permalink' => '永久连接',
+        'post' => '发送',
+        'reply' => '回复',
+        'reset' => '重置',
+        'save' => '保存',
+        'show_more' => '显示更多',
+        'upload_image' => '上传图片',
+    ],
+
+    'count' => [
+        'item' => ':count unit|:count units',//需要上下文
+    ],
+
+    'dropzone' => [
+        'target' => '拖动到此处以上传',
+    ],
+
+    'pagination' => [
+        'previous' => '上一个',//可能不准确
+        'next' => '下一个',    //需要上下文
+    ],
+
+    'time' => [
+        'days_ago' => ':count day ago|:count days ago',
+        'hours_ago' => ':count hour ago|:count hours ago',
+        'now' => 'now',
+        'remaining' => 'Time Remaining',
+    ],
+
+    'title' => [
+        'notice' => 'Notice',
+    ],
+];
