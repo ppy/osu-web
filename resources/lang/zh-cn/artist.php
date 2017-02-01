112ea76bd9effbdeaa9cfeb89433c93d01a96f11
kj415j45 <919815238@qq.com>
2017-02-01 19:57:49 +0800

    finish artist

---
+++ resources/lang/zh-cn/artist.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'page_description' => 'osu! 杰出艺术家',
+    'title' => '杰出艺术家',
+
+    'beatmaps' => [
+        '_' => '谱面',
+        'download' => '下载谱面模版',
+        'download-na' => '谱面模版暂时不可用',
+    ],
+    'index' => [
+        'description' => '为了给osu!带来新的原创歌曲,我们正在和杰出艺术家合作. 他们的部分专辑被osu!团队选中作为制谱的绝佳素材.有的杰出艺术家还为osu!提供了高质量的新专辑.<br><br>这些专辑均提供了预先调时的.osz文件,并且已经获得了官方授权(Use in osu! and osu!-related content)',  //TODO 翻译可能不准确,不会翻译最后一句
+    ],
+    'links' => [
+        'site' => '官方网站',
+    ],
+    'songs' => [
+        '_' => 'Songs',
+        'count' => '1 首歌|:count 首歌',
+        'exclusive' => 'osu! exclusive', //需要上下文
+    ],
+];
