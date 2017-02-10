691c1b6425e2097bc79a221cc841766872becd80
MuhunKim <yale2a11@gmail.com>
2017-02-10 15:04:26 +0900

    Support Splash Screen for Installed Web App Chrome Web docs : https://developers.google.com/web/updates/2015/10/splashscreen

--- resources/views/layout/metadata.blade.php
+++ resources/views/layout/metadata.blade.php
@@ -17,6 +17,9 @@
 --}}
 <link rel="shortcut icon" href="{{ Config::get("osu.static", "//s.ppy.sh") }}/favicon.ico" type="image/vnd.microsoft.icon">
 <link rel="icon" href="{{ Config::get("osu.static", "//s.ppy.sh") }}/favicon.ico" type="image/vnd.microsoft.icon">
+
+<link rel="manifest" href="/manifest.json">
+
 <meta charset="utf-8">
 <meta name="description" content="{{ $pageDescription or trans('layout.defaults.page_description') }}">
 <meta name="keywords" content="osu, peppy, ouendan, elite, beat, agents, ds, windows, game, taiko, tatsujin, simulator, sim, xna, ddr, beatmania, osu!, osume">
