d581ef4d96916fcf608639c3ce474faf04a1964d
Kuron-kun <kuron.kun@outlook.com>
2017-05-18 21:58:38 -0300

    Updating artist.php
    Fixes some small grammar issues and added new lines (previously outdated).

--- resources/lang/pt-br/artist.php
+++ resources/lang/pt-br/artist.php
@@ -22,13 +22,16 @@ return [
     'page_description' => 'Artistas em destaque no osu!',
     'title' => 'Artistas em destaque',
 
+    'admin' => [
+        'hidden' => 'ARTISTA ATUALMENTE OCULTO',
+    ],
     'beatmaps' => [
         '_' => 'Beatmaps',
         'download' => 'Baixar modelo de beatmap',
         'download-na' => 'Modelo indisponível no momento',
     ],
     'index' => [
-        'description' => 'Artistas em destaque são artistas com quem estamos trabalhando em colaboração para que possamos trazer músicas novas e originais para o osu!. Esses artistas e suas músicas são escolhidos a dedo pela equipe do osu! pelas suas incríveis capacidades musicais e capacidade de mapeamento. Alguns desses artistas também criaram novas músicas exclusivas para uso no osu!.<br><br>Todas as músicas presentes nessa seção são fornecidas como arquivos .osz previamente temporizados e foram oficialmente licenciadas para uso no osu! e qualquer conteúdo relacionado ao mesmo.',
+        'description' => 'Artistas em destaque são artistas com quem estamos trabalhando em colaboração para que possamos trazer músicas novas e originais para o osu!. Esses artistas e suas músicas são escolhidas a dedo pela equipe do osu! e por sua ótima viabilidade no editor . Alguns desses artistas também criaram novas músicas exclusivas para uso no osu!.<br><br>Todas as músicas presentes nessa seção são fornecidas como arquivos .osz com BPM e Offset pré-disponibilizados e foram oficialmente licenciadas para uso no osu! e qualquer conteúdo relacionado.',
     ],
     'links' => [
         'site' => 'Site oficial',
