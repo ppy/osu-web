fc66edf54ab972d7047c07dd1c890f00d46ad574
Kuron-kun <kuron.kun@outlook.com>
2017-05-19 22:18:43 -0300

    Added password_reset.php
    Added the password_reset.php file.

---
+++ resources/lang/pt-br/password_reset.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'title' => 'Redefinir senha',
+
+    'button' => [
+        'cancel' => 'Cancelar',
+        'resend' => 'Reenviar email de verificação',
+        'set' => 'Definir senha',
+        'start' => 'Começar',
+    ],
+
+    'email' => [
+        'subject' => 'Recuperação da conta no osu!',
+    ],
+
+    'error' => [
+        'contact_support' => 'Por favor entre em contato com o suporte para recuperar a conta.',
+        'is_privileged' => 'Fala com o peppy lul.',
+        'missing_key' => 'Necessário.',
+        'too_many_tries' => 'Muitas tentativas falhas.',
+        'user_not_found' => 'Usuário selecionado não existe.',
+        'wrong_key' => 'Código incorreto.',
+    ],
+
+    'notice' => [
+        'sent' => 'Verifique seu email para obter o código.',
+        'saved' => 'Nova senha salva!',
+    ],
+
+    'started' => [
+        'password' => 'Nova senha',
+        'password_confirmation' => 'Confirmar senha',
+        'title' => 'Redefinindo senha na conta <strong>:username</strong>.',
+        'verification_key' => 'Código de verificação',
+    ],
+
+    'starting' => [
+        'username' => 'Insira endereço de email ou nome de usuário',
+    ],
+];
