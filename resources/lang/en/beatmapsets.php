4d69f5af95f571e21a405e31a522a6ed7c0fc3f4
walle303 <walle303@users.noreply.github.com>
2017-05-27 18:38:38 +0000

    Fix some awkward wording

--- resources/lang/en/beatmapsets.php
+++ resources/lang/en/beatmapsets.php
@@ -92,7 +92,7 @@ return [
             ],
             'no_scores' => [
                 'country' => 'No one from your country has set a score on this map yet!',
-                'friend' => 'No one of your friends has set a score on this map yet!',
+                'friend' => 'None of your friends has set a score on this map yet!',
                 'global' => 'No scores yet. Maybe you should try setting some?',
                 'loading' => 'Loading scores...',
                 'unranked' => 'Unranked beatmap.',
