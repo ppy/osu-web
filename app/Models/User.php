60cb9c8387c7155eec2740e884f2ecfa7046f6a0
nanaya <me@nanaya.pro>
2017-05-29 17:33:05 +0900

    Search that username

--- app/Models/User.php
+++ app/Models/User.php
@@ -161,6 +161,18 @@ class User extends Model implements AuthenticatableContract, Messageable
         return [];
     }
 
+    public static function search($params)
+    {
+        $params['query'] = $params['query'] ?? null;
+        $params['limit'] = clamp(get_int($params['limit'] ?? 50), 1, 50);
+        $params['page'] = max(1, get_int($params['page'] ?? 1));
+
+        return static::where('username', 'LIKE', "{$params['query']}%")
+            ->orderBy(DB::raw('LENGTH(username)'))
+            ->limit($params['limit'])
+            ->offset(($params['page'] - 1) * $params['limit']);
+    }
+
     public function validateUsernameChangeTo($username)
     {
         if (!$this->hasSupported()) {
