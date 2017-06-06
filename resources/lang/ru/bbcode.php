ab861d1c723b9599ce985f7da8dc53a507037acb
Leonard <nekonyx@mail.ru>
2017-06-06 21:43:39 +0600

    done: bbcode

---
+++ resources/lang/ru/bbcode.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'bold' => 'Жирный',
+    'heading' => 'Заголовок',
+    'image' => 'Изображение',
+    'italic' => 'Курсив',
+    'link' => 'Ссылка',
+    'list' => 'Список',
+    'list_numbered' => 'Нумерованный список',
+    'size' => [
+        '_' => 'Размер',
+        'tiny' => 'Мелкий',
+        'small' => 'Маленький',
+        'normal' => 'Нормальный',
+        'large' => 'Большой',
+    ],
+    'spoilerbox' => 'Спойлер',
+    'strikethrough' => 'Зачёркнутый',
+];
