56261323a8b9dd0419dd62854017ae7df657b9ae
Leonard <nekonyx@mail.ru>
2017-06-06 11:33:35 +0600

    common

---
+++ resources/lang/ru/common.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'confirmation' => 'Вы уверены?',
+    'saved' => 'Сохранено',
+
+    'buttons' => [
+        'cancel' => 'Отмена',
+        'delete' => 'Удалить',
+        'permalink' => 'прямая ссылка',
+        'post' => 'Пост',
+        'reply' => 'Ответить',
+        'reset' => 'Сбросить',
+        'save' => 'Сохранить',
+        'saving' => 'Сохранение...',
+        'show_more' => 'показать больше',
+        'upload_image' => 'загрузить изображение',
+    ],
+
+    'count' => [
+        'item' => ':count единица|:count единиц',
+    ],
+
+    'device' => [
+        'keyboard' => 'Клавиатурой',
+        'mouse' => 'Мышкой',
+        'tablet' => 'Планшетом',
+        'touch' => 'Сенсорным экраном',
+    ],
+
+    'dropzone' => [
+        'target' => 'отпустите тут для загрузки',
+    ],
+
+    'pagination' => [
+        'previous' => 'пред',
+        'next' => 'след',
+    ],
+
+    'score_count' => [
+        'count_100' => '100',
+        'count_300' => '300',
+        'count_50' => '50',
+        'count_geki' => 'МАКС',
+        'count_katu' => '200',
+        'count_miss' => 'Промах',
+    ],
+
+    'time' => [
+        'days_ago' => ':count день назад|:count дней назад',
+        'hours_ago' => ':count час назад|:count часов назад',
+        'now' => 'сейчас',
+        'remaining' => 'Времени осталось',
+    ],
+
+    'title' => [
+        'notice' => 'Замечание',
+    ],
+];
