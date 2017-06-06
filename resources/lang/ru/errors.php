351f0c1298564315fe4f92a0ffc1825680ac62d6
Leonard <nekonyx@mail.ru>
2017-06-06 13:36:45 +0600

    done: errors

---
+++ resources/lang/ru/errors.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'codes' => [
+        'http-401' => 'Авторизируйтесь для продолжения.',
+        'http-403' => 'Доступ запрещён.',
+        'http-429' => 'Слишком много попыток. Попробуйте позже.',
+    ],
+    'account' => [
+        'profile-order' => [
+            'generic' => 'Возникла ошибка. Попробуйте перезагрузить страницу.',
+        ],
+    ],
+    'beatmaps' => [
+        'invalid_mode' => 'Указан недопустимый мод.',
+        'standard_converts_only' => 'Для запрашиваемого мода в этой карте результаты недоступны.',
+    ],
+    'beatmapsets' => [
+        'too-many-favourites' => 'У Вас слишком много избранных карт! Пожалуйста, удалите одну из них для продолжения.',
+    ],
+    'logged_out' => 'Вы вышли из аккаунта. Пожалуйста войдите и попробуйте ещё раз.',
+    'supporter_only' => 'Вы должны быть владеть тег саппортера, для использования этой возможности.',
+    'no_restricted_access' => 'Вы не можете использовать данную функцию пока Ваш аккаунт находится в ограниченном режиме',
+    'unknown' => 'Возникла неизвестная ошибка.',
+];
