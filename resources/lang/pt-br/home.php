9b6e0598a1a00864eebd08045af17debd186e333
gustarrini <gustarrini@outlook.com>
2017-07-13 14:19:25 -0300

    Update home.php

--- resources/lang/pt-br/home.php
+++ resources/lang/pt-br/home.php
@@ -37,6 +37,77 @@ return [
         ],
     ],
 
+    'search' => [
+        'advanced_link' => 'Pesquisa Avançada',
+        'button' => 'Pesquisar',
+        'empty_result' => 'Nada encontrado!',
+        'missing_query' => 'Search :n characters is required',
+        'title' => 'Resultados da Pesquisa',
+
+        'beatmapset' => [
+            'more' => ':count mais resultados de beatmaps',
+            'more_simple' => 'Ver mais resultados de beatmaps',
+            'title' => 'Beatmaps',
+        ],
+
+        'forum_post' => [
+            'all' => 'All forums',
+            'link' => 'Pesquisar no forum',
+            'more_simple' => 'Ver mais resultados do forum',
+            'title' => 'Forum',
+
+            'label' => [
+                'forum' => 'Pesquisar no forum',
+                'forum_children' => 'incluir subforums',
+                'username' => 'autor',
+            ],
+        ],
+
+        'mode' => [
+            'all' => 'all',
+            'beatmapset' => 'beatmap',
+            'forum_post' => 'forum',
+            'user' => 'player',
+            'wiki_page' => 'wiki',
+        ],
+
+        'user' => [
+            'more' => ':count more player search results',
+            'more_simple' => 'See more player search results',
+            'title' => 'Players',
+        ],
+
+        'wiki_page' => [
+            'link' => 'Search the wiki',
+            'more_simple' => 'See more wiki search results',
+            'title' => 'Wiki',
+        ],
+    ],
+
+    'download' => [
+      'header' => [
+          '1' => "vamos",
+          '2' => 'começar.',
+          '3' => 'baixe o cliente do osu! para Windows',
+      ],
+      'steps' => [
+          '1' => [
+              'name' => 'Passo 1',
+              'content' => 'baixe o cliente do osu!',
+          ],
+          '2' => [
+              'name' => 'Passo 2',
+              'content' => 'crie uma conta no osu!',
+          ],
+          '3' => [
+              'name' => 'Passo 3',
+              'content' => '???',
+          ],
+      ],
+      'more' => 'Quer saber mais?',
+      'more_text' => 'Olhe o canal do <a href="https://www.youtube.com/user/osuacademy/">osu!academy no youtube</a> para ver tutoriais atualizados e dicas em como conseguir o melhor possivel do osu!',
+    ],
+
     'user' => [
         'title' => 'notícias',
         'news' => [
@@ -47,12 +118,15 @@ return [
             'welcome' => 'Olá, <strong>:username</strong>!',
             'messages' => 'Você tem 1 nova mensagem|Você tem :count novas mensagens',
             'stats' => [
+				'games' => 'Partidas',
                 'online' => 'Usuários online',
             ],
         ],
         'beatmaps' => [
             'new' => 'Novos beatmaps aprovados',
             'popular' => 'Beatmaps populares',
+            'by' => 'por',
+            'plays' => ':count partidas',
         ],
         'buttons' => [
             'download' => 'Baixar osu!',
