82c63a2e22eb3d41bf947a7d37a1f769e6000d22
notbakaneko <notbakaneko@users.noreply.github.com>
2017-07-21 18:17:54 +0900

    overlay blurb over header background image

--- resources/views/packs/index.blade.php
+++ resources/views/packs/index.blade.php
@@ -26,17 +26,19 @@
     <div class="beatmap-packs">
         <div class="osu-page">
             <div class="osu-page-header osu-page-header--beatmappacks">
-                <div class="osu-page-header__icon osu-page-header__icon--beatmappacks"></div>
-                <div class="osu-page-header__title">{{trans('beatmappacks.index.title')}}</div>
-            </div>
-            <div class="beatmap-packs__blurb">
-                <p class="beatmap-packs__important">READ THIS BEFORE DOWNLOADING</p>
-                <p>Installation: Once a pack has been downloaded, extract the .rar into your osu! Songs directory.
-                   All songs are still .zip'd and/or .osz'd inside the pack, so osu! will need to extract the beatmaps itself the next time you go into Play mode.
-                   <span class="beatmap-packs__scary">Do NOT</span> extract the zip's/osz's yourself,
-                   or the beatmaps will display incorrectly in osu and will not function properly.
-                </p>
-                <p>Also note that it is highly recommended to <span class="beatmap-packs__scary">download the packs from latest to earliest</span>, since the oldest maps are of much lower quality than most recent maps.</p>
+                <div class="beatmap-packs__page-header">
+                    <div class="beatmap-packs__page-icon osu-page-header__icon--beatmappacks"></div>
+                    <div class="beatmap-packs__page-title">{{trans('beatmappacks.index.title')}}</div>
+                </div>
+                <div class="beatmap-packs__blurb">
+                    <p class="beatmap-packs__important">READ THIS BEFORE DOWNLOADING</p>
+                    <p>Installation: Once a pack has been downloaded, extract the .rar into your osu! Songs directory.
+                    All songs are still .zip'd and/or .osz'd inside the pack, so osu! will need to extract the beatmaps itself the next time you go into Play mode.
+                    <span class="beatmap-packs__scary">Do NOT</span> extract the zip's/osz's yourself,
+                    or the beatmaps will display incorrectly in osu and will not function properly.
+                    </p>
+                    <p>Also note that it is highly recommended to <span class="beatmap-packs__scary">download the packs from latest to earliest</span>, since the oldest maps are of much lower quality than most recent maps.</p>
+                </div>
             </div>
             <ul class="page-mode">
                 @foreach(['standard', 'chart', 'theme', 'artist'] as $mode)
