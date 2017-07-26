b345086494da5811abf833737f59ef28b62058dc
VenixOSU <30481900+VenixOSU@users.noreply.github.com>
2017-07-27 01:09:58 +0200

    Create accounts.php

---
+++ resources/lang/pl/accounts.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+return [
+    'edit' => [
+        'title' => 'Ustawienia <strong>Konta</strong>',
+        'title_compact' => 'Ustawienia',
+        'avatar' => [
+            'title' => 'Zmień Avatar',
+        ],
+        'email' => [
+            'current' => 'aktualny email',
+            'new' => 'nowy email',
+            'new_confirmation' => 'potwierdź email',
+            'title' => 'Email',
+        ],
+        'password' => [
+            'current' => 'aktualne hasło',
+            'new' => 'nowe hasło',
+            'new_confirmation' => 'potwierdź hasło',
+            'title' => 'Hasło',
+        ],
+        'profile' => [
+            'title' => 'Edytuj Profil',
+            'user' => [
+                'user_from' => 'current location',
+                'user_interests' => 'zainteresowania',
+                'user_msnm' => 'skype',
+                'user_occ' => 'zajęcia',
+                'user_twitter' => 'twitter',
+                'user_website' => 'strona internetowa',
+            ],
+        ],
+        'signature' => [
+            'title' => 'Sygnatura',
+            'update' => 'zaktualizuj',
+        ],
+    ],
+    'update_email' => [
+        'email_subject' => 'potwierdź zmiany maila',
+        'update' => 'zaktualizuj',
+        'updated' => 'Zaktualizowano maila',
+    ],
+    'update_password' => [
+        'email_subject' => 'potwierdź zmianę hasła',
+        'update' => 'zaktualizuj',
+        'updated' => 'Zaktualizowano hasło',
+    ],
+    'playstyles' => [
+        'title' => 'Style gry',
+        'mouse' => 'myszka',
+        'keyboard' => 'klawiatura',
+        'tablet' => 'tablet',
+        'touch' => 'ekran dotykowy',
+    ],
+];
