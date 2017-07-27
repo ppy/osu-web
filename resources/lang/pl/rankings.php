e9c0456a40ec64833cecd233baf7b554cba128d8
VenixOSU <30481900+VenixOSU@users.noreply.github.com>
2017-07-27 11:28:12 +0200

    Update rankings.php

--- resources/lang/pl/rankings.php
+++ resources/lang/pl/rankings.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *    Copyright 2015-2017 ppy Pty. Ltd.
  *
@@ -17,5 +16,25 @@
  *    You should have received a copy of the GNU Affero General Public License
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-return [];
+return [
+    'header' => 'Ranking :type',
+    'type' => [
+        'performance' => 'Globalny',
+        'charts' => 'Wyróżnionych',
+        'score' => 'Punktowy',
+        'country' => 'Krajowy',
+    ],
+    'stat' => [
+        'accuracy' => 'Celność',
+        'active_users' => 'Aktywnych użytkowników',
+        'country' => 'Kraj',
+        'play_count' => 'Ilość zagrań',
+        'performance' => 'Celność',
+        'total_score' => 'Ilość punktów',
+        'ranked_score' => 'Ilość punktów rankingowych',
+        'average_score' => 'Średni wynik',
+        'average_performance' => 'Avg. Perf.',
+        'ss' => 'SS',
+        's' => 'S',
+        'a' => 'A',
+    ],
