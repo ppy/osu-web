e89b747590bb581899a298c4ee3c6efef7a9bfe2
VenixOSU <30481900+VenixOSU@users.noreply.github.com>
2017-07-27 14:50:15 +0200

    Update rankings.php

--- resources/lang/pl/rankings.php
+++ resources/lang/pl/rankings.php
@@ -18,4 +18,43 @@
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-return [];
+<?php
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+return [
+    'header' => 'Ranking :type',
+    'type' => [
+        'performance' => 'Globalny',
+        'charts' => 'Wyróżnionych',
+        'score' => 'Punktowy',
+        'country' => 'Krajowy',
+    ],
+    'stat' => [
+        'accuracy' => 'Celność',
+        'active_users' => 'Aktywnych użytkowników',
+        'country' => 'Kraj',
+        'play_count' => 'Ilość zagrań',
+        'performance' => 'Celność',
+        'total_score' => 'Ilość punktów',
+        'ranked_score' => 'Ilość punktów rankingowych',
+        'average_score' => 'Średni wynik',
+        'average_performance' => 'Średnia wydajność',
+        'ss' => 'SS',
+        's' => 'S',
+        'a' => 'A',
+    ],
