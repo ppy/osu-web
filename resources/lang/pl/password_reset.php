d2443b0e0224cfaa5a95af2b8acb610a18fe12a0
VenixOSU <30481900+VenixOSU@users.noreply.github.com>
2017-07-27 15:27:28 +0200

    Create password_reset.php

---
+++ resources/lang/pl/password_reset.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+return [
+    'title' => 'Zresetuj Hasło',
+    'button' => [
+        'cancel' => 'Anuluj',
+        'resend' => 'Wyślij ponownie email weryfikacyjny',
+        'set' => 'Ustaw hasło',
+        'start' => 'Start',
+    ],
+    'email' => [
+        'subject' => 'odzyskiwanie konta osu!',
+    ],
+    'error' => [
+        'contact_support' => 'Skontaktuj się z supportem, żeby odsyskać konto.',
+        'is_privileged' => 'Skontaktuj się z peppym.',
+        'missing_key' => 'Wymagane.',
+        'too_many_tries' => 'Za dużo błędnych prób.',
+        'user_not_found' => 'Ten użytkownik nie istnieje.',
+        'wrong_key' => 'Nieprawidłowy kod.',
+    ],
+    'notice' => [
+        'sent' => 'Sprawdź email żeby uzyskać kod.',
+        'saved' => 'Nowe hasło zapisane!',
+    ],
+    'started' => [
+        'password' => 'Nowe hasło',
+        'password_confirmation' => 'Potwierdź hasło',
+        'title' => 'Resetowanie hasła dla konta <strong>:username</strong>.',
+        'verification_key' => 'Kod weryfikacyjny',
+    ],
+    'starting' => [
+        'username' => 'Wpisz email lub nazwę użytkownika',
+    ],
+];
