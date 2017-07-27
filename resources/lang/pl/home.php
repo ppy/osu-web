8797f2969bfc428c5985f0644d3449c854dc16d9
VenixOSU <30481900+VenixOSU@users.noreply.github.com>
2017-07-27 16:44:26 +0200

    Update home.php

--- resources/lang/pl/home.php
+++ resources/lang/pl/home.php
@@ -35,6 +35,7 @@ return [
             'sub' => 'rytm jest tylko *kliknięcie* stąd!',
         ],
     ],
+],
     'search' => [
         'advanced_link' => 'Zaawansowane wyszkukiwanie',
         'button' => 'Szukaj',
