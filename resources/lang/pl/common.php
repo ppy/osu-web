4cc2e473df388548a4639ed699d48304c6c753a1
TPGPL <mateuszkwiatkowski2002@gmail.com>
2017-08-04 11:58:40 +0200

    Update common.php translation

--- resources/lang/pl/common.php
+++ resources/lang/pl/common.php
@@ -19,39 +19,69 @@
  */
 
 return [
+    'confirmation' => 'Na pewno?',
+    'saved' => 'Zapisane',
+
+    'array_and' => [
+        'words_connector' => ', ',
+        'two_words_connector' => ' i ',
+        'last_word_connector' => ' oraz ',
+    ],
+
     'buttons' => [
         'cancel' => 'Anuluj',
-        'post' => 'Postuj',
+        'delete' => 'Usuń',
+        'permalink' => 'odnośnik bezpośredni',
+        'post' => 'Opublikuj',
         'reply' => 'Odpowiedz',
-        'reset' => 'Resetuj',
+        'reset' => 'Zresetuj',
         'save' => 'Zapisz',
+        'saving' => 'Zapisywanie...',
         'show_more' => 'pokaż więcej',
+        'upload_image' => 'prześlij zdjęcie',
     ],
 
     'count' => [
-        'item' => '{1} sztuka|[2,4] :count sztuki|{5,*} :count sztuk',
+        'item' => ':count sztuka|:count sztuki|:count sztuk',
+    ],
+
+    'datetime' => [
+        'year_month' => [
+            'moment' => 'MMMM YYYY',
+            'php' => 'MMMM y',
+        ],
     ],
 
     'device' => [
-        'keyboard' => 'Klawiatury',
-        'mouse' => 'Myszki',
-        'tablet' => 'Tableta',
-        'touch' => 'Ekranu dotykowego',
+        'keyboard' => 'Klawiatura',
+        'mouse' => 'Myszka',
+        'tablet' => 'Tablet',
+        'touch' => 'Ekran dotykowy',
     ],
 
     'dropzone' => [
-        'target' => 'upuść tutaj, aby wrzucić plik',
+        'target' => 'upuść tutaj, aby przesłać plik',
     ],
 
     'pagination' => [
-        'previous' => 'poprzednia',
-        'next' => 'następna',
+        'previous' => 'poprzedni',
+        'next' => 'następny',
+    ],
+
+    'score_count' => [
+        'count_100' => '100',
+        'count_300' => '300',
+        'count_50' => '50',
+        'count_geki' => 'MAX',
+        'count_katu' => '200',
+        'count_miss' => 'Pudło',
     ],
 
     'time' => [
-        'days_ago' => '{1} dzień temu|:count dni temu',
-        'hours_ago' => '{0} godzin temu|{1} godzinę temu|[2,4] :count godziny temu|{5,*} :count godzin temu',
+        'days_ago' => ':count dzień temu|:count dni temu|:count dni temu',
+        'hours_ago' => ':count godzinę temu|:count godziny temu|:count godzin temu',
         'now' => 'teraz',
+        'remaining' => 'Pozostało',
     ],
 
     'title' => [
