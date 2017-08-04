51066f99dcd3ee403fa664bf27a13659e4d3f318
TPGPL <mateuszkwiatkowski2002@gmail.com>
2017-08-04 14:52:40 +0200

    Heavy update for model_validation.php

--- resources/lang/pl/model_validation.php
+++ resources/lang/pl/model_validation.php
@@ -20,11 +20,47 @@
 
 return [
     'required' => ':attribute jest wymagany.',
+    'wrong_confirmation' => 'Potwierdzenie się nie zgadza.',
+
+    'beatmap_discussion_post' => [
+        'first_post' => 'Nie można usunąć wątku rozpoczynającego.',
+    ],
 
     'forum' => [
         'feature_vote' => [
-            'not_feature_topic' => 'Możesz zagłosować tylko na prośby o funkcje.',
-            'not_enough_feature_votes' => 'Nie uzyskano wystarczającej ilości głosów.',
+            'not_feature_topic' => 'Można głosować tylko na prośby o funkcję.',
+            'not_enough_feature_votes' => 'Niewystarczająco dużo głosów.',
         ],
+
+        'poll_vote' => [
+            'invalid' => 'Wybrano nieprawidłową opcję.',
+        ],
+
+        'topic_poll' => [
+            'duplicate_options' => 'Duplikaty nie są dozwolone.',
+            'invalid_max_options' => 'Ilość możliwości wyboru na użytkownika nie może przekroczyć ich całościowej liczby.',
+            'minimum_one_selection' => 'Potrzeba minimalnie jedną opcję.',
+            'minimum_two_options' => 'Potrzeba minimalnie dwóch opcji.',
+            'too_many_options' => 'Przekroczono maksymalną ilość możliwości wyboru.',
+        ],
+
+        'topic_vote' => [
+            'too_many' => 'Wybrano więcej opcji, niż jest dozwolone.',
+        ],
+    ],
+
+    'user_email' => [
+        'invalid' => "To nie wygląda na adres email.",
+        'already_used' => 'Ten adres email jest już w użyciu.',
+        'wrong_confirmation' => 'Email potwierdzający się nie zgadza.',
+        'wrong_current_password' => 'Obecne hasło jest niepoprawne.',
+    ],
+
+    'user_password' => [
+        'contains_username' => 'Hasło nie może zawierać pseudonimu.',
+        'too_short' => 'Nowe hasło jest za krótkie.',
+        'weak' => 'Hasło jest za słabe.',
+        'wrong_confirmation' => 'Hasło potwierdzające się nie zgadza.',
+        'wrong_current_password' => 'Obecne hasło jest niepoprawne.',
     ],
 ];
