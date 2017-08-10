23dcfa4e167cf4be37d7ea778cdc022a85a9b786
nanaya <me@nanaya.pro>
2017-08-10 23:01:24 +0900

    Social stuff

--- resources/views/layout/_popup_menu.blade.php
+++ resources/views/layout/_popup_menu.blade.php
@@ -119,6 +119,17 @@
         </div>
     </div>
 
+    <div class="nav-popup__socials">
+        <a href="{{ route('support-the-game') }}" class="nav-popup__social nav-popup__social--support">
+            <span class="fa fa-heart"></span>
+        </a>
+        <a href="{{ osu_url("social.twitter") }}" class="nav-popup__social nav-popup__social--twitter">
+            <span class="fa fa-twitter"></span>
+        </a>
+        <a href="{{ osu_url("social.facebook") }}" class="nav-popup__social nav-popup__social--facebook">
+            <span class="fa fa-facebook-official"></span>
+        </a>
+    </div>
     <div class="nav-popup__bar">
         <span class="bar u-section-bg"></span>
     </div>
