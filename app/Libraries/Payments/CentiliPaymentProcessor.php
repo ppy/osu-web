7ac3fb20883e56385bd2b388f2ba311d6da35561
notbakaneko <notbakaneko@users.noreply.github.com>
2017-08-24 19:57:09 +0900

    start adding payment processor for centili

---
+++ app/Libraries/Payments/CentiliPaymentProcessor.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Libraries\Payments;
+
+use App\Exceptions\InvalidSignatureException;
+use App\Models\Store\Order;
+use Carbon\Carbon;
+use DB;
+
+// FIXME: rename?
+class CentiliPaymentProcessor extends PaymentProcessor
+{
+    private $explodedOrderNumber;
+    private $orderId;
+
+    public function __construct(\Illuminate\Http\Request $request)
+    {
+        parent::__construct($request);
+        $this->explodedOrderNumber = explode('-', $this->getOrderNumber(), 3);
+        if (count($this->explodedOrderNumber) > 2) {
+            $this->orderId = (int) $this->explodedOrderNumber[2];
+        }
+    }
+
+    public function isSkipped()
+    {
+        return false;
+    }
+
+    public function getOrderId()
+    {
+        return $this->orderId;
+    }
+
+    public function getOrderNumber()
+    {
+        return $this['reference']; // or clientid?
+    }
+
+    public function getTransactionId()
+    {
+        return "centili-{$this['transactionid']}";
+    }
+
+    public function getPaymentAmount()
+    {
+        return $this['enduserprice'];
+    }
+
+    public function getPaymentDate()
+    {
+        return Carbon::now();
+    }
+
+    public function getNotificationType()
+    {
+        return 'payment';
+    }
+
+    public function ensureValidSignature()
+    {
+        $signature = $this['sign'];
+        // TODO: post many warnings
+        if (!$signature->isValid()) {
+            $this->validationErrors()->add('header.signature', '.signature.not_match');
+            $this->dispatchValidationFailed();
+            throw new InvalidSignatureException();
+        }
+    }
+
+    public function validateTransaction()
+    {
+        $this->ensureValidSignature();
+
+        $order = $this->getOrder();
+        // order should exist
+        if ($order === null) {
+            $this->validationErrors()->add('order', '.order');
+            return false;
+        }
+
+        // id in order number should be correct
+        if (count($this->explodedOrderNumber) !== 3) {
+            $this->validationErrors()->add('reference', '.transaction.external_id');
+        }
+
+        if ((int) $this->explodedOrderNumber[1] !== $order['user_id']) {
+            $this->validationErrors()->add('reference', '.transaction.user_id_mismatch');
+        }
+
+
+        \Log::debug("purchase.checkout.amount: {$this['enduserprice']}, {$order->getTotal()}");
+        if ($this['enduserprice'] < $order->getTotal()) {
+            $this->validationErrors()->add(
+                'enduserprice',
+                '.purchase.checkout.amount',
+                ['expected' => $order->getTotal(), 'actual' => $this['enduserprice']]
+            );
+        }
+
+        return $this->validationErrors()->isEmpty();
+    }
+}
---
+++ app/Libraries/Payments/CentiliSignature.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Libraries\Payments;
+
+use Illuminate\Http\Request;
+
+class CentiliSignature
+{
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function isValid()
+    {
+        $received = $this->receivedSignature();
+        \Log::debug("CentiliSignature::isValidSignature calc: {$this->calculatedSignature()}, signed: {$received}");
+        if ($received === null) {
+            return false;
+        }
+
+        return hash_equals($this->calculatedSignature(), $received);
+    }
+
+    public static function calculateSignature(string $content)
+    {
+        return sha1($content . config('payments.centili.secret_key'));
+    }
+
+    private function receivedSignature()
+    {
+        return $request->input('sign');
+    }
+
+    private function calculatedSignature()
+    {
+        $input = $this->request->input();
+        // sort input
+        ksort($input);
+        unset($input['sign']);
+        $string = array_reduce($input, function ($carry, $item) {
+            return $carry.$item;
+        }, '');
+
+        return static::calculateSignature($string);
+    }
+}
