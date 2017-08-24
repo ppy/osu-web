aa8ca0e8d91310ecbe1b7a65d5a38ccbee86979f
nanaya <me@nanaya.pro>
2017-08-24 21:53:55 +0900

    Refactor the menu

---
+++ resources/views/layout/header_mobile/nav.blade.php
@@ -0,0 +1,55 @@
+{{--
+    Copyright 2015-2017 ppy Pty. Ltd.
+
+    This file is part of osu!web. osu!web is distributed with the hope of
+    attracting more community contributions to the core ecosystem of osu!.
+
+    osu!web is free software: you can redistribute it and/or modify
+    it under the terms of the Affero GNU General Public License version 3
+    as published by the Free Software Foundation.
+
+    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+--}}
+@foreach (nav_links() as $section => $links)
+    <li class="navbar-mobile-item dropdown">
+        <a
+            data-toggle="dropdown"
+            class="navbar-mobile-item__main dropdown-toggle"
+            href="{{ array_values($links)[0] }}"
+        >
+            <span class="navbar-mobile-item__icon navbar-mobile-item__icon--main navbar-mobile-item__icon--closed">
+                <i class="fa fa-chevron-right"></i>
+            </span>
+
+            <span class="navbar-mobile-item__icon navbar-mobile-item__icon--main navbar-mobile-item__icon--opened">
+                <i class="fa fa-chevron-down"></i>
+            </span>
+
+            {{ trans("layout.menu.{$section}._") }}
+        </a>
+
+        <ul class="dropdown-menu" role="menu" aria-labelledby="expand-{{ $section }}">
+            @foreach ($links as $action => $link)
+                <li>
+                    <a
+                        class="navbar-mobile-item__submenu"
+                        href="{{ $link }}"
+                        data-toggle="collapse"
+                        data-target=".js-navbar-mobile--menu"
+                    >
+                        <span class="navbar-mobile-item__icon">
+                            <i class="fa fa-chevron-right"></i>
+                        </span>
+
+                        {{ trans("layout.menu.$section.$action") }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </li>
+@endforeach
---
+++ resources/views/layout/header_mobile/user.blade.php
@@ -0,0 +1,58 @@
+{{--
+    Copyright 2015-2017 ppy Pty. Ltd.
+
+    This file is part of osu!web. osu!web is distributed with the hope of
+    attracting more community contributions to the core ecosystem of osu!.
+
+    osu!web is free software: you can redistribute it and/or modify
+    it under the terms of the Affero GNU General Public License version 3
+    as published by the Free Software Foundation.
+
+    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+--}}
+<li class="navbar-mobile-item navbar-mobile-item--user">
+    @if (Auth::check())
+        <a
+            class="navbar-mobile-item__main navbar-mobile-item__main--user"
+            href="{{ route('users.show', Auth::user()) }}"
+            data-toggle="collapse"
+            data-target=".js-navbar-mobile--menu"
+        >
+            <span
+                class="avatar avatar--navbar-mobile"
+                style="background-image: url('{{ Auth::user()->user_avatar }}');"
+            ></span>
+
+            {{ Auth::user()->username }}
+        </a>
+
+        <a
+            class="navbar-mobile-item__main navbar-mobile-item__main--logout js-logout-link"
+            href="{{ route('logout') }}"
+            data-method="DELETE"
+            data-confirm="{{ trans('users.logout_confirm') }}"
+            data-remote="1"
+            data-toggle="collapse"
+            data-target=".js-navbar-mobile--menu"
+        >
+            <i class="fa fa-sign-out"></i>
+        </a>
+    @else
+        <a
+            class="js-user-link navbar-mobile-item__main navbar-mobile-item__main--user"
+            href="#"
+            title="{{ trans('users.anonymous.login_link') }}"
+            data-toggle="collapse"
+            data-target=".js-navbar-mobile--menu"
+        >
+            <span class="avatar avatar--guest avatar--navbar-mobile"></span>
+
+            {{ trans('users.anonymous.username') }}
+        </a>
+    @endif
+</li>
