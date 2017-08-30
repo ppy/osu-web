82ca116f8d65e47b2140c5859033d5318f5aabdc
notbakaneko <notbakaneko@users.noreply.github.com>
2017-08-30 16:42:38 +0900

    hammering out tournament supporter banner store fulfillment base class

---
+++ app/Libraries/Fulfillments/BannerFulfillment.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Libraries\Fulfillments;
+
+use App\Models\Country;
+use App\Models\Store\OrderItem;
+
+/**
+ * Placeholder class for handling non-custom class order item fulfillment.
+ */
+abstract class BannerFulfillment extends OrderFulfiller
+{
+    abstract public function getTournamentId();
+
+    public function run()
+    {
+        $orderItems = $this->getOrderItems;
+        foreach ($orderItems as $orderItem) {
+            $this->applyBanner($orderItem);
+        }
+    }
+
+    public function revoke()
+    {
+    }
+
+    abstract protected function getOrderItems();
+
+    private function applyBanner(OrderItem $orderItem)
+    {
+        \Log::debug('applying banner');
+        \Log::debug($orderItem);
+    }
+
+    private function getCountryName(OrderItem $orderItem)
+    {
+        $product = $orderItem->product;
+
+        return preg_split("/\(|\)/", $product->name);
+    }
+
+    private function getCountryAcronym(OrderItem $orderItem)
+    {
+        $countryName = $this->getCountryName($orderItem);
+        \Log::debug($countryName);
+
+        return Country::where('name', $countryName)->first->acronym;
+    }
+
+
+    //================
+    // Validatable
+    //================
+    public function validationErrorsTranslationPrefix()
+    {
+        return 'fulfillments.banner-supporter';
+    }
+
+    public function validationErrorsKeyBase()
+    {
+        return 'model_validation/';
+    }
+}
