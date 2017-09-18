7a3333a2d2363afa3855217a068e08bf0eff64e7
notbakaneko <notbakaneko@users.noreply.github.com>
2017-09-18 19:22:44 +0900

    durr

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -10,7 +10,7 @@ $factory->define(App\Models\Store\Order::class, function (Faker\Generator $faker
 
 $factory->state(App\Models\Store\Order::class, 'incart', function (Faker\Generator $faker) {
     return [
-        'status' => 'checkout',
+        'status' => 'incart',
     ];
 });
 
