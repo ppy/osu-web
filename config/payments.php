2d16171b3253209a0ecd18d659826949a86398dc
notbakaneko <notbakaneko@users.noreply.github.com>
2017-09-19 11:43:37 +0900

    presence can set default

--- config/payments.php
+++ config/payments.php
@@ -4,7 +4,7 @@ return [
     'centili' => [
         'api_key' => env('CENTILI_API_KEY'),
         'secret_key' => env('CENTILI_SECRET_KEY'),
-        'conversion_rate' => (float) (presence(env('CENTILI_CONVERSION_RATE')) ?? 100),
+        'conversion_rate' => (float) presence(env('CENTILI_CONVERSION_RATE'), 100),
         'widget_url' => env('CENTILI_WIDGET_URL'),
     ],
     'paypal' => [
@@ -21,5 +21,5 @@ return [
         'secret_key' => env('XSOLLA_SECRET_KEY'),
     ],
 
-    'running_cost' => (int) (presence(env('OSU_RUNNING_COST')) ?? 3141592), // arbritary default >_>
+    'running_cost' => (int) presence(env('OSU_RUNNING_COST'), 3141592), // arbritary default >_>
 ];
