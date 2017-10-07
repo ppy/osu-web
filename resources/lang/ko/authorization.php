9b93cb885bd53b027318270884f9d7b72ffd01e1
Trostal <alswo7310@naver.com>
2017-10-07 20:15:35 +0900

    Added Korean(ko) translations

--- resources/lang/ko/authorization.php
+++ resources/lang/ko/authorization.php
@@ -21,132 +21,132 @@
 return [
     'beatmap_discussion' => [
         'destroy' => [
-            'has_reply' => 'Can not delete discussion with replies',
+            'has_reply' => '답글이 달린 토론은 삭제할 수 없습니다',
         ],
         'nominate' => [
-            'exhausted' => 'You have reached your nomination limit for the day, please try again tomorrow.',
+            'exhausted' => '오늘은 더이상 추천할 수 없습니다, 내일 다시 시도해주세요.',
         ],
         'resolve' => [
-            'not_owner' => 'Only thread starter and beatmap owner can resolve a discussion.',
+            'not_owner' => '게시글 작성자 또는 비트맵 제작자만 토론을 가결할 수 있습니다.',
         ],
 
         'vote' => [
-            'limit_exceeded' => 'Please wait a while before casting more votes',
-            'owner' => 'Can not vote own discussion!',
+            'limit_exceeded' => '투표를 더 받을 때 까지 좀 더 기다려주세요',
+            'owner' => '본인이 제시한 토론에는 투표할 수 없습니다!',
         ],
     ],
 
     'beatmap_discussion_post' => [
         'edit' => [
-            'system_generated' => 'Automatically generated post can not be edited.',
-            'not_owner' => 'Only the poster can edit post.',
+            'system_generated' => '자동으로 생성된 답글은 수정할 수 없습니다.',
+            'not_owner' => '본인이 쓴 답글만 수정할 수 있습니다.',
         ],
     ],
 
     'chat' => [
         'channel' => [
             'read' => [
-                'no_access' => 'Access to requested channel is not permitted.',
+                'no_access' => '요청하신 채널로의 입장이 허용되지 않았습니다.',
             ],
         ],
         'message' => [
             'send' => [
                 'channel' => [
-                    'no_access' => 'Access to target channel is required.',
-                    'moderated' => 'Channel is currently moderated.',
-                    'not_lazer' => 'You can only speak in #lazer at this time.',
+                    'no_access' => '대상 채널에 접근하기 위한 권한이 필요합니다.',
+                    'moderated' => '현재 채널에 사회자가 있습니다.',
+                    'not_lazer' => '현재 #lazer 채널을 통해서만 말할 수 있습니다.',
                 ],
 
-                'not_allowed' => 'Can not send message while banned/restricted/silenced.',
+                'not_allowed' => '차단/제한/침묵 상태에서는 메세지를 보낼 수 없습니다.',
             ],
         ],
     ],
 
     'contest' => [
-        'voting_over' => 'You cannot change your vote after the voting period for this contest has ended.',
+        'voting_over' => '투표 기간 이후에는 콘테스트가 끝날 때 까지 선택한 투표를 바꿀 수 없습니다.',
     ],
 
     'forum' => [
         'post' => [
             'delete' => [
-                'only_last_post' => 'Only last post can be deleted.',
-                'locked' => 'Can not delete post of a locked topic.',
-                'no_forum_access' => 'Access to requested forum is required.',
-                'not_owner' => 'Only poster can delete the post.',
+                'only_last_post' => '마지막 답글만 삭제할 수 있습니다.',
+                'locked' => '잠긴 주제에 달린 답글은 삭제할 수 없습니다.',
+                'no_forum_access' => '요청하신 포럼에의 접근 권한이 필요합니다.',
+                'not_owner' => '본인이 쓴 답글만 삭제할 수 있습니다.',
             ],
 
             'edit' => [
-                'deleted' => 'Can not edit deleted post.',
-                'locked' => 'The post is locked from editing.',
-                'no_forum_access' => 'Access to requested forum is required.',
-                'not_owner' => 'Only poster can edit the post.',
-                'topic_locked' => 'Can not edit post of a locked topic.',
+                'deleted' => '삭제된 답글은 수정할 수 없습니다.',
+                'locked' => '이 답글은 잠겨있어 수정할 수 없습니다.',
+                'no_forum_access' => '요청하신 포럼에의 접근 권한이 필요합니다.',
+                'not_owner' => '본인이 쓴 답글만 수정할 수 있습니다.',
+                'topic_locked' => '잠긴 주제에 달린 답글은 수정할 수 없습니다.',
             ],
         ],
 
         'topic' => [
             'reply' => [
-                'double_post' => 'You just posted. Wait a bit or edit your last post.',
-                'locked' => 'Can not reply to a locked thread.',
-                'no_forum_access' => 'Access to requested forum is required.',
-                'no_permission' => 'No permission to reply.',
+                'double_post' => '답글을 올린 지 얼마 되지 않았습니다. 잠시 기다려주시거나, 마지막에 올린 글을 수정하세요.',
+                'locked' => '게시글이 잠겨있어 답변할 수 없습니다.',
+                'no_forum_access' => '요청하신 포럼에의 접근 권한이 필요합니다.',
+                'no_permission' => '답변할 권한이 없습니다.',
 
                 'user' => [
-                    'require_login' => 'Please login to reply.',
-                    'restricted' => "Can't reply while restricted.",
-                    'silenced' => "Can't reply while silenced.",
+                    'require_login' => '답글을 게시하려면 로그인해 주세요.',
+                    'restricted' => "제한된 상태의 계정은 답글을 게시할 수 없습니다.",
+                    'silenced' => "침묵 상태의 계정은 답글을 게시할 수 없습니다.",
                 ],
             ],
 
             'store' => [
-                'no_forum_access' => 'Access to requested forum is required.',
-                'no_permission' => 'No permission to create new topic.',
-                'forum_closed' => 'Forum is closed and can not be posted to.',
+                'no_forum_access' => '요청하신 포럼에의 접근 권한이 필요합니다.',
+                'no_permission' => '새 주제글을 게시할 권한이 없습니다.',
+                'forum_closed' => '포럼이 닫혀있어 현재 포럼에 글을 올릴 수 없습니다.',
             ],
 
             'vote' => [
-                'no_forum_access' => 'Access to requested forum is required.',
-                'over' => 'Polling is over and can not be voted on anymore.',
-                'voted' => 'Changing vote is not allowed.',
+                'no_forum_access' => '요청하신 포럼에의 접근 권한이 필요합니다.',
+                'over' => '투표가 종료되어 더이상 투표할 수 없습니다.',
+                'voted' => '투표 변경이 허용되지 않았습니다.',
 
                 'user' => [
-                    'require_login' => 'Please login to vote.',
-                    'restricted' => "Can't vote while restricted.",
-                    'silenced' => "Can't vote while silenced.",
+                    'require_login' => '투표하려면 로그인해 주세요.',
+                    'restricted' => "제한된 상태의 계정은 투표할 수 없습니다.",
+                    'silenced' => "침묵 상태의 계정은 투표할 수 없습니다.",
                 ],
             ],
 
             'watch' => [
-                'no_forum_access' => 'Access to requested forum is required.',
+                'no_forum_access' => '요청하신 포럼에의 접근 권한이 필요합니다.',
             ],
         ],
 
         'topic_cover' => [
             'edit' => [
-                'uneditable' => 'Invalid cover specified.',
-                'not_owner' => 'Only owner can edit cover.',
+                'uneditable' => '유효하지 않은 표지가 명시되었습니다.',
+                'not_owner' => '주제글을 올린 사람만 표지를 수정할 수 있습니다.',
             ],
         ],
 
         'view' => [
-            'admin_only' => 'Only admin can view this forum.',
+            'admin_only' => '관리자만 열람이 가능한 포럼입니다.',
         ],
     ],
 
-    'require_login' => 'Please login to proceed.',
+    'require_login' => '계속하려면 로그인해 주세요.',
 
-    'unauthorized' => 'Access denied.',
+    'unauthorized' => '접근이 제한되었습니다.',
 
-    'silenced' => "Can't do that while silenced.",
+    'silenced' => "침묵 상태에서는 할 수 없습니다.",
 
-    'restricted' => "Can't do that while restricted.",
+    'restricted' => "제한된 상태에서는 할 수 없습니다.",
 
     'user' => [
         'page' => [
             'edit' => [
-                'locked' => 'User page is locked.',
-                'not_owner' => 'Can only edit own user page.',
-                'require_supporter_tag' => 'Supporter tag is required.',
+                'locked' => '사용자 페이지가 잠겨있습니다.',
+                'not_owner' => '본인의 사용자 페이지만 수정할 수 있습니다.',
+                'require_supporter_tag' => '서포터 태그가 필요합니다.',
             ],
         ],
     ],
