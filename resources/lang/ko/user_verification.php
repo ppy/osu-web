5e866d33d940f28ec0bb0c72800dae82fb64b0f9
Trostal <alswo7310@naver.com>
2017-10-11 11:20:39 +0900

    Added Korean(ko) translations

--- resources/lang/ko/user_verification.php
+++ resources/lang/ko/user_verification.php
@@ -20,30 +20,30 @@
 
 return [
     'box' => [
-        'sent' => 'An email has been sent to :mail with a verification code. Enter the code.',
-        'title' => 'Account Verification',
-        'verifying' => 'Verifying...',
-        'issuing' => 'Issuing new code...',
+        'sent' => '인증 코드가 들어있는 메일이 :mail 로 전송되었습니다. 인증 코드를 입력해주세요.',
+        'title' => '계정 인증',
+        'verifying' => '인증하는 중...',
+        'issuing' => '새 코드 발급 중...',
 
         'info' => [
-            'check_spam' => "Make sure to check your spam folder if you can't find the email.",
-            'recover' => "If you can't access your email or have forgotten what you used, please follow the :link.",
-            'recover_link' => 'email recovery process here',
-            'reissue' => 'You can also :reissue_link or :logout_link.',
-            'reissue_link' => 'request another code',
-            'logout_link' => 'logout',
+            'check_spam' => "아직 메일을 받지 못하셨다면, 스팸 메일함을 확인해보세요.",
+            'recover' => "현재 이메일을 확인할 수 없거나, 이전에 사용하시던 이메일을 잊어버리셨다면, :link을 따라주시기 바랍니다.",
+            'recover_link' => '이메일 복구 과정',
+            'reissue' => ':reissue_link받거나, :logout_link하실 수도 있습니다.',
+            'reissue_link' => '새 코드를 발급',
+            'logout_link' => '로그아웃',
         ],
     ],
 
     'email' => [
-        'subject' => 'osu! account verification',
+        'subject' => 'osu! 계정 인증',
     ],
 
     'errors' => [
-        'expired' => 'Verification code expired, new verification email sent.',
-        'incorrect_key' => 'Incorrect verification code.',
-        'retries_exceeded' => 'Incorrect verification code. Retry limit exceeded, new verification email sent.',
-        'reissued' => 'Verification code reissued, new verification email sent.',
-        'unknown' => 'Unknown problem occurred, new verification email sent.',
+        'expired' => '인증 코드가 만료되었습니다, 새 인증용 메일을 전송했습니다.',
+        'incorrect_key' => '인증 코드가 잘못되었습니다.',
+        'retries_exceeded' => '인증 코드가 잘못되었습니다. 재시도 횟수를 초과하여, 새 인증용 메일을 전송했습니다.',
+        'reissued' => '인증 코드가 재발급되었습니다, 새 인증용 메일을 전송했습니다.',
+        'unknown' => '알 수 없는 문제가 발생했습니다, 새 인증용 메일을 전송했습니다.',
     ],
 ];
