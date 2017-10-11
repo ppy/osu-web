42a4ea9fbdd090bb44385a5ab39d09000cb811d3
Trostal <alswo7310@naver.com>
2017-10-11 23:08:47 +0900

    Added Korean(ko) translations

--- resources/lang/ko/password_reset.php
+++ resources/lang/ko/password_reset.php
@@ -19,41 +19,41 @@
  */
 
 return [
-    'title' => 'Reset Password',
+    'title' => '비밀번호 재설정',
 
     'button' => [
-        'cancel' => 'Cancel',
-        'resend' => 'Resend verification email',
-        'set' => 'Set password',
-        'start' => 'Start',
+        'cancel' => '취소',
+        'resend' => '인증 메일 다시 받기',
+        'set' => '비밀번호 설정',
+        'start' => '시작',
     ],
 
     'email' => [
-        'subject' => 'osu! account recover',
+        'subject' => 'osu! 계정 복구',
     ],
 
     'error' => [
-        'contact_support' => 'Please contact support to recover account.',
-        'is_privileged' => 'Contact peppy lulz.',
-        'missing_key' => 'Required.',
-        'too_many_tries' => 'Too many failed attempts.',
-        'user_not_found' => 'Requested user does not exist.',
-        'wrong_key' => 'Incorrect code.',
+        'contact_support' => '계정을 복구하려면 지원 페이지를 통해 문의해주세요.',
+        'is_privileged' => 'peppy에게 문의하기 ㅎ.',
+        'missing_key' => '요구됨.', // Required
+        'too_many_tries' => '실패한 횟수가 너무 많습니다.',
+        'user_not_found' => '요청하신 사용자가 존재하지 않습니다.',
+        'wrong_key' => '잘못된 코드입니다.',
     ],
 
     'notice' => [
-        'sent' => 'Check your email for the verification code.',
-        'saved' => 'New password saved!',
+        'sent' => '이메일을 열어 인증 코드가 적힌 메일을 확인하세요.',
+        'saved' => '새 비밀번호 적용됨!', // New password saved
     ],
 
     'started' => [
-        'password' => 'New password',
-        'password_confirmation' => 'Password confirmation',
-        'title' => 'Resetting password for account <strong>:username</strong>.',
-        'verification_key' => 'Verification code',
+        'password' => '새 비밀번호',
+        'password_confirmation' => '비밀번호 확인',
+        'title' => '<strong>:username</strong> 계정에 대한 비밀번호 재설정.',
+        'verification_key' => '인증 코드',
     ],
 
     'starting' => [
-        'username' => 'Enter email address or username',
+        'username' => '유저 이름이나 이메일 주소를 입력하세요.',
     ],
 ];
