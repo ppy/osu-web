b2c50a5884fb7de5d928ebc445aa6bc997275f7f
nanaya <me@nanaya.pro>
2017-10-13 21:14:46 +0900

    Better migration

--- database/migrations/2017_10_10_082642_add_ip_bans.php
+++ database/migrations/2017_10_10_082642_add_ip_bans.php
@@ -23,21 +23,15 @@ class AddIpBans extends Migration
             $column = $table->string('ip', 40)->primary()->default('');
             $column->collation = 'utf8_bin';
             $table->unsignedMediumInteger('user_id')->nullable();
+            $table->timestamp('timestamp')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->integer('length')->default(72 * 3600);
             $table->boolean('active')->default(true);
 
             $table->index('user_id', 'user_id');
-        });
-
-        DB::statement('
-            ALTER TABLE osu_ip_bans
-            ADD COLUMN timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP AFTER user_id
-        ');
-        DB::statement('ALTER TABLE osu_ip_bans ROW_FORMAT=DYNAMIC');
-
-        Schema::table('osu_ip_bans', function ($table) {
             $table->index(['active', 'timestamp'], 'active');
         });
+
+        DB::statement('ALTER TABLE osu_ip_bans ROW_FORMAT=DYNAMIC');
     }
 
     /**
