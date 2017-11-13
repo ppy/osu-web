d359933b90714d94835709e495f25190b4b52f94
kj415j45 <kj415j45@gmail.com>
2017-11-13 20:22:34 +0800

    zh: patch-9.1
    A quick fix

--- resources/lang/zh/beatmapset_watches.php
+++ resources/lang/zh/beatmapset_watches.php
@@ -28,8 +28,8 @@ return [
 
     'index' => [
         'description' => '这里是你关注的谱面，你会在它们更新或者有新讨论的时候收到通知。',
-        'title_compact' => '摸图关注列表', //需要帮助
-        'title_main' => '摸图关注列表', //需要帮助
+        'title_compact' => '摸图',
+        'title_main' => '摸图',
 
         'table' => [
             'empty' => '没有正在关注的谱面',
