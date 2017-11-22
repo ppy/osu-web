05780a04541971275fb240ce4da1828928ccb40d
nanaya <me@nanaya.pro>
2017-11-22 19:17:36 +0900

    Add roll back migration to something valid

--- database/migrations/2017_11_22_070257_update_discussion_type_by_mapper.php
+++ database/migrations/2017_11_22_070257_update_discussion_type_by_mapper.php
@@ -23,11 +23,19 @@ class UpdateDiscussionTypeByMapper extends Migration
 
     /**
      * Reverse the migrations.
+     * Doesn't quite roll back because they could be praise/suggestion/problem.
      *
      * @return void
      */
     public function down()
     {
-        // you funny sir
+        DB::table('beatmap_discussions')
+            ->whereExists(function ($query) {
+                $query->selectRaw(1)
+                    ->from('osu_beatmapsets')
+                    ->whereRaw('beatmap_discussions.beatmapset_id = osu_beatmapsets.beatmapset_id')
+                    ->whereRaw('beatmap_discussions.user_id = osu_beatmapsets.user_id');
+            })
+            ->update(['message_type' => 1]);
     }
 }
