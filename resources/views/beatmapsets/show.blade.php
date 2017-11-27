15e2b7ad273fa5d8cd3421b59fb828dedc4c4bac
nanaya <me@nanaya.pro>
2017-11-27 16:51:10 +0900

    Remove irrelevant comments

--- resources/views/beatmapsets/show.blade.php
+++ resources/views/beatmapsets/show.blade.php
@@ -22,11 +22,6 @@
 
 @section("content")
     <div class="js-react--beatmapset-page"></div>
-    {{--
-        this should content a server side react.js render which doesn't exist in hhvm
-        because the only library for it, which is experimental, requires PHP extension
-        which isn't supported by hhvm (v8js).
-    --}}
 @endsection
 
 @section("script")
