111a91fe3c2cb2ab62653de0aa5fa226ec8abc94
nanaya <me@nanaya.pro>
2017-11-28 20:11:03 +0900

    Saner escape
    Because better.
    
    Fixes laravel/framework#12065.

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -2,6 +2,9 @@
 
 define('LARAVEL_START', microtime(true));
 
+// fixes some laravel features.
+require __DIR__.'/../app/framework_helper_overrides.php';
+
 /*
 |--------------------------------------------------------------------------
 | Register The Composer Auto Loader
