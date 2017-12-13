2b7b24fa075749caf10ec0b5fbc20c9af7c3a979
nanaya <me@nanaya.pro>
2017-12-13 18:42:46 +0900

    More hype cap

--- app/Transformers/BeatmapsetTransformer.php
+++ app/Transformers/BeatmapsetTransformer.php
@@ -98,8 +98,11 @@ class BeatmapsetTransformer extends Fractal\TransformerAbstract
             return;
         }
 
+        $hypePriv = priv_check_user($currentUser, 'BeatmapsetHype', $beatmapset);
+
         $ret = [
-            'can_hype' => priv_check_user($currentUser, 'BeatmapsetHype', $beatmapset)->can(),
+            'can_hype' => $hypePriv->can(),
+            'can_hype_reason' => $hypePriv->can() ? null : $hypePriv->message(),
             'remaining_hype' => $currentUser->remainingHype(),
         ];
 
