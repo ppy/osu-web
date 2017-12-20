72a4a58970e4c7ce3e1435e3e145ae44fd294455
teknoabu <34662187+teknoabu@users.noreply.github.com>
2017-12-20 19:14:14 +0100

    Copy from EN

---
+++ resources/lang/da/accounts.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'edit' => [
+        'title' => '<strong>Account</strong> Settings',
+        'title_compact' => 'settings',
+
+        'avatar' => [
+            'title' => 'Edit Avatar',
+        ],
+
+        'email' => [
+            'current' => 'current email',
+            'new' => 'new email',
+            'new_confirmation' => 'email confirmation',
+            'title' => 'Email',
+        ],
+
+        'password' => [
+            'current' => 'current password',
+            'new' => 'new password',
+            'new_confirmation' => 'password confirmation',
+            'title' => 'Password',
+        ],
+
+        'profile' => [
+            'title' => 'Edit Profile',
+
+            'user' => [
+                'user_from' => 'current location',
+                'user_interests' => 'interest',
+                'user_msnm' => 'skype',
+                'user_occ' => 'occupation',
+                'user_twitter' => 'twitter',
+                'user_website' => 'website',
+            ],
+        ],
+
+        'signature' => [
+            'title' => 'Signature',
+            'update' => 'update',
+        ],
+    ],
+
+    'update_email' => [
+        'email_subject' => 'osu! email change confirmation',
+        'update' => 'update',
+        'updated' => 'Email updated',
+    ],
+
+    'update_password' => [
+        'email_subject' => 'osu! password change confirmation',
+        'update' => 'update',
+        'updated' => 'Password updated',
+    ],
+
+    'playstyles' => [
+        'title' => 'Playstyles',
+        'mouse' => 'mouse',
+        'keyboard' => 'keyboard',
+        'tablet' => 'tablet',
+        'touch' => 'touch',
+    ],
+];
