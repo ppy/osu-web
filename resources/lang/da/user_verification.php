ecc0df3d5de0d6b9c5a6e1a975c2ec06c75f9918
Kristian Jensen <34662187+teknoabu@users.noreply.github.com>
2017-12-23 18:11:41 +0100

    Update user_verification.php

--- resources/lang/da/user_verification.php
+++ resources/lang/da/user_verification.php
@@ -20,30 +20,30 @@
 
 return [
     'box' => [
-        'sent' => 'An email has been sent to :mail with a verification code. Enter the code.',
-        'title' => 'Account Verification',
-        'verifying' => 'Verifying...',
-        'issuing' => 'Issuing new code...',
+        'sent' => 'En email er blevet sendt til :mail med en bekræftelseskode. Skriv koden her.',
+        'title' => 'Kontobekræftelse',
+        'verifying' => 'Bekræfter...',
+        'issuing' => 'Laver ny kode...',
 
         'info' => [
-            'check_spam' => "Make sure to check your spam folder if you can't find the email.",
-            'recover' => "If you can't access your email or have forgotten what you used, please follow the :link.",
-            'recover_link' => 'email recovery process here',
-            'reissue' => 'You can also :reissue_link or :logout_link.',
-            'reissue_link' => 'request another code',
-            'logout_link' => 'logout',
+            'check_spam' => "Husk at tjekke din spammappe, hvis ikke du kan finde emailen.",
+            'recover' => "Hvis ikke du har adgang til din email-adresse eller har glemt hvilken email-adresse, du bruger, følg venligst :link.",
+            'recover_link' => 'email-adresse genoprettelsesprocessen her',
+            'reissue' => 'Du kan også :reissue_link eller :logout_link.',
+            'reissue_link' => 'anmode om en ny kode',
+            'logout_link' => 'logge ud',
         ],
     ],
 
     'email' => [
-        'subject' => 'osu! account verification',
+        'subject' => 'osu! kontobekræftelse',
     ],
 
     'errors' => [
-        'expired' => 'Verification code expired, new verification email sent.',
-        'incorrect_key' => 'Incorrect verification code.',
-        'retries_exceeded' => 'Incorrect verification code. Retry limit exceeded, new verification email sent.',
-        'reissued' => 'Verification code reissued, new verification email sent.',
-        'unknown' => 'Unknown problem occurred, new verification email sent.',
+        'expired' => 'Bekræftelseskoden er udløbet, og en ny email med bekræftelseskode er blevet sendt.',
+        'incorrect_key' => 'Ugyldig bekræftelseskode.',
+        'retries_exceeded' => 'Ugyldig bekræftelseskode, og du har brugt for mange forsøg. En ny email med bekræftelseskode er blevet sendt.',
+        'reissued' => 'En email med en ny bekræftelseskode er blevet sendt.',
+        'unknown' => 'Der opstod et ukendt problem. En ny email med bekræftelseskode er blevet sendt.',
     ],
 ];
