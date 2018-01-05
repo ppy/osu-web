1016ad870ae612e7c8c6984b3d618a55a0da75c9
bakaneko <baka+github@nekonyan.net>
2018-01-05 18:57:57 +0900

    use index/alias name returned by model

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -4,9 +4,9 @@
 return [
     'hosts' => (array) env('ES_HOST', 'localhost:9200'),
     'index' => [
-        'beatmaps' => 'beatmaps',
-        'posts' => 'posts',
-        'users' => 'users',
+        'beatmaps' => App\Models\Beatmapset::esIndexName(),
+        'posts' => App\Models\Forum\Post::esIndexName(),
+        'users' => App\Models\User::esIndexName(),
         'wiki_pages' => 'osu:wiki_pages_20171130'
     ]
 ];
