4379af65c049ae679aa6dbfc0f75daec31ebc098
adv3006 <torikatok@gmail.com>
2018-01-06 21:38:51 -0800

    edited

--- resources/lang/en/community.php
+++ resources/lang/en/community.php
@@ -22,108 +22,108 @@ return [
     'support' => [
         'header' => [
             // size in font-size
-            'big_description' => 'Love osu!?<br/>
-                                Support the osu! dev :D',
+            'big_description' => 'Yêu thích osu!?<br/>
+                                Hãy ủng hộ sự phát triển của osu! :D',
             'small_description' => '',
-            'support_button' => 'I want to support osu!',
+            'support_button' => 'Tôi muốn ủng hộ osu!',
         ],
 
-        'dev_quote' => 'osu! is a completely free-to-play game, but running it is most definitely not so free. Between the cost of commissioning servers and high quality international bandwidth, the time spent maintaining the system and community, providing prizes for competitions, answering support questions and generally keeping people happy, osu! consumes quite a hefty amount of money! Oh, and don\'t forget the fact that we do it without any advertising or partnering with silly toolbars and the likes!
-            <br/><br/>osu! is at the end of the day largely run by myself, to which you may know best as "peppy".
-            I have had to quit my day job in order to keep up with osu!,
-            and do at times struggle to maintain the standards I strive for.
-            I would like to offer my personal thanks to those who have supported osu! thus far,
-            and just as much to those who continue to support this amazing game and community into the future :).',
+        'dev_quote' => 'osu! là một trò chơi hoàn toàn miễn phí, nhưng việc vận hành trò chơi này thì phần nhiều nó lại không hề miễn phí đến thế. Gộp lại những nhân tố, bao gồm chi phí cho việc vận hành các máy chủ và cho băng thông quốc tế chất lượng cao, thời gian dành ra để bảo trì hệ thống và cộng đồng osu!, cung cấp giải thưởng cho các cuộc thi, trả lời các câu hỏi giúp đỡ và giữ được sự vui vẻ cho phần lớn người chơi, số lượng tiền mà osu tiêu tốn khá là khổng lồ. À, và đừng quên là chúng tôi làm mà không nhờ đến sự quảng cáo nào hay hợp tác với những thanh công cụ vớ vẩn và tương tự!
+            <br/><br/>Việc vận hành osu!, suy cho cùng, đều phần lớn là bởi bản thân tôi, người mà bạn có thể biết đến rõ nhất với cái tên "peppy".
+            Tôi đã phải nghỉ công việc mưu sinh của mình để nhằm bắt kịp được với osu!,
+            và đôi khi còn phải xoay sở để duy trì những tiêu chuẩn mà tôi đặt ra.
+            Tôi muốn gửi tặng lời cảm ơn từ cá nhân tôi đến những người đã ủng hộ osu! từ lâu,
+            và hệt như vậy đến những người đang và sẽ tiếp tục ủng hộ trò chơi và cộng đồng tuyệt vời này trong tương lai tới :).',
 
         'why_support' => [
-            'title' => 'Why should I support osu!?',
+            'title' => 'Tại sao tôi nên ủng hộ osu!?',
             'blocks' => [
-                'dev' => 'Developed and maintained predominantly by one guy in Australia',
-                'time' => 'Takes so much time to keep running that it\'s no longer possible to call it a "hobby".',
-                'ads' => 'No ads anywhere. <br/><br/>
-                        Unlike 99.95% of the web, we don\'t profit off shoving stuff in your face.',
-                'goodies' => 'You get some extra goodies!',
+                'dev' => 'Chủ yếu được phát triển và duy trì bởi một gã sống ở Úc',
+                'time' => 'Dành quá nhiều thời gian để trò chơi chạy được, đến mức không còn có thể gọi nó là một "thú vui" nữa.',
+                'ads' => 'Không có quảng cáo ở bất cứ đâu. <br/><br/>
+                        Khác với 99.95% của web, chúng tôi không kiếm lợi nhuận bẳng cách đẩy đồ vào mặt bạn.',
+                'goodies' => 'Bạn sẽ được nhận thêm các phần quà hấp dẫn!',
             ],
         ],
 
         'perks' => [
-            'title' => 'Oh? What do I get?!',
+            'title' => 'Ồ, Vậy tôi sẽ nhận được gì?!',
             'osu_direct' => [
                 'title' => 'osu!direct',
-                'description' => 'quick and easy access to search beatmaps without leaving the game.',
+                'description' => 'Truy cập nhanh và dễ dàng để tìm beatmap mà không cần phải thoát phải trò chơi.',
             ],
 
             'auto_downloads' => [
-                'title' => 'Auto Downloads',
-                'description' => 'Automatic downloads when playing multiplayer, spectating others, or clicking links in chat!',
+                'title' => 'Tải xuống tự động',
+                'description' => 'Tải xuống tự động khi đang chơi multiplayer, theo dõi người chơi khác, hay khi nhấp link trong chat!',
             ],
 
             'upload_more' => [
-                'title' => 'Upload More',
-                'description' => 'Additional pending beatmap slots (per ranked beatmap) up to a max of 10.',
+                'title' => 'Tải Lên Thêm',
+                'description' => 'Thêm số lượng pending beatmap (cho mỗi beatmap được rank) tới tối đa là 10.',
             ],
 
             'early_access' => [
-                'title' => 'Early Access',
-                'description' => 'Access to early releases, where you can try new features before they go public!',
+                'title' => 'Tiếp Cận Sớm',
+                'description' => 'Tiếp cận những bản ra mắt sớm mà bạn có thể dùng thử các tính năng mới trước khi chúng được ra mắt công khai!',
             ],
 
             'customisation' => [
-                'title' => 'Customisation',
-                'description' => 'Customise your profile by adding a fully customisable user page.',
+                'title' => 'Tùy Chỉnh',
+                'description' => 'Tùy chỉnh đầy đủ cho profile của bạn bằng cách thêm một user page có thể chỉnh sửa.',
             ],
 
             'beatmap_filters' => [
-                'title' => 'Beatmap Filters',
-                'description' => 'Filter beatmap searches by played and unplayed maps and rank achieved (if any).',
+                'title' => 'Lọc Beatmap',
+                'description' => 'Tìm kiếm beatmap được lọc theo đã chơi, chưa chơi và rank đạt được (nếu có).',
             ],
 
             'yellow_fellow' => [
-                'title' => 'Yellow Fellow',
-                'description' => 'Be recognised in-game with your new bright yellow chat username colour.',
+                'title' => 'Anh Bạn Màu Vàng',
+                'description' => 'Được nhận diện trong game bằng màu vàng sáng trên tên tài khoản của bạn trong chat.',
             ],
 
             'speedy_downloads' => [
-                'title' => 'Speedy Downloads',
-                'description' => 'More lenient download restrictions, especially when using osu!direct.',
+                'title' => 'Tải Xuống Nhanh Chóng',
+                'description' => 'Tải ít bị giới hạn hơn, đặc biệt là khi dùng osu!direct.',
             ],
 
             'change_username' => [
-                'title' => 'Change Username',
-                'description' => 'The ability to change your username without additional costs. (once max)',
+                'title' => 'Đổi Tên Tài Khoản',
+                'description' => 'Được đổi tên tài khoản miễn phí. (tối đa một lần)',
             ],
 
             'skinnables' => [
-                'title' => 'Skinnables',
-                'description' => 'Extra in-game skinnables, like the main menu background.',
+                'title' => 'Thêm Skin',
+                'description' => 'Thêm in-game skin, như là màn hình background ở menu chính.',
             ],
 
             'feature_votes' => [
-                'title' => 'Feature Votes',
-                'description' => 'Votes for feature requests. (2 per month)',
+                'title' => 'Bầu Chọn Tính Năng',
+                'description' => 'Bầu chọn cho các tính năng theo yêu cầu. (2 lần mỗi tháng)',
             ],
 
             'sort_options' => [
-                'title' => 'Sort Options',
-                'description' => 'NEW: The ability to view beatmap country / friend / mod-specific rankings in-game.',
+                'title' => 'Tùy Chọn Sắp Xếp',
+                'description' => 'MỚI: Cho phép xem xếp hạng theo quốc gia / bạn bè / theo-mod của beatmap trong game.',
             ],
 
             'feel_special' => [
-                'title' => 'Feel Special',
-                'description' => 'The warm and fuzzy feeling of doing your part to keep osu! running smoothly!',
+                'title' => 'Cảm Thấy Đặc Biệt',
+                'description' => 'Cảm giác thân thiện và trìu mến khi chung tay giúp cho osu! chạy một cách trơn tru!',
             ],
 
             'more_to_come' => [
-                'title' => 'More to come',
+                'title' => 'Còn hơn thế nữa',
                 'description' => '',
             ],
         ],
 
         'convinced' => [
-            'title' => 'I\'m convinced! :D',
+            'title' => 'Tôi đã tin! :D',
             'support' => 'support osu!',
-            'gift' => 'or gift support to other players',
-            'instructions' => 'click the heart button to proceed to the osu!store',
+            'gift' => 'hay gửi support đến người chơi khác',
+            'instructions' => 'nhấp vào nút trái tim để đi đến osu!store',
         ],
     ],
 ];
