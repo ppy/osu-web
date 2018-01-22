e138d2d2fd3bda9c52db603e26e0861830a7d19c
bakaneko <baka+github@nekonyan.net>
2018-01-22 14:24:23 +0900

    move default elasticsearch client config

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -3,7 +3,10 @@
 
 return [
     'hosts' => (array) env('ES_HOST', 'localhost:9200'),
-
-    'search_timeout' => get_float(env('ES_SEARCH_TIMEOUT')) ?? 5,
-    'search_connect_timeout' => get_float(env('ES_SEARCH_CONNECT_TIMEOUT')) ?? 0.5,
+    'connectionParams' => [
+        'client' => [
+            'timeout' => get_float(env('ES_SEARCH_TIMEOUT')) ?? 5,
+            'connect_timeout' => get_float(env('ES_SEARCH_CONNECT_TIMEOUT')) ?? 0.5,
+        ]
+    ]
 ];
