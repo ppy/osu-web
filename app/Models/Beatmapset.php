4ad8a050e002d25581362b2483888d649982a5a7
Jamie Taylor <me@nekodex.net>
2018-01-24 16:37:33 +0900

    Move user daily beatmapset nomination limit to env var (and increase default to 10)

--- app/Models/Beatmapset.php
+++ app/Models/Beatmapset.php
@@ -83,7 +83,6 @@ class Beatmapset extends Model
     ];
     const HYPEABLE_STATES = [-1, 0, 3];
 
-    const NOMINATIONS_PER_DAY = 3;
     const RANKED_PER_DAY = 8;
     const MINIMUM_DAYS_FOR_RANKING = 7;
     const BUNDLED_IDS = [3756, 163112, 140662, 151878, 190390, 123593, 241526, 299224];
