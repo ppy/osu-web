86c4e8fc2730b3ceead2d1fecb375b00715b1bd0
bakaneko <baka+github@nekonyan.net>
2018-02-02 20:13:31 +0900

    rename env to ES_CLIENT_x

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -5,8 +5,8 @@ return [
     'hosts' => (array) env('ES_HOST', 'localhost:9200'),
     'connectionParams' => [
         'client' => [
-            'timeout' => get_float(env('ES_SEARCH_TIMEOUT')) ?? 5,
-            'connect_timeout' => get_float(env('ES_SEARCH_CONNECT_TIMEOUT')) ?? 0.5,
+            'timeout' => get_float(env('ES_CLIENT_TIMEOUT')) ?? 5,
+            'connect_timeout' => get_float(env('ES_CLIENT_CONNECT_TIMEOUT')) ?? 0.5,
         ],
     ],
 ];
