015982c66b0d131f3c73ba2237791cf6adc04b8b
Krzysztof <krzysio.gutkowski@gmail.com>
2018-02-12 21:52:48 +0100

    Move to a non-npm installed yarn setup

--- docker/yarn/setup.sh
+++ docker/yarn/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-userdel node
+# workaround for macos default groupid (20)
+groupmod -g 24 dialout
 
 groupadd -g $_gid osuweb
 useradd -m -u $_uid -g $_gid osuweb
-
-npm install -g yarn
