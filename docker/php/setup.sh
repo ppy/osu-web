5fbe04d29fd0c831c94b22f6abfaa9c65b323660
Krzysztof <krzysio.gutkowski@gmail.com>
2018-02-12 20:41:03 +0100

    Upgrade to PHP 7.2

--- docker/php/setup.sh
+++ docker/php/setup.sh
@@ -3,5 +3,8 @@
 touch /usr/local/bin/yarn
 chmod +x /usr/local/bin/yarn
 
+# workaround for macos default groupid (20)
+groupmod -g 11 dialout
+
 groupadd -g $_gid osuweb
 useradd -m -u $_uid -g $_gid osuweb
