e0a66a80e6c2bdae3d800856cef26008d719cb8e
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2018-03-01 12:58:57 +0100

    Clean up factory

--- database/factories/UserBanHistoryFactory.php
+++ database/factories/UserBanHistoryFactory.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\UserBanHistory;
 
-$factory->define(App\Models\UserBanHistory::class, function (Faker\Generator $faker) {
+$factory->define(UserBanHistory::class, function (Faker\Generator $faker) {
     return [
         'reason' => $faker->bs,
         // 5 minutes (300 seconds) times 2 to the nth power (as in the standard osu silence durations)
@@ -10,20 +11,14 @@ $factory->define(App\Models\UserBanHistory::class, function (Faker\Generator $fa
     ];
 });
 
-$factory->state(App\Models\UserBanHistory::class, 'silence', function (Faker\Generator $faker) {
-    return [
-        'ban_status' => 2,
-    ];
+$factory->state(UserBanHistory::class, 'silence', function (Faker\Generator $faker) {
+    return ['ban_status' => 2];
 });
 
-$factory->state(App\Models\UserBanHistory::class, 'restriction', function (Faker\Generator $faker) {
-    return [
-        'ban_status' => 1,
-    ];
+$factory->state(UserBanHistory::class, 'restriction', function (Faker\Generator $faker) {
+    return ['ban_status' => 1];
 });
 
-$factory->state(App\Models\UserBanHistory::class, 'note', function (Faker\Generator $faker) {
-    return [
-        'ban_status' => 3,
-    ];
+$factory->state(UserBanHistory::class, 'note', function (Faker\Generator $faker) {
+    return ['ban_status' => 3];
 });
