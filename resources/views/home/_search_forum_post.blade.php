5746d2fdfe3227aa193c2310063f3d6758e1e5b3
bakaneko <baka+github@nekonyan.net>
2018-03-07 19:31:11 +0900

    placeholders 三三三ヽ( ﾟ ヮﾟ)ﾉ

--- resources/views/home/_search_forum_post.blade.php
+++ resources/views/home/_search_forum_post.blade.php
@@ -23,20 +23,29 @@
     $firstPostUrl = route('forum.topics.show', $entry->source('topic_id'));
 @endphp
 <div class="search-entry-thread">
-    <a class="search-entry" href="{{ $firstPostUrl }}">
-        <h1 class="search-entry__row search-entry__row--title">
-            {{ $entry->source('search_content') }}
-        </h1>
-        <div class="search-entry__row search-entry__row--excerpt">
-            @foreach ($firstPost as $post)
-                <span>{!! html_excerpt($post->source('search_content')) !!}</span>
-            @endforeach
-        </div>
-        <p class="search-entry__row search-entry__row--footer">
-            {{ $firstPostUrl }}
-        </p>
-    </a>
-
+    <div class="search-entry">
+        <a class="search-forum-post" href="{{ $firstPostUrl }}">
+            <div class="search-forum-post__avatar">
+                {{ $entry->source('poster_id') }}
+            </div>
+            <div class="search-forum-post__content">
+                <h1 class="search-entry__row search-entry__row--title">
+                    {{ $entry->source('search_content') }}
+                </h1>
+                <div class="search-entry__row search-entry__row--excerpt">
+                    @foreach ($firstPost as $post)
+                        <span>{!! html_excerpt($post->source('search_content')) !!}</span>
+                    @endforeach
+                </div>
+                <div class="search-entry__row search-entry__row--footer">
+                    <span>posted by </span>
+                    <span class="search-forum-post__username">username</span>
+                    <span class="search-forum-post__link">{{ $firstPostUrl }}</span>
+                    <span class="search-forum-post__time">{{ $entry->source('post_time') }}</span>
+                </div>
+            </div>
+        </a>
+    </div>
 
     @foreach ($innerHits as $innerHit)
         @php
@@ -44,23 +53,31 @@
         @endphp
 
         <div class="search-entry-thread__sub-item">
-            <a class="search-entry" href="{{ $postUrl }}">
-                <div class="search-entry__row search-entry__row--excerpt">
-                    <span class="search-entry__highlight">
-                        {!!
-                            implode(
-                                ' ... ',
-                                $innerHit->highlights(
-                                    'search_content',
-                                    App\Libraries\ForumSearch::HIGHLIGHT_FRAGMENT_SIZE * 2
-                                )
-                            )
-                        !!}
-                    </span>
+            <a class="search-forum-post" href="{{ $postUrl }}">
+                <div class="search-forum-post__avatar">
+                    {{ $innerHit->source('poster_id') }}
                 </div>
-                <p class="search-entry__row search-entry__row--footer">
-                    {{ $postUrl }}
-                </p>
+                <div class="search-forum-post__content">
+                    <div class="search-entry__row search-entry__row--excerpt">
+                        <span class="search-entry__highlight">
+                            {!!
+                                implode(
+                                    ' ... ',
+                                    $innerHit->highlights(
+                                        'search_content',
+                                        App\Libraries\ForumSearch::HIGHLIGHT_FRAGMENT_SIZE * 2
+                                    )
+                                )
+                            !!}
+                        </span>
+                    </div>
+                    <div class="search-entry__row search-entry__row--footer">
+                        <span>posted by </span>
+                        <span class="search-forum-post__username">username</span>
+                        <span class="search-forum-post__link">{{ $postUrl }}</span>
+                        <span class="search-forum-post__time">{{ $innerHit->source('post_time') }}</span>
+                    </div>
+                </div>
             </a>
         </div>
     @endforeach
