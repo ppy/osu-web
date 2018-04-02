6b43c198ecd28ac161eed4a2a12876dd13df742b
Soukyuen <tsuyugami@gmail.com>
2018-04-02 06:08:50 -0700

    completed layout, artist, api, wiki, validation, user_verification, users, livestreams, home, admin, accounts.php

---
+++ resources/lang/ja/artist.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'page_description' => 'Featured artists on osu!',
+    'title' => 'Featured Artists',
+
+    'admin' => [
+        'hidden' => 'ARTIST IS CURRENTLY HIDDEN',
+    ],
+
+    'beatmaps' => [
+        '_' => 'Beatmaps',
+        'download' => 'Download Beatmap Template',
+        'download-na' => 'Beatmap Template not yet available',
+    ],
+
+    'index' => [
+        'description' => 'Featured artists are artists that we are working in collaboration with in order to bring new and original music to osu!. These artists and a selection of their tracks have been hand-picked by the osu! team as being awesomesauce and suitable for mapping. Some of these featured artists have also created exclusive new tracks for use in osu!.<br><br>All tracks in this section are provided as pre-timed .osz files and have been officially licensed for use in osu! and osu!-related content.',
+    ],
+
+    'links' => [
+        'site' => 'Official Website',
+    ],
+
+    'songs' => [
+        '_' => 'Songs',
+        'count' => '1 song|:count songs',
+        'exclusive' => 'osu! exclusive',
+    ],
+];
---
+++ resources/lang/ja/multiplayer.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ *    Copyright 2015-2017 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'match' => [
+        'header' => 'Multiplayer Matches',
+        'team-types' => [
+            'head-to-head' => 'Head-to-head',
+            'tag-coop' => 'Tag Co-op',
+            'team-vs' => 'Team VS',
+            'tag-team-vs' => 'Tag Team VS',
+        ],
+        'events' => [
+            'player-left' => ':user left the match',
+            'player-joined' => ':user joined the match',
+            'player-kicked' => ':user has been kicked from the match',
+            'match-created' => ':user created the match',
+            'match-disbanded' => 'the match was disbanded',
+            'host-changed' => ':user became the host',
+
+            'player-left-no-user' => 'a player left the match',
+            'player-joined-no-user' => 'a player joined the match',
+            'player-kicked-no-user' => 'a player has been kicked from the match',
+            'match-created-no-user' => 'the match was created',
+            'match-disbanded-no-user' => 'the match was disbanded',
+            'host-changed-no-user' => 'the host was changed',
+        ],
+        'in-progress' => '(match in progress)',
+        'score' => [
+            'stats' => [
+                'accuracy' => 'Accuracy',
+                'combo' => 'Combo',
+                'score' => 'Score',
+            ],
+        ],
+        'failed' => 'FAILED',
+        'teams' => [
+            'blue' => 'Blue Team',
+            'red' => 'Red Team',
+        ],
+        'winner' => ':team wins',
+        'difference' => 'by :difference',
+        'loading-events' => 'Loading events...',
+        'more-events' => 'view all...',
+        'beatmap-deleted' => 'deleted beatmap',
+    ],
+    'game' => [
+        'scoring-type' => [
+            'score' => 'Highest Score',
+            'accuracy' => 'Highest Accuracy',
+            'combo' => 'Highest Combo',
+            'scorev2' => 'Score V2',
+        ],
+    ],
+];
