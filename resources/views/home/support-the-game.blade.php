e374960237fcfcfc491c6c8954617072c600cb13
Jamie Taylor <me@nekodex.net>
2018-04-03 17:48:17 +0900

    Use timeago instead of i18n_date for the expiration date

--- resources/views/home/support-the-game.blade.php
+++ resources/views/home/support-the-game.blade.php
@@ -71,7 +71,7 @@
                     @if ($supporterStatus['tags'] > 0)
                     <div class="stg-status__text stg-status__text--first">
                         {!! trans('community.support.supporter_status.'.($supporterStatus['current'] ? 'valid_until' : 'was_valid_until'), [
-                            'date' => '<strong>'.i18n_date($supporterStatus['expiration']).'</strong>'
+                            'date' => '<strong>'.timeago($supporterStatus['expiration']).'</strong>'
                         ]) !!}
                     </div>
                     <div class="stg-status__text">
