438df9b32d3a8edbcb7abcacbccdd5c994973bab
bakaneko <notbakaneko@users.noreply.github.com>
2018-04-12 17:33:17 +0900

    include a message for non-shipped items

--- resources/views/emails/store/payment_completed-en.blade.php
+++ resources/views/emails/store/payment_completed-en.blade.php
@@ -23,9 +23,11 @@ Thanks for your osu!store order!
 @foreach ($order->items as $item)
 {{ $item->quantity }} x {{ $item->getDisplayName() }} ({{ currency($item->subtotal()) }})
 @endforeach
-@if ($order->requiresShipping())
 
+@if ($order->requiresShipping())
 We have received your payment and are preparing your order for shipping. It may take a few days for us to send it out, depending on the quantity of orders. You can follow the progress of your order at {{ route('store.invoice.show', $order) }}, including tracking details where available.
+@else
+We have received your payment and are currently processing your order. You can follow the progress of your order at {{ route('store.invoice.show', $order) }}.
 @endif
 
 If you have any questions, don't hesitate to reply to this email.
