3b5ccb7cf1bfd0ebc306eb3565ef1106c0a3596b
Jamie Taylor <me@nekodex.net>
2018-04-18 19:35:22 +0900

    Simplify username validation

--- app/Models/User.php
+++ app/Models/User.php
@@ -1552,8 +1552,8 @@ class User extends Model implements AuthenticatableContract, Messageable
         if ($this->isDirty('user_jabber') && present($this->user_discord)) {
             // This is a basic check and not 100% compliant to Discord's spec, only validates that input:
             // - is a 2-32 char username (excluding chars @#:)
-            // - ends with a # and 4-digit discriminator (number being greater than 0000)
-            if (!preg_match('/^[^@#:]{2,32}#\d{3}[1-9]$/i', $this->user_discord)) {
+            // - ends with a # and 4-digit discriminator
+            if (!preg_match('/^[^@#:]{2,32}#\d{4}$/i', $this->user_discord)) {
                 $this->validationErrors()->add('user_discord', '.invalid_discord');
             }
         }
