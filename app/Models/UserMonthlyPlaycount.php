bed1d9b3322be3ee79861123c2bb6973336c57a5
nanaya <me@nanaya.pro>
2018-04-20 23:41:37 +0900

    `false` isn't a valid $primaryKey value
    Explodes during insert.

--- app/Models/UserMonthlyPlaycount.php
+++ app/Models/UserMonthlyPlaycount.php
@@ -25,7 +25,6 @@ use Carbon\Carbon;
 class UserMonthlyPlaycount extends Model
 {
     protected $table = 'osu_user_month_playcount';
-    protected $primaryKey = false;
     protected $guarded = [];
     public $timestamps = false;
 
