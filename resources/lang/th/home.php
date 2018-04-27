8b0de87c09a777e98f23c51fb1c8e5998b611d14
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-04-28 03:58:07 +0700

    ♻️ Update content for home.php

--- resources/lang/th/home.php
+++ resources/lang/th/home.php
@@ -20,19 +20,13 @@
 
 return [
     'landing' => [
+        'download' => 'Download now',
         'online' => '<strong>:players</strong> currently online in <strong>:games</strong> games',
         'peak' => 'Peak, :count online users',
         'players' => '<strong>:count</strong> registered players',
 
-        'download' => [
-            '_' => 'Download now',
-            'soon' => 'osu! coming soon to other operating systems',
-            'for' => 'for :os',
-            'other' => 'click here for :os1 or :os2',
-        ],
-
         'slogan' => [
-            'main' => 'free-to-play rhythm game',
+            'main' => 'the bestest free-to-win rhythm game',
             'sub' => 'rhythm is just a click away',
         ],
     ],
@@ -42,7 +36,8 @@ return [
         'button' => 'Search',
         'empty_result' => 'Nothing found!',
         'missing_query' => 'Search keyword of minimum :n characters is required',
-        'title' => 'Search Results',
+        'placeholder' => 'type to search',
+        'title' => 'Search',
 
         'beatmapset' => [
             'more' => ':count more beatmap search results',
@@ -87,38 +82,44 @@ return [
     ],
 
     'download' => [
-      'header' => [
-          '1' => "let's get",
-          '2' => 'you started',
-          '3' => 'download osu! game client for Windows',
-      ],
-      'steps' => [
-          '1' => [
-              'name' => 'Step 1',
-              'content' => 'Download the osu! game client',
-          ],
-          '2' => [
-              'name' => 'Step 2',
-              'content' => 'Create an osu! player account',
-          ],
-          '3' => [
-              'name' => 'Step 3',
-              'content' => '???',
-          ],
-      ],
-      'more' => 'Learn more?',
-      'more_text' => 'Check out the <a href="https://www.youtube.com/user/osuacademy/">osu!academy YouTube Channel</a> for up-to-date tutorials and tips on how to get the most out of osu!',
+        'tagline' => "let's get<br>you started!",
+        'action' => 'Download osu!',
+        'os' => [
+            'windows' => 'for Windows',
+            'macos' => 'for macOS',
+            'linux' => 'for Linux',
+        ],
+        'mirror' => 'mirror',
+        'macos-fallback' => 'macOS users',
+        'steps' => [
+            'register' => [
+                'title' => 'get an account',
+                'description' => 'follow the prompts when starting the game to sign in or make a new account',
+            ],
+            'download' => [
+                'title' => 'download the game',
+                'description' => 'click the button above to download the installer, then run it!',
+            ],
+            'beatmaps' => [
+                'title' => 'get beatmaps',
+                'description' => [
+                    '_' => ':browse the vast library of user-created beatmaps and start playing!',
+                    'browse' => 'browse',
+                ],
+            ],
+        ],
+        'video-guide' => 'video guide',
     ],
 
     'user' => [
-        'title' => 'news',
+        'title' => 'dashboard',
         'news' => [
             'title' => 'News',
             'error' => 'Error loading news, try refreshing the page?...',
         ],
         'header' => [
             'welcome' => 'Hello, <strong>:username</strong>!',
-            'messages' => 'You have 1 new message|You have :count new messages',
+            'messages' => 'You have :count new message|You have :count new messages',
             'stats' => [
                 'friends' => 'Online Friends',
                 'games' => 'Games',
