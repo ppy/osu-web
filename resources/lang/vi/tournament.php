bbb4ec7e4340eec17e0b59106cfd5a4723bd3fc6
Dean Herbert <pe@ppy.sh>
2018-05-14 17:23:18 +0900

    New translations tournament.php (Vietnamese)

--- resources/lang/vi/tournament.php
+++ resources/lang/vi/tournament.php
@@ -27,16 +27,27 @@ return [
         'none_running' => 'Hiên tại không có giải đấu nào đang diễn ra, vui lòng kiểm tra lại sau!',
         'registration_period' => 'Đăng kí: :start đến :end',
     ],
+
     'show' => [
-        'button' => [
-            'register' => 'Đăng kí!',
-            'cancel' => 'Hủy Đăng Kí',
-        ],
+        'banner' => '',
         'entered' => 'Bạn đã đăng kí cuộc thi này.<br><br>Lưu ý rằng điều này không có nghĩa là bạn đã được chia đội.<br><br>Hướng dẫn thêm sẽ được gửi cho bạn qua email vào khoảng thời gian gần ngày thi đấu, vì thế hãy chắc chắn rằng email của tài khoản osu! của bạn hợp lệ',
+        'info_page' => '',
         'login_to_register' => 'Vui lòng :login để xem chi tiết đăng kí!',
         'not_yet_entered' => 'Bạn chưa đăng kí cuộc thi này.',
         'rank_too_low' => 'Xin lỗi, bạn không đáp ứng yêu cầu về thứ hạng cho giải đấu này!',
         'registration_ends' => 'Đăng kí sẽ kết thúc vào :date',
+
+        'button' => [
+            'cancel' => 'Hủy Đăng Kí',
+            'register' => 'Đăng kí!',
+        ],
+
+        'state' => [
+            'before_registration' => '',
+            'ended' => '',
+            'registration_closed' => '',
+            'running' => '',
+        ],
     ],
     'tournament_period' => ':start đến :end',
 ];
