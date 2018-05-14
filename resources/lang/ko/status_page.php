642ae6fbfb7fa3e409dd161bf6563be0f72a3ab0
Dean Herbert <pe@ppy.sh>
2018-05-14 17:21:06 +0900

    New translations status_page.php (Korean)

--- resources/lang/ko/status_page.php
+++ resources/lang/ko/status_page.php
@@ -26,7 +26,7 @@ return [
 
     'incidents' => [
         'title' => '진행중인 문제',
-        'automated' => '자동', // automated
+        'automated' => '자동',
     ],
 
     'online' => [
@@ -40,7 +40,7 @@ return [
 
     'recent' => [
         'incidents' => [
-            'title' => '최근 사건', // 확인 시 사건보다 문제가 더 부드러울 경우 문제로
+            'title' => '최근 사건',
             'state' => [
                 'resolved' => '해결됨',
                 'resolving' => '해결 중',
