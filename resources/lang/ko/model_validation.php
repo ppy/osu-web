a06eb29edc2f0b7cf084f09ad5cdba934250368d
Dean Herbert <pe@ppy.sh>
2018-05-14 17:21:02 +0900

    New translations model_validation.php (Korean)

--- resources/lang/ko/model_validation.php
+++ resources/lang/ko/model_validation.php
@@ -19,12 +19,37 @@
  */
 
 return [
-    'not_negative' => ':attribute 속성은 음수가 될 수 없습니다.', // :attribute cannot be negative.
-    'required' => ':attribute 속성이 필요합니다.', // :attribute is required.
+    'not_negative' => ':attribute 속성은 음수가 될 수 없습니다.',
+    'required' => ':attribute 속성이 필요합니다.',
+    'too_long' => '',
     'wrong_confirmation' => '확인란이 일치하지 않습니다.',
 
     'beatmap_discussion_post' => [
-        'first_post' => '시작글은 삭제할 수 없습니다.', // Can not delete starting post
+        'discussion_locked' => '',
+        'first_post' => '시작글은 삭제할 수 없습니다.',
+    ],
+
+    'beatmapset_discussion' => [
+        'beatmap_missing' => '',
+        'beatmapset_no_hype' => "",
+        'hype_requires_null_beatmap' => '',
+        'invalid_beatmap_id' => '',
+        'invalid_beatmapset_id' => '',
+        'locked' => '',
+        'mapper_note_wrong_user' => '',
+
+        'hype' => [
+            'guest' => '',
+            'hyped' => '',
+            'limit_exceeded' => '',
+            'not_hypeable' => '',
+            'owner' => '',
+        ],
+
+        'timestamp' => [
+            'exceeds_beatmapset_length' => '',
+            'negative' => "",
+        ],
     ],
 
     'forum' => [
@@ -37,6 +62,11 @@ return [
             'invalid' => '항목 선택이 잘못되었습니다.',
         ],
 
+        'post' => [
+            'beatmapset_post_no_delete' => '',
+            'beatmapset_post_no_edit' => '',
+        ],
+
         'topic_poll' => [
             'duplicate_options' => '지정하려는 항목이 이미 존재합니다.',
             'invalid_max_options' => '지정된 항목보다 많이 투표하도록 설정할 수 없습니다.',
@@ -46,6 +76,7 @@ return [
         ],
 
         'topic_vote' => [
+            'required' => '',
             'too_many' => '허용된 것 보다 많은 항목을 선택하셨습니다.',
         ],
     ],
@@ -54,14 +85,31 @@ return [
         'contains_username' => '비밀번호는 유저 이름을 포함할 수 없습니다.',
         'email_already_used' => '이미 사용중인 이메일 주소입니다.',
         'invalid_country' => '해당하는 국가가 데이터베이스에 존재하지 않습니다.',
-        'invalid_email' => '이메일 주소가 잘못되었습니다.', // Doesn't seem to be an email address
+        'invalid_discord' => '',
+        'invalid_email' => "이메일 주소가 잘못되었습니다.",
         'too_short' => '새 비밀번호가 너무 짧습니다.',
         'unknown_duplicate' => '유저 이름 또는 이메일 주소가 이미 사용중입니다.',
+        'username_available_in' => '',
+        'username_available_soon' => '',
+        'username_invalid_characters' => '',
+        'username_in_use' => '',
+        'username_no_space_userscore_mix' => '',
+        'username_no_spaces' => "",
+        'username_not_allowed' => '',
         'username_too_short' => '요청하신 유저 이름이 너무 짧습니다.',
-        'weak' => '비밀번호에 사용할 수 없는 문자나 패턴이 포함되어 있습니다.', // Blacklisted password
+        'username_too_long' => '',
+        'weak' => '비밀번호에 사용할 수 없는 문자나 패턴이 포함되어 있습니다.',
         'wrong_current_password' => '현재 비밀번호가 일치하지 않습니다.',
-        'wrong_email_confirmation' => '이메일과 이메일 확인란이 일치하지 않습니다.', // Email confirmation does not match
+        'wrong_email_confirmation' => '이메일과 이메일 확인란이 일치하지 않습니다.',
         'wrong_password_confirmation' => '비밀번호와 비밀번호 확인란이 일치하지 않습니다.',
         'too_long' => '최대 길이를 초과하셨습니다 - :limit자리 까지만 가능합니다.',
+
+        'change_username' => [
+            'supporter_required' => [
+                '_' => '',
+                'link_text' => '',
+            ],
+            'username_is_same' => '',
+        ],
     ],
 ];
