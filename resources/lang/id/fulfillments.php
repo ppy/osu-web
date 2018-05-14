4c92414c0844b52dc9ea8fc1b7a0216b6ab72e91
Dean Herbert <pe@ppy.sh>
2018-05-14 13:20:31 +0900

    New translations fulfillments.php (Indonesian)

--- resources/lang/id/fulfillments.php
+++ resources/lang/id/fulfillments.php
@@ -19,12 +19,12 @@
  */
 
 return [
-    'mail' => [
-        'donation_thanks' => [
-            'subject' => 'Terima kasih, osu! mencintaimu',
-        ],
-        'supporter_gift' => [
-            'subject' => 'Anda memiliki osu! supporter tag!',
-        ],
+    'username_change' => [
+        'only_one' => 'only 1 username change allowed per order fulfillment.',
+        'insufficient_paid' => 'Username change cost exceeds amount paid (:expected > :actual)',
+        'reverting_username_mismatch' => 'Current username (:current) is not the same as change to revoke (:username)',
+    ],
+    'supporter_tag' => [
+        'insufficient_paid' => 'Donation is less than required for supporter tag gift (:actual > :expected)',
     ],
 ];
