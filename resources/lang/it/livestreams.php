bf3d478a12c93827cdbe5cbd0158d6b8edca8e42
Dean Herbert <pe@ppy.sh>
2018-05-14 17:27:36 +0900

    New translations livestreams.php (Italian)

--- resources/lang/it/livestreams.php
+++ resources/lang/it/livestreams.php
@@ -20,10 +20,10 @@
 
 return [
     'top-headers' => [
-        'headline' => 'Live Streams',
-        'description' => 'Data is fetched from twitch.tv every five minutes based on the directory listing. Feel free to start streaming and get yourself listed! For more information on how to get setup, please check out the wiki page on live streaming.',
+        'headline' => '',
+        'description' => '',
     ],
     'headers' => [
-        'regular' => 'Currently Streaming',
+        'regular' => '',
     ],
 ];
