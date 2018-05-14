b5a42a99178afa4bbcb53d4672b63ee33d9e26c5
Dean Herbert <pe@ppy.sh>
2018-05-14 17:22:08 +0900

    New translations status_page.php (German)

--- resources/lang/de/status_page.php
+++ resources/lang/de/status_page.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *    Copyright 2015-2017 ppy Pty. Ltd.
+ *    Copyright 2015-2018 ppy Pty. Ltd.
  *
  *    This file is part of osu!web. osu!web is distributed with the hope of
  *    attracting more community contributions to the core ecosystem of osu!.
@@ -20,7 +20,7 @@
 
 return [
     'header' => [
-        'title' => 'status',
+        'title' => '',
         'description' => 'was geht ab?',
     ],
 
@@ -51,7 +51,7 @@ return [
         'uptime' => [
             'title' => 'Verfügbarkeit',
             'graphs' => [
-                'server' => 'server',
+                'server' => '',
                 'web' => 'webseite',
             ],
         ],
