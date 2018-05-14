873cb59be65b93330caa8ea3ebca22ec69f14f97
Dean Herbert <pe@ppy.sh>
2018-05-14 18:15:26 +0900

    New translations common.php (Chinese Traditional)

--- resources/lang/zh-tw/common.php
+++ resources/lang/zh-tw/common.php
@@ -87,12 +87,12 @@ return [
     ],
 
     'score_count' => [
-        'count_100' => '',
-        'count_300' => '',
-        'count_50' => '',
-        'count_geki' => '',
-        'count_katu' => '',
-        'count_miss' => '',
+        'count_100' => '100',
+        'count_300' => '300',
+        'count_50' => '50',
+        'count_geki' => 'MAX',
+        'count_katu' => '200',
+        'count_miss' => 'Miss',
     ],
 
     'time' => [
