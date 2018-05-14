b1b718b1a2c01fb9a5d913b81febf1497867f07a
Dean Herbert <pe@ppy.sh>
2018-05-14 17:22:47 +0900

    New translations rankings.php (Thai)

--- resources/lang/th/rankings.php
+++ resources/lang/th/rankings.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *    Copyright 2015-2017 ppy Pty. Ltd.
+ *    Copyright 2015-2018 ppy Pty. Ltd.
  *
  *    This file is part of osu!web. osu!web is distributed with the hope of
  *    attracting more community contributions to the core ecosystem of osu!.
@@ -36,8 +36,8 @@ return [
         'ranked_score' => 'อันดับที่',
         'average_score' => 'คะแนนเฉลี่ย',
         'average_performance' => 'ค่าศักยภาพเฉลี่ย',
-        'ss' => 'SS',
-        's' => 'S',
-        'a' => 'A',
+        'ss' => '',
+        's' => '',
+        'a' => '',
     ],
 ];
