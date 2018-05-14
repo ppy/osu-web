a6225164c044b727415c882346ae78234384fcb9
Dean Herbert <pe@ppy.sh>
2018-05-14 17:24:49 +0900

    New translations wiki.php (Danish)

--- resources/lang/da/wiki.php
+++ resources/lang/da/wiki.php
@@ -25,6 +25,7 @@ return [
         'missing' => 'Den anmodede side kunne ikke blive fundet.',
         'missing_title' => 'Ikke fundet',
         'missing_translation' => 'Den anmodede side kunne ikke blive fundet på det valgte sprog.',
+        'search' => '',
         'toc' => 'Indhold',
 
         'edit' => [
