edf0768f4fcd74f46e36a935322069ab4b957b5c
Dean Herbert <pe@ppy.sh>
2018-05-14 17:27:56 +0900

    New translations beatmaps.php (Korean)

--- resources/lang/ko/beatmaps.php
+++ resources/lang/ko/beatmaps.php
@@ -38,18 +38,20 @@ return [
         'deny_kudosu' => 'kudosu 거절',
         'edit' => '편집',
         'edited' => ':update_time에 :editor님에 의해 마지막으로 수정됨',
+        'kudosu_denied' => '',
         'message_placeholder' => '게시할 답글의 내용을 입력하세요.',
+        'message_placeholder_deleted_beatmap' => '',
         'message_type_select' => '게시할 답글의 형식을 선택하세요',
         'reply_notice' => '답변을 보내려면 엔터를 누르세요.',
         'reply_placeholder' => '보내실 답변의 내용을 입력하세요.',
-        'require-login' => '답글을 올리려면 로그인해 주세요', // Base text changed from "log" to "sign"
-        'resolved' => '결정됨', // Resolved, 해결됨이 적절할 수 있으나, 비트맵 토론에 관련된 내용이므로 결정으로 표현
+        'require-login' => '답글을 올리려면 로그인해 주세요',
+        'resolved' => '결정됨',
         'restore' => '복구됨',
         'title' => '토론',
 
         'collapse' => [
-            'all-collapse' => '모두 축소하기', // Collapse All, 스포일러 박스와 관련된 기능으로 추정됨
-            'all-expand' => '모두 확대하기', // Expand All
+            'all-collapse' => '모두 축소하기',
+            'all-expand' => '모두 확대하기',
         ],
 
         'empty' => [
@@ -63,6 +65,10 @@ return [
         ],
 
         'message_type' => [
+            'disqualify' => '',
+            'hype' => '',
+            'mapper_note' => '',
+            'nomination_reset' => '',
             'praise' => '칭찬',
             'problem' => '문제',
             'suggestion' => '제안',
@@ -70,7 +76,12 @@ return [
 
         'mode' => [
             'events' => '기록',
+            'general' => '',
             'timeline' => '타임라인',
+            'scopes' => [
+                'general' => '',
+                'generalAll' => '',
+            ],
         ],
 
         'new' => [
@@ -80,21 +91,29 @@ return [
         ],
 
         'show' => [
-            'title' => ':title, :mapper님이 ', // ':title mapped by :mapper'
+            'title' => ':title, :mapper님이 ',
+        ],
+
+        'sort' => [
+            '_' => '',
+            'created_at' => '',
+            'timeline' => '',
+            'updated_at' => '',
         ],
 
         'stats' => [
             'deleted' => '삭제됨',
-            'mine' => 'Mine', // 내 비트맵(?)
-            'pending' => 'Pending', // 보류됨
-            'praises' => 'Praises', // 칭찬
-            'resolved' => 'Resolved', // 결정됨
+            'mapper_notes' => '',
+            'mine' => '',
+            'pending' => '',
+            'praises' => '',
+            'resolved' => '',
             'total' => '모두',
         ],
 
         'status-messages' => [
             'approved' => '이 비트맵은 :date에 Approved 되었습니다!',
-            'graveyard' => '이 비트맵은 :date 이후로는 업데이트되지 않았고, 제작자가 제작을 거의 포기한 것 같습니다...',
+            'graveyard' => "이 비트맵은 :date 이후로는 업데이트되지 않았고, 제작자가 제작을 거의 포기한 것 같습니다...",
             'loved' => '이 비트맵은 :date에 Loved 비트맵으로 추가되었습니다!',
             'ranked' => '이 비트맵은 :date에 Ranked 되었습니다!',
             'wip' => 'Note: 이 비트맵은 제작자가 \'제작중\'이라고 표시한 맵입니다.',
@@ -105,8 +124,18 @@ return [
     'hype' => [
         'button' => '비트맵 홍보하기!',
         'button_done' => '이미 이 비트맵을 홍보하셨습니다!',
-        'section_title' => 'Hype Train', // 영어 슬랭으로, 적당한 말을 찾지 못해 번역
-        'title' => '비트맵 홍보', // 원문 'Hype'
+        'confirm' => "",
+        'explanation' => '',
+        'explanation_guest' => '',
+        'new_time' => "",
+        'remaining' => '',
+        'required_text' => '',
+        'section_title' => '',
+        'title' => '비트맵 홍보',
+    ],
+
+    'feedback' => [
+        'button' => '',
     ],
 
     'nominations' => [
@@ -114,13 +143,25 @@ return [
         'disqualified_at' => ':time_ago에 실격처리 되었습니다 (:reason).',
         'disqualified_no_reason' => '사유가 명시되지 않았습니다',
         'disqualify' => '실격시키기',
-        'incorrect-state' => '작업을 처리하는 과정에서 문제가 발생했습니다, 페이지를 새로고침 해보세요.',
-        'nominate' => '추천하기', // Nominate
-        'nominate_confirm' => '이 비트맵을 추천할까요?', // Nominate
+        'incorrect_state' => '',
+        'nominate' => '추천하기',
+        'nominate_confirm' => '이 비트맵을 추천할까요?',
+        'nominated_by' => '',
         'qualified' => '아무런 문제가 발견되지 않으면, :date에 Ranked될 것입니다.',
         'qualified_soon' => '아무런 문제가 발견되지 않으면, 곧 Ranked될 것입니다.',
         'required_text' => '추천 수: :current/:required',
-        'title' => '추천 상태', // Nomination Status
+        'reset_message_deleted' => '',
+        'title' => '추천 상태',
+        'unresolved_issues' => '',
+
+        'reset_at' => [
+            'nomination_reset' => '',
+            'disqualify' => '',
+        ],
+
+        'reset_confirm' => [
+            'nomination_reset' => '',
+        ],
     ],
 
     'listing' => [
@@ -130,35 +171,42 @@ return [
             'not-found' => '결과 없음',
             'not-found-quote' => '... 없네요, 아무것도 못 찾았습니다.',
             'filters' => [
+                'general' => '',
                 'mode' => '모드',
-                'status' => '등록 상태', // Rank Status
+                'status' => '등록 상태',
                 'genre' => '장르',
                 'language' => '언어 분류',
                 'extra' => '부가 기능',
                 'rank' => '순위 기록됨',
+                'played' => '',
             ],
         ],
         'mode' => '모드',
-        'status' => '등록 상태', // Rank Status
+        'status' => '등록 상태',
         'mapped-by' => ':mapper님이 제작',
         'source' => '원작: :source',
         'load-more' => '더 불러오기...',
     ],
+    'general' => [
+        'recommended' => '',
+        'converts' => '',
+    ],
     'mode' => [
         'any' => '모두',
-        'osu' => 'osu!',
-        'taiko' => 'osu!taiko',
-        'fruits' => 'osu!catch',
-        'mania' => 'osu!mania',
+        'osu' => '',
+        'taiko' => '',
+        'fruits' => '',
+        'mania' => '',
     ],
     'status' => [
         'any' => '모두',
-        'ranked-approved' => 'Ranked & Approved', // 랭크 & 공인됨
-        'approved' => 'Approved', // 공인됨
-        'loved' => 'Loved',
+        'ranked-approved' => '',
+        'approved' => '',
+        'qualified' => '',
+        'loved' => '',
         'faves' => '즐겨찾기',
-        'pending' => 'Pending', // 보류됨
-        'graveyard' => 'Graveyard', // 사장됨
+        'pending' => '',
+        'graveyard' => '',
         'my-maps' => '내 비트맵',
     ],
     'genre' => [
@@ -169,32 +217,33 @@ return [
         'rock' => '락',
         'pop' => '팝',
         'other' => '기타',
-        'novelty' => '이색적인 장르', // 참신한 장르에 붙음
+        'novelty' => '이색적인 장르',
         'hip-hop' => '힙합',
         'electronic' => '일렉트로닉',
     ],
     'mods' => [
-        'NF' => 'No Fail', // 노 페일
-        'EZ' => 'Easy Mode', // 이지 모드
-        'HD' => 'Hidden', // 히든
-        'HR' => 'Hard Rock', // 하드 락
-        'SD' => 'Sudden Death', // 서든 데스
-        'DT' => 'Double Time', // 더블 타임, 2배속
-        'Relax' => 'Relax', // 릴렉스
-        'HT' => 'Half Time', // 하프 타임
-        'NC' => 'Nightcore', // 나이트코어
-        'FL' => 'Flashlight', // 플래시라이트
-        'SO' => 'Spun Out', // 스펀 아웃
-        'AP' => 'Auto Pilot', // 오토 파일럿
-        'PF' => 'Perfect', // 퍼펙트
-        '4K' => '4키', // 4K
-        '5K' => '5키', // 5K
-        '6K' => '6키', // 6K
-        '7K' => '7키', // 7K
-        '8K' => '8키', // 8K
-        'FI' => 'Fade In', // 페이드 인
-        '9K' => '9키', // 9K
+        '4K' => '4키',
+        '5K' => '5키',
+        '6K' => '6키',
+        '7K' => '7키',
+        '8K' => '8키',
+        '9K' => '9키',
+        'AP' => '',
+        'DT' => '',
+        'EZ' => '',
+        'FI' => '',
+        'FL' => '',
+        'HD' => '',
+        'HR' => '',
+        'HT' => '',
+        'NC' => '',
+        'NF' => '',
         'NM' => '모드 없음',
+        'PF' => '',
+        'Relax' => '',
+        'SD' => '',
+        'SO' => '',
+        'TD' => '',
     ],
     'language' => [
         'any' => '모두',
@@ -210,19 +259,24 @@ return [
         'instrumental' => '가사 없음',
         'other' => '기타',
     ],
+    'played' => [
+        'any' => '',
+        'played' => '',
+        'unplayed' => '',
+    ],
     'extra' => [
         'video' => '비디오 있음',
         'storyboard' => '스토리보드 있음',
     ],
     'rank' => [
         'any' => '모두',
-        'XH' => '은장 SS', // 실버 SS
-        'X' => 'SS',
-        'SH' => '은장 S', // 실버 S
-        'S' => 'S',
-        'A' => 'A',
-        'B' => 'B',
-        'C' => 'C',
-        'D' => 'D',
+        'XH' => '은장 SS',
+        'X' => '',
+        'SH' => '은장 S',
+        'S' => '',
+        'A' => '',
+        'B' => '',
+        'C' => '',
+        'D' => '',
     ],
 ];
