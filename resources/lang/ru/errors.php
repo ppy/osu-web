243ee88da08b77998fb4d3f908656aeb3c547b12
Dean Herbert <pe@ppy.sh>
2018-05-14 13:02:16 +0900

    New translations errors.php (Russian)

--- resources/lang/ru/errors.php
+++ resources/lang/ru/errors.php
@@ -20,24 +20,24 @@
 
 return [
     'codes' => [
-        'http-401' => 'Войдите для продолжения.', // Base text changed from "log" to "sign"
-        'http-403' => 'Доступ запрещён.',
-        'http-429' => 'Слишком много попыток. Попробуйте позже.',
+        'http-401' => 'Please sign in to proceed.',
+        'http-403' => 'Access denied.',
+        'http-429' => 'Too many attempts. Try again later.',
     ],
     'account' => [
         'profile-order' => [
-            'generic' => 'Возникла ошибка. Попробуй перезагрузить страницу.',
+            'generic' => 'An error occured. Try refreshing the page.',
         ],
     ],
     'beatmaps' => [
-        'invalid_mode' => 'Указан недопустимый мод.',
-        'standard_converts_only' => 'Результатов для запрашиваемого мода нет.',
+        'invalid_mode' => 'Invalid mode specified.',
+        'standard_converts_only' => 'No scores are available for the requested mode on this beatmap difficulty.',
     ],
     'beatmapsets' => [
-        'too-many-favourites' => 'У вас слишком много избранных карт! Удалите одну из них для продолжения.',
+        'too-many-favourites' => 'You have to many favourited beatmaps! Please unfavourite one before continuing.',
     ],
-    'logged_out' => 'Вы вышли из аккаунта. Пожалуйста войдите и попробуйте ещё раз.', // Base text changed from "log" to "sign"
-    'supporter_only' => 'Вы должны иметь osu!supporter для использования этой возможности.',
-    'no_restricted_access' => 'Вы не можете использовать данную функцию пока ваши права ограничены.',
-    'unknown' => 'Возникла неизвестная ошибка.',
+    'logged_out' => 'You have been signed out. Please sign in and retry.',
+    'supporter_only' => 'You must be a supporter to use this feature.',
+    'no_restricted_access' => 'You are not able to perform this action while your account is in a restricted state.',
+    'unknown' => 'Unknown error occurred.',
 ];
