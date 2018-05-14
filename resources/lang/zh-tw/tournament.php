8a791adba241ceb733e486eae122d7a7de32e6cb
Dean Herbert <pe@ppy.sh>
2018-05-14 17:26:41 +0900

    New translations tournament.php (Chinese Traditional)

--- resources/lang/zh-tw/tournament.php
+++ resources/lang/zh-tw/tournament.php
@@ -27,16 +27,27 @@ return [
         'none_running' => '目前沒有正在進行的比賽，過段時間再來看看吧！',
         'registration_period' => '報名時間： :start 到 :end',
     ],
+
     'show' => [
-        'button' => [
-            'register' => '我要報名！',
-            'cancel' => '取消報名',
-        ],
+        'banner' => '支持你的队伍',
         'entered' => '你已報名此次比賽。<br><br>這不意味著你已經被分組。<br><br>比賽開始前你將收到郵件通知，所以請確保你的 osu! 郵箱可用！',
+        'info_page' => '信息页',
         'login_to_register' => '請 :login 以查看報名細節！',
         'not_yet_entered' => '你還沒有報名此次比賽。',
         'rank_too_low' => '抱歉，你還沒有達到本次比賽的排名要求！',
         'registration_ends' => '報名於 :date 結束',
+
+        'button' => [
+            'cancel' => '取消報名',
+            'register' => '我要報名！',
+        ],
+
+        'state' => [
+            'before_registration' => '本次比赛还未开始报名。',
+            'ended' => '本次比赛已经结束。移步信息页查看比赛结果。',
+            'registration_closed' => '本次比赛已停止报名。移步信息页查看最新信息。',
+            'running' => '本次比赛正在进行。移步信息页查看赛况。',
+        ],
     ],
     'tournament_period' => ':start 到 :end',
 ];
