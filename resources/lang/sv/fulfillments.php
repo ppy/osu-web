0b1f360d18b614853e0052851bc79aac21626407
Dean Herbert <pe@ppy.sh>
2018-05-14 14:11:01 +0900

    New translations fulfillments.php (Swedish)

--- resources/lang/sv/fulfillments.php
+++ resources/lang/sv/fulfillments.php
@@ -19,12 +19,12 @@
  */
 
 return [
-    'username_change' => [
-        'only_one' => 'only 1 username change allowed per order fulfillment.',
-        'insufficient_paid' => 'Username change cost exceeds amount paid (:expected > :actual)',
-        'reverting_username_mismatch' => 'Current username (:current) is not the same as change to revoke (:username)',
-    ],
-    'supporter_tag' => [
-        'insufficient_paid' => 'Donation is less than required for supporter tag gift (:actual > :expected)',
+    'mail' => [
+        'donation_thanks' => [
+            'subject' => 'Thanks, osu! <3s you',
+        ],
+        'supporter_gift' => [
+            'subject' => 'You have an osu! supporter tag!',
+        ],
     ],
 ];
