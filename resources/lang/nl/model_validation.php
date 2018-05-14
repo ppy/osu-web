0f6aa2b9c0dc515467ac9317e2b8653bfdefcf84
Dean Herbert <pe@ppy.sh>
2018-05-14 17:25:36 +0900

    New translations model_validation.php (Dutch)

--- resources/lang/nl/model_validation.php
+++ resources/lang/nl/model_validation.php
@@ -19,12 +19,97 @@
  */
 
 return [
+    'not_negative' => '',
     'required' => ':attribute is nodig.',
+    'too_long' => '',
+    'wrong_confirmation' => '',
+
+    'beatmap_discussion_post' => [
+        'discussion_locked' => '',
+        'first_post' => '',
+    ],
+
+    'beatmapset_discussion' => [
+        'beatmap_missing' => '',
+        'beatmapset_no_hype' => "",
+        'hype_requires_null_beatmap' => '',
+        'invalid_beatmap_id' => '',
+        'invalid_beatmapset_id' => '',
+        'locked' => '',
+        'mapper_note_wrong_user' => '',
+
+        'hype' => [
+            'guest' => '',
+            'hyped' => '',
+            'limit_exceeded' => '',
+            'not_hypeable' => '',
+            'owner' => '',
+        ],
+
+        'timestamp' => [
+            'exceeds_beatmapset_length' => '',
+            'negative' => "",
+        ],
+    ],
 
     'forum' => [
         'feature_vote' => [
             'not_feature_topic' => 'Kan alleen maar stemmen op een feature aanvraag.',
             'not_enough_feature_votes' => 'Niet genoeg stemmen.',
+        ],
+
+        'poll_vote' => [
+            'invalid' => '',
+        ],
+
+        'post' => [
+            'beatmapset_post_no_delete' => '',
+            'beatmapset_post_no_edit' => '',
+        ],
+
+        'topic_poll' => [
+            'duplicate_options' => '',
+            'invalid_max_options' => '',
+            'minimum_one_selection' => '',
+            'minimum_two_options' => '',
+            'too_many_options' => '',
+        ],
+
+        'topic_vote' => [
+            'required' => '',
+            'too_many' => '',
+        ],
+    ],
+
+    'user' => [
+        'contains_username' => '',
+        'email_already_used' => '',
+        'invalid_country' => '',
+        'invalid_discord' => '',
+        'invalid_email' => "",
+        'too_short' => '',
+        'unknown_duplicate' => '',
+        'username_available_in' => '',
+        'username_available_soon' => '',
+        'username_invalid_characters' => '',
+        'username_in_use' => '',
+        'username_no_space_userscore_mix' => '',
+        'username_no_spaces' => "",
+        'username_not_allowed' => '',
+        'username_too_short' => '',
+        'username_too_long' => '',
+        'weak' => '',
+        'wrong_current_password' => '',
+        'wrong_email_confirmation' => '',
+        'wrong_password_confirmation' => '',
+        'too_long' => '',
+
+        'change_username' => [
+            'supporter_required' => [
+                '_' => '',
+                'link_text' => '',
+            ],
+            'username_is_same' => '',
         ],
     ],
 ];
