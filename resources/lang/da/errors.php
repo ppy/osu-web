73b4241643854c250a03680fa8efb90740ed58a1
Dean Herbert <pe@ppy.sh>
2018-05-14 14:11:17 +0900

    New translations errors.php (Danish)

--- resources/lang/da/errors.php
+++ resources/lang/da/errors.php
@@ -20,24 +20,24 @@
 
 return [
     'codes' => [
-        'http-401' => 'Log venligst ind for at fortsætte.', // Base text changed from "log" to "sign"
-        'http-403' => 'Adgang nægtet.',
-        'http-429' => 'For mange forsøg. Prøv igen senere.',
+        'http-401' => 'Please sign in to proceed.',
+        'http-403' => 'Access denied.',
+        'http-429' => 'Too many attempts. Try again later.',
     ],
     'account' => [
         'profile-order' => [
-            'generic' => 'Der opstod en fejl. Prøv at genindlæse siden.',
+            'generic' => 'An error occured. Try refreshing the page.',
         ],
     ],
     'beatmaps' => [
-        'invalid_mode' => 'Ugyldig spiltilstand er specificeret.',
-        'standard_converts_only' => 'Ingen scores er tilgængelige for den anmodede spiltilstand på denne beatmap sværhedsgrad.',
+        'invalid_mode' => 'Invalid mode specified.',
+        'standard_converts_only' => 'No scores are available for the requested mode on this beatmap difficulty.',
     ],
     'beatmapsets' => [
-        'too-many-favourites' => 'Du har for mange favoritter! Fjern venligst en favorit for at tilføje en ny.',
+        'too-many-favourites' => 'You have to many favourited beatmaps! Please unfavourite one before continuing.',
     ],
-    'logged_out' => 'Du er blevet logget ud. Log venligst ind og prøv igen.', // Base text changed from "log" to "sign"
-    'supporter_only' => 'Du skal være supporter for at udføre denne handling.',
-    'no_restricted_access' => 'Du kan ikke udføre denne handling eftersom, at din konto er begrænset.',
-    'unknown' => 'En ukendt fejl er opstået.',
+    'logged_out' => 'You have been signed out. Please sign in and retry.',
+    'supporter_only' => 'You must be a supporter to use this feature.',
+    'no_restricted_access' => 'You are not able to perform this action while your account is in a restricted state.',
+    'unknown' => 'Unknown error occurred.',
 ];
