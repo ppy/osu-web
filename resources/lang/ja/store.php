cb8e885a7267d5cda189fed38b28de1fa5d3f44a
Dean Herbert <pe@ppy.sh>
2018-05-14 13:11:45 +0900

    New translations store.php (Japanese)

--- resources/lang/ja/store.php
+++ resources/lang/ja/store.php
@@ -28,6 +28,7 @@ return [
         'cart_problems_edit' => 'クリックで変更',
         'declined' => 'お支払いはキャンセルされました。',
         'error' => '精算中に問題が発生しました。',
+        'old_cart' => 'Your cart appears to be out of date and has been reloaded, please try again.',
         'pay' => 'Paypalで支払う',
         'pending_checkout' => [
             'line_1' => '前回の精算が完了していません。',
@@ -38,6 +39,12 @@ return [
     ],
 
     'discount' => ':percent%の割引',
+
+    'mail' => [
+        'payment_completed' => [
+            'subject' => 'We received your osu!store order!',
+        ],
+    ],
 
     'order' => [
         'item' => [
