5bbfdffa385fb3945930706bd58858c860dd27c9
Dean Herbert <pe@ppy.sh>
2018-05-14 17:25:27 +0900

    New translations multiplayer.php (Dutch)

---
+++ resources/lang/nl/multiplayer.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ *    Copyright 2015-2018 ppy Pty. Ltd.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'match' => [
+        'header' => '',
+        'team-types' => [
+            'head-to-head' => '',
+            'tag-coop' => '',
+            'team-vs' => '',
+            'tag-team-vs' => '',
+        ],
+        'events' => [
+            'player-left' => '',
+            'player-joined' => '',
+            'player-kicked' => '',
+            'match-created' => '',
+            'match-disbanded' => '',
+            'host-changed' => '',
+
+            'player-left-no-user' => '',
+            'player-joined-no-user' => '',
+            'player-kicked-no-user' => '',
+            'match-created-no-user' => '',
+            'match-disbanded-no-user' => '',
+            'host-changed-no-user' => '',
+        ],
+        'in-progress' => '',
+        'score' => [
+            'stats' => [
+                'accuracy' => '',
+                'combo' => '',
+                'score' => '',
+            ],
+        ],
+        'failed' => '',
+        'teams' => [
+            'blue' => '',
+            'red' => '',
+        ],
+        'winner' => '',
+        'difference' => '',
+        'loading-events' => '',
+        'more-events' => '',
+        'beatmap-deleted' => '',
+    ],
+    'game' => [
+        'scoring-type' => [
+            'score' => '',
+            'accuracy' => '',
+            'combo' => '',
+            'scorev2' => '',
+        ],
+    ],
+];
