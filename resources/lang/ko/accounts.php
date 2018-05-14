b35c27d1fbd5945f316b1d2bea9e7cc25b53358b
Dean Herbert <pe@ppy.sh>
2018-05-14 13:02:02 +0900

    New translations accounts.php (Korean)

--- resources/lang/ko/accounts.php
+++ resources/lang/ko/accounts.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *    Copyright 2015-2017 ppy Pty. Ltd.
  *
@@ -16,50 +17,63 @@
  *    You should have received a copy of the GNU Affero General Public License
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 return [
     'edit' => [
         'title' => '<strong>계정</strong> 설정',
         'title_compact' => '설정',
+        'username' => 'username',
+
         'avatar' => [
-            'title' => '아바타 수정', // Base text changed, please check.
+            'title' => '아바타 수정',
         ],
+
         'email' => [
             'current' => '현재 이메일 주소',
             'new' => '새 이메일 주소',
             'new_confirmation' => '이메일 주소 확인',
             'title' => '이메일',
         ],
+
         'password' => [
             'current' => '현재 비밀번호',
             'new' => '새 비밀번호',
             'new_confirmation' => '비밀번호 확인',
             'title' => '비밀번호',
         ],
+
         'profile' => [
-            'title' => '프로필 수정', // Base text changed, please check.
+            'title' => '프로필 수정',
+
             'user' => [
                 'user_from' => '지역',
+                'user_interests' => 'interests',
                 'user_msnm' => '스카이프',
                 'user_occ' => '직업',
                 'user_twitter' => '트위터',
                 'user_website' => '사이트',
+                'user_discord' => 'discord',
             ],
         ],
+
         'signature' => [
             'title' => '시그니처',
             'update' => '업데이트',
         ],
     ],
+
     'update_email' => [
         'email_subject' => 'osu! 이메일 주소 변경 확인',
         'update' => '변경',
     ],
+
     'update_password' => [
         'email_subject' => 'osu! 비밀번호 변경 확인',
         'update' => '변경',
     ],
+
     'playstyles' => [
-        'title' => '플레이 방식', // 플
+        'title' => '플레이 방식',
         'mouse' => '마우스',
         'keyboard' => '키보드',
         'tablet' => '태블릿',
