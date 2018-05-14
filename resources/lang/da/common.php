1abf6cfb1e1a946fbc3ecbd05dbdcde189e18a31
Dean Herbert <pe@ppy.sh>
2018-05-14 18:15:59 +0900

    New translations common.php (Danish)

--- resources/lang/da/common.php
+++ resources/lang/da/common.php
@@ -23,7 +23,7 @@ return [
     'saved' => 'Gemt',
 
     'array_and' => [
-        'words_connector' => '',
+        'words_connector' => ', ',
         'two_words_connector' => ' og ',
         'last_word_connector' => ', og ',
     ],
@@ -34,7 +34,7 @@ return [
         'click_to_copy' => '',
         'click_to_copy_copied' => '',
         'delete' => 'Slet',
-        'permalink' => '',
+        'permalink' => 'permalink',
         'permalink_copied' => '',
         'post' => 'Slå op',
         'reply' => 'Svar',
@@ -53,7 +53,7 @@ return [
         'days' => '',
         'hour_short_unit' => '',
         'hours' => '',
-        'item' => '',
+        'item' => ':count unit|:count units',
         'minute_short_unit' => '',
         'months' => ':count måned|:count måneder',
         'second_short_unit' => '',
@@ -62,8 +62,8 @@ return [
 
     'datetime' => [
         'year_month' => [
-            'moment' => '',
-            'php' => '',
+            'moment' => 'MMMM YYYY',
+            'php' => 'MMMM y',
         ],
         'year_month_short' => [
             'moment' => '',
@@ -73,7 +73,7 @@ return [
     'device' => [
         'keyboard' => 'Tastatur',
         'mouse' => 'Mus',
-        'tablet' => '',
+        'tablet' => 'Tablet',
         'touch' => 'Touch Skærm',
     ],
 
@@ -87,12 +87,12 @@ return [
     ],
 
     'score_count' => [
-        'count_100' => '',
-        'count_300' => '',
-        'count_50' => '',
-        'count_geki' => '',
-        'count_katu' => '',
-        'count_miss' => '',
+        'count_100' => '100',
+        'count_300' => '300',
+        'count_50' => '50',
+        'count_geki' => 'MAX',
+        'count_katu' => '200',
+        'count_miss' => 'Miss',
     ],
 
     'time' => [
