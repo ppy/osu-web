b93b7ba5ca652d941d5ceec76ebdcf702fc6ce02
Dean Herbert <pe@ppy.sh>
2018-05-14 17:22:03 +0900

    New translations news.php (German)

--- resources/lang/de/news.php
+++ resources/lang/de/news.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *    Copyright 2015-2017 ppy Pty. Ltd.
+ *    Copyright 2015-2018 ppy Pty. Ltd.
  *
  *    This file is part of osu!web. osu!web is distributed with the hope of
  *    attracting more community contributions to the core ecosystem of osu!.
@@ -21,11 +21,11 @@
 return [
     'breadcrumbs' => [
         'news-index' => 'liste',
-        'news-show' => 'post',
+        'news-show' => '',
     ],
 
     'index' => [
-        'title' => 'osu!news',
+        'title' => '',
 
         'nav' => [
             'newer' => 'Neuere Posts',
