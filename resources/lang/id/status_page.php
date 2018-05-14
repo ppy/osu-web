4f7c80ec5972b0b4173f949af8b055bc7b7b2793
Dean Herbert <pe@ppy.sh>
2018-05-14 18:16:58 +0900

    New translations status_page.php (Indonesian)

--- resources/lang/id/status_page.php
+++ resources/lang/id/status_page.php
@@ -20,7 +20,7 @@
 
 return [
     'header' => [
-        'title' => '',
+        'title' => 'status',
         'description' => 'apa yang terjadi bung?',
     ],
 
@@ -51,8 +51,8 @@ return [
         'uptime' => [
             'title' => 'Waktu Aktif',
             'graphs' => [
-                'server' => '',
-                'web' => '',
+                'server' => 'server',
+                'web' => 'web',
             ],
         ],
 
