75dd4ddf52ec2ab1ef34c7d89da4afd7bb8adde3
Dean Herbert <pe@ppy.sh>
2018-05-14 17:22:01 +0900

    New translations community.php (German)

--- resources/lang/de/community.php
+++ resources/lang/de/community.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *    Copyright 2015-2017 ppy Pty. Ltd.
+ *    Copyright 2015-2018 ppy Pty. Ltd.
  *
  *    This file is part of osu!web. osu!web is distributed with the hope of
  *    attracting more community contributions to the core ecosystem of osu!.
@@ -35,6 +35,15 @@ return [
             Ich würde gerne meinen persönlichen Dank an all die richten, die osu! bisher unterstützt haben,
             genau so auch an diejenigen, die es in Zukunft weiterführen werden :).',
 
+        'supporter_status' => [
+            'contribution' => '',
+            'gifted' => '',
+            'not_yet' => "",
+            'title' => '',
+            'valid_until' => '',
+            'was_valid_until' => '',
+        ],
+
         'why_support' => [
             'title' => 'Warum sollte ich osu! unterstützen?',
             'blocks' => [
@@ -49,7 +58,7 @@ return [
         'perks' => [
             'title' => 'Oh? Was gibt\'s denn?!',
             'osu_direct' => [
-                'title' => 'osu!direct',
+                'title' => '',
                 'description' => 'Schneller und einfacher Zugriff auf die Beatmapsuche, sogar innerhalb des Spiels.',
             ],
 
