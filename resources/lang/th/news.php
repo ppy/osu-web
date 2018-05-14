62e581c63f28fb96af41f33387aa22ec64c4d928
Dean Herbert <pe@ppy.sh>
2018-05-14 17:22:45 +0900

    New translations news.php (Thai)

--- resources/lang/th/news.php
+++ resources/lang/th/news.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *    Copyright 2015-2017 ppy Pty. Ltd.
+ *    Copyright 2015-2018 ppy Pty. Ltd.
  *
  *    This file is part of osu!web. osu!web is distributed with the hope of
  *    attracting more community contributions to the core ecosystem of osu!.
@@ -25,7 +25,7 @@ return [
     ],
 
     'index' => [
-        'title' => 'osu!news',
+        'title' => '',
 
         'nav' => [
             'newer' => 'โพสต์ใหม่กว่า',
