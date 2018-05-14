b775b537c414036c9b221aa0b05536388f86786b
Dean Herbert <pe@ppy.sh>
2018-05-14 18:16:57 +0900

    New translations rankings.php (Indonesian)

--- resources/lang/id/rankings.php
+++ resources/lang/id/rankings.php
@@ -22,7 +22,7 @@ return [
     'header' => 'Peringkat :type',
     'type' => [
         'performance' => 'Performa',
-        'charts' => '',
+        'charts' => 'Spotlights',
         'score' => 'Skor',
         'country' => 'Negara',
     ],
@@ -36,8 +36,8 @@ return [
         'ranked_score' => 'Skor Ranked',
         'average_score' => 'Skor Rata-Rata',
         'average_performance' => 'pp Rata-Rata',
-        'ss' => '',
-        's' => '',
-        'a' => '',
+        'ss' => 'SS',
+        's' => 'S',
+        'a' => 'A',
     ],
 ];
