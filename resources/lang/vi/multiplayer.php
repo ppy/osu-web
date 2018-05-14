f6e69c6a660295c2e711db1433edd76423d32f5a
Dean Herbert <pe@ppy.sh>
2018-05-14 17:24:13 +0900

    New translations multiplayer.php (Vietnamese)

--- resources/lang/vi/multiplayer.php
+++ resources/lang/vi/multiplayer.php
@@ -22,10 +22,10 @@ return [
     'match' => [
         'header' => 'Trận Đấu Multiplayer',
         'team-types' => [
-            'head-to-head' => 'Head-to-head',
-            'tag-coop' => 'Tag Co-op',
-            'team-vs' => 'Team VS',
-            'tag-team-vs' => 'Tag Team VS',
+            'head-to-head' => '',
+            'tag-coop' => '',
+            'team-vs' => '',
+            'tag-team-vs' => '',
         ],
         'events' => [
             'player-left' => ':user đã rời khỏi trận đấu',
@@ -46,7 +46,7 @@ return [
         'score' => [
             'stats' => [
                 'accuracy' => 'Độ chính xác',
-                'combo' => 'Combo',
+                'combo' => '',
                 'score' => 'Điểm',
             ],
         ],
@@ -66,7 +66,7 @@ return [
             'score' => 'Điểm Cao Nhất',
             'accuracy' => 'Độ Chính Xác Cao Nhất',
             'combo' => 'Combo Cao Nhất',
-            'scorev2' => 'Score V2',
+            'scorev2' => '',
         ],
     ],
 ];
