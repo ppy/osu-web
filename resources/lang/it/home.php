42779e173e9bdc14d5bc08a5cb7c626834dc253b
Dean Herbert <pe@ppy.sh>
2018-05-14 13:19:55 +0900

    New translations home.php (Italian)

--- resources/lang/it/home.php
+++ resources/lang/it/home.php
@@ -19,9 +19,6 @@
  */
 
 return [
-    'news' => [
-        'slogan' => 'Il ritmo è solo ad un *click* di distanza!',
-    ],
     'landing' => [
         'download' => 'Scarica ora',
         'online' => '<strong>:players</strong> giocatori online ora in <strong>:games</strong> games',
@@ -29,9 +26,127 @@ return [
         'players' => '<strong>:count</strong> utenti registrati',
 
         'slogan' => [
-            // free to win => vinci gratuitamente, but maybe there's a better way to translate that
             'main' => 'simulatore di cerchi dove vinci gratuitamente',
             'sub' => 'il ritmo è solo ad un click di distanza',
         ],
+    ],
+
+    'search' => [
+        'advanced_link' => 'Advanced search',
+        'button' => 'Search',
+        'empty_result' => 'Nothing found!',
+        'missing_query' => 'Search keyword of minimum :n characters is required',
+        'placeholder' => 'type to search',
+        'title' => 'Search',
+
+        'beatmapset' => [
+            'more' => ':count more beatmap search results',
+            'more_simple' => 'See more beatmap search results',
+            'title' => 'Beatmaps',
+        ],
+
+        'forum_post' => [
+            'all' => 'All forums',
+            'link' => 'Search the forum',
+            'more_simple' => 'See more forum search results',
+            'title' => 'Forum',
+
+            'label' => [
+                'forum' => 'search in forums',
+                'forum_children' => 'include subforums',
+                'topic_id' => 'topic #',
+                'username' => 'author',
+            ],
+        ],
+
+        'mode' => [
+            'all' => 'all',
+            'beatmapset' => 'beatmap',
+            'forum_post' => 'forum',
+            'user' => 'player',
+            'wiki_page' => 'wiki',
+        ],
+
+        'user' => [
+            'more' => ':count more player search results',
+            'more_simple' => 'See more player search results',
+            'more_hidden' => 'Player search is limited to :max players. Try refining search query.',
+            'title' => 'Players',
+        ],
+
+        'wiki_page' => [
+            'link' => 'Search the wiki',
+            'more_simple' => 'See more wiki search results',
+            'title' => 'Wiki',
+        ],
+    ],
+
+    'download' => [
+        'tagline' => "let's get<br>you started!",
+        'action' => 'Download osu!',
+        'os' => [
+            'windows' => 'for Windows',
+            'macos' => 'for macOS',
+            'linux' => 'for Linux',
+        ],
+        'mirror' => 'mirror',
+        'macos-fallback' => 'macOS users',
+        'steps' => [
+            'register' => [
+                'title' => 'get an account',
+                'description' => 'follow the prompts when starting the game to sign in or make a new account',
+            ],
+            'download' => [
+                'title' => 'download the game',
+                'description' => 'click the button above to download the installer, then run it!',
+            ],
+            'beatmaps' => [
+                'title' => 'get beatmaps',
+                'description' => [
+                    '_' => ':browse the vast library of user-created beatmaps and start playing!',
+                    'browse' => 'browse',
+                ],
+            ],
+        ],
+        'video-guide' => 'video guide',
+    ],
+
+    'user' => [
+        'title' => 'dashboard',
+        'news' => [
+            'title' => 'News',
+            'error' => 'Error loading news, try refreshing the page?...',
+        ],
+        'header' => [
+            'welcome' => 'Hello, <strong>:username</strong>!',
+            'messages' => 'You have :count new message|You have :count new messages',
+            'stats' => [
+                'friends' => 'Online Friends',
+                'games' => 'Games',
+                'online' => 'Online Users',
+            ],
+        ],
+        'beatmaps' => [
+            'new' => 'New Approved Beatmaps',
+            'popular' => 'Popular Beatmaps',
+            'by' => 'by',
+            'plays' => ':count plays',
+        ],
+        'buttons' => [
+            'download' => 'Download osu!',
+            'support' => 'Support osu!',
+            'store' => 'osu!store',
+        ],
+    ],
+
+    'support-osu' => [
+        'title' => 'Wow!',
+        'subtitle' => 'You seem to be having a good time! :D',
+        'body' => [
+            'part-1' => 'Did you know that osu! runs with no advertising, and relies on players to support its development and running costs?',
+            'part-2' => 'Did you also know that by supporting osu! you get a heap of useful features, such as <strong>in-game downloading</strong> which automatically triggers in spectator and multiplayer games?',
+        ],
+        'find-out-more' => 'Click here to find out more!',
+        'download-starting' => "Oh, and don't worry - your download has already been started for you already ;)",
     ],
 ];
