93be9c1bc2665565cba28af8fe9e712bc76810da
Dean Herbert <pe@ppy.sh>
2018-05-14 13:41:19 +0900

    New translations authorization.php (Dutch)

--- resources/lang/nl/authorization.php
+++ resources/lang/nl/authorization.php
@@ -20,8 +20,21 @@
 
 return [
     'beatmap_discussion' => [
+        'destroy' => [
+            'is_hype' => 'Can not undo hyping.',
+            'has_reply' => 'Can not delete discussion with replies',
+        ],
+        'nominate' => [
+            'exhausted' => 'You have reached your nomination limit for the day, please try again tomorrow.',
+        ],
         'resolve' => [
             'not_owner' => 'Alleen de eigenaar van de thread of de eigenaar van de beatmap kan een discussie als opgelost markeren.',
+        ],
+
+        'vote' => [
+            'limit_exceeded' => 'Please wait a while before casting more votes',
+            'owner' => "Can't vote on own discussion.",
+            'wrong_beatmapset_state' => 'Can only vote on discussions of pending beatmaps.',
         ],
     ],
 
@@ -43,11 +56,16 @@ return [
                 'channel' => [
                     'no_access' => 'Toegang tot dit kanaal is vereist.',
                     'moderated' => 'Kanaal wordt op het moment gemodereerd.',
+                    'not_lazer' => 'You can only speak in #lazer at this time.',
                 ],
 
                 'not_allowed' => 'Je kunt geen berichten sturen terwijl je bent verbannen/restricted/silenced.',
             ],
         ],
+    ],
+
+    'contest' => [
+        'voting_over' => 'You cannot change your vote after the voting period for this contest has ended.',
     ],
 
     'forum' => [
@@ -60,10 +78,16 @@ return [
             ],
 
             'edit' => [
+                'deleted' => 'Can not edit deleted post.',
                 'locked' => 'De post is afgesloten voor bewerkingen.',
                 'no_forum_access' => 'Toegang tot dit forum is nodig.',
                 'not_owner' => 'Alleen de eigenaar kan de post bewerken.',
                 'topic_locked' => 'Kan geen post in een gesloten onderwerp bewerken.',
+            ],
+
+            'store' => [
+                'play_more' => 'Try playing the game before posting on the forums, please! If you have a problem with playing, please post to the Help and Support forum.',
+                'too_many_help_posts' => "You need to play the game more before you can make additional posts. If you're still having trouble playing the game, email support@ppy.sh", // FIXME: unhardcode email address.
             ],
         ],
 
@@ -73,12 +97,34 @@ return [
                 'locked' => 'Je kunt niet antwoorden op een gesloten onderwerp.',
                 'no_forum_access' => 'Toegang tot dit forum is nodig.',
                 'no_permission' => 'Geen toestemming om te antwoorden.',
+
+                'user' => [
+                    'require_login' => 'Please sign in to reply.',
+                    'restricted' => "Can't reply while restricted.",
+                    'silenced' => "Can't reply while silenced.",
+                ],
             ],
 
             'store' => [
                 'no_forum_access' => 'Toegang tot dit forum is nodig.',
                 'no_permission' => 'Geen toestemming om een onderwerp te starten.',
                 'forum_closed' => 'Forum is gesloten en kan niet in gepost worden.',
+            ],
+
+            'vote' => [
+                'no_forum_access' => 'Access to requested forum is required.',
+                'over' => 'Polling is over and can not be voted on anymore.',
+                'voted' => 'Changing vote is not allowed.',
+
+                'user' => [
+                    'require_login' => 'Please sign in to vote.',
+                    'restricted' => "Can't vote while restricted.",
+                    'silenced' => "Can't vote while silenced.",
+                ],
+            ],
+
+            'watch' => [
+                'no_forum_access' => 'Access to requested forum is required.',
             ],
         ],
 
@@ -94,13 +140,13 @@ return [
         ],
     ],
 
-    'require_login' => 'Log in om verder te gaan.', // Base text changed from "log" to "sign"
+    'require_login' => 'Log in om verder te gaan.',
 
     'unauthorized' => 'Toegang geweigerd.',
 
-    'silenced' => 'Je kunt dit niet doen terwijl je silenced bent.',
+    'silenced' => "Je kunt dit niet doen terwijl je silenced bent.",
 
-    'restricted' => 'Je kunt dit niet doen terwijl je restricted bent.',
+    'restricted' => "Je kunt dit niet doen terwijl je restricted bent.",
 
     'user' => [
         'page' => [
