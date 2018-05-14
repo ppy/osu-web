7ff767849cc5dbf329b0dafd2c9a572570f4fc76
Dean Herbert <pe@ppy.sh>
2018-05-14 13:40:53 +0900

    New translations errors.php (French)

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -19,7 +19,25 @@
  */
 
 return [
-    'instrument_declined' => 'The selected payment method was rejected by Paypal.',
-    'invalid_resource_id' => 'No payment information was found.',
-    'unknown' => "Erreur inconnue survenue.",
+    'codes' => [
+        'http-401' => 'Please sign in to proceed.',
+        'http-403' => 'Access denied.',
+        'http-429' => 'Too many attempts. Try again later.',
+    ],
+    'account' => [
+        'profile-order' => [
+            'generic' => 'An error occured. Try refreshing the page.',
+        ],
+    ],
+    'beatmaps' => [
+        'invalid_mode' => 'Invalid mode specified.',
+        'standard_converts_only' => 'No scores are available for the requested mode on this beatmap difficulty.',
+    ],
+    'beatmapsets' => [
+        'too-many-favourites' => 'You have to many favourited beatmaps! Please unfavourite one before continuing.',
+    ],
+    'logged_out' => 'You have been signed out. Please sign in and retry.',
+    'supporter_only' => 'You must be a supporter to use this feature.',
+    'no_restricted_access' => 'You are not able to perform this action while your account is in a restricted state.',
+    'unknown' => 'Unknown error occurred.',
 ];
