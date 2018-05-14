3fa5977a5f6d52a5ae90e9d618e352428fecd8d4
Dean Herbert <pe@ppy.sh>
2018-05-14 18:14:04 +0900

    New translations livestreams.php (Dutch)

--- resources/lang/nl/livestreams.php
+++ resources/lang/nl/livestreams.php
@@ -20,7 +20,7 @@
 
 return [
     'top-headers' => [
-        'headline' => '',
+        'headline' => 'Live Streams',
         'description' => 'Op basis van de index wordt de data elke vijf minuten van twitch.tv afgehaald. Start gerust zelf een stream en kom in de lijst te staan! Check voor meer informatie de wiki pagina over livestreamen.',
     ],
     'headers' => [
