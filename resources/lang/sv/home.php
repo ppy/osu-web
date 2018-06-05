9a077c070a3440b1dfec489db43388d4c1e339d0
Dean Herbert <pe@ppy.sh>
2018-06-05 17:07:20 +0900

    Update translations

--- resources/lang/sv/home.php
+++ resources/lang/sv/home.php
@@ -103,7 +103,7 @@ return [
             'beatmaps' => [
                 'title' => 'skaffa beatmaps',
                 'description' => [
-                    '_' => ':browse det stora bibloteket av beatmaps skapade av användare och börja spela!',
+                    '_' => ':browse i det stora bibloteket av beatmaps skapade av användare och börja spela!',
                     'browse' => 'bläddra',
                 ],
             ],
