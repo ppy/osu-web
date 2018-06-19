918c274a18275956460349a12a973f540f0b7222
Joehu <jmadamba07@hotmail.com>
2018-06-19 12:09:25 -0700

    Fix spacing of osu! brands

--- resources/lang/en/community.php
+++ resources/lang/en/community.php
@@ -23,13 +23,13 @@ return [
         'header' => [
             // size in font-size
             'big_description' => 'Love osu!?<br/>
-                                Support the osu! dev :D',
+                                Support the osu!dev :D',
             'small_description' => '',
             'support_button' => 'I want to support osu!',
         ],
 
-        'dev_quote' => 'osu! is a completely free-to-play game, but running it is most definitely not so free. 
-        Between the cost of commissioning servers and high quality international bandwidth, the time spent maintaining the system and community, 
+        'dev_quote' => 'osu! is a completely free-to-play game, but running it is most definitely not so free.
+        Between the cost of commissioning servers and high quality international bandwidth, the time spent maintaining the system and community,
         providing prizes for competitions, answering support questions and generally keeping people happy, osu! consumes quite a hefty amount of money!
         Oh, and don\'t forget the fact that we do it without any advertising or partnering with silly toolbars and the likes!
             <br/><br/>osu! is at the end of the day largely run by myself, to which you may know best as "peppy".
