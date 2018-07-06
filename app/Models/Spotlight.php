085c8317a1c66c3654af07b7f665053cf2a4b76e
bakaneko <notbakaneko@users.noreply.github.com>
2018-07-06 15:39:58 +0900

    helpers to create additional spotlight tables

--- app/Models/Spotlight.php
+++ app/Models/Spotlight.php
@@ -23,6 +23,8 @@ namespace App\Models;
 use App\Models\Score\Best as ScoreBest;
 use App\Models\UserStatistics;
 use DB;
+use Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 class Spotlight extends Model
 {
@@ -107,5 +109,127 @@ class Spotlight extends Model
         } else {
             return "{$this->acronym}_user_stats_{$mode}";
         }
+    }
+
+    public function createTables()
+    {
+        \Log::debug('creating tables');
+        // create tables
+        DB::connection('mysql-charts')->transaction(function () {
+            $modes = ['osu', 'fruits', 'taiko', 'mania'];
+            // beatmapsets
+            if ($this->mode_specific) {
+                foreach ($modes as $mode) {
+                    static::createBeatmapsetTable($this->beatmapsetsTableName($mode));
+                }
+            } else {
+                static::createBeatmapsetTable($this->beatmapsetsTableName('osu'));
+            }
+
+            foreach ($modes as $mode) {
+                // scores
+                static::createBestScoresTable($this->bestScoresTableName($mode));
+                // user_stats
+                static::createUserStatsTable($this->userStatsTableName($mode));
+            }
+        });
+    }
+
+    public static function newSpotlight(string $acronym, string $name, bool $modeSpecific, string $type = 'monthly')
+    {
+        /** @var self $spotlight */
+        $spotlight = new Spotlight([
+            'acronym' => $acronym,
+            'name' => $name,
+            'mode_specific' => $modeSpecific,
+            'type' => $type,
+        ]);
+
+        DB::transaction(function () use ($spotlight) {
+            $spotlight->createTables();
+            $spotlight->saveOrExplode();
+        });
+
+        $spotlight->fresh();
+
+        return $spotlight;
+    }
+
+    private static function createBeatmapsetTable($name)
+    {
+        \Log::debug("create table {$name}");
+
+        Schema::connection('mysql-charts')->create($name, function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+
+            $table->unsignedMediumInteger('beatmapset_id')->primary();
+        });
+    }
+
+    private static function createBestScoresTable($name)
+    {
+        \Log::debug("create table {$name}");
+
+        Schema::connection('mysql-charts')->create($name, function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+
+            $table->increments('score_id');
+            $table->unsignedMediumInteger('beatmap_id')->default(0);
+            $table->unsignedMediumInteger('beatmapset_id')->default(0);
+            $table->mediumInteger('user_id')->default(0);
+            $table->integer('score')->default(0);
+            $table->unsignedSmallInteger('maxcombo')->default(0);
+            $table->enum('rank', ['A', 'B', 'C', 'D', 'S', 'SH', 'X', 'XH']);
+            $table->unsignedSmallInteger('count50')->default(0);
+            $table->unsignedSmallInteger('count100')->default(0);
+            $table->unsignedSmallInteger('count300')->default(0);
+            $table->unsignedSmallInteger('countmiss')->default(0);
+            $table->unsignedSmallInteger('countgeki')->default(0);
+            $table->unsignedSmallInteger('countkatu')->default(0);
+            $table->boolean('perfect')->default(0);
+            $table->unsignedMediumInteger('enabled_mods')->default(0);
+            $table->timestamp('date')->useCurrent();
+            $table->unique(['user_id', 'beatmap_id'], 'user_beatmap');
+            $table->index(['beatmap_id', 'score'], 'beatmap_score');
+            $table->index(['user_id', 'beatmapset_id'], 'user_beatmapset');
+        });
+    }
+
+    private static function createUserStatsTable($name)
+    {
+        \Log::debug("create table {$name}");
+
+        Schema::connection('mysql-charts')->create($name, function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+
+            $table->mediumInteger('user_id')->primary();
+            $table->unsignedMediumInteger('count300')->default(0);
+            $table->unsignedMediumInteger('count100')->default(0);
+            $table->unsignedMediumInteger('count50')->default(0);
+            $table->unsignedMediumInteger('countMiss')->default(0);
+            $table->unsignedBigInteger('accuracy_total');
+            $table->unsignedBigInteger('accuracy_count');
+            $table->float('accuracy')->unsigned();
+            $table->mediumInteger('playcount');
+            $table->bigInteger('ranked_score');
+            $table->bigInteger('total_score');
+            $table->mediumInteger('x_rank_count');
+            $table->mediumInteger('s_rank_count');
+            $table->mediumInteger('a_rank_count');
+            $table->mediumInteger('rank');
+            $table->float('level')->unsigned();
+            $table->unsignedMediumInteger('replay_popularity')->default(0);
+            $table->unsignedMediumInteger('fail_count')->default(0);
+            $table->unsignedMediumInteger('exit_count')->default(0);
+            $table->unsignedSmallInteger('max_combo')->default(0);
+            $table->index('total_score', 'total_score');
+            $table->index('ranked_score', 'ranked_score');
+            $table->index('playcount', 'playcount');
+            $table->index('accuracy', 'accuracy');
+            $table->index('rank', 'rank');
+        });
     }
 }
