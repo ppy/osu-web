d71f6c8827c5e77dd6a3572a744336537eac092d
bakaneko <notbakaneko@users.noreply.github.com>
2018-07-13 18:49:24 +0900

    add seeder for best of year spotlights

--- database/factories/SpotlightFactory.php
+++ database/factories/SpotlightFactory.php
@@ -38,3 +38,27 @@ $factory->defineAs(App\Models\Spotlight::class, 'monthly', function (Faker\Gener
         'active' => true,
     ];
 });
+
+$factory->defineAs(App\Models\Spotlight::class, 'yearly', function (Faker\Generator $faker) {
+    $startDate = Carbon\Carbon::instance($faker->dateTimeBetween('-6 years', 'now'))->endOfYear()->addMonths(1);
+    $endDate = $startDate->copy()->addMonths(1);
+
+    return  [
+        // Monthly Spotlight is generated for the previous month.
+        'acronym' => function (array $self) {
+            $date = $self['start_date']->copy()->subYears(1);
+
+            return "BEST{$date->format('Y')}";
+        },
+        'name' => function (array $self) {
+            $date = $self['start_date']->copy()->subYears(1);
+
+            return "Best of {$date->format('Y')}";
+        },
+        'start_date' => $startDate,
+        'end_date' => $endDate,
+        'mode_specific' => true,
+        'type' => 'yearly',
+        'active' => true,
+    ];
+});
--- database/seeds/ModelSeeders/SpotlightSeeder.php
+++ database/seeds/ModelSeeders/SpotlightSeeder.php
@@ -21,6 +21,10 @@ class SpotlightSeeder extends Seeder
         $now = Carbon::now()->startOfMonth();
         while ($date <= $now) {
             $this->seedMonthly($date);
+            if ($date->month === 1) {
+                $this->seedYearly($date);
+            }
+
             $date->addMonth(1);
         }
     }
@@ -36,59 +40,79 @@ class SpotlightSeeder extends Seeder
 
             $spotlight->saveOrExplode();
 
-            DB::connection('mysql-charts')->transaction(function () use ($spotlight) {
-                $spotlight->createTables();
+            $this->seedData($spotlight);
+        });
+    }
 
-                // users
-                $users = User::orderByRaw('RAND()')->limit(1000)->get();
+    public function seedYearly($date)
+    {
+        DB::transaction(function () use ($date) {
+            $spotlight = factory(Spotlight::class, 'yearly')->make([
+                'start_date' => $date->copy()->addDays(rand(0, 27)),
+                'end_date' => $date->copy()->addMonth(1)->addDays(rand(0, 27)),
+            ]);
 
-                foreach (Beatmap::MODES as $mode => $v) {
-                    // beatmaps
-                    $beatmaps = Beatmap::where('playmode', $v)->orderByRaw('RAND()')->limit(rand(4, 10))->get();
-                    $beatmapsetIds = array_unique($beatmaps->pluck('beatmapset_id')->toArray());
+            $spotlight->saveOrExplode();
 
-                    DB::connection('mysql-charts')->table($spotlight->beatmapsetsTableName($mode))->insert(
-                        array_map(function ($id) {
-                            return ['beatmapset_id' => $id];
-                        }, $beatmapsetIds)
-                    );
+            $this->seedData($spotlight);
+        });
+    }
 
-                    foreach ($users as $user) {
-                        // user_stats
-                        $stats = factory(UserStatisticsModel::getClass($mode))->make(['user_id' => $user->user_id]);
-                        $stats->setTable($spotlight->userStatsTableName($mode));
+    private function seedData($spotlight)
+    {
 
-                        $stats->save();
+        DB::connection('mysql-charts')->transaction(function () use ($spotlight) {
+            $spotlight->createTables();
 
-                        // scores
-                        $scoresClass = ScoresBestModel::getClass($v);
-                        $possible_ranks = ['A', 'S', 'B', 'SH', 'XH', 'X'];
+            // users
+            $users = User::orderByRaw('RAND()')->limit(1000)->get();
 
-                        foreach ($beatmaps as $beatmap) {
-                            $maxcombo = rand(1, $beatmap->countTotal);
-                            $score = new $scoresClass([
-                                'user_id' => $user->user_id,
-                                'beatmap_id' => $beatmap->beatmap_id,
-                                'beatmapset_id' => $beatmap->beatmapset_id,
-                                'score' => rand(50000, 100000000),
-                                'maxcombo' => $maxcombo,
-                                'rank' => array_rand_val($possible_ranks),
-                                'count300' => round($maxcombo * 0.8),
-                                'count100' => rand(0, round($maxcombo * 0.15)),
-                                'count50' => rand(0, round($maxcombo * 0.05)),
-                                'countgeki' => round($maxcombo * 0.3),
-                                'countmiss' => round($maxcombo * 0.05),
-                                'countkatu' => round($maxcombo * 0.05),
-                                'date' => rand($spotlight->start_date->timestamp, $spotlight->end_date->timestamp),
-                            ]);
+            foreach (Beatmap::MODES as $mode => $v) {
+                // beatmaps
+                $beatmaps = Beatmap::where('playmode', $v)->orderByRaw('RAND()')->limit(rand(4, 10))->get();
+                $beatmapsetIds = array_unique($beatmaps->pluck('beatmapset_id')->toArray());
 
-                            $score->setConnection('mysql-charts');
-                            $score->setTable($spotlight->bestScoresTableName($mode));
-                            $score->save();
-                        }
+                DB::connection('mysql-charts')->table($spotlight->beatmapsetsTableName($mode))->insert(
+                    array_map(function ($id) {
+                        return ['beatmapset_id' => $id];
+                    }, $beatmapsetIds)
+                );
+
+                foreach ($users as $user) {
+                    // user_stats
+                    $stats = factory(UserStatisticsModel::getClass($mode))->make(['user_id' => $user->user_id]);
+                    $stats->setTable($spotlight->userStatsTableName($mode));
+
+                    $stats->save();
+
+                    // scores
+                    $scoresClass = ScoresBestModel::getClass($v);
+                    $possible_ranks = ['A', 'S', 'B', 'SH', 'XH', 'X'];
+
+                    foreach ($beatmaps as $beatmap) {
+                        $maxcombo = rand(1, $beatmap->countTotal);
+                        $score = new $scoresClass([
+                            'user_id' => $user->user_id,
+                            'beatmap_id' => $beatmap->beatmap_id,
+                            'beatmapset_id' => $beatmap->beatmapset_id,
+                            'score' => rand(50000, 100000000),
+                            'maxcombo' => $maxcombo,
+                            'rank' => array_rand_val($possible_ranks),
+                            'count300' => round($maxcombo * 0.8),
+                            'count100' => rand(0, round($maxcombo * 0.15)),
+                            'count50' => rand(0, round($maxcombo * 0.05)),
+                            'countgeki' => round($maxcombo * 0.3),
+                            'countmiss' => round($maxcombo * 0.05),
+                            'countkatu' => round($maxcombo * 0.05),
+                            'date' => rand($spotlight->start_date->timestamp, $spotlight->end_date->timestamp),
+                        ]);
+
+                        $score->setConnection('mysql-charts');
+                        $score->setTable($spotlight->bestScoresTableName($mode));
+                        $score->save();
                     }
                 }
-            });
+            }
         });
     }
 }
