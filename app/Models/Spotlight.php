87280996ccfe3a1dc3b5f60b19d4635098b8b115
bakaneko <notbakaneko@users.noreply.github.com>
2018-07-24 16:22:05 +0900

    missing \Model

--- app/Models/Spotlight.php
+++ app/Models/Spotlight.php
@@ -73,7 +73,7 @@ class Spotlight extends Model
      */
     public function scores(string $mode)
     {
-        $clazz = ScoreBest::getClass(Beatmap::MODES[$mode]);
+        $clazz = ScoreBest\Model::getClass(Beatmap::MODES[$mode]);
         $model = new $clazz;
         $model->setTable($this->bestScoresTableName($mode));
         $model->setConnection('mysql-charts');
