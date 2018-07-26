d27ef18699a8aafb163f061fff4a560fee066022
bakaneko <notbakaneko@users.noreply.github.com>
2018-07-26 21:46:53 +0900

    add message

--- app/Models/BeatmapsetEvent.php
+++ app/Models/BeatmapsetEvent.php
@@ -72,6 +72,7 @@ class BeatmapsetEvent extends Model
         'discussion_post_restore',
 
         'nomination_reset',
+        'love',
     ];
 
     public static function log($type, $user, $object, $extraData = [])
