622741aaebd2a46eabebb21f940e858ff4ab48d9
bakaneko <notbakaneko@users.noreply.github.com>
2018-08-05 00:46:44 +0900

    add reserved to orderitems

--- app/Models/Store/OrderItem.php
+++ app/Models/Store/OrderItem.php
@@ -36,6 +36,7 @@ class OrderItem extends Model
     protected $casts = [
         'cost' => 'float',
         'extra_data' => 'array',
+        'reserved' => 'boolean',
     ];
 
     protected $guarded = [];
