d59cef46cf58f3f6dcc30aca6bb3e247db5d9752
bakaneko <notbakaneko@users.noreply.github.com>
2018-08-20 15:39:25 +0900

    phpdocs

--- app/Libraries/UserBestScoresCheck.php
+++ app/Libraries/UserBestScoresCheck.php
@@ -24,16 +24,33 @@ use App\Libraries\Elasticsearch\Es;
 use App\Models\Score\Best;
 use App\Models\User;
 
+/**
+ * Used to check the best scores for a user if it exists in elasticsearch but not longer
+ * exists in the database. A mistach tends to break the current pager, so this can be run to
+ * fix it by deleting the invalid score from elasticsearch.
+ *
+ * e.g.
+ * (new UserBestScoresCheck(User::find($user_id)))->run($mode).
+ */
 class UserBestScoresCheck
 {
     /** @var User */
     private $user;
 
+    /**
+     * @param User $user The User to check the scores for.
+     */
     public function __construct(User $user)
     {
         $this->user = $user;
     }
 
+    /**
+     * Checks for extraneous scores in Elasticsearch.
+     *
+     * @param string $mode The game mode.
+     * @return array List of score_ids that exist in Elasticsearch but not the database.
+     */
     public function check(string $mode)
     {
         $clazz = Best\Model::getClassByString($mode);
@@ -44,6 +61,13 @@ class UserBestScoresCheck
         return array_values(array_diff($esIds, $dbIds));
     }
 
+    /**
+     * Removes extraneous scores from Elasticsearch.
+     *
+     * @param string $mode The game mode.
+     * @param array $ids List of score_ids to remove.
+     * @return array The raw response from Elasticsearch.
+     */
     public function removeFromEs(string $mode, array $ids)
     {
         return Es::getClient('scores')->deleteByQuery([
@@ -52,6 +76,12 @@ class UserBestScoresCheck
         ]);
     }
 
+    /**
+     * Checks for extraneous scores in Elasticsearch and removes them.
+     *
+     * @param string $mode The game mode.
+     * @return array The raw response from Elasticsearch.
+     */
     public function run(string $mode)
     {
         return $this->removeFromEs($mode, $this->check($mode));
