4c94c130f085da14cdcd9c461a44d04f22869514
clayton <cl8n@users.noreply.github.com>
2018-08-23 06:58:29 -0700

    Write disclaimer

--- resources/lang/en/wiki.php
+++ resources/lang/en/wiki.php
@@ -34,7 +34,7 @@ return [
         ],
 
         'translation' => [
-            'legal' => 'This is not legally binding bla bla please read the :default.',
+            'legal' => 'This translation is provided for convenience only. The original :default shall be the sole legally binding version of this text.',
             'outdated' => 'This page contains an outdated translation of the original content. Please check the :default for the most accurate information (and consider updating the translation if you are able to help out)!',
 
             'default' => 'English version',
