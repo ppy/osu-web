41818a91c59b373ab581a1a420244eebeff8a2fb
lassem22 <20264763+lassem22@users.noreply.github.com>
2018-10-13 15:31:43 +0200

    don't allow mappers to resolve discussions when their map is qualified
    resolves https://github.com/ppy/osu-web/issues/2128
    no idea if it works like that, but worth a try since we came to the conclusion that having this would be great after discussing with other qat

--- app/Libraries/OsuAuthorize.php
+++ app/Libraries/OsuAuthorize.php
@@ -151,7 +151,7 @@ class OsuAuthorize
             return 'ok';
         }
 
-        if ($user->user_id === $discussion->beatmapset->user_id) {
+        if ($user->user_id === $discussion->beatmapset->user_id && $beatmapset->approved !== Beatmapset::STATES['qualified']) {
             return 'ok';
         }
 
