aa274ec1687e326f692cd56c871d99f447928fe8
bakaneko <notbakaneko@users.noreply.github.com>
2018-10-16 14:47:37 +0900

    separate file for block

--- resources/views/layout/_header_mobile.blade.php
+++ resources/views/layout/_header_mobile.blade.php
@@ -16,7 +16,7 @@
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
 <div class="visible-xs no-print js-header--main">
-    <div class="navbar-mobile-padding-before"></div>
+    <div class="navbar-mobile-before"></div>
 
     <div
         class="
