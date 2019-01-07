e7aa988a03c1ddf77a4a928ecc235ad5839e914f
bakaneko <notbakaneko@users.noreply.github.com>
2019-01-07 13:16:06 +0900

    move more files

--- resources/assets/lib/gallery-contest.ts
+++ resources/assets/lib/gallery-contest.ts
@@ -16,9 +16,9 @@
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import GalleryContestVoteButton from './gallery-contest-vote-button';
 import { createElement } from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import GalleryContestVoteButton from './gallery-contest-vote-button';
 
 export default class GalleryContest {
   private eventId: string;
