13853cfe0c9aecf58a72d4a8b82da3c969d597e5
bakaneko <notbakaneko@users.noreply.github.com>
2019-02-08 20:35:34 +0900

    use all the things

--- app/Libraries/Markdown/Indexing/RendererExtension.php
+++ app/Libraries/Markdown/Indexing/RendererExtension.php
@@ -2,42 +2,45 @@
 
 namespace App\Libraries\Markdown\Indexing;
 
+use League\CommonMark\Block\Element as Block;
 use League\CommonMark\Extension\CommonMarkCoreExtension;
+use League\CommonMark\Inline\Element as Inline;
+use Webuni\CommonMark\TableExtension;
 
 class RendererExtension extends CommonMarkCoreExtension
 {
     public function getBlockRenderers()
     {
         return [
-            'League\CommonMark\Block\Element\BlockQuote' => new BlockRenderer,
-            'League\CommonMark\Block\Element\Document' => new BlockRenderer,
-            'League\CommonMark\Block\Element\FencedCode' => new NoopRenderer,
-            'League\CommonMark\Block\Element\Heading' => new NoopRenderer,
-            'League\CommonMark\Block\Element\HtmlBlock' => new BlockRenderer,
-            'League\CommonMark\Block\Element\IndentedCode' => new BlockRenderer,
-            'League\CommonMark\Block\Element\ListBlock' => new ListBlockRenderer,
-            'League\CommonMark\Block\Element\ListItem' => new ListItemRenderer,
-            'League\CommonMark\Block\Element\Paragraph' => new BlockRenderer,
-            'League\CommonMark\Block\Element\ThematicBreak' => new BlockRenderer,
-            'Webuni\CommonMark\TableExtension\Table' => new TableRenderer,
-            'Webuni\CommonMark\TableExtension\TableCaption' => new NoopRenderer,
-            'Webuni\CommonMark\TableExtension\TableRows' => new TableRenderer,
-            'Webuni\CommonMark\TableExtension\TableRow' => new TableRenderer,
-            'Webuni\CommonMark\TableExtension\TableCell' => new TableRenderer,
+            Block\BlockQuote::class => new BlockRenderer,
+            Block\Document::class => new BlockRenderer,
+            Block\FencedCode::class => new NoopRenderer,
+            Block\Heading::class => new NoopRenderer,
+            Block\HtmlBlock::class => new BlockRenderer,
+            Block\IndentedCode::class => new BlockRenderer,
+            Block\ListBlock::class => new ListBlockRenderer,
+            Block\ListItem::class => new ListItemRenderer,
+            Block\Paragraph::class => new BlockRenderer,
+            Block\ThematicBreak::class => new BlockRenderer,
+            TableExtension\Table::class => new TableRenderer,
+            TableExtension\TableCaption::class => new NoopRenderer,
+            TableExtension\TableRows::class => new TableRenderer,
+            TableExtension\TableRow::class => new TableRenderer,
+            TableExtension\TableCell::class => new TableRenderer,
         ];
     }
 
     public function getInlineRenderers()
     {
         return [
-            'League\CommonMark\Inline\Element\Code' => new InlineRenderer,
-            'League\CommonMark\Inline\Element\Emphasis' => new InlineRenderer,
-            'League\CommonMark\Inline\Element\HtmlInline' => new InlineRenderer,
-            'League\CommonMark\Inline\Element\Image' => new NoopRenderer,
-            'League\CommonMark\Inline\Element\Link' => new InlineRenderer,
-            'League\CommonMark\Inline\Element\Newline' => new NoopRenderer,
-            'League\CommonMark\Inline\Element\Strong' => new InlineRenderer,
-            'League\CommonMark\Inline\Element\Text' => new InlineRenderer,
+            Inline\Code::class => new InlineRenderer,
+            Inline\Emphasis::class => new InlineRenderer,
+            Inline\HtmlInline::class => new InlineRenderer,
+            Inline\Image::class => new NoopRenderer,
+            Inline\Link::class => new InlineRenderer,
+            Inline\Newline::class => new NoopRenderer,
+            Inline\Strong::class => new InlineRenderer,
+            Inline\Text::class => new InlineRenderer,
         ];
     }
 }
