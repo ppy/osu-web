90c0b3c736eba69ab224362dd9c76cdf1f1d3fb1
nanaya <me@nanaya.pro>
2019-02-13 15:01:56 +0900

    Also add name to broadcast data
    Will be moved to transformer

--- app/Events/All.php
+++ app/Events/All.php
@@ -53,6 +53,7 @@ class All implements ShouldBroadcast
     {
         return [
             'id' => $this->notification->getKey(),
+            'name' => $this->notification->name,
             'object_type' => $this->notification->notifiable_type,
             'object_id' => $this->notification->notifiable_id,
             'source_user_id' => $this->notification->source_user_id,
