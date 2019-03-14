c6a2458732d08159587e05b68a52518c53cb2dc2
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-03-14 20:10:27 +0100

    Own Calculations for restricted users
    splits reserved usernames and restricted users. Restricted users under 1k playcount are only given the minimum timeout, while other restricted users are given 3 years.

--- app/Models/User.php
+++ app/Models/User.php
@@ -384,17 +384,30 @@ class User extends Model implements AuthenticatableContract
 
     public function getUsernameAvailableAt() : Carbon
     {
-        if ($this->group_id !== 2 || $this->user_type === 1) {
-            //reserved usernames
-            return Carbon::now()->addYears(3);
-        }
-
         $playCount = array_reduce(array_keys(Beatmap::MODES), function ($result, $mode) {
             return $result + $this->statistics($mode, true)->value('playcount');
         }, 0);
+        
+        if ($this->group_id !== 2){
+            //reserved usernames
+            return Carbon::now()->addYears(10);  //This will always be in the future, which is wanted
+        }
 
+        if ( $this->user_type === 1){
+            //restricted user
+            if ($playCount>1000){
+                return $this->user_lastvisit
+                ->addDays(static::INACTIVE_DAYS); //base inactivity period for all accounts
+            }
+
+            else {
+                return $this->user_lastvisit
+                ->addYears(3);
+            }
+        }
+        
         return $this->user_lastvisit
-            ->addDays(static::INACTIVE_DAYS) //base inactivity period for all accounts
+            ->addDays(static::INACTIVE_DAYS)
             ->addDays(intval(1095 * (1 - pow(M_E, -$playCount / 11000))));    //bonus based on playcount
     }
 
