4b82a1ba9e7926a7eaa51598fa57e8481924e7a1
nanaya <me@nanaya.pro>
2019-05-30 15:56:03 +0900

    Show query string for bash

--- resources/views/vendor/apidoc/partials/example-requests/bash.blade.php
+++ resources/views/vendor/apidoc/partials/example-requests/bash.blade.php
@@ -8,9 +8,15 @@
             $authorization .= ' \\';
         }
     }
+
+    $queryString = http_build_query($route['cleanQueryParameters']);
+
+    if ($queryString !== '') {
+        $queryString = "?{$queryString}";
+    }
 ?>
 ```bash
-curl -X {{$route['methods'][0]}} {{$route['methods'][0] == 'GET' ? '-G ' : ''}}"{{ trim(config('app.docs_url') ?: config('app.url'), '/')}}/{{ ltrim($route['boundUri'], '/') }}" @if(count($route['headers']) || $authorization !== '')\
+curl -X {{$route['methods'][0]}} {{$route['methods'][0] == 'GET' ? '-G ' : ''}}"{{ trim(config('app.docs_url') ?: config('app.url'), '/')}}/{{ ltrim($route['boundUri'], '/') }}{!! $queryString !!}" @if(count($route['headers']) || $authorization !== '')\
 {!! $authorization !!}
 @foreach($route['headers'] as $header => $value)
     -H "{{$header}}: {{$value}}"@if(! ($loop->last) || ($loop->last && count($route['bodyParameters']))) \
