2db33a817585fc065aa9d74d6c16c28b0c82bbc1
bakaneko <notbakaneko@users.noreply.github.com>
2019-06-14 14:51:04 +0900

    define a component that can handle the inject props typings

---
+++ resources/assets/lib/core-component.ts
@@ -0,0 +1,18 @@
+import Dispatcher from 'dispatcher';
+import core from 'osu-core-singleton';
+import { Component } from 'react';
+import RootDataStore from 'stores/root-data-store';
+
+interface CoreProps {
+  dataStore: RootDataStore;
+  dispatcher: Dispatcher;
+}
+
+// defaults for the injected props.
+// but then do we need to even inject them?
+const dispatcher = core.dispatcher;
+const dataStore = core.dataStore;
+
+export class CoreComponent<T> extends Component<JSX.LibraryManagedAttributes<T, T & CoreProps>> {
+  static defaultProps: CoreProps = { dataStore, dispatcher };
+}
