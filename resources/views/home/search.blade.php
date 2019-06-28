75d623f8d6ac7ca20b0503f3bb54ce7eab4f71ea
Jamie Taylor <me@nekodex.net>
2019-06-28 12:58:05 +0900

    Colour update for Search

--- resources/views/home/search.blade.php
+++ resources/views/home/search.blade.php
@@ -15,7 +15,9 @@
     You should have received a copy of the GNU Affero General Public License
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
-@extends('master')
+@extends('master', [
+    'baseHue' => 255,
+])
 
 @section('content')
     <form action="{{ route('search') }}" data-loading-overlay="0" class="js-search" autocomplete="off">
