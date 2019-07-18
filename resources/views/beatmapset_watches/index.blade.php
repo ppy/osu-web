94d8ae02e1b8044fbe158130f61e094e6028f544
Jamie Taylor <me@nekodex.net>
2019-07-18 16:39:38 +0900

    Fix incorrect mage modifier

--- resources/views/beatmapset_watches/index.blade.php
+++ resources/views/beatmapset_watches/index.blade.php
@@ -109,7 +109,7 @@
                 @endif
             </table>
 
-            @include('objects._pagination_v0', ['object' => $watches, 'modifiers' => ['light-bg']])
+            @include('objects._pagination_v0', ['object' => $watches])
         </div>
     </div>
 @endsection
