9f2a4c49d65471d6e6b552e02298830725aabfe9
nanaya <me@nanaya.pro>
2019-09-06 17:59:53 +0900

    Remove unused wrapper div

--- resources/views/forum/topics/_cover_editor.blade.php
+++ resources/views/forum/topics/_cover_editor.blade.php
@@ -15,66 +15,64 @@
     You should have received a copy of the GNU Affero General Public License
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
-<div>
-    <button
-        type="button"
-        class="js-click-menu btn-osu-big btn-osu-big--forum-secondary"
-        data-click-menu-target="forum-cover-edit"
-    >
-        {{ trans('forum.covers.edit') }}
-    </button>
+<button
+    type="button"
+    class="js-click-menu btn-osu-big btn-osu-big--forum-secondary"
+    data-click-menu-target="forum-cover-edit"
+>
+    {{ trans('forum.covers.edit') }}
+</button>
+
+<div
+    class="js-click-menu js-forum-cover--modal forum-cover-edit"
+    data-click-menu-id="forum-cover-edit"
+    data-visibility="hidden"
+>
+    <div class="forum-cover-edit__cover js-forum-cover--header"></div>
+    <div class="forum-cover-edit__buttons">
+        <label
+            class="btn-osu-big
+                btn-osu-big--forum-cover
+                btn-osu-big--forum-cover-edit
+                js-forum-cover--upload-button
+                fileupload
+                forum-category-header__cover-uploader-label"
+            data-default-file-url="{{ $cover['defaultFileUrl'] or '' }}"
+            data-file-url="{{ $cover['fileUrl'] or '' }}"
+            data-url="{{ $cover['url'] }}"
+            data-custom-method="{{ $cover['method'] }}"
+        >
+            <span class="btn-osu-big__loading-spinner">
+                {!! spinner(['center-inline']) !!}
+            </span>
+            {{ trans('forum.covers.create.button') }}
+            <input class="fileupload__input" type="file" />
+        </label>
+
+        <button
+            type="button"
+            class="
+                js-forum-cover--remove-button
+                btn-osu-big btn-osu-big--forum-cover
+                btn-osu-big--forum-cover-edit
+            "
+            data-destroy-confirm="{{ trans('forum.covers.destroy.confirm') }}"
+        >
+            <span class="btn-osu-big__loading-spinner">
+                {!! spinner(['center-inline']) !!}
+            </span>
+
+            {{ trans('forum.covers.destroy._') }}
+        </button>
+    </div>
+
+    <p class="forum-cover-edit__info">
+        {{ trans('forum.covers.create.info', ['dimensions' => implode('x', $cover['dimensions'])]) }}
+    </p>
 
     <div
-        class="js-click-menu js-forum-cover--modal forum-cover-edit"
-        data-click-menu-id="forum-cover-edit"
-        data-visibility="hidden"
-    >
-        <div class="forum-cover-edit__cover js-forum-cover--header"></div>
-        <div class="forum-cover-edit__buttons">
-            <label
-                class="btn-osu-big
-                    btn-osu-big--forum-cover
-                    btn-osu-big--forum-cover-edit
-                    js-forum-cover--upload-button
-                    fileupload
-                    forum-category-header__cover-uploader-label"
-                data-default-file-url="{{ $cover['defaultFileUrl'] or '' }}"
-                data-file-url="{{ $cover['fileUrl'] or '' }}"
-                data-url="{{ $cover['url'] }}"
-                data-custom-method="{{ $cover['method'] }}"
-            >
-                <span class="btn-osu-big__loading-spinner">
-                    {!! spinner(['center-inline']) !!}
-                </span>
-                {{ trans('forum.covers.create.button') }}
-                <input class="fileupload__input" type="file" />
-            </label>
-
-            <button
-                type="button"
-                class="
-                    js-forum-cover--remove-button
-                    btn-osu-big btn-osu-big--forum-cover
-                    btn-osu-big--forum-cover-edit
-                "
-                data-destroy-confirm="{{ trans('forum.covers.destroy.confirm') }}"
-            >
-                <span class="btn-osu-big__loading-spinner">
-                    {!! spinner(['center-inline']) !!}
-                </span>
-
-                {{ trans('forum.covers.destroy._') }}
-            </button>
-        </div>
-
-        <p class="forum-cover-edit__info">
-            {{ trans('forum.covers.create.info', ['dimensions' => implode('x', $cover['dimensions'])]) }}
-        </p>
-
-        <div
-            class="forum-cover-edit__overlay js-forum-cover--overlay"
-            data-state="hidden">
-                {{ trans('common.dropzone.target') }}
-        </div>
+        class="forum-cover-edit__overlay js-forum-cover--overlay"
+        data-state="hidden">
+            {{ trans('common.dropzone.target') }}
     </div>
 </div>
