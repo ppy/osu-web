f5a761779dcbd158208de1b4ff5a9b4cf98ed29d
nanaya <me@nanaya.pro>
2019-09-20 11:45:49 +0900

    Convert to ts

--- resources/assets/lib/turbolinks-reload.ts
+++ resources/assets/lib/turbolinks-reload.ts
@@ -1,47 +1,60 @@
-###
-#    Copyright (c) ppy Pty Ltd <contact@ppy.sh>.
-#
-#    This file is part of osu!web. osu!web is distributed with the hope of
-#    attracting more community contributions to the core ecosystem of osu!.
-#
-#    osu!web is free software: you can redistribute it and/or modify
-#    it under the terms of the Affero GNU General Public License version 3
-#    as published by the Free Software Foundation.
-#
-#    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
-#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#    See the GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
-###
+/**
+ *    Copyright (c) ppy Pty Ltd <contact@ppy.sh>.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-class @TurbolinksReload
-  className = 'js-extra-script'
+const className = 'js-extra-script';
 
-  constructor: ->
-    @loaded = {}
+interface ScriptList {
+  [key: string]: boolean;
+}
 
-    addEventListener 'turbolinks:before-cache', @unload
+export default class TurbolinksReload {
+  private loaded: ScriptList = {};
 
+  constructor() {
+    document.addEventListener('turbolinks:before-cache', this.unload);
+  }
 
-  load: (src, onload) =>
-    return if @loaded[src]?
+  load = (src: string, onload: () => void | undefined | null) => {
+    if (this.loaded[src]) {
+      return;
+    }
 
-    el = document.createElement('script')
-    el.classList.add className
-    el.onload = ->
-      # abort if the element has been removed (on navigation etc)
-      return if !el.parentElement?
+    const el = document.createElement('script');
+    el.classList.add(className);
+    el.onload = () => {
+      // abort if the element has been removed (on navigation etc)
+      if (el.parentElement == null) {
+        return;
+      }
 
-      el.parentElement.removeChild el
-      onload?()
-    el.src = src
+      el.parentElement.removeChild(el);
 
-    document.body.appendChild el
+      if (onload != null) {
+        onload();
+      }
+    };
 
-    @loaded[src] = true
+    el.src = src;
+    document.body.appendChild(el);
+    this.loaded[src] = true;
+  }
 
-
-  unload: =>
-     document.body.removeChild(el) for el in document.querySelectorAll(".#{className}")
+  unload = () => {
+    $(`.${className}`).remove();
+  }
+}
