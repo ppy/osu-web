86d299a1ad0cb0fdc4712e477b1d52c4d43dcb89
Jamie Taylor <me@nekodex.net>
2019-09-26 19:24:48 +0900

    difficulty/beatmap selector working

--- resources/assets/coffee/react/beatmap-discussions/newer-discussion.tsx
+++ resources/assets/coffee/react/beatmap-discussions/newer-discussion.tsx
@@ -27,21 +27,103 @@ import SoftBreak from 'slate-soft-break';
 let initialValue: string = '{"document":{"nodes":[{"object":"block","type":"paragraph","nodes":[{"object":"text","text":"A line of text in a paragraph."}]}]}}';
 
 class TestDropdown extends React.Component<any, any> {
-  onClick = (event: Event) => {
-    event.preventDefault();
-    const { editor, node } = this.props;
-    const data = node.data.merge({beatmapId: this.props.beatmaps[0].id});
-    editor.setNodeByKey(node.key, { data });
+  portal: HTMLDivElement;
+  private topRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props: {}) {
+    super(props);
+    this.portal = window.wang = document.createElement('div');
+    document.body.appendChild(this.portal);
+    this.topRef = React.createRef<HTMLDivElement>();
+
+    this.state = {
+      visible: false,
+    };
   }
 
-  render(): React.ReactNode {
-    const beatmap = this.props.node.data.get('beatmapId') ? _.find(this.props.beatmaps, (b) => b.id === this.props.node.data.get('beatmapId')) : this.props.beatmaps[0];
+  select = (event: Event) => {
+    event.preventDefault();
+
+    const target = event.currentTarget;
+    let id = parseInt(target.dataset['id']);
+
+    this.setState({visible: false}, () => {
+      const {editor, node} = this.props;
+      const data = node.data.merge({beatmapId: id});
+      editor.setNodeByKey(node.key, {data});
+    });
+  }
+
+  toggleMenu = (event: Event) => {
+    event.preventDefault();
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
+
+  beatmapThing = (beatmap) => {
+    if (beatmap.deleted_at) {
+      return null;
+    }
+    const menuItemClasses = ""
+
     return (
-      <a href='#' className='beatmap-discussion-newer__dropdown' contentEditable={false} onClick={this.onClick} {...this.props.attributes}>
+      <a
+        href='#'
+        className={menuItemClasses}
+        key={beatmap.id}
+        data-id={beatmap.id}
+        style={{
+          display: 'flex',
+        }}
+        onClick={this.select}
+      >
         <BeatmapIcon
           beatmap={beatmap}
         />
+        <div
+          style={{
+            paddingLeft: '5px',
+          }}
+        >
+          {beatmap.version}
+        </div>
       </a>
+    );
+  }
+
+  componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any): void {
+    if (!this.topRef) {
+      return;
+    }
+
+    const { top, left } = $(this.topRef.current).offset();
+
+    this.portal.style.position = 'absolute';
+    this.portal.style.top = `${Math.floor(top + this.topRef.current.offsetHeight)}px`;
+    this.portal.style.left = `${Math.floor(left)}px`;
+
+  }
+
+  renderList = () => {
+    return ReactDOM.createPortal(
+        <div href='#' className='beatmap-discussion-newer__dropdown-menu' contentEditable={false}>
+          {this.props.beatmaps.map((bm) => this.beatmapThing(bm))}
+        </div>
+      , this.portal);
+  }
+
+  render(): React.ReactNode {
+    const beatmap = this.props.node.data.get('beatmapId') ? _.find(this.props.beatmaps, (b) => b.id === this.props.node.data.get('beatmapId')) : this.props.currentBeatmap];
+    return (
+      <React.Fragment>
+        <a href='#' className='beatmap-discussion-newer__dropdown' onClick={this.toggleMenu} contentEditable={false} {...this.props.attributes} ref={this.topRef}>
+          <BeatmapIcon
+            beatmap={beatmap}
+          />
+        </a>
+        {this.state.visible && this.renderList()}
+      </React.Fragment>
     );
   }
 }
