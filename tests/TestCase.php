ec38fce16cedd476bb483cddce582100f8524515
nanaya <me@nanaya.pro>
2019-10-02 15:47:51 +0900

    Use existing trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,7 +28,7 @@ use ReflectionProperty;
 
 class TestCase extends BaseTestCase
 {
-    use DatabaseTransactions;
+    use CreatesApplication, DatabaseTransactions;
 
     protected $connectionsToTransact = [
         'mysql',
@@ -39,20 +39,6 @@ class TestCase extends BaseTestCase
     ];
 
     protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-        return $app;
-    }
 
     public function setUp()
     {
