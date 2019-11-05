6ac0b9d7675699fba3ffe526b2e062d36bf21b01
bakaneko <notbakaneko@users.noreply.github.com>
2019-11-05 14:56:51 +0900

    wait for all watches to build at least once.

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,32 @@ Mix.dispatch('init', Mix);
 let WebpackConfig = require(path.resolve(mixPath, 'src/builder/WebpackConfig'));
 const config = new WebpackConfig().build();
 
+const routesFile = path.resolve(__dirname, 'routes/web.php');
+const langDir = path.resolve(__dirname, 'resources/lang');
+
+let resolved = false;
+
+const watches = [
+  {
+    callback: () => {
+      spawnSync('php', ['artisan', 'ziggy:generate'], spawnOptions);
+    },
+    path: routesFile,
+    type: 'file',
+  },
+  {
+    callback: () => {
+      spawnSync('yarn', ['generate-localizations'], spawnOptions);
+      // touching the file on first build might cause karma's watchers to fire after tests start.
+      if (resolved) {
+        spawnSync('touch', [path.resolve(__dirname, 'resources/assets/coffee/main.coffee')], spawnOptions);
+      }
+    },
+    path: langDir,
+    type: 'dir',
+  },
+]
+
 function configPromise(env, argv) {
   return new Promise((resolve) => {
     const options = {
@@ -46,28 +72,29 @@ function configPromise(env, argv) {
     // same as webpack-cli's handling
     if (argv['watch-poll'] === 'true' || argv['watch-poll'] === '') options.poll = true;
 
-    const routesFile = path.resolve(__dirname, 'routes/web.php');
-    const langDir = path.resolve(__dirname, 'resources/lang');
     const wp = new Watchpack(options);
-    let resolved = false;
+    wp.watch(
+      watches.filter(x => x.type === 'file').map(x => x.path),
+      watches.filter(x => x.type === 'dir').map(x => x.path)
+    ); // files and directories are different arguments.
 
-    wp.watch([routesFile], [langDir]);
+    // directory watchers cause change events on start, file watchers don't;
+    // run the callback for each file watcher once.
+    watches.filter(x => x.type === 'file').forEach((watched) => {
+      watched.callback();
+      watched.ranOnce = true;
+    })
 
-    wp.on('aggregated', function (changes, removals) {
-      if (changes.includes(routesFile) || removals.includes(routesFile)) {
-        spawnSync('php', ['artisan', 'ziggy:generate'], spawnOptions);
-      }
-
-      if (changes.includes(langDir) || removals.includes(langDir)) {
-        spawnSync('yarn', ['generate-localizations'], spawnOptions);
-        // touching the file on first build might cause karma's watchers to fire after tests start.
-        if (resolved) {
-          spawnSync('touch', [path.resolve(__dirname, 'resources/assets/coffee/main.coffee')], spawnOptions);
+    wp.on('aggregated', (changes, removals) => {
+      watches.forEach((watched) => {
+        if (changes.includes(watched.path) || removals.includes(watched.path)) {
+          watched.callback();
+          watched.ranOnce = true;
         }
-      }
+      });
 
       // let webpack run after the first build.
-      if (!resolved) {
+      if (!resolved && watches.reduce((value, watched) => value && watched.ranOnce, true)) {
         resolved = true;
         resolve(config);
       }
