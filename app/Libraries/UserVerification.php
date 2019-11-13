482c5004bce92cf16fc72f606ac24d41c3503fda
nanaya <me@nanaya.pro>
2019-11-13 18:35:06 +0900

    Return correct status code for verification page

--- app/Libraries/UserVerification.php
+++ app/Libraries/UserVerification.php
@@ -76,7 +76,7 @@ class UserVerification
                 ),
             ], 401);
         } else {
-            return response()->view('users.verify', compact('email'));
+            return response()->view('users.verify', compact('email'))->setStatusCode(401);
         }
     }
 
