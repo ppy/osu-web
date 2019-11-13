e8ecd5d6436f8fae6f7d5cdba39b41c366659842
Jamie Taylor <me@nekodex.net>
2019-11-13 15:01:42 +0900

    Clean up tokenizer a bit

--- resources/assets/lib/beatmap-discussions/markdown-embed-tokenizer.ts
+++ resources/assets/lib/beatmap-discussions/markdown-embed-tokenizer.ts
@@ -1,18 +1,19 @@
-import * as remarkParse from 'remark-parse';
+import { Eat } from 'remark-parse';
+import { Node } from 'unist';
 
 function embedTokenizer() {
   function locator(value: string, fromIndex: number) {
     return value.indexOf('%[', fromIndex);
   }
 
-  function inlineTokenizer(eat: remarkParse.Eat, value: string, silent: true): boolean | void {
+  function inlineTokenizer(eat: Eat, value: string, silent?: true): Node | boolean | void {
     // Custom Markdown to Embed a Discussion
     // e.g. given a discussion_id of 123: %[](#123)
     const regex = new RegExp(/%\[\]\(#(\d+)\)\n*/);
     const result = regex.exec(value);
 
     if (silent) {
-      return silent;
+      return true;
     }
 
     if (!result || result.index !== 0) {
@@ -33,6 +34,8 @@ function embedTokenizer() {
     });
   }
 
+  // Ideally 'Parser' here should be typed like Parser from 'remark-parse', but the provided types appear wonky --
+  // they causes issues with the inlineTokenizer definition below, so we're gonna leave it as an implicit 'any' for now.
   const Parser = this.Parser;
   const inlineTokenizers = Parser.prototype.inlineTokenizers;
   const inlineMethods = Parser.prototype.inlineMethods;
@@ -40,7 +43,7 @@ function embedTokenizer() {
   // Inject inlineTokenizer
   inlineTokenizer.locator = locator;
   inlineTokenizers.embed = inlineTokenizer;
-  inlineMethods.splice(0, 0, 'embed');
+  inlineMethods.unshift('embed');
 }
 
 module.exports = embedTokenizer;
