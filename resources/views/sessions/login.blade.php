b640bc4d21deed0cb968e9e8e6a430a592be572f
nanaya <me@nanaya.pro>
2019-11-21 12:23:45 +0900

    Rename class

--- resources/views/sessions/login.blade.php
+++ resources/views/sessions/login.blade.php
@@ -22,24 +22,24 @@
 @section('content')
     {!! Form::open([
         'url' => route('login'),
-        'class' => 'oauth-form js-login-form',
+        'class' => 'dialog-form js-login-form',
         'data-remote' => true,
     ]) !!}
-        <div class="oauth-form__dialog">
-            <div class="oauth-form__row oauth-form__row--header"></div>
+        <div class="dialog-form__dialog">
+            <div class="dialog-form__row dialog-form__row--header"></div>
 
-            <div class="oauth-form__row oauth-form__row--title">
-                <div class="oauth-form__logo"></div>
-                <h1 class="oauth-form__title">{{ trans('oauth.login.title') }}</h1>
+            <div class="dialog-form__row dialog-form__row--title">
+                <div class="dialog-form__logo"></div>
+                <h1 class="dialog-form__title">{{ trans('oauth.login.title') }}</h1>
             </div>
 
-            <div class="oauth-form__row oauth-form__row--label">
+            <div class="dialog-form__row dialog-form__row--label">
                 {{ trans('oauth.login.label') }}
             </div>
 
-            <div class="oauth-form__row oauth-form__row--input">
+            <div class="dialog-form__row dialog-form__row--input">
                 <input
-                    class="oauth-form__input js-login-form-input"
+                    class="dialog-form__input js-login-form-input"
                     name="username"
                     placeholder="{{ trans('layout.popup_login.login.email') }}"
                     required
@@ -47,9 +47,9 @@
                 />
             </div>
 
-            <div class="oauth-form__row oauth-form__row--input">
+            <div class="dialog-form__row dialog-form__row--input">
                 <input
-                    class="oauth-form__input js-login-form-input"
+                    class="dialog-form__input js-login-form-input"
                     name="password"
                     type="password"
                     placeholder="{{ trans('layout.popup_login.login.password') }}"
@@ -57,25 +57,25 @@
                 />
             </div>
 
-            <div class="oauth-form__row oauth-form__row--error js-login-form--error">
+            <div class="dialog-form__row dialog-form__row--error js-login-form--error">
             </div>
 
-            <div class="oauth-form__row oauth-form__row--extra-link">
-                <a href="{{ route('password-reset') }}" class="oauth-form__extra-link">
+            <div class="dialog-form__row dialog-form__row--extra-link">
+                <a href="{{ route('password-reset') }}" class="dialog-form__extra-link">
                     {{ trans('layout.popup_login.login.forgot') }}
                 </a>
             </div>
 
-            <div class="oauth-form__row oauth-form__row--extra-link">
+            <div class="dialog-form__row dialog-form__row--extra-link">
                 {{ trans('layout.popup_login.register.title') }}
-                <a href="{{ route('download') }}" class="oauth-form__extra-link">
+                <a href="{{ route('download') }}" class="dialog-form__extra-link">
                     {{ trans('oauth.login.download') }}
                 </a>
             </div>
 
-            <div class="oauth-form__row oauth-form__row--buttons">
+            <div class="dialog-form__row dialog-form__row--buttons">
                 <button
-                    class="oauth-form__button"
+                    class="dialog-form__button"
                     data-disable-with="{{ trans('users.login.button_posting') }}"
                 >
                     {{ trans('users.login._') }}
@@ -83,7 +83,7 @@
 
                 <a
                     href="{{ $cancelUrl ?? route('home') }}"
-                    class="oauth-form__button oauth-form__button--cancel"
+                    class="dialog-form__button dialog-form__button--cancel"
                 >
                     {{ trans('common.buttons.cancel') }}
                 </a>
