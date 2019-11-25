6f0c2cc5c3533ae235b5bc69c7eee307a6ae26be
bakaneko <notbakaneko@users.noreply.github.com>
2019-11-25 16:18:26 +0900

    don't issue verification code without email address

--- app/Libraries/UserVerification.php
+++ app/Libraries/UserVerification.php
@@ -89,8 +89,12 @@ class UserVerification
     public function issue()
     {
         $user = $this->user;
-        $email = $user->user_email;
         $to = $user->user_email;
+
+        if (!present($to)) {
+            return;
+        }
+
         $keys = $this->state->issue();
 
         $requestCountry = Country
