f6bf7032789741f1f9461acc699044bcbb4c4ff0
Jamie Taylor <me@nekodex.net>
2019-12-13 20:00:31 +0900

    Handle the case wherein there's no news

--- resources/assets/lib/landing-news.tsx
+++ resources/assets/lib/landing-news.tsx
@@ -25,21 +25,25 @@ import { ShowMoreLink } from 'show-more-link';
 export function LandingNews({posts}: {posts: PostJson[]}) {
   return (
     <div className='landing-news'>
-      <div className='landing-news__posts'>
-        {<PostItem post={posts[0]} modifiers={['landing', 'hover']} />}
-      </div>
-      <div className='landing-news__posts'>
-        {_.slice(posts, 1).map((post: PostJson, i: number) => <PostItem post={post} modifiers={['landing', 'hover']} key={i}/>)}
-      </div>
-      <div className='landing-news__link'>
-        <ShowMoreLink
-          hasMore={true}
-          loading={false}
-          hideIcon={true}
-          label={osu.trans('home.landing.see_more_news')}
-          url={route('news.index')}
-        />
-      </div>
+      {posts.length > 0 &&
+        <>
+          <div className='landing-news__posts'>
+            {<PostItem post={posts[0]} modifiers={['landing', 'hover']} />}
+          </div>
+          <div className='landing-news__posts'>
+            {_.slice(posts, 1).map((post: PostJson, i: number) => <PostItem post={post} modifiers={['landing', 'hover']} key={i}/>)}
+          </div>
+          <div className='landing-news__link'>
+            <ShowMoreLink
+              hasMore={true}
+              loading={false}
+              hideIcon={true}
+              label={osu.trans('home.landing.see_more_news')}
+              url={route('news.index')}
+            />
+          </div>
+        </>
+      }
     </div>
   );
 }
