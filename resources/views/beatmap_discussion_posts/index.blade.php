3ac5fbfaff98987e6d4e5101bd56c690b9686478
nanaya <me@nanaya.pro>
2019-12-17 16:04:31 +0900

    Update header of beatmap discussion-related pages

--- resources/views/beatmap_discussion_posts/index.blade.php
+++ resources/views/beatmap_discussion_posts/index.blade.php
@@ -15,18 +15,21 @@
     You should have received a copy of the GNU Affero General Public License
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
-@extends('master')
+@extends('master', ['legacyNav' => false])
 
 {{-- FIXME: move to user modding history --}}
 @section('content')
-    <div class="osu-layout__row osu-layout__row--page">
+    @include('layout._page_header_v4', ['params' => [
+        'section' => trans('layout.header.beatmapsets._'),
+        'subSection' => trans('beatmap_discussion_posts.index.title'),
+    ]])
+    <div class="osu-page osu-page--generic">
         <div class="beatmapset-activities">
             @if (isset($user))
                 <h2>{{ trans('users.beatmapset_activities.title', ['user' => $user->username]) }}</h2>
             @endif
 
             <div>
-                <h3>{{ trans('beatmap_discussion_posts.index.title') }}</h3>
                 @foreach ($posts as $post)
                     @include('beatmap_discussion_posts._item', compact('post'))
                 @endforeach
