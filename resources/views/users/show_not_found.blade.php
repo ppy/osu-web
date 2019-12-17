8be068ec8e425c609f75e6b90f52db3ccbba0932
nanaya <me@nanaya.pro>
2019-12-17 16:29:20 +0900

    Update header of user 404 page

--- resources/views/users/show_not_found.blade.php
+++ resources/views/users/show_not_found.blade.php
@@ -18,9 +18,15 @@
 @extends('master', [
     'currentSection' => 'community',
     'currentAction' => 'profile',
+    'legacyNav' => false,
 ])
 
 @section('content')
+    @include('layout._page_header_v4', ['params' => [
+        'section' => trans('layout.header.users._'),
+        'subSection' => trans('layout.header.users.show'),
+        'theme' => 'default',
+    ]])
     <div class="osu-page osu-page--generic">
         <h1>{{ trans('users.show.not_found.title') }}</h1>
 
