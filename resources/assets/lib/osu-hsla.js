e5955a95d50711c7fe1fb81493e28aee7af2a019
nanaya <me@nanaya.pro>
2019-12-24 12:49:46 +0900

    Use tslint for js as well

--- resources/assets/lib/osu-hsla.js
+++ resources/assets/lib/osu-hsla.js
@@ -16,37 +16,36 @@
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// This file uses `standard` linter.
 // `number` function is taken from: https://github.com/less/less.js/blob/master/lib/less/functions/color.js
 module.exports = {
-  install: function (less, pluginManager, functions) {
-    const number = (n) => {
+  install: function(less, pluginManager, functions) {
+    const getNumber = (n) => {
       if (n instanceof less.tree.Dimension) {
-        return parseFloat(n.unit.is('%') ? n.value / 100 : n.value)
+        return parseFloat(n.unit.is('%') ? n.value / 100 : n.value);
       } else if (typeof n === 'number') {
-        return n
+        return n;
       } else {
-        throw new Error(`unsupported number: ${n}`)
+        throw new Error(`unsupported number: ${n}`);
       }
-    }
+    };
 
     const osuHsla = (colour, alpha) => {
       if (colour instanceof less.tree.Color) {
-        colour.alpha = number(alpha)
+        colour.alpha = getNumber(alpha);
       } else if (colour instanceof less.tree.Call) {
         if (colour.name === 'hsl') {
-          colour.name = 'hsla'
-          colour.args.push(alpha)
+          colour.name = 'hsla';
+          colour.args.push(alpha);
         } else {
-          throw new Error(`unsupported colour function: ${colour.name}`)
+          throw new Error(`unsupported colour function: ${colour.name}`);
         }
       } else {
-        throw new Error(`unsupported colour parameter: ${colour}`)
+        throw new Error(`unsupported colour parameter: ${colour}`);
       }
 
-      return colour
-    }
+      return colour;
+    };
 
-    functions.add('osu-hsla', osuHsla)
-  }
-}
+    functions.add('osu-hsla', osuHsla);
+  },
+};
