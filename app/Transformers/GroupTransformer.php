364effc7f57b363adc14b953e0e93f1b6157802d
nanaya <me@nanaya.pro>
2020-01-16 16:48:51 +0900

    Fix group colour attribute

--- app/Transformers/GroupTransformer.php
+++ app/Transformers/GroupTransformer.php
@@ -33,7 +33,7 @@ class GroupTransformer extends Fractal\TransformerAbstract
             'name' => $group->group_name,
             'short_name' => $group->short_name,
             'description' => $group->group_desc,
-            'colour' => $group->colour,
+            'colour' => $group->group_colour,
         ];
     }
 }
