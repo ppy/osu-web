5559fc10d2c75070eb04b548c25addd88c94e968
bakaneko <notbakaneko@users.noreply.github.com>
2020-01-27 14:05:59 +0900

    include optional translation of supporter donation email

--- resources/views/emails/store/donation_thanks.blade.php
+++ resources/views/emails/store/donation_thanks.blade.php
@@ -15,6 +15,26 @@
     You should have received a copy of the GNU Affero General Public License
     along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
 --}}
+
+Hi {{ $donor->username }},
+
+Thanks a lot for your {{ $continued ? 'continued ': '' }}support towards osu!.
+It is thanks to people like you that osu! is able to keep the game and community running smoothly without any advertisements or forced payments.
+{{ $isGift ? 'Your giftee(s)' : 'You' }} will now have access to osu!direct and many other supporter benefits{{ !$isGift ? ' for '.$duration : '' }}.
+More new supporter benefits will appear over time, as well!
+
+Your support keeps osu! running for around {{ $minutes }} minutes! It may not seem like much, but it all adds up :).
+
+If you have any questions or feedback, don't hesitate to reply to this mail; I'll get back to you as soon as possible!
+
+Regards,
+Dean Herbert (peppy)
+@if (app()->getLocale() !== 'en')
+
+{!! trans('mail.donation_thanks.translation') !!}
+
+==============================
+
 {!! trans('mail.common.hello', ['user' => $donor->username]) !!}
 
 {!! trans('mail.donation_thanks.support._', [
@@ -34,3 +54,4 @@
 
 {!! trans('mail.common.closing') !!}
 Dean Herbert (peppy)
+@endif
