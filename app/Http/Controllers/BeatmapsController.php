4cdc8b1a80698eedb753a4e2ed1fb99437a6965f
bakaneko <notbakaneko@users.noreply.github.com>
2020-02-26 18:45:36 +0900

    Revert "don't set position if restricted"
    This reverts commit 6998d37b7b6c6c8f208c30981de9568388ebced1.

--- app/Http/Controllers/BeatmapsController.php
+++ app/Http/Controllers/BeatmapsController.php
@@ -82,14 +82,10 @@ class BeatmapsController extends Controller
                 ];
 
                 if (isset($score)) {
-                    $position = $score->userRank(compact('type', 'mods'));
                     $results['userScore'] = [
+                        'position' => $score->userRank(compact('type', 'mods')),
                         'score' => json_item($score, 'Score', ['user', 'user.country']),
                     ];
-
-                    if ($position !== null) {
-                        $results['userScore']['position'] = $position;
-                    }
                 }
 
                 return $results;
