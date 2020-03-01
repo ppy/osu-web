1a3fc74a57c5cef8af8494a52d3040014330c64a
ekisu <dts.ramon@gmail.com>
2020-03-01 16:24:33 -0300

    Add test case for a concurrent post after resolving a discussion
    This consistently exposes the issue with beatmap discussions, where a
    normal reply might reopen a discussion under some conditions.
    
    This also adds a additional '/_dusk/verify' route, only available on the
    testing environment, that automatically verifies an user.

---
+++ app/Providers/AdditionalDuskServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use App\Libraries\UserVerification;
+use Illuminate\Support\ServiceProvider;
+use Route;
+
+class AdditionalDuskServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Route::get('/_dusk/verify', function() {
+            return UserVerification::fromCurrentRequest()->markVerifiedAndRespond();
+        });
+    }
+}
