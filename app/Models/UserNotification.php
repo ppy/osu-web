827193c2e4df88d0d5136ea26dc6e7c67faa01a0
bakaneko <notbakaneko@users.noreply.github.com>
2020-03-03 15:59:09 +0900

    type notification identities.
    Otherwise numbers get treated as strings when the request isn't
    specified as json, which causes the notification event payload to
    contain the wrong types.

--- app/Models/UserNotification.php
+++ app/Models/UserNotification.php
@@ -30,10 +30,21 @@ class UserNotification extends Model
         'is_read' => 'boolean',
     ];
 
-    public static function markAsReadByIds(User $user, $identities)
+    public static function markAsReadByIds(User $user, array $params)
     {
-        // TODO: validate schema
-        $ids = collect($identities)->pluck('id');
+        $ids = [];
+        $identities = array_map(function ($param) use (&$ids) {
+            $identity = get_params($param, null, [
+                'category',
+                'id:int',
+                'object_id:int',
+                'object_type',
+            ]);
+
+            $ids[] = $identity['id'] ?? null;
+
+            return $identity;
+        }, $params);
 
         $count = $user
             ->userNotifications()
@@ -46,7 +57,7 @@ class UserNotification extends Model
         }
     }
 
-    public static function markAsReadByNotificationIdentifier(User $user, $params)
+    public static function markAsReadByNotificationIdentifier(User $user, array $params)
     {
         $category = presence($params['category'] ?? null);
         $objectId = $params['object_id'] ?? null;
