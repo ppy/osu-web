5078ab93773a989898f92dd09614270931addd7b
nanaya <me@nanaya.pro>
2020-03-11 18:12:33 +0900

    Add missing licences

--- resources/views/vendor/apidoc/partials/info.blade.php
+++ resources/views/vendor/apidoc/partials/info.blade.php
@@ -1,3 +1,7 @@
+{{--
+    Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+    See the LICENCE file in the repository root for full licence text.
+--}}
 # Introduction
 
 Welcome to the documentation for osu!api v2. You can use this API to get information on various circles and those who click them.
