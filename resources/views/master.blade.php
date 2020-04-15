872b313349037b4c86e548316be47d5eb1b3e5f8
nanaya <me@nanaya.pro>
2020-04-15 16:58:19 +0900

    Incorrect class being applied

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -103,7 +103,7 @@
             </div>
         </div>
 
-        <div id="main-player" class="audio-player audio-player--main" data-turbolinks-permanent></div>
+        <div id="main-player" data-turbolinks-permanent></div>
         @include("layout._global_variables")
         @include('layout._loading_overlay')
         @include('layout.popup-container')
