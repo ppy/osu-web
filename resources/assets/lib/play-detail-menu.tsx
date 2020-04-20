49780992b20844afd7dcebb0bd470d88ba6aef03
nanaya <me@nanaya.pro>
2020-04-20 16:22:52 +0900

    Rename to match interface name

--- resources/assets/lib/play-detail-menu.tsx
+++ resources/assets/lib/play-detail-menu.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import ScoreJson from 'interfaces/score';
+import ScoreJson from 'interfaces/score-json';
 import { route } from 'laroute';
 import * as _ from 'lodash';
 import { PopupMenuPersistent } from 'popup-menu-persistent';
