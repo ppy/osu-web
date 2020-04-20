3f82635a7847df797fdfa4eaf14af82f0e45e016
bakaneko <notbakaneko@users.noreply.github.com>
2020-04-20 14:04:16 +0900

    JSON is made of lies

--- resources/assets/lib/beatmapsets/beatmapset-json.ts
+++ resources/assets/lib/beatmapsets/beatmapset-json.ts
@@ -11,7 +11,7 @@ interface BeatmapsetCovers {
   slimcover: string;
 }
 
-export interface BeatmapsetJson extends JSON {
+export interface BeatmapsetJson {
   artist: string;
   covers: BeatmapsetCovers;
   creator: string;
