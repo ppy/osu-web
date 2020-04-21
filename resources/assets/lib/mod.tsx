9d8c3dfd140542496d13fc272bc47b998643f527
nanaya <me@nanaya.pro>
2020-04-21 18:45:06 +0900

    Actual mod.tsx

--- resources/assets/lib/mod.tsx
+++ resources/assets/lib/mod.tsx
@@ -1,14 +1,21 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import * as React from 'react'
-import { div } from 'react-dom-factories'
+import * as React from 'react';
 
-export Mod = ({modifiers = [], mod}) ->
-    blockClass = 'mod'
-    blockClass += " mod--#{m}" for m in modifiers
-    blockClass += " mod--#{mod}"
+interface Props {
+  mod: string;
+  modifiers?: string[];
+}
 
-    div
-      className: blockClass
-      title: osu.trans("beatmaps.mods.#{mod}")
+export default function Mod(props: Props) {
+  let blockClass = osu.classWithModifiers('mod', props.modifiers);
+  blockClass += ` mod--${props.mod}`;
+
+  return (
+    <div
+      className={blockClass}
+      title={osu.trans(`beatmaps.mods.${props.mod}`)}
+    />
+  );
+}
