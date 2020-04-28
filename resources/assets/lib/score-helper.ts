726db01385475a4bd24f924dca16e5ba180a0934
nanaya <me@nanaya.pro>
2020-04-28 13:02:14 +0900

    Actual typescript score helper
    Convert to export individual functions

--- resources/assets/lib/score-helper.ts
+++ resources/assets/lib/score-helper.ts
@@ -1,7 +1,18 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-# TODO: move to application state repository thingy later
-export class ScoreHelper
-  @hasMenu: (score) ->
-    score.replay || (currentUser.id? && score.user_id != currentUser.id)
+import Score from 'interfaces/score';
+
+export const canBeReported = (score: Score) => {
+  return currentUser.id != null
+    && score.user_id !== currentUser.id;
+};
+
+// TODO: move to application state repository thingy later
+export const hasMenu = (score: Score) => {
+  return hasReplay(score) || canBeReported(score);
+};
+
+export const hasReplay = (score: Score) => {
+  return score.replay;
+};
