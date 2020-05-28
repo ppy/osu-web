ab983b905e5e6fe58bf31e7eb0a376d9c8085c52
bakaneko <notbakaneko@users.noreply.github.com>
2020-05-28 16:59:47 +0900

    add index for top scores

---
+++ database/migrations/2020_05_28_072624_add_multiplayer_scores_high_top_scores_index.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMultiplayerScoresHighTopScoresIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('multiplayer_scores_high', function (Blueprint $table) {
+            $table->index(['playlist_item_id', DB::raw('total_score DESC'), 'score_id'], 'top_scores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('multiplayer_scores_high', function (Blueprint $table) {
+            $table->dropIndex('top_scores');
+        });
+    }
+}
