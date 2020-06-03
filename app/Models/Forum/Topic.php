4f8d28ac5de3a4903eda59aae2255dd72d825cd5
nanaya <me@nanaya.pro>
2020-06-03 15:49:28 +0900

    Trim topic title on assignment

--- app/Models/Forum/Topic.php
+++ app/Models/Forum/Topic.php
@@ -419,6 +419,11 @@ class Topic extends Model implements AfterCommit
         $this->attributes['topic_last_poster_colour'] = ltrim($value, '#');
     }
 
+    public function setTopicTitleAttribute($value)
+    {
+        $this->attributes['topic_title'] = preg_replace('/(^\pZ+|\pZ+$)/u', '', trim($value));
+    }
+
     public function save(array $options = [])
     {
         if (!$this->isValid()) {
