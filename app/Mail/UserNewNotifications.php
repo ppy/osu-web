0203ff0c5b75a109fbb03c7313173e9b4ee1b2fa
bakaneko <notbakaneko@users.noreply.github.com>
2020-06-08 17:54:10 +0900

    group by notifiables

--- app/Mail/UserNewNotifications.php
+++ app/Mail/UserNewNotifications.php
@@ -7,6 +7,7 @@ namespace App\Mail;
 
 use App\Exceptions\InvalidNotificationException;
 use App\Jobs\Notifications\BroadcastNotificationBase;
+use App\Models\Notification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -15,6 +16,8 @@ use Illuminate\Queue\SerializesModels;
 class UserNewNotifications extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
+
+    private $keyed = [];
 
     private $notifications;
 
@@ -28,6 +31,14 @@ class UserNewNotifications extends Mailable implements ShouldQueue
         $this->notifications = $notifications;
     }
 
+    private function addKeyed(Notification $notification)
+    {
+        $key = $notification->notifiable_type;
+        $id = $notification->notifiable_id;
+
+        $this->keyed[$key][$id][] = $notification;
+    }
+
     /**
      * Build the message.
      *
@@ -35,15 +46,26 @@ class UserNewNotifications extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        $lines = [];
         foreach ($this->notifications as $notification) {
-            try {
-                $class = BroadcastNotificationBase::getNotificationClassFromNotification($notification);
-                $lines[] = $class::getMailText($notification);
-            } catch (InvalidNotificationException $e) {
-                log_error($e);
-            }
+            $this->addKeyed($notification);
+        }
 
+        $lines = [];
+        foreach ($this->keyed as $notifiableType => $notificationsByNotifiableId) {
+            foreach ($notificationsByNotifiableId as $notifiableId => $notifications) {
+                $lines[] = "Updates in {$notifiableType}#{$notifiableId}";
+
+                foreach ($notifications as $notification) {
+                    try {
+                        $class = BroadcastNotificationBase::getNotificationClassFromNotification($notification);
+                        $lines[] = $class::getMailText($notification);
+                    } catch (InvalidNotificationException $e) {
+                        log_error($e);
+                    }
+                }
+
+                $lines[] = '';
+            }
         }
 
         return $this->text('emails.user_new_notifications', compact('lines'));
