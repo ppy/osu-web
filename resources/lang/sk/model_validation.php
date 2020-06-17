67a379dcae169b02442a43abb3ba7a6f362a5f43
Dean Herbert <pe@ppy.sh>
2020-06-17 14:49:31 +0900

    Update translations from crowdin

--- resources/lang/sk/model_validation.php
+++ resources/lang/sk/model_validation.php
@@ -4,6 +4,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 return [
+    'invalid' => '',
     'not_negative' => ':attribute nemôže byť negatívny.',
     'required' => ':attribute je vyžadovaný.',
     'too_long' => ':attribute presiahol maximálnu dĺžku - môže mať maximálne :limit znakov.',
@@ -17,7 +18,13 @@ return [
         'invalid_beatmapset_id' => 'Bola zadaná neplatná beatmapa.',
         'locked' => 'Diskusia je uzamknutá.',
 
+        'attributes' => [
+            'message_type' => '',
+            'timestamp' => '',
+        ],
+
         'hype' => [
+            'discussion_locked' => "",
             'guest' => 'Musíš byť prihláseny k nadšeniu.',
             'hyped' => 'Túto beatmapu si už nadchol.',
             'limit_exceeded' => 'Použil si všetky svoje nadšenia.',
@@ -34,14 +41,23 @@ return [
     'beatmapset_discussion_post' => [
         'discussion_locked' => 'Diskusia je uzamknutá.',
         'first_post' => 'Počiatočný príspevok sa nedá odstrániť.',
+
+        'attributes' => [
+            'message' => '',
+        ],
     ],
 
     'comment' => [
         'deleted_parent' => 'Odpovedať na odstránený komentár nie je povolené.',
+        'top_only' => '',
 
         'attributes' => [
             'message' => 'Správa',
         ],
+    ],
+
+    'follow' => [
+        'invalid' => '',
     ],
 
     'forum' => [
@@ -58,15 +74,30 @@ return [
             'beatmapset_post_no_delete' => 'Mazanie metadát beatmapy nie je povolené.',
             'beatmapset_post_no_edit' => 'Editovanie metadát beatmapy nie je povolené.',
             'only_quote' => '',
+
+            'attributes' => [
+                'post_text' => '',
+            ],
+        ],
+
+        'topic' => [
+            'attributes' => [
+                'topic_title' => '',
+            ],
         ],
 
         'topic_poll' => [
             'duplicate_options' => 'Duplikované možnosti nie su povolené.',
             'grace_period_expired' => '',
+            'hiding_results_forever' => '',
             'invalid_max_options' => 'Možnosť jedného použivatela by nemala presiahnúť hodnotu povolených možností.',
             'minimum_one_selection' => 'Minimálne jedná možnosť na používatela je vyžadovaná.',
             'minimum_two_options' => 'Sú vyžadované aspoň dve možnosti.',
             'too_many_options' => 'Bol prekročený limit povolených možností.',
+
+            'attributes' => [
+                'title' => '',
+            ],
         ],
 
         'topic_vote' => [
@@ -77,8 +108,12 @@ return [
 
     'oauth' => [
         'client' => [
+            'too_many' => '',
+            'url' => '',
+
             'attributes' => [
                 'name' => 'Názov aplikácie',
+                'redirect' => '',
             ],
         ],
     ],
@@ -89,6 +124,7 @@ return [
         'invalid_country' => 'Krajina nie je v databáze.',
         'invalid_discord' => 'Nesprávne užívateľské meno na Discorde.',
         'invalid_email' => "Vyzerá to na neplatnú e-mailovú adresu.",
+        'invalid_twitter' => '',
         'too_short' => 'Nové heslo je príliš krátke.',
         'unknown_duplicate' => 'Užívateľské meno alebo e-mailová adresa sa už používa.',
         'username_available_in' => 'Toto užívateľské meno bude k dispozícií za :duration.',
@@ -114,6 +150,7 @@ return [
         ],
 
         'change_username' => [
+            'restricted' => '',
             'supporter_required' => [
                 '_' => 'Musíš mať :link pre zmenu mena!',
                 'link_text' => 'podporiť osu!',
@@ -123,12 +160,14 @@ return [
     ],
 
     'user_report' => [
+        'reason_not_valid' => '',
         'self' => "",
     ],
 
     'store' => [
         'order_item' => [
             'attributes' => [
+                'quantity' => '',
                 'cost' => 'Cena',
             ],
         ],
