13a1a564d1965151388abf6b144503eef57f4744
nanaya <me@nanaya.pro>
2020-07-22 15:06:15 +0900

    Wrong index name

--- database/migrations/2020_07_22_054643_add_user_read_index_to_user_notifications.php
+++ database/migrations/2020_07_22_054643_add_user_read_index_to_user_notifications.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserReadNotificationToUserNotifications extends Migration
+class AddUserReadIndexToUserNotifications extends Migration
 {
     /**
      * Run the migrations.
