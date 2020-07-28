d58eb0de890ec393aac5ad8531c379d4f9a97a59
bakaneko <notbakaneko@users.noreply.github.com>
2020-07-28 19:28:01 +0900

    separate notification for owner of replied comment.
    exclude owner of comment being replied to from new comment notification
    and dispatch a comment reply notification.

--- app/Jobs/Notifications/CommentNew.php
+++ app/Jobs/Notifications/CommentNew.php
@@ -8,6 +8,7 @@ namespace App\Jobs\Notifications;
 use App\Exceptions\InvalidNotificationException;
 use App\Models\Comment;
 use App\Models\Follow;
+use App\Models\Notification;
 use App\Models\User;
 
 class CommentNew extends BroadcastNotificationBase
@@ -37,10 +38,15 @@ class CommentNew extends BroadcastNotificationBase
 
     public function getListeningUserIds(): array
     {
-        return Follow::whereNotifiable($this->comment->commentable)
-            ->where(['subtype' => 'comment'])
-            ->pluck('user_id')
-            ->all();
+        $userIds = Follow::whereNotifiable($this->comment->commentable)
+            ->where(['subtype' => 'comment']);
+
+        if ($this->comment->parent_id !== null) {
+            $userIds->where('user_id', '<>', $this->comment->parent->user_id);
+            broadcast_notification(Notification::COMMENT_REPLY, $this->comment, $this->source);
+        }
+
+        return $userIds->pluck('user_id')->all();
     }
 
     public function getNotifiable()
