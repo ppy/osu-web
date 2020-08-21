914cc4c9e927bc585470bf49b7355667e8505801
bakaneko <notbakaneko@users.noreply.github.com>
2020-08-21 17:13:25 +0900

    move declaring entry and output up

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,62 +16,9 @@ const TerserPlugin = require('terser-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 const inProduction = process.env.NODE_ENV === 'production' || process.argv.includes('-p');
-
-let min = '';
-let reactMin = 'development';
-if (inProduction) {
-  min = '.min';
-  reactMin = 'production.min';
-}
-
 const paymentSandbox = !(process.env.PAYMENT_SANDBOX === '0'
                          || process.env.PAYMENT_SANDBOX === 'false'
                          || !process.env.PAYMENT_SANDBOX);
-
-// relative from root?
-const nodeRoot = path.resolve(__dirname, 'node_modules');
-
-const vendor = [
-  'clipboard-polyfill/build/clipboard-polyfill.js',
-  `url-polyfill/url-polyfill${min}.js`,
-  'turbolinks/dist/turbolinks.js',
-  `jquery/dist/jquery${min}.js`,
-  'jquery-ujs/src/rails.js',
-  `qtip2/dist/jquery.qtip${min}.js`,
-  'jquery.scrollto/jquery.scrollTo.js',
-  'jquery-ui/ui/data.js',
-  'jquery-ui/ui/scroll-parent.js',
-  'jquery-ui/ui/widget.js',
-  'jquery-ui/ui/widgets/mouse.js',
-  'jquery-ui/ui/widgets/slider.js',
-  'jquery-ui/ui/widgets/sortable.js',
-  'jquery-ui/ui/keycode.js',
-  'timeago/jquery.timeago.js',
-  'blueimp-file-upload/js/jquery.fileupload.js',
-  'bootstrap/dist/js/bootstrap.js',
-  'lodash/lodash.js',
-  'layzr.js/dist/layzr.js',
-  `react/umd/react.${reactMin}.js`,
-  'react-dom-factories/index.js',
-  `react-dom/umd/react-dom.${reactMin}.js`,
-  `prop-types/prop-types${min}.js`,
-  'photoswipe/dist/photoswipe.js',
-  'photoswipe/dist/photoswipe-ui-default.js',
-  `d3/dist/d3${min}.js`,
-  'moment/moment.js',
-  'js-cookie/src/js.cookie.js',
-  `imagesloaded/imagesloaded.pkgd${min}.js`,
-].map((name) => path.join(nodeRoot, name));
-
-vendor.forEach(function(script) {
-  if (!fs.existsSync(script)) {
-    throw new Error(`${script} doesn't exist`);
-  }
-});
-
-if (!fs.readdirSync('resources/assets/build/locales').some((file) => file.endsWith('.js'))) {
-  throw new Error('missing locale files.');
-}
 
 // TODO: move to own file
 class ConcatPlugin {
@@ -90,11 +37,11 @@ class ConcatPlugin {
       const logger = compilation.getLogger('concat-webpack-plugin');
       compilation.hooks.additionalAssets.tapAsync('concat-webpack-plugin', async (callback) => {
         const assets = this.patterns.map((pattern) => {
-          const webpackTo = path.resolve(compiler.options.output.path, pattern.output);
+          const webpackTo = path.resolve(compiler.options.output.path, pattern.to);
 
           return {
-            source: new RawSource(concatenate.sync(pattern.input)),
-            targetPath: pattern.output,
+            source: new RawSource(concatenate.sync(pattern.from)),
+            targetPath: pattern.to,
             webpackTo: webpackTo,
           };
         });
@@ -116,6 +63,7 @@ class ConcatPlugin {
   }
 }
 
+// declare entrypoints and output first.
 const entry = {
   '/js/app': [
     './resources/assets/app.js',
@@ -161,9 +109,112 @@ for (const name of tsReactComponents) {
   entry[`/js/react/${name}`] = [path.resolve(__dirname, `resources/assets/lib/${name}.ts`)];
 }
 
+const output = {
+  chunkFilename: '[name].[chunkhash:8].js',
+  filename: '[name].[contenthash:8].js',
+  path: path.resolve(__dirname, 'public'),
+};
+
+const plugins = (function() {
+  const concatPlugin = (function() {
+    let min = '';
+    let reactMin = 'development';
+    if (inProduction) {
+      min = '.min';
+      reactMin = 'production.min';
+    }
+
+    // vendor and locale files.
+    const vendor = [
+      'clipboard-polyfill/build/clipboard-polyfill.js',
+      `url-polyfill/url-polyfill${min}.js`,
+      'turbolinks/dist/turbolinks.js',
+      `jquery/dist/jquery${min}.js`,
+      'jquery-ujs/src/rails.js',
+      `qtip2/dist/jquery.qtip${min}.js`,
+      'jquery.scrollto/jquery.scrollTo.js',
+      'jquery-ui/ui/data.js',
+      'jquery-ui/ui/scroll-parent.js',
+      'jquery-ui/ui/widget.js',
+      'jquery-ui/ui/widgets/mouse.js',
+      'jquery-ui/ui/widgets/slider.js',
+      'jquery-ui/ui/widgets/sortable.js',
+      'jquery-ui/ui/keycode.js',
+      'timeago/jquery.timeago.js',
+      'blueimp-file-upload/js/jquery.fileupload.js',
+      'bootstrap/dist/js/bootstrap.js',
+      'lodash/lodash.js',
+      'layzr.js/dist/layzr.js',
+      `react/umd/react.${reactMin}.js`,
+      'react-dom-factories/index.js',
+      `react-dom/umd/react-dom.${reactMin}.js`,
+      `prop-types/prop-types${min}.js`,
+      'photoswipe/dist/photoswipe.js',
+      'photoswipe/dist/photoswipe-ui-default.js',
+      `d3/dist/d3${min}.js`,
+      'moment/moment.js',
+      'js-cookie/src/js.cookie.js',
+      `imagesloaded/imagesloaded.pkgd${min}.js`,
+    ].map((name) => path.join(path.resolve(__dirname, 'node_modules'), name));
+
+    vendor.forEach(function(script) {
+      if (!fs.existsSync(script)) {
+        throw new Error(`${script} doesn't exist`);
+      }
+    });
+
+    if (!fs.readdirSync('resources/assets/build/locales').some((file) => file.endsWith('.js'))) {
+      throw new Error('missing locale files.');
+    }
+
+    return new ConcatPlugin({
+      patterns: [
+        {
+          from: [
+            'resources/assets/js/ga.js',
+            'resources/assets/build/lang.js',
+            'resources/assets/js/bootstrap-lang.js',
+          ],
+          to: '/js/app-deps.js',
+        },
+        {
+          from: vendor, to: '/js/vendor.js',
+        },
+      ],
+    });
+  }());
+
+  const copyPlugin = new CopyPlugin({
+    patterns: [
+      { from: 'resources/assets/build/locales', to: 'js/locales' },
+      { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'vendor/fonts/font-awesome' },
+      { from: 'node_modules/photoswipe/dist/default-skin', to: 'vendor/_photoswipe-default-skin' },
+      { from: 'node_modules/moment/locale', to: 'vendor/js/moment-locales' },
+    ],
+  });
+
+  return [
+    new webpack.DefinePlugin({
+      'process.env.DOCS_URL': JSON.stringify(process.env.DOCS_URL || 'https://docs.ppy.sh'),
+      'process.env.PAYMENT_SANDBOX': JSON.stringify(paymentSandbox),
+      'process.env.SHOPIFY_DOMAIN': JSON.stringify(process.env.SHOPIFY_DOMAIN),
+      'process.env.SHOPIFY_STOREFRONT_TOKEN': JSON.stringify(process.env.SHOPIFY_STOREFRONT_TOKEN),
+    }),
+    new MiniCssExtractPlugin({
+      chunkFilename: '/css/app.[hash:8].css',
+      filename: '/css/app.[hash:8].css',
+    }),
+    concatPlugin,
+    copyPlugin,
+    new ManifestPlugin({
+      fileName: 'mix-manifest.json',
+    }),
+  ];
+}());
+
 const webpackConfig = {
   devtool: 'source-map',
-  entry: entry,
+  entry,
   externals: {
     'd3': 'd3',
     'lodash': '_',
@@ -317,49 +368,8 @@ const webpackConfig = {
       },
     },
   },
-  output: {
-    chunkFilename: '[name].[chunkhash:8].js',
-    filename: '[name].[contenthash:8].js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.DOCS_URL': JSON.stringify(process.env.DOCS_URL || 'https://docs.ppy.sh'),
-      'process.env.PAYMENT_SANDBOX': JSON.stringify(paymentSandbox),
-      'process.env.SHOPIFY_DOMAIN': JSON.stringify(process.env.SHOPIFY_DOMAIN),
-      'process.env.SHOPIFY_STOREFRONT_TOKEN': JSON.stringify(process.env.SHOPIFY_STOREFRONT_TOKEN),
-    }),
-    new MiniCssExtractPlugin({
-      chunkFilename: '/css/app.[hash:8].css',
-      filename: '/css/app.[hash:8].css',
-    }),
-    new ConcatPlugin({
-      patterns: [
-        {
-          input: [
-            'resources/assets/js/ga.js',
-            'resources/assets/build/lang.js',
-            'resources/assets/js/bootstrap-lang.js',
-          ],
-          output: '/js/app-deps.js',
-        },
-        {
-          input: vendor, output: '/js/vendor.js',
-        },
-      ],
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'resources/assets/build/locales', to: 'js/locales' },
-        { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'vendor/fonts/font-awesome' },
-        { from: 'node_modules/photoswipe/dist/default-skin', to: 'vendor/_photoswipe-default-skin' },
-        { from: 'node_modules/moment/locale', to: 'vendor/js/moment-locales' },
-      ],
-    }),
-    new ManifestPlugin({
-      fileName: 'mix-manifest.json',
-    }),
-  ],
+  output,
+  plugins,
   resolve: {
     alias: {
       'ziggy': path.resolve(__dirname, 'resources/assets/js/ziggy.js'),
