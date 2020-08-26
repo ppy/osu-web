daae660f6ef9a2a1f8d6aad5a19d63d5c15c44fb
nanaya <me@nanaya.pro>
2020-08-26 14:32:01 +0900

    Locally cache groups data

--- app/Libraries/Groups.php
+++ app/Libraries/Groups.php
@@ -16,7 +16,7 @@ class Groups
     public function all()
     {
         if (!isset($this->groups)) {
-            $this->groups = Group::orderBy('display_order')->get();
+            $this->fetch();
         }
 
         return $this->groups;
@@ -55,8 +55,33 @@ class Groups
         return $group;
     }
 
+    public function fetch()
+    {
+        $localCacheVersion = cache()->get('groups_local_cache_version');
+        $localStorage = config('cache.local');
+
+        if ($localCacheVersion === null) {
+            $localCacheVersion = hrtime(true);
+            cache()->forever('groups_local_cache_version', $localCacheVersion);
+        }
+
+        $localCacheKey = "groups:v{$localCacheVersion}";
+        $this->groups = cache()->store($localStorage)->get($localCacheKey);
+
+        if ($this->groups === null) {
+            $this->groups = Group::orderBy('display_order')->get();
+            cache()->store($localStorage)->forever($localCacheKey, $this->groups);
+        }
+    }
+
+    public function resetCache()
+    {
+        cache()->forget('groups_local_cache_version');
+    }
+
     private function reset()
     {
+        $this->resetCache();
         $this->groups = null;
         $this->groupsById = null;
         $this->groupsByIdentifier = null;
