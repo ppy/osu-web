9d3b2ffe90f07017e98c82fe774fe21458aeea96
nanaya <me@nanaya.pro>
2020-08-27 13:22:33 +0900

    Remove unused check

--- resources/views/beatmapset_events/index.blade.php
+++ resources/views/beatmapset_events/index.blade.php
@@ -18,19 +18,17 @@
                     {{ trans('beatmap_discussions.index.form._') }}
                 </h2>
 
-                @if ($showUserSearch ?? true)
-                    <label class="simple-form__row">
-                        <div class="simple-form__label">
-                            {{ trans('beatmap_discussions.index.form.username') }}
-                        </div>
+                <label class="simple-form__row">
+                    <div class="simple-form__label">
+                        {{ trans('beatmap_discussions.index.form.username') }}
+                    </div>
 
-                        <input
-                            class="simple-form__input"
-                            name="user"
-                            value="{{ $params['user'] ?? '' }}"
-                        >
-                    </label>
-                @endif
+                    <input
+                        class="simple-form__input"
+                        name="user"
+                        value="{{ $params['user'] ?? '' }}"
+                    >
+                </label>
 
                 <div class="simple-form__row">
                     <div class="simple-form__label">
