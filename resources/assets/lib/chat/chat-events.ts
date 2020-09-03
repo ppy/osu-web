fdc4b4d0ba8bbbaf3ddd9974fce7eff484b94dee
bakaneko <notbakaneko@users.noreply.github.com>
2020-09-03 17:13:52 +0900

    already an action for leaving channels :thinking:

--- resources/assets/lib/chat/chat-events.ts
+++ resources/assets/lib/chat/chat-events.ts
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import DispatcherAction from 'actions/dispatcher-action';
+import { ChatChannelPartAction } from 'actions/chat-actions';
 import { dispatch } from 'app-dispatcher';
 
 // tslint:disable: max-classes-per-file
@@ -20,15 +20,8 @@ export interface ChatChannelPartEventJson {
   event: 'chat.channel.part';
 }
 
-export class ChatChannelPartEvent extends DispatcherAction {
-  constructor(readonly channelId: number) {
-    super();
-  }
-}
-
 export function dispatchChatChannelEvent(json: ChatChannelEventJson) {
-  console.log(json);
-  dispatch(new ChatChannelPartEvent(json.data.channel_id));
+  dispatch(new ChatChannelPartAction(json.data.channel_id, false));
 }
 
 export function isChatChannelEventJson(arg: any): arg is ChatChannelEventJson {
