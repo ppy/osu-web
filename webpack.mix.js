7e3a811eb042952dbe69858b2d5bee76a0c0c3a2
bakaneko <notbakaneko@users.noreply.github.com>
2020-09-03 12:03:02 +0900

    organize webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,12 +3,7 @@
 
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const { minify } = require('terser');
-const webpack = require('webpack');
-const loaderUtils = require('loader-utils');
-
+//#region plugin imports
 const Autoprefixer = require('autoprefixer');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -17,11 +12,36 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
+//#endregion
+
+// #region non-plugin imports
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+
+//#endregion
+
+//#region env
 const inProduction = process.env.NODE_ENV === 'production' || process.argv.includes('-p');
 const paymentSandbox = !(process.env.PAYMENT_SANDBOX === '0'
                          || process.env.PAYMENT_SANDBOX === 'false'
                          || !process.env.PAYMENT_SANDBOX);
 
+//#endregion
+
+//#region helpers
+function outputFilename(name, ext = 'js', hashType = 'contenthash:8') {
+  return `${name}.${ext}?id=[${hashType}]`;
+  // return `${name}.[${hashType}].${ext}`;
+}
+
+function resolvePath(...segments) {
+  return path.resolve(__dirname, ...segments);
+}
+
+//#endregion
+
+//#region Custom plugins
 // Custom manifest dumper
 // Dumps a manifest file and:
 // Strip the hashes out - the problem is when adding assets via additionalAssets, as the copy and concat plugins do,
@@ -45,7 +65,7 @@ class Manifest {
 
       const manifest = {};
       json.assets.forEach((asset) => {
-        let assetName = asset.name
+        let assetName = asset.name;
         // ensure lookup name starts with / because mix helper is dumb.
         // also ensure assets are relative to root.
         if (!assetName.startsWith('/')) {
@@ -60,7 +80,7 @@ class Manifest {
         } else {
           // hash in filename version
           let extname = path.extname(name);
-          let basename = name.substring(0, name.lastIndexOf(extname))
+          let basename = name.substring(0, name.lastIndexOf(extname));
           if (extname === '.map') {
             extname = `${path.extname(basename)}.map`;
           }
@@ -72,14 +92,16 @@ class Manifest {
         }
 
         manifest[name] = assetName;
-      })
+      });
 
       fs.writeFileSync(this.fileName, JSON.stringify(manifest, null, 2));
     });
   }
 }
 
-// declare entrypoints and output first.
+//#endregion
+
+//#region entrypoints and output
 const entry = {
   'app': [
     './resources/assets/app.js',
@@ -118,31 +140,29 @@ const tsReactComponents = [
 ];
 
 for (const name of coffeeReactComponents) {
-  entry[`react/${name}`] = [path.resolve(__dirname, `resources/assets/coffee/react/${name}.coffee`)];
+  entry[`react/${name}`] = [resolvePath(`resources/assets/coffee/react/${name}.coffee`)];
 }
 
 for (const name of tsReactComponents) {
-  entry[`react/${name}`] = [path.resolve(__dirname, `resources/assets/lib/${name}.ts`)];
-}
-
-function outputFilename(name, ext = 'js', hashType = 'contenthash:8') {
-  return `${name}.${ext}?id=[${hashType}]`;
-  // return `${name}.[${hashType}].${ext}`;
+  entry[`react/${name}`] = [resolvePath(`resources/assets/lib/${name}.ts`)];
 }
 
 const output = {
   chunkFilename: outputFilename('js/[name]'),
   filename: outputFilename('js/[name]'),
-  path: path.resolve(__dirname, 'public'),
+  path: resolvePath('public'),
 };
 
+//#endregion
+
+//#region plugin list
 const plugins = [
   new webpack.ProvidePlugin({
-    _: 'lodash',
     $: 'jquery',
-    jQuery: 'jquery',
+    _: 'lodash',
     Cookies: 'js-cookie',
     d3: 'd3', // TODO: d3 is fat and probably should have it's own chunk
+    jQuery: 'jquery',
     moment: 'moment',
     React: 'react',
     ReactDOM: 'react-dom',
@@ -170,210 +190,8 @@ const plugins = [
   new Manifest({ fileName: 'public/mix-manifest.json'}),
 ];
 
-const webpackConfig = {
-  devtool: 'source-map',
-  entry,
-  mode: inProduction ? 'production' : 'development',
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        exclude: /(node_modules)/,
-        loader: 'import-glob-loader',
-        test: /\.(js|coffee)$/,
-      },
-      {
-        exclude: /(node_modules|bower_components)/,
-        test: /\.jsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              plugins: [
-                '@babel/plugin-syntax-dynamic-import',
-                '@babel/plugin-proposal-object-rest-spread',
-                [
-                  '@babel/plugin-transform-runtime',
-                  {
-                    helpers: false,
-                  },
-                ],
-              ],
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    forceAllTransforms: true,
-                    modules: false,
-                  },
-                ],
-              ],
-            },
-          },
-        ],
-      },
-      {
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-        test: /\.tsx?$/,
-      },
-      {
-        // loader for preexisting global coffeescript
-        exclude: [
-          path.resolve(__dirname, 'resources/assets/coffee/react'),
-        ],
-        include: [
-          path.resolve(__dirname, 'resources/assets/coffee'),
-        ],
-        test: /\.coffee$/,
-        use: ['imports-loader?jQuery=jquery,$=jquery,this=>window', 'coffee-loader'],
-      },
-      {
-        // loader for import-based coffeescript
-        include: [
-          path.resolve(__dirname, 'resources/assets/coffee/react'),
-          path.resolve(__dirname, 'resources/assets/lib'),
-        ],
-        test: /\.coffee$/,
-        use: ['coffee-loader'],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              sourceMap: true,
-              url: (url) => !url.startsWith('/'),
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [Autoprefixer],
-              sourceMap: true,
-            },
-          },
-          { loader: 'less-loader', options: { sourceMap: true } },
-        ],
-      },
-      {
-        loaders: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: (filepath) => {
-                if (!/node_modules|bower_components/.test(filepath)) {
-                  return '/images/[name].[ext]?[hash]';
-                }
-
-                const cleanPath = filepath
-                  .replace(/\\/g, '/')
-                  .replace(/((.*(node_modules|bower_components))|images|image|img|assets)\//g, '');
-
-                return `/images/vendor/${cleanPath}?[hash]`;
-              },
-            },
-          },
-          {
-            loader: 'img-loader',
-          },
-        ],
-        test: /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/,
-      },
-      {
-        loader: 'file-loader',
-        options: {
-          name: (filepath) => {
-            if (!/node_modules|bower_components/.test(filepath)) {
-              return '/fonts/[name].[ext]?[hash]';
-            }
-
-            const cleanPath = filepath
-              .replace(/\\/g, '/')
-              .replace(/((.*(node_modules|bower_components))|fonts|font|assets)\//g, '');
-
-            return `/fonts/vendor/${cleanPath}?[hash]`;
-          },
-        },
-        test: /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/,
-      },
-    ],
-  },
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: {
-      name: 'runtime',
-    },
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          chunks: 'initial',
-          minChunks: 2,
-          name: 'commons',
-          priority: -20,
-        },
-        vendor: {
-          chunks: 'initial',
-          priority: -10,
-          name: 'vendor',
-          reuseExistingChunk: true,
-          test: (module, chunks) => {
-            // Doing it this way doesn't split the css imported via app.less from the main css bundle.
-            return module.resource && module.resource.includes(`${path.sep}node_modules${path.sep}`);
-          },
-        },
-      },
-    },
-  },
-  output,
-  plugins,
-  resolve: {
-    alias: {
-      'layzr': path.resolve(__dirname, 'node_modules/layzr.js/dist/layzr.module.js'),
-      'ziggy': path.resolve(__dirname, 'resources/assets/js/ziggy.js'),
-      'ziggy-route': path.resolve(__dirname, 'vendor/tightenco/ziggy/dist/js/route.js'),
-    },
-    extensions: ['*', '.js', '.coffee', '.ts', '.tsx'],
-    modules: [
-      path.resolve(__dirname, 'resources/assets/coffee'),
-      path.resolve(__dirname, 'resources/assets/lib'),
-      path.resolve(__dirname, 'resources/assets/coffee/react/_components'),
-      path.resolve(__dirname, 'node_modules'),
-    ],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  stats: {
-    entrypoints: false,
-    errorDetails: false,
-    excludeAssets: [
-      // exclude copied files
-      /^js\/locales\//,
-      /^\/fonts\//,
-      /^vendor\//,
-    ],
-  }
-};
-
-if (inProduction) {
-  webpackConfig.optimization.minimizer = [
-    new TerserPlugin({
-      sourceMap: true,
-      terserOptions: {
-        safari10: true,
-      },
-    }),
-    new CssMinimizerPlugin({
-      sourceMap: true,
-    }),
-  ];
-}
-
 if (process.env.SENTRY_RELEASE === '1') {
-  webpackConfig.plugins.push(
+  plugins.push(
     new SentryPlugin({
       apiKey: process.env.SENTRY_API_KEY,
       organisation: process.env.SENTRY_ORG,
@@ -391,4 +209,224 @@ if (process.env.SENTRY_RELEASE === '1') {
   );
 }
 
-module.exports = webpackConfig;
+//#endregion
+
+//#region Loader rules
+const rules = [
+  {
+    enforce: 'pre',
+    exclude: /(node_modules)/,
+    loader: 'import-glob-loader',
+    test: /\.(js|coffee)$/,
+  },
+  {
+    exclude: /(node_modules|bower_components)/,
+    test: /\.jsx?$/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          plugins: [
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-proposal-object-rest-spread',
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                helpers: false,
+              },
+            ],
+          ],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                forceAllTransforms: true,
+                modules: false,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  {
+    exclude: /node_modules/,
+    loader: 'ts-loader',
+    test: /\.tsx?$/,
+  },
+  {
+    // loader for preexisting global coffeescript
+    exclude: [
+      resolvePath('resources/assets/coffee/react'),
+    ],
+    include: [
+      resolvePath('resources/assets/coffee'),
+    ],
+    test: /\.coffee$/,
+    use: ['imports-loader?jQuery=jquery,$=jquery,this=>window', 'coffee-loader'],
+  },
+  {
+    // loader for import-based coffeescript
+    include: [
+      resolvePath('resources/assets/coffee/react'),
+      resolvePath('resources/assets/lib'),
+    ],
+    test: /\.coffee$/,
+    use: ['coffee-loader'],
+  },
+  {
+    test: /\.less$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          sourceMap: true,
+          url: (url) => !url.startsWith('/'),
+        },
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: [Autoprefixer],
+          sourceMap: true,
+        },
+      },
+      { loader: 'less-loader', options: { sourceMap: true } },
+    ],
+  },
+  {
+    loaders: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: (filepath) => {
+            if (!/node_modules|bower_components/.test(filepath)) {
+              return '/images/[name].[ext]?[hash]';
+            }
+
+            const cleanPath = filepath
+              .replace(/\\/g, '/')
+              .replace(/((.*(node_modules|bower_components))|images|image|img|assets)\//g, '');
+
+            return `/images/vendor/${cleanPath}?[hash]`;
+          },
+        },
+      },
+      {
+        loader: 'img-loader',
+      },
+    ],
+    test: /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/,
+  },
+  {
+    loader: 'file-loader',
+    options: {
+      name: (filepath) => {
+        if (!/node_modules|bower_components/.test(filepath)) {
+          return '/fonts/[name].[ext]?[hash]';
+        }
+
+        const cleanPath = filepath
+          .replace(/\\/g, '/')
+          .replace(/((.*(node_modules|bower_components))|fonts|font|assets)\//g, '');
+
+        return `/fonts/vendor/${cleanPath}?[hash]`;
+      },
+    },
+    test: /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/,
+  },
+];
+
+//#endregion
+
+//#region resolvers
+const resolve = {
+  alias: {
+    'layzr': resolvePath('node_modules/layzr.js/dist/layzr.module.js'),
+    'ziggy': resolvePath('resources/assets/js/ziggy.js'),
+    'ziggy-route': resolvePath('vendor/tightenco/ziggy/dist/js/route.js'),
+  },
+  extensions: ['*', '.js', '.coffee', '.ts', '.tsx'],
+  modules: [
+    resolvePath('resources/assets/coffee'),
+    resolvePath('resources/assets/lib'),
+    resolvePath('resources/assets/coffee/react/_components'),
+    resolvePath('node_modules'),
+  ],
+  plugins: [new TsconfigPathsPlugin()],
+};
+
+//#endregion
+
+//#region optimization and chunk splitting settings
+const cacheGroups = {
+  commons: {
+    chunks: 'initial',
+    minChunks: 2,
+    name: 'commons',
+    priority: -20,
+  },
+  vendor: {
+    chunks: 'initial',
+    name: 'vendor',
+    priority: -10,
+    reuseExistingChunk: true,
+    test: (module, chunks) => {
+      // Doing it this way doesn't split the css imported via app.less from the main css bundle.
+      return module.resource && module.resource.includes(`${path.sep}node_modules${path.sep}`);
+    },
+  },
+};
+
+const optimization = {
+  moduleIds: 'hashed',
+  runtimeChunk: {
+    name: 'runtime',
+  },
+  splitChunks: {
+    cacheGroups,
+  },
+
+};
+
+if (inProduction) {
+  optimization.minimizer = [
+    new TerserPlugin({
+      sourceMap: true,
+      terserOptions: {
+        safari10: true,
+      },
+    }),
+    new CssMinimizerPlugin({
+      sourceMap: true,
+    }),
+  ];
+}
+
+//#endregion
+
+module.exports = {
+  devtool: 'source-map',
+  entry,
+  mode: inProduction ? 'production' : 'development',
+  module: {
+    rules,
+  },
+  optimization,
+  output,
+  plugins,
+  resolve,
+  stats: {
+    entrypoints: false,
+    errorDetails: false,
+    excludeAssets: [
+      // exclude copied files
+      /^js\/locales\//,
+      /^\/fonts\//,
+      /^vendor\//,
+    ],
+  },
+};
