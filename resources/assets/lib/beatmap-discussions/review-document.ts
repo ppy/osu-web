87aca3d2bd51957f66d5882e9d0e03c6bbcc911f
bakaneko <notbakaneko@users.noreply.github.com>
2020-09-16 14:39:19 +0900

    maybe fix webpack watch not picking up changes in files that only have interfaces or types.

--- resources/assets/lib/beatmap-discussions/review-document.ts
+++ resources/assets/lib/beatmap-discussions/review-document.ts
@@ -4,7 +4,7 @@
 import * as markdown from 'remark-parse';
 import { Node as SlateNode } from 'slate';
 import * as unified from 'unified';
-import { Node as UnistNode } from 'unist';
+import type { Node as UnistNode } from 'unist';
 import { BeatmapDiscussionReview, PersistedDocumentIssueEmbed } from '../interfaces/beatmap-discussion-review';
 import { disableTokenizersPlugin } from './disable-tokenizers-plugin';
 
