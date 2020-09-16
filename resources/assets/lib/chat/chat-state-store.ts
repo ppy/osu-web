b1419bd964811afab5c8fce082b7f3ed6976ab62
bakaneko <notbakaneko@users.noreply.github.com>
2020-09-16 20:24:55 +0900

    lastReadId can be inferred from current channel

--- resources/assets/lib/chat/chat-state-store.ts
+++ resources/assets/lib/chat/chat-state-store.ts
@@ -1,30 +1,30 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { ChatChannelSwitchAction, ChatMessageSendAction } from 'actions/chat-actions';
+import { ChatMessageSendAction } from 'actions/chat-actions';
 import DispatcherAction from 'actions/dispatcher-action';
 import { UserLogoutAction } from 'actions/user-login-actions';
 import { dispatchListener } from 'app-dispatcher';
-import { action, observable } from 'mobx';
+import { action, computed, observable } from 'mobx';
 import Store from 'stores/store';
 
 @dispatchListener
 export default class ChatStateStore extends Store {
   @observable autoScroll: boolean = false;
-  @observable lastReadId: number = -1;
   @observable selected: number = -1;
+
+  @computed
+  get lastReadId() {
+    return this.root.channelStore.get(this.selected)?.lastReadId ?? -1;
+  }
 
   @action
   flushStore() {
     this.selected = -1;
-    this.lastReadId = -1;
   }
 
   handleDispatchAction(dispatchedAction: DispatcherAction) {
-    if (dispatchedAction instanceof ChatChannelSwitchAction) {
-      this.lastReadId = this.root.channelStore.getOrCreate(dispatchedAction.channelId).lastReadId || -1;
-    } else if (dispatchedAction instanceof ChatMessageSendAction) {
-      this.lastReadId = this.root.channelStore.getOrCreate(dispatchedAction.message.channelId).lastMessageId || -1;
+    if (dispatchedAction instanceof ChatMessageSendAction) {
       this.autoScroll = true;
     } else if (dispatchedAction instanceof UserLogoutAction) {
       this.flushStore();
