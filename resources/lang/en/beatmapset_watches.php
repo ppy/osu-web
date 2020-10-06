cedb1061107146dfc11cf2fe21b82e6dde8808a2
clayton <me@clayton.cc>
2020-10-05 19:03:24 -0700

    Add total and unread counts to modding watchlist

--- resources/lang/en/beatmapset_watches.php
+++ resources/lang/en/beatmapset_watches.php
@@ -8,6 +8,11 @@ return [
         'description' => 'These are the beatmap discussions you are following. You will be notified when there are new posts or updates.',
         'title_compact' => 'modding watchlist',
 
+        'counts' => [
+            'total' => 'Discussions watched',
+            'unread' => 'Discussions with new replies',
+        ],
+
         'table' => [
             'empty' => 'No beatmap discussions watched.',
             'open_issues' => 'Open issues',
