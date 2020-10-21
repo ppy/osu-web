067a4c23b27f21dd8355c4cf1bcd5b18f762c703
nanaya <me@nanaya.pro>
2020-10-21 21:38:21 +0900

    Add locale menu
    Remove line height setting for simple menu so the menu can have consistent height.

--- resources/views/wiki/_actions.blade.php
+++ resources/views/wiki/_actions.blade.php
@@ -2,6 +2,10 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
+@php
+    $availableLocales = $page->availableLocales();
+    $showLocalesMenu = count($availableLocales) > 1;
+@endphp
 <div class="header-buttons">
     <div class="header-buttons__item">
         <a
@@ -27,4 +31,56 @@
             </button>
         </div>
     @endif
+
+    <div class="header-buttons__item">
+        <a
+            href="{{ wiki_url($page->path, $page->locale) }}"
+            class="btn-osu-big btn-osu-big--rounded-thin js-click-menu"
+            data-click-menu-target="wiki-locales"
+        >
+            <span class="btn-osu-big__content">
+                <span class="btn-osu-big__icon-inline btn-osu-big__icon-inline--left">
+                    @include('objects._flag_country', [
+                        'countryCode' => locale_flag($page->locale),
+                        'modifiers' => ['flat'],
+                    ])
+                </span>
+
+                {{ locale_name($page->locale) }}
+
+                @if ($showLocalesMenu)
+                    <span class="btn-osu-big__icon-inline btn-osu-big__icon-inline--right">
+                        <i class="fas fa-angle-down"></i>
+                    </span>
+                @endif
+            </span>
+        </a>
+
+        @if ($showLocalesMenu)
+            <div class="header-buttons__item-menu">
+                <div
+                    class="simple-menu simple-menu--wiki-locales js-click-menu"
+                    data-visibility="hidden"
+                    data-click-menu-id="wiki-locales"
+                >
+                    <div class="simple-menu__content">
+                        @foreach ($availableLocales as $locale)
+                            <a class="simple-menu__item" href="{{ wiki_url($page->path, $locale) }}">
+                                <span class="nav2-locale-item nav2-locale-item--no-padding">
+                                    <span class="nav2-locale-item__flag">
+                                        @include('objects._flag_country', [
+                                            'countryCode' => locale_flag($locale),
+                                            'modifiers' => ['flat'],
+                                        ])
+                                    </span>
+
+                                    {{ locale_name($locale) }}
+                                </span>
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
 </div>
