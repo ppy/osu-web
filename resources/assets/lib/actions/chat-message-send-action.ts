997ea97ac96c43033928bdc8e72c3b6761a66d46
bakaneko <notbakaneko@users.noreply.github.com>
2020-10-23 17:22:15 +0900

    now a single class file

--- resources/assets/lib/actions/chat-message-send-action.ts
+++ resources/assets/lib/actions/chat-message-send-action.ts
@@ -1,8 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
-// tslint:disable:max-classes-per-file
+
+import DispatcherAction from 'actions/dispatcher-action';
 import Message from 'models/chat/message';
-import DispatcherAction from './dispatcher-action';
 
 export class ChatMessageSendAction implements DispatcherAction {
   constructor(public message: Message) {
