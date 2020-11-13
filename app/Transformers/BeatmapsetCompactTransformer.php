e148db8ddcd025e54c1fe65159c7ba80c1da7795
Jamie Taylor <me@nekodex.net>
2020-11-13 22:14:53 +0900

    use strict in_array comparisons

--- app/Transformers/BeatmapsetCompactTransformer.php
+++ app/Transformers/BeatmapsetCompactTransformer.php
@@ -49,7 +49,7 @@ class BeatmapsetCompactTransformer extends TransformerAbstract
             'covers' => $beatmapset->allCoverURLs(),
             'creator' => $beatmapset->creator,
             'favourite_count' => $beatmapset->favourite_count,
-            'hype' => in_array($beatmapset->status(), ['wip', 'pending', 'qualified']) ? [
+            'hype' => in_array($beatmapset->status(), ['wip', 'pending', 'qualified'], true) ? [
                 'current' => $beatmapset->hype,
                 'required' => $beatmapset->requiredHype(),
             ] : null,
