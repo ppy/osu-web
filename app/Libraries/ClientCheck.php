8f4fd036b774f15a18007f5d62f653dd8149b36b
nanaya <me@nanaya.pro>
2021-03-24 17:44:09 +0900

    Less indent

--- app/Libraries/ClientCheck.php
+++ app/Libraries/ClientCheck.php
@@ -11,20 +11,22 @@ class ClientCheck
 {
     public static function assert($user, $params)
     {
-        if ($user->findUserGroup(app('groups')->byIdentifier('admin'), true) === null) {
-            $clientHash = presence(get_string($params['version_hash'] ?? null));
-            abort_if($clientHash === null, 422, 'missing client version');
-
-            // temporary measure to allow android builds to submit without access to the underlying dll to hash
-            if (strlen($clientHash) !== 32) {
-                $clientHash = md5($clientHash);
-            }
-
-            $buildExists = Build::where([
-                'hash' => hex2bin($clientHash),
-                'allow_ranking' => true,
-            ])->exists();
-            abort_if(!$buildExists, 422, 'invalid client hash');
+        if ($user->findUserGroup(app('groups')->byIdentifier('admin'), true) !== null) {
+            return;
         }
+
+        $clientHash = presence(get_string($params['version_hash'] ?? null));
+        abort_if($clientHash === null, 422, 'missing client version');
+
+        // temporary measure to allow android builds to submit without access to the underlying dll to hash
+        if (strlen($clientHash) !== 32) {
+            $clientHash = md5($clientHash);
+        }
+
+        $buildExists = Build::where([
+            'hash' => hex2bin($clientHash),
+            'allow_ranking' => true,
+        ])->exists();
+        abort_if(!$buildExists, 422, 'invalid client hash');
     }
 }
