7e7c5b1c2aa5ec4f9805c6823668388c045c6ccd
bakaneko <notbakaneko@users.noreply.github.com>
2021-03-30 17:18:17 +0900

    separate renders

--- resources/assets/lib/chat/conversation-list.tsx
+++ resources/assets/lib/chat/conversation-list.tsx
@@ -1,33 +1,47 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+import { computed } from 'mobx';
 import { observer } from 'mobx-react';
-import Channel from 'models/chat/channel';
 import core from 'osu-core-singleton';
 import * as React from 'react';
 import ConversationListItem from './conversation-list-item';
 
 @observer
 export default class ConversationList extends React.Component {
+  @computed
+  private get nonPmChannels() {
+    return core.dataStore.channelStore.nonPmChannels;
+  }
+
+  @computed
+  private get pmChannels() {
+    return core.dataStore.channelStore.pmChannels;
+  }
+
   render(): React.ReactNode {
-    const nonPmChannels: Channel[] = core.dataStore.channelStore.nonPmChannels;
-    const pmChannels: Channel[] = core.dataStore.channelStore.pmChannels;
-    const conversationList: React.ReactNode[] = [];
+    return (
+      <div className='chat-conversation-list'>
+        {this.renderNonPmChannels()}
+        {this.renderSeparator()}
+        {this.renderPmChannels()}
+      </div>
+    );
+  }
 
-    nonPmChannels.forEach((conversation) => {
-      conversationList.push(<ConversationListItem key={conversation.channelId} channelId={conversation.channelId} />);
-    });
+  private renderNonPmChannels() {
+    return this.nonPmChannels.map((conversation) => <ConversationListItem key={conversation.channelId} channelId={conversation.channelId} />);
+  }
 
-    if (nonPmChannels.length > 0 && pmChannels.length > 0) {
-      conversationList.push(
-        <div key='separator' className='chat-conversation-list-separator' />,
-      );
+  private renderPmChannels() {
+    return this.pmChannels.map((conversation) => <ConversationListItem key={conversation.channelId} channelId={conversation.channelId} />);
+  }
+
+  private renderSeparator() {
+    if (this.nonPmChannels.length === 0 || this.pmChannels.length === 0) {
+      return null;
     }
 
-    pmChannels.forEach((conversation) => {
-      conversationList.push(<ConversationListItem key={conversation.channelId} channelId={conversation.channelId} />);
-    });
-
-    return <div className='chat-conversation-list'>{conversationList}</div>;
+    return <div key='separator' className='chat-conversation-list-separator' />;
   }
 }
