84b7d75def57917b7f1f0095ab0d856cc3d59214
nanaya <me@nanaya.pro>
2021-04-05 17:34:04 +0900

    Restore old content

--- resources/assets/lib/utils/html.ts
+++ resources/assets/lib/utils/html.ts
@@ -1,3 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+export function createClickCallback(target: unknown) {
+  if (target instanceof HTMLElement) {
+    // plain javascript here doesn't trigger submit events
+    // which means jquery-ujs handler won't be triggered
+    // reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit
+    if (target instanceof HTMLFormElement) {
+      return () => $(target).submit();
+    }
+
+    // inversely, using jquery here won't actually click the thing
+    // reference: https://github.com/jquery/jquery/blob/f5aa89af7029ae6b9203c2d3e551a8554a0b4b89/src/event.js#L586
+    return () => target.click();
+  }
+}
+
 export function formatNumberSuffixed(num?: number, precision?: number, options?: Intl.NumberFormatOptions) {
   const suffixes = ['', 'k', 'm', 'b', 't'];
   const k = 1000;
