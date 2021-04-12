e04df2bef4cb01000384ac29b9643b1f33a6fa63
nanaya <me@nanaya.pro>
2021-04-12 20:29:46 +0900

    Split off more things

--- resources/assets/lib/beatmapset-panel/beatmaps-popup.tsx
+++ resources/assets/lib/beatmapset-panel/beatmaps-popup.tsx
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 import { BeatmapGroup } from 'beatmapset-panel';
+import BeatmapJson from 'interfaces/beatmap-json';
 import { route } from 'laroute';
 import { observer } from 'mobx-react';
 import * as React from 'react';
@@ -24,6 +25,38 @@ const beatmapsPopupTransitionStyles: Record<TransitionStatus, React.CSSPropertie
   unmounted: {},
 };
 
+const Item = observer(({ beatmaps, mode }: BeatmapGroup) => (
+  <div className='beatmaps-popup__group'>
+    {beatmaps.map((beatmap) => <ItemRow key={beatmap.id} beatmap={beatmap} />)}
+  </div>
+));
+
+const ItemRow = observer(({ beatmap }: { beatmap: BeatmapJson }) => (
+  <a
+    className='beatmaps-popup-item'
+    href={route('beatmaps.show', { beatmap: beatmap.id })}
+    key={beatmap.id}
+  >
+    <span className='beatmaps-popup-item__col beatmaps-popup-item__col--mode'>
+      <span className={`fal fa-extra-mode-${beatmap.mode}`} />
+    </span>
+    <span
+      className='beatmaps-popup-item__col beatmaps-popup-item__col--difficulty'
+      style={{
+        '--bg': `var(--diff-${getDiffRating(beatmap.difficulty_rating)})`,
+      } as React.CSSProperties}
+    >
+      <span className='beatmaps-popup-item__difficulty-icon'>
+        <span className='fas fa-star' />
+      </span>
+      {osu.formatNumber(beatmap.difficulty_rating, 2)}
+    </span>
+    <span className='beatmaps-popup-item__col beatmaps-popup-item__col--name'>
+      {beatmap.version}
+    </span>
+  </a>
+));
+
 @observer
 export default class BeatmapsPopup extends React.Component<Props> {
   render() {
@@ -39,35 +72,7 @@ export default class BeatmapsPopup extends React.Component<Props> {
         }}
       >
         <div className='beatmaps-popup__content'>
-          {this.props.groupedBeatmaps.map(({ beatmaps, mode }: BeatmapGroup) => (
-            <div key={mode} className='beatmaps-popup__group'>
-              {beatmaps.map((beatmap) => (
-                <a
-                  className='beatmaps-popup-item'
-                  href={route('beatmaps.show', { beatmap: beatmap.id })}
-                  key={beatmap.id}
-                >
-                  <span className='beatmaps-popup-item__col beatmaps-popup-item__col--mode'>
-                    <span className={`fal fa-extra-mode-${beatmap.mode}`} />
-                  </span>
-                  <span
-                    className='beatmaps-popup-item__col beatmaps-popup-item__col--difficulty'
-                    style={{
-                      '--bg': `var(--diff-${getDiffRating(beatmap.difficulty_rating)})`,
-                    } as React.CSSProperties}
-                  >
-                    <span className='beatmaps-popup-item__difficulty-icon'>
-                      <span className='fas fa-star' />
-                    </span>
-                    {osu.formatNumber(beatmap.difficulty_rating, 2)}
-                  </span>
-                  <span className='beatmaps-popup-item__col beatmaps-popup-item__col--name'>
-                    {beatmap.version}
-                  </span>
-                </a>
-              ))}
-            </div>
-          ))}
+          {this.props.groupedBeatmaps.map((props) => <Item key={props.mode} {...props} />)}
         </div>
       </div>
     );
