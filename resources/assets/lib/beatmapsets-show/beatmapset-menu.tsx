2b8327b2533ee7eb7900aefac6fabfce1baa022c
Venix <30481900+venix12@users.noreply.github.com>
2021-04-13 17:21:37 +0200

    lint

--- resources/assets/lib/beatmapsets-show/beatmapset-menu.tsx
+++ resources/assets/lib/beatmapsets-show/beatmapset-menu.tsx
@@ -1,7 +1,10 @@
-import { BeatmapsetJson } from "beatmapsets/beatmapset-json";
-import { PopupMenuPersistent } from "popup-menu-persistent";
-import * as React from "react";
-import { ReportReportable } from "report-reportable";
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import { BeatmapsetJson } from 'beatmapsets/beatmapset-json';
+import { PopupMenuPersistent } from 'popup-menu-persistent';
+import * as React from 'react';
+import { ReportReportable } from 'report-reportable';
 
 interface Props {
     beatmapset: BeatmapsetJson;
@@ -14,7 +17,7 @@ export default class BeatmapsetMenu extends React.PureComponent<Props> {
         const children = () => (
             <ReportReportable
                 className='simple-menu__item'
-                icon={true}
+                icon
                 key='report'
                 reportableId={beatmapset.id.toString()}
                 reportableType='beatmapset'
