52662ec9d3267b5819abbe20e7890c0ef2198d3d
bakaneko <notbakaneko@users.noreply.github.com>
2021-04-14 04:21:17 +0900

    undefined works

--- resources/assets/lib/beatmap-discussions/editor-beatmap-selector.tsx
+++ resources/assets/lib/beatmap-discussions/editor-beatmap-selector.tsx
@@ -50,8 +50,7 @@ export default class EditorBeatmapSelector extends React.Component<Props> {
   }
 
   select = (id: string) => {
-    // FIXME: used 0 here because null breaks other things atm (and some other stuff assumes number).
-    const beatmapId = id !== 'all' ? parseInt(id, 10) : 0;
+    const beatmapId = id !== 'all' ? parseInt(id, 10) : undefined;
 
     const path = ReactEditor.findPath(this.context, this.props.element);
     Transforms.setNodes(this.context, {beatmapId}, {at: path});
