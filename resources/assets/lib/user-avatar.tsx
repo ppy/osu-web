e14829af794d5dfb4602038c35dbae419911f648
nanaya <me@nanaya.pro>
2021-04-20 15:48:47 +0900

    Typescript UserAvatar

--- resources/assets/lib/user-avatar.tsx
+++ resources/assets/lib/user-avatar.tsx
@@ -1,16 +1,32 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import * as React from 'react'
-import { div } from 'react-dom-factories'
+import * as React from 'react';
+import { classWithModifiers, Modifiers } from 'utils/css';
 
-bn = 'avatar'
+const bn = 'avatar';
 
-export UserAvatar = (props) ->
-  if props.user.id?
-    div
-      className: osu.classWithModifiers(bn, props.modifiers)
-      style:
-        backgroundImage: osu.urlPresence(props.user.avatar_url)
-  else
-    div className: osu.classWithModifiers(bn, _.concat(props.modifiers, 'guest'))
+interface UserForAvatarJson {
+  avatar_url?: string | null;
+}
+
+interface Props {
+  modifiers?: Modifiers;
+  user: UserForAvatarJson;
+}
+
+export default class UserAvatar extends React.PureComponent<Props> {
+  render() {
+    let blockClass = classWithModifiers(bn, this.props.modifiers);
+
+    const style = {
+      backgroundImage: osu.urlPresence(this.props.user.avatar_url),
+    };
+
+    if (style.backgroundImage == null) {
+      blockClass += classWithModifiers(bn, ['guest'], true);
+    }
+
+    return <div className={blockClass} style={style} />;
+  }
+}
