0b011f902ed886819490362388274b4e78fb908f
Venix <30481900+venix12@users.noreply.github.com>
2021-04-21 18:14:13 +0200

    a missing dot

--- tests/Controllers/InterOp/BeatmapsetsControllerTest.php
+++ tests/Controllers/InterOp/BeatmapsetsControllerTest.php
@@ -1,7 +1,7 @@
 <?php
 
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-// See the LICENCE file in the repository root for full licence text
+// See the LICENCE file in the repository root for full licence text.
 
 namespace Tests\Controllers\InterOp;
 
