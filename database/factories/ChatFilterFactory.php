7883db9a79878d362ebc04ef85d2c235ac2686b5
Venix <30481900+venix12@users.noreply.github.com>
2021-04-25 16:44:22 +0200

    test

--- database/factories/ChatFilterFactory.php
+++ database/factories/ChatFilterFactory.php
@@ -3,13 +3,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-namespace App\Models;
-
-/**
- * @property string $match
- * @property string $replacement
- */
-class ChatFilter extends Model
-{
-    public $timestamps = false;
-}
+$factory->define(App\Models\ChatFilter::class, function (Faker\Generator $faker) {
+    return [
+        'match' => $faker->word,
+        'replacement' => $faker->word,
+    ];
+});
