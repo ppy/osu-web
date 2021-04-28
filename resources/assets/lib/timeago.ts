c93841f112309891ec91a4d3aa1a14880b121300
nanaya <me@nanaya.pro>
2021-04-28 14:52:20 +0900

    Typescript timeago

--- resources/assets/lib/timeago.ts
+++ resources/assets/lib/timeago.ts
@@ -1,17 +1,25 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-class @Timeago
-  constructor: ->
-    @observer = new MutationObserver (mutations) ->
-      # Third-party scripts may init conflicting versions of jquery
-      return unless $.fn.timeago
+export default class Timeago {
+  private observer: MutationObserver;
 
-      mutations.forEach (mutation) ->
-        $nodes = $(mutation.addedNodes)
-        $nodes.find('.js-timeago').add($nodes.filter('.js-timeago')).timeago()
+  constructor() {
+    this.observer = new MutationObserver((mutations) => {
+      // Third-party scripts may init conflicting versions of jquery
+      if ($.fn.timeago == null) return;
 
+      mutations.forEach((mutation) => {
+        $(mutation.addedNodes)
+          .find('.js-timeago')
+          .addBack('.js-timeago')
+          .timeago();
+      });
+    });
 
-    $(document).on 'turbolinks:load', =>
-      $('.js-timeago').timeago()
-      @observer.observe document.body, childList: true, subtree: true
+    $(document).on('turbolinks:load', () => {
+      $('.js-timeago').timeago();
+      this.observer.observe(document.body, { childList: true, subtree: true })
+    });
+  }
+}
