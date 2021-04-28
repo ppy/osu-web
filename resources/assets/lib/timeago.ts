1d02ec2188dacd4bbad80e3dbce56c9605366061
nanaya <me@nanaya.pro>
2021-04-28 14:58:58 +0900

    Simplify applying timeago
    The observer already handles page load.

--- resources/assets/lib/timeago.ts
+++ resources/assets/lib/timeago.ts
@@ -1,25 +1,25 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+const searchQuery = '.js-timeago';
+
 export default class Timeago {
   private observer: MutationObserver;
 
   constructor() {
-    this.observer = new MutationObserver((mutations) => {
-      // Third-party scripts may init conflicting versions of jquery
-      if ($.fn.timeago == null) return;
+    this.observer = new MutationObserver(this.observePage);
+    this.observer.observe(document, { childList: true, subtree: true })
+  }
 
-      mutations.forEach((mutation) => {
-        $(mutation.addedNodes)
-          .find('.js-timeago')
-          .addBack('.js-timeago')
-          .timeago();
-      });
-    });
+  private observePage = (mutations: MutationRecord[]) => {
+    // Third-party scripts may init conflicting versions of jquery
+    if ($.fn.timeago == null) return;
 
-    $(document).on('turbolinks:load', () => {
-      $('.js-timeago').timeago();
-      this.observer.observe(document.body, { childList: true, subtree: true })
+    mutations.forEach((mutation) => {
+      $(mutation.addedNodes)
+        .find(searchQuery)
+        .addBack(searchQuery)
+        .timeago();
     });
   }
 }
