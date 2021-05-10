1ff31f18cf89d84c36d97fc8a27abbef03528346
bakaneko <notbakaneko@users.noreply.github.com>
2021-05-10 17:48:01 +0900

    common notification icon render

--- resources/assets/lib/main-notification-icon.tsx
+++ resources/assets/lib/main-notification-icon.tsx
@@ -3,6 +3,7 @@
 
 import { computed } from 'mobx';
 import { observer } from 'mobx-react';
+import { typeNames } from 'models/notification-type';
 import core from 'osu-core-singleton';
 import * as React from 'react';
 import NotificationIcon from 'notification-icon';
@@ -12,11 +13,14 @@ interface Props {
 }
 
 @observer
-export default class ChatIcon extends React.Component<Props> {
+export default class MainNotificationIcon extends React.Component<Props> {
   @computed
   private get unreadCount() {
-    const count = core.dataStore.notificationStore.unreadStacks.getOrCreateType({ objectType: 'channel' }).total;
+    // TODO: need a better way of propagating the exclusion list to this (but it's global anyway?)
+    const types = typeNames.filter((name) => !(name == null || name === 'channel'));
+    const count = types.reduce((acc, current) => acc + core.dataStore.notificationStore.unreadStacks.getOrCreateType({ objectType: current }).total, 0);
 
+    // combination of latency and delays processing marking as read can cause the display count to go negative.
     return count > 0 ? count : 0;
   }
 
--- resources/assets/lib/notification-icon.tsx
+++ resources/assets/lib/notification-icon.tsx
@@ -1,32 +1,21 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { computed } from 'mobx';
 import { observer } from 'mobx-react';
-import { typeNames } from 'models/notification-type';
 import core from 'osu-core-singleton';
 import * as React from 'react';
 
 interface Props {
   type?: string;
+  count: number;
 }
 
 @observer
 export default class NotificationIcon extends React.Component<Props> {
-  @computed
-  private get unreadCount() {
-    // TODO: need a better way of propagating the exclusion list to this (but it's global anyway?)
-    const types = typeNames.filter((name) => !(name == null || name === 'channel'));
-    const count = types.reduce((acc, current) => acc + core.dataStore.notificationStore.unreadStacks.getOrCreateType({ objectType: current }).total, 0);
-
-    // combination of latency and delays processing marking as read can cause the display count to go negative.
-    return count > 0 ? count : 0;
-  }
-
   render() {
     return (
       <span className={this.mainClass()}>
-        <i className='fas fa-inbox' />
+        <i className='fas fa-comment-alt' />
         <span className='notification-icon__count'>
           {this.unreadCountDisplay()}
         </span>
@@ -37,7 +26,7 @@ export default class NotificationIcon extends React.Component<Props> {
   private mainClass() {
     let ret = 'notification-icon';
 
-    if (this.unreadCount > 0) {
+    if (this.props.count > 0) {
       ret += ' notification-icon--glow';
     }
 
@@ -50,7 +39,7 @@ export default class NotificationIcon extends React.Component<Props> {
 
   private unreadCountDisplay() {
     if (core.notificationsWorker.hasData) {
-      return osu.formatNumber(this.unreadCount);
+      return osu.formatNumber(this.props.count);
     } else {
       return '...';
     }
