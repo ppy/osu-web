14c86ed78cabd151013eb3707554f77487784678
bakaneko <notbakaneko@users.noreply.github.com>
2021-05-14 16:53:32 +0900

    can be just props

--- resources/assets/lib/notification-icon.tsx
+++ resources/assets/lib/notification-icon.tsx
@@ -1,18 +1,16 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { observer } from 'mobx-react';
-import core from 'osu-core-singleton';
 import * as React from 'react';
 import { classWithModifiers } from 'utils/css';
 
 interface Props {
   count: number;
   iconClassName: string;
+  ready: boolean;
   type?: string;
 }
 
-@observer
 export default class NotificationIcon extends React.Component<Props> {
   static defaultProps = { iconClassName: 'fas fa-inbox' };
 
@@ -26,14 +24,14 @@ export default class NotificationIcon extends React.Component<Props> {
       <span className={classWithModifiers('notification-icon', modifiers)}>
         <i className={this.props.iconClassName} />
         <span className='notification-icon__count'>
-          {this.unreadCountDisplay()}
+          {this.unreadCountDisplay}
         </span>
       </span>
     );
   }
 
-  private unreadCountDisplay() {
-    if (core.notificationsWorker.hasData) {
+  private get unreadCountDisplay() {
+    if (this.props.ready) {
       // combination of latency and delays processing marking as read can cause the display count to go negative.
       const count = this.props.count > 0 ? this.props.count : 0;
       return osu.formatNumber(count);
