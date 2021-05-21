bb4785778533ead73aefc1d870b12e1dc38f8398
nanaya <me@nanaya.pro>
2021-05-21 18:35:00 +0900

    Simplify script loading using jquery

--- resources/assets/lib/turbolinks-reload.ts
+++ resources/assets/lib/turbolinks-reload.ts
@@ -1,47 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-const className = 'js-extra-script';
-
 export default class TurbolinksReload {
   private loaded = new Set<string>();
-
-  constructor() {
-    document.addEventListener('turbolinks:before-cache', this.cleanup);
-  }
-
-  cleanup = () => {
-    $(`.${className}`).remove();
-  };
 
   forget = (src: string) => {
     this.loaded.delete(src);
   };
 
-  load = (src: string, onload?: () => void) => {
-    if (this.loaded.has(src)) {
-      return;
-    }
+  load(src: string) {
+    if (this.loaded.has(src)) return;
 
-    const el = document.createElement('script');
-    el.classList.add(className);
-    el.onload = () => {
-      // abort if the element has been removed (on navigation etc)
-      if (el.parentElement == null) {
-        return;
-      }
-
-      el.parentElement.removeChild(el);
-
-      if (onload != null) {
-        onload();
-      }
-    };
-
-    el.src = src;
-    document.body.appendChild(el);
     this.loaded.add(src);
 
-    return true;
-  };
+    return $.ajax(src, { dataType: 'script' }) as JQuery.jqXHR<void>;
+  }
 }
