ebc7f16e18fbad9119961c803f9b5dd7c9553637
nanaya <me@nanaya.pro>
2021-05-28 21:21:42 +0900

    Forgot to lint the new config file

--- config/scribe.php
+++ config/scribe.php
@@ -370,5 +370,5 @@ INTRO
      * For response calls, api resource responses and transformer responses, Scribe will try to start database transactions, so no changes are persisted to your database.
      * Tell Scribe which connections should be transacted here. If you only use the default db connection, you can leave this as is.
      */
-    'database_connections_to_transact' => [config('database.default')]
+    'database_connections_to_transact' => [config('database.default')],
 ];
