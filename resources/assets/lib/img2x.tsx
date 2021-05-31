85e72a4100f87f6e5fd1ba0b3b0058365d372802
bakaneko <notbakaneko@users.noreply.github.com>
2021-05-31 14:26:44 +0900

    convert to typescript

--- resources/assets/lib/img2x.tsx
+++ resources/assets/lib/img2x.tsx
@@ -1,9 +1,9 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import { img } from 'react-dom-factories'
+import * as React from 'react';
+import { make2x } from 'utils/html';
 
-export Img2x = (props) ->
-  allProps = _.extend osu.src2x(props.src), props
-
-  img allProps, props.children
+export default function Img2x(props: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>) {
+  return <img srcSet={`${props.src} 1x, ${make2x(props.src)} 2x`} {...props} />;
+}
