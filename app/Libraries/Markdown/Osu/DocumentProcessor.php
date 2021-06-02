18c05f08b168d0088e5d5d1afaa6f4d4e3ce2b53
clayton <me@clayton.cc>
2021-06-01 17:17:30 -0700

    Easier to read style

--- app/Libraries/Markdown/Osu/DocumentProcessor.php
+++ app/Libraries/Markdown/Osu/DocumentProcessor.php
@@ -278,16 +278,18 @@ class DocumentProcessor
             }
 
             if (OsuWiki::isImage($path)) {
-                return route('wiki.image', compact('path'), false).$matches['query'].$matches['hash'];
-            }
+                $url = route('wiki.image', compact('path'), false);
+            } else {
+                $url = wiki_url(
+                    $path,
+                    $locale ?? $this->wikiLocale ?? config('app.fallback_locale'),
+                    false,
+                    false,
+                );
 
-            if (!isset($locale)) {
-                $locale = $this->wikiLocale ?? config('app.fallback_locale');
-            }
-
-            $url = wiki_url($path, $locale, false, false);
-            if (starts_with($url, $this->wikiAbsoluteRootPath)) {
-                $url = $this->wikiPathToRoot.substr($url, strlen($this->wikiAbsoluteRootPath));
+                if (starts_with($url, $this->wikiAbsoluteRootPath)) {
+                    $url = $this->wikiPathToRoot.substr($url, strlen($this->wikiAbsoluteRootPath));
+                }
             }
 
             return "{$url}{$matches['query']}{$matches['hash']}";
