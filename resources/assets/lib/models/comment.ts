3ec91bb6f2e365d7a335381726b545aa94dff121
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-06-22 14:34:33 +0700

    change deleted by id to number

--- resources/assets/lib/models/comment.ts
+++ resources/assets/lib/models/comment.ts
@@ -11,7 +11,7 @@ export class Comment {
   commentableType: string; // TODO: type?
   createdAt: string;
   deletedAt: string | null;
-  deletedById?: string | null;
+  deletedById?: number | null;
   editedAt: string | null;
   editedById: number | null;
   id: number;
