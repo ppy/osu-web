26eb1613090e8f8ad91fdabed01a1ee9c147a9b0
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-06-24 14:15:34 +0700

    rename gallery contest to tsx

--- resources/assets/lib/gallery-contest.tsx
+++ resources/assets/lib/gallery-contest.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { createElement, Fragment } from 'react';
+import * as React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import GalleryContestVoteButton from './gallery-contest-vote-button';
 import GalleryContestVoteProgress from './gallery-contest-vote-progress';
@@ -13,10 +13,14 @@ export default class GalleryContest {
   constructor(container: HTMLElement, pswp: any) {
     this.root = container.querySelector('.js-pswp-buttons') as HTMLElement;
 
-    const voteButton = createElement(GalleryContestVoteButton, { pswp });
-    const voteProgress = createElement(GalleryContestVoteProgress);
+    render(
+      <>
+        <GalleryContestVoteButton pswp={pswp} />
+        <GalleryContestVoteProgress />
+      </>,
+      this.root,
+    );
 
-    render(createElement(Fragment, null, [voteButton, voteProgress]), this.root);
     this.eventId = `gallery-contest-${osu.uuid()}`;
 
     $(document).on(`turbolinks:before-cache.${this.eventId}`, this.destroy);
