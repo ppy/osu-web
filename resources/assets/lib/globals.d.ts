faf1308b7d3a1bae9677b4767f74cbddc24a3bdf
nanaya <me@nanaya.pro>
2021-06-25 21:15:53 +0900

    Missed cleaning up previous experiment

--- resources/assets/lib/globals.d.ts
+++ resources/assets/lib/globals.d.ts
@@ -96,7 +96,7 @@ interface OsuCommon {
   xhrErrorMessage: (xhr: JQuery.jqXHR) => string;
   formatNumber(num: number, precision?: number, options?: Intl.NumberFormatOptions, locale?: string): string;
   formatNumber(num: null, precision?: number, options?: Intl.NumberFormatOptions, locale?: string): null;
-  parseJson<T = any>(id: string, remove?: boolean, body?: HTMLElement): T;
+  parseJson<T = any>(id: string, remove?: boolean): T;
   updateQueryString(url: string | null, params: { [key: string]: string | null | undefined }): string;
 }
 
