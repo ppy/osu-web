367080682edf8932969a0490df23220776ecd7b7
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-01 15:19:59 +0700

    add beatmap list method

--- resources/assets/lib/beatmapsets-show/beatmap-list.tsx
+++ resources/assets/lib/beatmapsets-show/beatmap-list.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+import Blackout from 'blackout';
 import BeatmapJsonExtended from 'interfaces/beatmap-json-extended';
 import BeatmapsetExtendedJson from 'interfaces/beatmapset-extended-json';
 import * as React from 'react';
@@ -34,6 +35,15 @@ export default class BeatmapList extends React.PureComponent<Props, State> {
     };
   }
 
+  componentDidMount() {
+    $(document).on('click.beatmapList', this.onDocumentClick);
+    $(document).on('turbolinks:before-cache.beatmapList', this.hideSelector);
+  }
+
+  componentWillUnmount() {
+    $(document).off('.beatmapList');
+  }
+
   render() {
     return (
       <div className={classWithModifiers('beatmap-list', { selecting: this.state.showingSelector })}>
@@ -41,6 +51,7 @@ export default class BeatmapList extends React.PureComponent<Props, State> {
           <a
             className='beatmap-list__item beatmap-list__item--selected beatmap-list__item--large js-beatmap-list-selector'
             href={BeatmapDiscussionHelper.url({ beatmap: this.props.currentBeatmap })}
+            onClick={this.toggleSelector}
           >
             <BeatmapListItem
               beatmap={this.props.currentBeatmap}
@@ -70,6 +81,7 @@ export default class BeatmapList extends React.PureComponent<Props, State> {
         })}
         data-id={beatmap.id}
         href={BeatmapDiscussionHelper.url({ beatmap })}
+        onClick={this.selectBeatmap}
       >
         <BeatmapListItem
           beatmap={beatmap}
@@ -79,4 +91,54 @@ export default class BeatmapList extends React.PureComponent<Props, State> {
       </a>
     );
   }
+
+  private hideSelector = () => {
+    if (this.state.showingSelector) {
+      this.setSelector(false);
+    }
+  };
+
+  private onDocumentClick = (e: JQuery.ClickEvent) => {
+    if (e.button !== 0) {
+      return;
+    }
+
+    if ($(e.target).closest('.js-beatmap-list-selector').length > 0) {
+      return;
+    }
+
+    this.hideSelector();
+  };
+
+  private selectBeatmap = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.button !== 0) {
+      return;
+    }
+
+    e.preventDefault();
+
+    $.publish('beatmapsetDiscussions:update', {
+      beatmapId: parseInt(e.currentTarget.dataset.id ?? '', 10),
+      mode: BeatmapDiscussionHelper.DEFAULT_MODE,
+    });
+  };
+
+  private setSelector = (state: boolean) => {
+    if (this.state.showingSelector !== state) {
+      this.setState({ showingSelector: state }, this.syncBlackout);
+    }
+  };
+
+  private syncBlackout = () => {
+    Blackout.toggle(this.state.showingSelector, 0.5);
+  };
+
+  private toggleSelector = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.button !== 0) {
+      return;
+    }
+
+    e.preventDefault();
+    this.setSelector(!this.state.showingSelector);
+  };
 }
