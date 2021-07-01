8b38eab369d6ca2c9b7f4408fe0236ee26f60d30
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-01 11:43:26 +0700

    add typescript beatmap list item

---
+++ resources/assets/lib/beatmapsets-show/beatmap-list-item.tsx
@@ -0,0 +1,51 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import BeatmapJsonExtended from 'interfaces/beatmap-json-extended';
+import * as React from 'react';
+import { classWithModifiers } from 'utils/css';
+
+interface Props {
+  beatmap: BeatmapJsonExtended;
+  count?: number;
+  large: boolean;
+  mode?: string;
+  withButton?: string;
+}
+
+export default class BeatmapListItem extends React.PureComponent<Props> {
+  static defaultProps = {
+    large: false,
+  };
+
+  render() {
+    const deleted = this.props.beatmap.deleted_at !== null;
+    const version = `${this.props.beatmap.version}${deleted ? ` ${osu.trans('beatmap_discussions.index.deleted_beatmap')}` : ''}`;
+
+    return (
+      <div
+        className={classWithModifiers('beatmap-list-item', {
+          deleted,
+          large: this.props.large,
+          [version]: true,
+        })}
+      >
+        <div className='beatmap-list-item__col beatmap-list-item__main'>
+          <div className='u-ellipsis-overflow'>{version}</div>
+        </div>
+
+        {this.props.withButton !== undefined && (
+          <div className='beatmap-list-item__col'>
+            <i className={`fas fa-chevron-${this.props.withButton}`} />
+          </div>
+        )}
+
+        {this.props.count !== undefined && (
+          <div className='beatmap-list-item__col'>
+            <div className='beatmap-list-item__counter'>{this.props.count}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
