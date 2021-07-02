31da25a82ff407096ee199c0f25bcf7216510b89
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-02 13:48:09 +0700

    add icon only for beatmap list item

--- resources/assets/lib/beatmapsets-show/beatmap-list-item.tsx
+++ resources/assets/lib/beatmapsets-show/beatmap-list-item.tsx
@@ -9,6 +9,7 @@ import { classWithModifiers } from 'utils/css';
 interface Props {
   beatmap: BeatmapJsonExtended;
   count?: number;
+  iconOnly: boolean;
   large: boolean;
   mode?: string;
   withButton?: string;
@@ -16,6 +17,7 @@ interface Props {
 
 export default class BeatmapListItem extends React.PureComponent<Props> {
   static defaultProps = {
+    iconOnly: false,
     large: false,
   };
 
@@ -31,31 +33,54 @@ export default class BeatmapListItem extends React.PureComponent<Props> {
       <div
         className={classWithModifiers('beatmap-list-item', {
           deleted,
+          'icon-only': this.props.iconOnly,
           large: this.props.large,
         })}
       >
-        <div className='beatmap-list-item__col'>
+        {this.props.iconOnly
+          ? this.renderIconOnly()
+          : (
+            <>
+              <div className='beatmap-list-item__col'>
+                <BeatmapIcon
+                  beatmap={this.props.beatmap}
+                  modifier={this.props.large ? 'large' : ''}
+                />
+              </div>
+
+              <div className='beatmap-list-item__col beatmap-list-item__col--main'>
+                <div className='u-ellipsis-overflow'>{version}</div>
+              </div>
+
+              {this.props.withButton !== undefined && (
+                <div className='beatmap-list-item__col'>
+                  <i className={`fas fa-chevron-${this.props.withButton}`} />
+                </div>
+              )}
+
+              {this.props.count !== undefined && (
+                <div className='beatmap-list-item__col'>
+                  <div className='beatmap-list-item__counter'>{this.props.count}</div>
+                </div>
+              )}
+            </>
+          )
+        }
+      </div>
+    );
+  }
+
+  renderIconOnly() {
+    return (
+      <div className='beatmap-list-item__icon-only'>
+        {Array.from({ length: 4 }).map((_, idx) => (
           <BeatmapIcon
+            key={idx}
             beatmap={this.props.beatmap}
-            modifier={this.props.large ? 'large' : ''}
+            modifier='beatmap-list-icon-only'
+            showTitle={false}
           />
-        </div>
-
-        <div className='beatmap-list-item__col beatmap-list-item__col--main'>
-          <div className='u-ellipsis-overflow'>{version}</div>
-        </div>
-
-        {this.props.withButton !== undefined && (
-          <div className='beatmap-list-item__col'>
-            <i className={`fas fa-chevron-${this.props.withButton}`} />
-          </div>
-        )}
-
-        {this.props.count !== undefined && (
-          <div className='beatmap-list-item__col'>
-            <div className='beatmap-list-item__counter'>{this.props.count}</div>
-          </div>
-        )}
+        ))}
       </div>
     );
   }
