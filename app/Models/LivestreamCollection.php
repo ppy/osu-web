a3521d7955d85c79622e6777c1ac2b0db3137d61
nanaya <me@nanaya.pro>
2021-07-08 17:58:49 +0900

    Simplify callback definition

--- app/Models/LivestreamCollection.php
+++ app/Models/LivestreamCollection.php
@@ -27,9 +27,7 @@ class LivestreamCollection
             $this->streams = cache_remember_mutexed('livestreams:arr:v2', 300, [], function () {
                 $streams = $this->downloadStreams()['data'] ?? [];
 
-                return array_map(function ($stream) {
-                    return new Twitch\Stream($stream);
-                }, $streams);
+                return array_map(fn ($stream) => new Twitch\Stream($stream), $streams);
             });
         }
 
