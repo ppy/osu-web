980880e7ca37493c7dc4ef5425e05f33bbd798cb
nanaya <me@nanaya.pro>
2021-07-09 16:53:10 +0900

    Lock token for update

--- app/Http/Controllers/Solo/ScoresController.php
+++ app/Http/Controllers/Solo/ScoresController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller as BaseController;
 use App\Libraries\Multiplayer\Mod;
 use App\Models\Solo\Score;
 use App\Models\Solo\ScoreToken;
+use DB;
 
 class ScoresController extends BaseController
 {
@@ -19,44 +20,46 @@ class ScoresController extends BaseController
 
     public function store($beatmapId, $tokenId)
     {
-        $user = auth()->user();
-        $scoreToken = ScoreToken::where([
-            'beatmap_id' => $beatmapId,
-            'user_id' => $user->getKey(),
-        ])->findOrFail($tokenId);
+        $score = DB::transaction(function () use ($beatmapId, $tokenId) {
+            $user = auth()->user();
+            $scoreToken = ScoreToken::where([
+                'beatmap_id' => $beatmapId,
+                'user_id' => $user->getKey(),
+            ])->lockForUpdate()->findOrFail($tokenId);
 
-        // return existing score otherwise (assuming duplicated submission)
-        if ($scoreToken->score_id === null) {
-            $params = get_params(request()->all(), null, [
-                'accuracy:float',
-                'max_combo:int',
-                'mods:array',
-                'passed:bool',
-                'rank:string',
-                'statistics:array',
-                'total_score:int',
-            ]);
+            // return existing score otherwise (assuming duplicated submission)
+            if ($scoreToken->score_id === null) {
+                $params = get_params(request()->all(), null, [
+                    'accuracy:float',
+                    'max_combo:int',
+                    'mods:array',
+                    'passed:bool',
+                    'rank:string',
+                    'statistics:array',
+                    'total_score:int',
+                ]);
 
-            $params = array_merge($params, [
-                'beatmap_id' => $scoreToken->beatmap_id,
-                'ended_at' => now(),
-                'mods' => Mod::parseInputArray($params['mods'] ?? [], $scoreToken->ruleset_id),
-                'ruleset_id' => $scoreToken->ruleset_id,
-                'started_at' => $scoreToken->created_at,
-                'user_id' => $scoreToken->user_id,
-            ]);
+                $params = array_merge($params, [
+                    'beatmap_id' => $scoreToken->beatmap_id,
+                    'ended_at' => now(),
+                    'mods' => Mod::parseInputArray($params['mods'] ?? [], $scoreToken->ruleset_id),
+                    'ruleset_id' => $scoreToken->ruleset_id,
+                    'started_at' => $scoreToken->created_at,
+                    'user_id' => $scoreToken->user_id,
+                ]);
 
-            $score = new Score();
+                $score = new Score();
 
-            $score->getConnection()->transaction(function () use ($params, $score, $scoreToken) {
                 $score->complete($params);
                 $score->createLegacyEntry();
                 $scoreToken->fill(['score_id' => $score->getKey()])->saveOrExplode();
-            });
-        } else {
-            // assume score exists and is valid
-            $score = $scoreToken->score;
-        }
+            } else {
+                // assume score exists and is valid
+                $score = $scoreToken->score;
+            }
+
+            return $score;
+        });
 
         return json_item($score, 'Solo\Score');
     }
