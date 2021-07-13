f9a7b889f6c20513af1329137fa684051ab08575
Nathan Du <nathandu@outlook.com>
2021-07-13 16:50:57 +0800

    clamp difficulty spectrum
    All values before the start of the spectrum are now represented by
    the color for easy. This allows the removal for the gradient stop at
    star rating = 0, along with the negative value check.
    
    Null values are now treated at 0.

--- resources/assets/lib/utils/beatmap-helper.ts
+++ resources/assets/lib/utils/beatmap-helper.ts
@@ -20,8 +20,9 @@ function isVisibleBeatmap(beatmap: BeatmapJson) {
 }
 
 const difficultyColourSpectrum = d3.scaleLinear<string>()
-  .domain([0, 1.5, 2, 2.5, 3.375, 4.625, 5.875, 7, 8])
-  .range(['#4FC0FF', '#4FC0FF', '#4FFFD5', '#7CFF4F', '#F7F05C', '#FF8068', '#FF3C71', '#6563DE', '#18158E', '#18158E'])
+  .domain([1.5, 2, 2.5, 3.375, 4.625, 5.875, 7, 8])
+  .clamp(true)
+  .range(['#4FC0FF', '#4FFFD5', '#7CFF4F', '#F7F05C', '#FF8068', '#FF3C71', '#6563DE', '#18158E', '#18158E'])
   .interpolate(d3.interpolateRgb.gamma(2.2));
 
 interface FindDefaultParams<T> {
@@ -89,8 +90,8 @@ export function getDiffRating(rating: number) {
 }
 
 export function getDiffColour(rating?: number | null) {
-  if (rating == null || rating < 0 || rating > 8) return '#000000';
-  return difficultyColourSpectrum(rating);
+  if (rating ?? 0 > 8) return '#000000';
+  return difficultyColourSpectrum(rating ?? 0);
 }
 
 // TODO: should make a Beatmapset proxy object or something
