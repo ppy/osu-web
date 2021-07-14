ef77d7455efa863e06d7277d6062fac36ece9b58
nanaya <me@nanaya.pro>
2021-07-14 15:11:37 +0900

    Fix incorrect DiscussionsContext typing

--- resources/assets/lib/beatmap-discussions/discussions-context.ts
+++ resources/assets/lib/beatmap-discussions/discussions-context.ts
@@ -3,4 +3,4 @@
 
 import * as React from 'react';
 
-export const DiscussionsContext = React.createContext([] as BeatmapsetDiscussionJson[]);
+export const DiscussionsContext = React.createContext({} as Partial<Record<number, BeatmapsetDiscussionJson>>);
