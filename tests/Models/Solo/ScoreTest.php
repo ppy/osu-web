e84d7505077d73469603dccc1fe95fb5280d726c
Dean Herbert <pe@ppy.sh>
2021-07-19 14:10:32 +0900

    Add test coverage

--- tests/Models/Solo/ScoreTest.php
+++ tests/Models/Solo/ScoreTest.php
@@ -8,6 +8,7 @@ namespace Tests\Models\Solo;
 use App\Exceptions\InvariantException;
 use App\Models\Solo\Score;
 use stdClass;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class ScoreTest extends TestCase
@@ -19,9 +20,62 @@ class ScoreTest extends TestCase
         $this->expectException(InvariantException::class);
 
         $score->createLegacyEntry();
+    }
+
+    public function testLegacyPassScoreRetainsRank()
+    {
+        $score = new Score([
+            'mods' => [],
+            'user_id' => 1,
+            'beatmap_id' => 1,
+            'ruleset_id' => 1,
+        ]);
+
+        $score->complete([
+            'ended_at' => Carbon::now(),
+            'passed' => true,
+            'total_score' => 1000,
+            'max_combo' => 100,
+            'statistics' => ['great' => 10],
+            'accuracy' => 1,
+            'rank' => 'S', // lazer may send an incorrect rank for a failed score.
+        ]);
+
+        $this->assertTrue($score->passed);
+        $this->assertEquals($score->rank, 'S');
+
+        $legacy = $score->createLegacyEntry();
+
+        $this->assertTrue($legacy->perfect);
+        $this->assertEquals($legacy->rank, 'S');
+    }
+
+    public function testLegacyFailScoreIsRankF()
+    {
+        $score = new Score([
+            'mods' => [],
+            'user_id' => 1,
+            'beatmap_id' => 1,
+            'ruleset_id' => 1,
+        ]);
+
+        $score->complete([
+            'ended_at' => Carbon::now(),
+            'passed' => false,
+            'total_score' => 1000,
+            'max_combo' => 100,
+            'statistics' => ['great' => 10],
+            'accuracy' => 1,
+            'rank' => 'S', // lazer may send an incorrect rank for a failed score.
+        ]);
 
         $this->assertFalse($score->passed);
         $this->assertEquals($score->rank, 'F');
+
+        $legacy = $score->createLegacyEntry();
+
+        $this->assertFalse($legacy->perfect);
+        $this->assertEquals($legacy->rank, 'F');
     }
 
     public function testModsPropertyType()
