364a3fb3a4ba0426f43b722f833fc26d6a631e8f
Dean Herbert <pe@ppy.sh>
2021-07-20 18:39:25 +0900

    Fix back-to-front conditional

--- app/Models/Solo/Score.php
+++ app/Models/Solo/Score.php
@@ -106,7 +106,7 @@ class Score extends Model
             'enabled_mods' => ModsHelper::toBitset(array_column($this->mods, 'acronym')),
             'maxcombo' => $this->max_combo,
             'pass' => $this->passed,
-            'perfect' => !$this->passed && $statistics['Miss'] + $statistics['LargeTickMiss'] === 0,
+            'perfect' => $this->passed && $statistics['Miss'] + $statistics['LargeTickMiss'] === 0,
             'rank' => $this->rank,
             'score' => $this->total_score,
             'scorechecksum' => "\0",
