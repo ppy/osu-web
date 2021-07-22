f5f108ad5dbde9e1bac1d8edda3b7b90cc0658cf
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-23 01:02:35 +0700

    initial osu common typescript

---
+++ resources/assets/lib/osu-common.ts
@@ -0,0 +1,6 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+const osuCommon = {
+  isIos: /iPad|iPhone|iPod/.test(navigator.platform),
+};
