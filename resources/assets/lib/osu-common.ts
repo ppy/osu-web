3193842d066e4bf8591fe4032c54b0e670b891ec
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-23 08:15:39 +0700

    add currentUserIsFriendsWith

--- resources/assets/lib/osu-common.ts
+++ resources/assets/lib/osu-common.ts
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+import { find } from 'lodash';
+
 const osuCommon = {
   bottomPage: () => osuCommon.bottomPageDistance() === 0,
   bottomPageDistance: () => {
@@ -18,6 +20,7 @@ const osuCommon = {
 
     return ret;
   },
+  currentUserIsFriendsWith: (userId: number) => find(currentUser.friends, { target_id: userId }),
   isIos: /iPad|iPhone|iPod/.test(navigator.platform),
   urlRegex: /(https?:\/\/((?:(?:[a-z0-9]\.|[a-z0-9][a-z0-9-]*[a-z0-9]\.)*[a-z][a-z0-9-]*[a-z0-9](?::\d+)?)(?:(?:(?:\/+(?:[a-z0-9$_\.\+!\*',;:@&=-]|%[0-9a-f]{2})*)*(?:\?(?:[a-z0-9$_\.\+!\*',;:@&=-]|%[0-9a-f]{2})*)?)?(?:#(?:[a-z0-9$_\.\+!\*',;:@&=/?-]|%[0-9a-f]{2})*)?)?(?:[^\.,:\s])))/ig,
 };
