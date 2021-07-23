9297c0e4923ae9e1c7695992a88dd3b67e31fc3b
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-23 07:57:26 +0700

    add bottomPage and bottomPageDistance

--- resources/assets/lib/osu-common.ts
+++ resources/assets/lib/osu-common.ts
@@ -2,6 +2,11 @@
 // See the LICENCE file in the repository root for full licence text.
 
 const osuCommon = {
+  bottomPage: () => osuCommon.bottomPageDistance() === 0,
+  bottomPageDistance: () => {
+    const body = document.documentElement ?? document.body.parentElement ?? document.body;
+    return (body.scrollHeight - body.scrollTop) - body.clientHeight;
+  },
   isIos: /iPad|iPhone|iPod/.test(navigator.platform),
   urlRegex: /(https?:\/\/((?:(?:[a-z0-9]\.|[a-z0-9][a-z0-9-]*[a-z0-9]\.)*[a-z][a-z0-9-]*[a-z0-9](?::\d+)?)(?:(?:(?:\/+(?:[a-z0-9$_\.\+!\*',;:@&=-]|%[0-9a-f]{2})*)*(?:\?(?:[a-z0-9$_\.\+!\*',;:@&=-]|%[0-9a-f]{2})*)?)?(?:#(?:[a-z0-9$_\.\+!\*',;:@&=/?-]|%[0-9a-f]{2})*)?)?(?:[^\.,:\s])))/ig,
 };
