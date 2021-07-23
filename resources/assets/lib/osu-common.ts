161444ae6bf962c49ce0e2b1f65cd874c154a629
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-23 08:48:12 +0700

    add setHash

--- resources/assets/lib/osu-common.ts
+++ resources/assets/lib/osu-common.ts
@@ -4,6 +4,7 @@
 import GroupJson from 'interfaces/group-json';
 import { find } from 'lodash';
 import * as React from 'react';
+import { currentUrl as getCurrentUrl } from 'utils/turbolinks';
 
 const osuCommon = {
   bottomPage: () => osuCommon.bottomPageDistance() === 0,
@@ -26,5 +27,16 @@ const osuCommon = {
   diffColour: (difficultyRating?: string | null) => ({ '--diff': `var(--diff-${difficultyRating ?? 'default'})` } as React.CSSProperties),
   groupColour: (group?: GroupJson) => ({ '--group-colour': group?.colour ?? 'initial' } as React.CSSProperties),
   isIos: /iPad|iPhone|iPod/.test(navigator.platform),
+  setHash: (newHash: string) => {
+    const currentUrl = getCurrentUrl().href;
+    let newUrl = currentUrl.replace(/#.*/, '');
+    newUrl += newHash;
+
+    if (newUrl === currentUrl) {
+      return;
+    }
+
+    history.replaceState(history.state, '', newUrl);
+  },
   urlRegex: /(https?:\/\/((?:(?:[a-z0-9]\.|[a-z0-9][a-z0-9-]*[a-z0-9]\.)*[a-z][a-z0-9-]*[a-z0-9](?::\d+)?)(?:(?:(?:\/+(?:[a-z0-9$_\.\+!\*',;:@&=-]|%[0-9a-f]{2})*)*(?:\?(?:[a-z0-9$_\.\+!\*',;:@&=-]|%[0-9a-f]{2})*)?)?(?:#(?:[a-z0-9$_\.\+!\*',;:@&=/?-]|%[0-9a-f]{2})*)?)?(?:[^\.,:\s])))/ig,
 };
