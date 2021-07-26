b9bdb092db545bb680843d0b678728485bed1d71
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-26 19:46:43 +0700

    remove unused import

--- resources/assets/lib/beatmap-icon.tsx
+++ resources/assets/lib/beatmap-icon.tsx
@@ -3,7 +3,6 @@
 
 import BeatmapJsonExtended from 'interfaces/beatmap-json-extended';
 import * as _ from 'lodash';
-import * as osu from 'osu-common';
 import * as React from 'react';
 import { getDiffColour, getDiffRating } from 'utils/beatmap-helper';
 
