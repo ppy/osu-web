562eb010d62a1745e30aaaa72c98fbe1a6964d08
nanaya <me@nanaya.pro>
2021-07-27 21:43:50 +0900

    Add random delay

--- resources/assets/lib/utils/connection-delay.ts
+++ resources/assets/lib/utils/connection-delay.ts
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+import { random } from 'lodash';
+
 const initialConnectionDelay = 7500;
 const maxConnectionDelay = 1800000; // 30 minutes
 
@@ -8,7 +10,7 @@ export default class ConnectionDelay {
   private connectionDelay = initialConnectionDelay;
 
   get() {
-    const ret = this.connectionDelay;
+    const ret = this.connectionDelay + random(5000, 20000);
 
     this.connectionDelay = Math.min(this.connectionDelay * 2, maxConnectionDelay);
 
