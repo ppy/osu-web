cf2205c04fe2aa93340e86f8000f3650518f367f
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-29 23:39:13 +0700

    use observable for selectedBeatmapIds

--- resources/assets/lib/beatmap-discussions/love-beatmap-modal.tsx
+++ resources/assets/lib/beatmap-discussions/love-beatmap-modal.tsx
@@ -5,7 +5,7 @@ import { BeatmapsetJson } from 'beatmapsets/beatmapset-json';
 import BeatmapJson from 'interfaces/beatmap-json';
 import GameMode from 'interfaces/game-mode';
 import { route } from 'laroute';
-import { computed } from 'mobx';
+import { computed, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 import { group as groupBeatmaps } from 'utils/beatmap-helper';
@@ -15,18 +15,14 @@ interface Props {
   onClose: () => void;
 }
 
-interface State {
-  selectedBeatmapIds: Set<number>;
-}
-
 @observer
-export default class LoveConfirmation extends React.PureComponent<Props, State> {
+export default class LoveConfirmation extends React.PureComponent<Props> {
+  @observable private selectedBeatmapIds: Set<number>;
+
   constructor(props: Props) {
     super(props);
 
-    this.state = {
-      selectedBeatmapIds: new Set(this.props.beatmapset.beatmaps?.map((beatmap) => beatmap.id) ?? []),
-    };
+    this.selectedBeatmapIds = new Set(this.beatmaps.map((beatmap) => beatmap.id));
   }
 
   @computed
@@ -53,7 +49,7 @@ export default class LoveConfirmation extends React.PureComponent<Props, State> 
         <div className='love-beatmap-modal__row love-beatmap-modal__row--footer'>
           <button
             className='btn-osu-big btn-osu-big--rounded-thin'
-            disabled={this.state.selectedBeatmapIds.size === 0}
+            disabled={this.selectedBeatmapIds.size === 0}
             onClick={this.handleSubmit}
             type='button'
           >
@@ -73,9 +69,9 @@ export default class LoveConfirmation extends React.PureComponent<Props, State> 
   }
 
   private checkIsModeSelected = (mode: GameMode) => {
-    const modeBeatmap = this.groupedBeatmaps.get(mode) ?? [];
-    const isAllSelected = modeBeatmap.every((beatmap) => this.state.selectedBeatmapIds.has(beatmap.id));
-    const isAllUnselected = modeBeatmap.every((beatmap) => !this.state.selectedBeatmapIds.has(beatmap.id));
+    const modeBeatmaps = this.groupedBeatmaps.get(mode) ?? [];
+    const isAllSelected = modeBeatmaps.every((beatmap) => this.selectedBeatmapIds.has(beatmap.id));
+    const isAllUnselected = modeBeatmaps.every((beatmap) => !this.selectedBeatmapIds.has(beatmap.id));
 
     if (!isAllSelected && !isAllUnselected) {
       return null;
@@ -84,38 +80,25 @@ export default class LoveConfirmation extends React.PureComponent<Props, State> 
     return isAllSelected;
   };
 
-  private getModeBeatmapIds = (mode: GameMode) => this.groupedBeatmaps.get(mode)?.map((beatmap) => beatmap.id) ?? [];
-
   private handleCheckboxDifficulty = (e: React.ChangeEvent<HTMLInputElement>) => {
     const beatmapId = parseInt(e.target.value, 10);
 
-    const newSelectedIds = new Set(this.state.selectedBeatmapIds);
-
-    if (this.state.selectedBeatmapIds.has(beatmapId)) {
-      newSelectedIds.delete(beatmapId);
+    if (this.selectedBeatmapIds.has(beatmapId)) {
+      this.selectedBeatmapIds.delete(beatmapId);
     } else {
-      newSelectedIds.add(beatmapId);
+      this.selectedBeatmapIds.add(beatmapId);
     }
-
-    this.setState({ selectedBeatmapIds: newSelectedIds });
   };
 
   private handleCheckboxMode = (e: React.ChangeEvent<HTMLInputElement>) => {
     const mode = e.target.value as GameMode;
-    const modeBeatmapIds = this.getModeBeatmapIds(mode);
-    const newSelectedIds = new Set(this.state.selectedBeatmapIds);
+    const modeBeatmaps = this.groupedBeatmaps.get(mode) ?? [];
 
     if (this.checkIsModeSelected(mode) === true) {
-      modeBeatmapIds.forEach((id) => {
-        newSelectedIds.delete(id);
-      });
+      modeBeatmaps.forEach((beatmap) => this.selectedBeatmapIds.delete(beatmap.id));
     } else {
-      modeBeatmapIds.forEach((id) => {
-        newSelectedIds.add(id);
-      });
+      modeBeatmaps.forEach((beatmap) => this.selectedBeatmapIds.add(beatmap.id));
     }
-
-    this.setState({ selectedBeatmapIds: newSelectedIds });
   };
 
   private handleSubmit = () => {
@@ -123,7 +106,7 @@ export default class LoveConfirmation extends React.PureComponent<Props, State> 
       return;
     }
 
-    if (this.state.selectedBeatmapIds.size === 0) {
+    if (this.selectedBeatmapIds.size === 0) {
       return;
     }
 
@@ -131,7 +114,7 @@ export default class LoveConfirmation extends React.PureComponent<Props, State> 
 
     const url = route('beatmapsets.love', { beatmapset: this.props.beatmapset.id });
     const params = {
-      data: { beatmap_ids: [...this.state.selectedBeatmapIds] },
+      data: { beatmap_ids: [...this.selectedBeatmapIds] },
       method: 'PUT',
     };
 
@@ -173,7 +156,7 @@ export default class LoveConfirmation extends React.PureComponent<Props, State> 
             >
               <label className='osu-switch-v2'>
                 <input
-                  checked={this.state.selectedBeatmapIds.has(beatmap.id)}
+                  checked={this.selectedBeatmapIds.has(beatmap.id)}
                   className='osu-switch-v2__input'
                   onChange={this.handleCheckboxDifficulty}
                   type='checkbox'
