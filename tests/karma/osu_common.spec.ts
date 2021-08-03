c550b378b06ba84e5811634d196ece94886e7018
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-08-03 15:49:07 +0700

    fix missing semicolon

--- tests/karma/osu_common.spec.ts
+++ tests/karma/osu_common.spec.ts
@@ -23,8 +23,8 @@ describe('osu_common', () => {
   describe('locale file loaded in test runner', () => {
     it('should be loaded', () => {
       expect(Lang.has('common.confirmation')).toBe(true);
-    })
-  })
+    });
+  });
 
   describe('groupColour', () => {
     const group: GroupJson = {
@@ -36,26 +36,26 @@ describe('osu_common', () => {
       is_probationary: false,
       name: 'ABC',
       short_name: 'abc',
-    }
+    };
 
     it('should return CSS object with correct colour', () => {
       expect(osu.groupColour(group)).toEqual({
         '--group-colour': '#abcdef',
-      } as React.CSSProperties)
-    })
+      } as React.CSSProperties);
+    });
 
     it('should return CSS object with initial value when colour is null', () => {
       expect(osu.groupColour({ ...group, colour: null })).toEqual({
         '--group-colour': 'initial',
-      } as React.CSSProperties)
-    })
+      } as React.CSSProperties);
+    });
 
     it('should return CSS object with initial value when group is undefined', () => {
       expect(osu.groupColour()).toEqual({
         '--group-colour': 'initial',
-      } as React.CSSProperties)
-    })
-  })
+      } as React.CSSProperties);
+    });
+  });
 
   describe('jsonClone', () => {
     it('should clone object', () => {
@@ -64,7 +64,7 @@ describe('osu_common', () => {
 
       expect(result).toEqual(obj);
       expect(result).not.toBe(obj);
-    })
+    });
 
     it('should clone nested object', () => {
       const obj = { test: { inner: '1234' } };
@@ -72,142 +72,142 @@ describe('osu_common', () => {
 
       expect(result.test).toEqual(obj.test);
       expect(result.test).not.toBe(obj.test);
-    })
+    });
 
     it('should clone null', () => {
       expect(osu.jsonClone(null)).toBe(null);
-    })
+    });
 
     it('should clone undefined to null', () => {
       expect(osu.jsonClone(undefined)).toBe(null);
-    })
-  })
+    });
+  });
 
   describe('linkify', () => {
     const textWithLink = 'Please visit https://link.com';
 
     it('replaces the link with an anchor element', () => {
       expect(osu.linkify(textWithLink)).toBe(
-        `Please visit <a href="https://link.com" rel="nofollow noreferrer">link.com</a>`
-      )
-    })
+        `Please visit <a href="https://link.com" rel="nofollow noreferrer">link.com</a>`,
+      );
+    });
 
     it('replaces the link with an anchor element with target blank', () => {
       expect(osu.linkify(textWithLink, true)).toBe(
-        `Please visit <a href="https://link.com" rel="nofollow noreferrer" target="_blank">link.com</a>`
-      )
-    })
+        `Please visit <a href="https://link.com" rel="nofollow noreferrer" target="_blank">link.com</a>`,
+      );
+    });
 
     it('does not change the string', () => {
       expect(osu.linkify('plain text')).toBe('plain text');
-    })
-  })
+    });
+  });
 
   describe('formatBytes', () => {
     it('cenvert to same value in bytes', () => {
       expect(osu.formatBytes(100)).toBe('100 B');
-    })
+    });
 
     it('convert value to KB', () => {
       expect(osu.formatBytes(1000)).toBe('1.00 KB');
-    })
+    });
 
     it('convert value to MB', () => {
       expect(osu.formatBytes(1000000)).toBe('1.00 MB');
-    })
+    });
 
     it('convert value to KB without precision', () => {
       expect(osu.formatBytes(1000, 0)).toBe('1 KB');
-    })
-  })
+    });
+  });
 
   describe('formatNumber', () => {
     it('format null', () => {
       expect(osu.formatNumber(null)).toBe(null);
-    })
+    });
 
     it('formats the number with correct precision', () => {
       expect(osu.formatNumber(12.345, 2)).toBe('12.35');
-    })
+    });
 
     it('formats the number without precision', () => {
       expect(osu.formatNumber(12.34, 0)).toBe('12');
-    })
-  })
+    });
+  });
 
   describe('presence', () => {
     it('should return same string', () => {
       expect(osu.presence('test')).toBe('test');
-    })
+    });
 
     it('should check for empty string', () => {
       expect(osu.presence('')).toBe(null);
-    })
+    });
 
     it('should check for null', () => {
       expect(osu.presence(null)).toBe(null);
-    })
+    });
 
     it('should check for undefined', () => {
       expect(osu.presence()).toBe(null);
-    })
-  })
+    });
+  });
 
   describe('present', () => {
     it('should check for non empty string', () => {
       expect(osu.present('test')).toBe(true);
-    })
+    });
 
     it('should check for empty string', () => {
       expect(osu.present('')).toBe(false);
-    })
+    });
 
     it('should check for null', () => {
       expect(osu.present(null)).toBe(false);
-    })
+    });
 
     it('should check for undefined', () => {
       expect(osu.present()).toBe(false);
-    })
-  })
+    });
+  });
 
   describe('trans', () => {
     it('returns the translated key', () => {
       expect(osu.trans('common.confirmation')).toBe('Are you sure?');
-    })
+    });
 
     it('should return key for non existed translation', () => {
       expect(osu.trans('common.this_is_not_existed')).toBe('common.this_is_not_existed');
-    })
-  })
+    });
+  });
 
   describe('transArray', () => {
     it('should empty string for empty array', () => {
       expect(osu.transArray([])).toBe('');
-    })
+    });
 
     it('should return correct translation for single item array', () => {
       expect(osu.transArray(['Me'])).toBe('Me');
-    })
+    });
 
     it('should return correct translation for two items array', () => {
       expect(osu.transArray(['Me', 'My Self'])).toBe('Me and My Self');
-    })
+    });
 
     it('should return correct translation for three items array', () => {
       expect(osu.transArray(['Me', 'My Self', 'I'])).toBe('Me, My Self, and I');
-    })
-  })
+    });
+  });
 
   describe('transExists', () => {
     it('check if translation exists', () => {
       expect(osu.transExists('common.confirmation')).toBe(true);
-    })
+    });
 
     it('check if translation does not exist', () => {
       expect(osu.transExists('common.this_is_not_existed')).toBe(false);
-    })
-  })
+    });
+  });
 
   describe('updateQueryString', () => {
     it('should add the new parameter to the query string', () => {
@@ -254,5 +254,5 @@ describe('osu_common', () => {
 
       expect(result).toBe(expected);
     });
-  })
+  });
 });
