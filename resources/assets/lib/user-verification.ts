cdb86379cbe774a790be6852b78971163d71efab
nanaya <me@nanaya.pro>
2021-08-10 20:06:52 +0900

    Sort import

--- resources/assets/lib/user-verification.ts
+++ resources/assets/lib/user-verification.ts
@@ -1,9 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { fadeIn, fadeOut, fadeToggle } from 'utils/fade';
 import { route } from 'laroute';
 import core from 'osu-core-singleton';
+import { fadeIn, fadeOut, fadeToggle } from 'utils/fade';
 import { createClickCallback } from 'utils/html';
 
 interface ReissueCodeJson {
