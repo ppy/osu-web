b460c05716714ffd67f06d6a13ffa1af51e79111
nanaya <me@nanaya.pro>
2021-08-19 18:39:49 +0900

    Convert ValueDisplay to typescript

--- resources/assets/lib/value-display.tsx
+++ resources/assets/lib/value-display.tsx
@@ -1,16 +1,24 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import * as React from 'react'
-import { div } from 'react-dom-factories'
-import { classWithModifiers } from 'utils/css'
+import * as React from 'react';
+import { Modifiers, classWithModifiers } from 'utils/css';
 
-bn = 'value-display'
+const bn = 'value-display';
 
-export ValueDisplay = ({label, value, description, modifiers}) ->
-  div
-    className: classWithModifiers(bn, modifiers)
-    div className: "#{bn}__label", label
-    div className: "#{bn}__value", value
-    if description?
-      div className: "#{bn}__description", description
+interface Props {
+  description?: JSX.Element;
+  label: string;
+  modifiers?: Modifiers;
+  value: string | number | JSX.Element;
+}
+
+export default function ValueDisplay({ description, label, modifiers, value }: Props) {
+  return (
+    <div className={classWithModifiers(bn, modifiers)}>
+      <div className={`${bn}__label`}>{label}</div>
+      <div className={`${bn}__value`}>{value}</div>
+      {description != null && <div className={`${bn}__description`}>{description}</div>}
+    </div>
+  );
+}
