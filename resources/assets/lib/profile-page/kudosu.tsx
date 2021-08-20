f1f65a8267fbf41158a696e237ebd026fd9fe3b4
nanaya <me@nanaya.pro>
2021-08-20 14:52:53 +0900

    Convert profile page Kudosu component to typescript

--- resources/assets/lib/profile-page/kudosu.tsx
+++ resources/assets/lib/profile-page/kudosu.tsx
@@ -1,72 +1,112 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import ExtraHeader from 'profile-page/extra-header'
-import * as React from 'react'
-import { a, div, h3, ul, li, p, span } from 'react-dom-factories'
-import ShowMoreLink from 'show-more-link'
-import { StringWithComponent } from 'string-with-component'
-import { wikiUrl } from 'utils/url'
-import ValueDisplay from 'value-display'
-el = React.createElement
+import KudosuHistoryJson from 'interfaces/kudosu-history-json';
+import UserExtendedJson from 'interfaces/user-json-extended';
+import { route } from 'laroute';
+import ExtraHeader from 'profile-page/extra-header';
+import * as React from 'react';
+import ShowMoreLink from 'show-more-link';
+import { StringWithComponent } from 'string-with-component';
+import TimeWithTooltip from 'time-with-tooltip';
+import { wikiUrl } from 'utils/url';
+import ValueDisplay from 'value-display';
 
-export class Kudosu extends React.Component
-  render: =>
-    div className: 'page-extra',
-      el ExtraHeader, name: @props.name, withEdit: @props.withEdit
+interface Props {
+  name: string;
+  pagination: {
+    recentlyReceivedKudosu: {
+      hasMore: boolean;
+      loading: boolean;
+    };
+  };
+  recentlyReceivedKudosu?: KudosuHistoryJson[];
+  user: UserExtendedJson;
+  withEdit: boolean;
+}
 
-      div className: 'kudosu-box',
-        el ValueDisplay,
-          modifiers: ['kudosu']
-          label: osu.trans('users.show.extra.kudosu.total')
-          value: osu.formatNumber(@props.user.kudosu.total)
-          description:
-            el StringWithComponent,
-              mappings:
-                ':link': a
-                  href: wikiUrl('Kudosu')
-                  key: 'link'
-                  osu.trans 'users.show.extra.kudosu.total_info.link'
-              pattern: osu.trans('users.show.extra.kudosu.total_info._')
+export default class Kudosu extends React.Component<Props> {
+  render() {
+    return (
+      <div className='page-extra'>
+        <ExtraHeader name={this.props.name} withEdit={this.props.withEdit} />
 
-      if @props.recentlyReceivedKudosu?.length
-        ul className: 'profile-extra-entries profile-extra-entries--kudosu',
-          for kudosu in @props.recentlyReceivedKudosu
-            continue if !kudosu.id?
+        <div className='kudosu-box'>
+          <ValueDisplay
+            description={(
+              <StringWithComponent
+                mappings={{
+                  ':link': (
+                    <a key='link' href={wikiUrl('Kudosu')}>
+                      {osu.trans('users.show.extra.kudosu.total_info.link')}
+                    </a>
+                  ),
+                }}
+                pattern={osu.trans('users.show.extra.kudosu.total_info._')}
+              />
+            )}
+            label={osu.trans('users.show.extra.kudosu.total')}
+            modifiers='kudosu'
+            value={osu.formatNumber(this.props.user.kudosu.total)}
+          />
+        </div>
 
-            giver =
-              if kudosu.giver?
-                osu.link kudosu.giver.url, kudosu.giver.username
-              else
-                _.escape osu.trans('users.deleted')
+        {this.props.recentlyReceivedKudosu != null && this.props.recentlyReceivedKudosu.length > 0
+          ? (
+            <ul className='profile-extra-entries profile-extra-entries--kudosu'>
+              {this.props.recentlyReceivedKudosu.map((kudosu) => {
+                if (kudosu.id === null) return null;
 
-            post =
-              if kudosu.post?.url?
-                osu.link kudosu.post?.url, kudosu.post?.title
-              else
-                kudosu.post?.title
+                const textMappings = {
+                  ':amount': (
+                    <strong key='amount' className='profile-extra-entries__kudosu-amount'>
+                      {osu.trans('users.show.extra.kudosu.entry.amount', { amount: Math.abs(kudosu.amount) })}
+                    </strong>
+                  ),
+                  ':giver': kudosu.giver == null
+                    ? osu.trans('users.deleted')
+                    : <a key='giver' href={kudosu.giver.url}>{kudosu.giver.username}</a>,
+                  ':post': kudosu.post.url == null
+                    ? kudosu.post.title
+                    : <a key='post' href={kudosu.post.url}>{kudosu.post.title}</a>,
+                };
 
-            li key: "kudosu-#{kudosu.id}", className: 'profile-extra-entries__item',
-              div className: 'profile-extra-entries__detail',
-                div className: 'profile-extra-entries__text', dangerouslySetInnerHTML:
-                  __html: osu.trans "users.show.extra.kudosu.entry.#{kudosu.model}.#{kudosu.action}",
-                    amount: "<strong class='profile-extra-entries__kudosu-amount'>#{osu.trans 'users.show.extra.kudosu.entry.amount', amount: Math.abs(kudosu.amount)}</strong>"
-                    giver: giver
-                    post: post
-              div className: 'profile-extra-entries__time', dangerouslySetInnerHTML:
-                __html: osu.timeago(kudosu.created_at)
+                return (
+                  <li key={`kudosu-${kudosu.id}`} className='profile-extra-entries__item'>
+                    <div className='profile-extra-entries__detail'>
+                      <div className='profile-extra-entries__text'>
+                        <StringWithComponent
+                          mappings={textMappings}
+                          pattern={osu.trans(`users.show.extra.kudosu.entry.${kudosu.model}.${kudosu.action}`)}
+                        />
+                      </div>
+                    </div>
+                    <div className='profile-extra-entries__time'>
+                      <TimeWithTooltip dateTime={kudosu.created_at} relative />
+                    </div>
+                  </li>
+                );
+              })}
 
-          li className: 'profile-extra-entries__item',
-            el ShowMoreLink,
-              modifiers: ['profile-page', 't-greyseafoam-dark']
-              event: 'profile:showMore'
-              hasMore: @props.pagination.recentlyReceivedKudosu.hasMore
-              loading: @props.pagination.recentlyReceivedKudosu.loading
-              data:
-                name: 'recentlyReceivedKudosu'
-                url: laroute.route 'users.kudosu', user: @props.user.id
-
-      else
-        div
-          className: 'profile-extra-entries profile-extra-entries--kudosu'
-          osu.trans('users.show.extra.kudosu.entry.empty')
+              <li className='profile-extra-entries__item'>
+                <ShowMoreLink
+                  data={{
+                    name: 'recentlyReceivedKudosu',
+                    url: route('users.kudosu', { user: this.props.user.id }),
+                  }}
+                  event='profile:showMore'
+                  hasMore={this.props.pagination.recentlyReceivedKudosu.hasMore}
+                  loading={this.props.pagination.recentlyReceivedKudosu.loading}
+                  modifiers={['profile-page', 't-greyseafoam-dark']}
+                />
+              </li>
+            </ul>
+          ) : (
+            <div className='profile-extra-entries profile-extra-entries--kudosu'>
+              {osu.trans('users.show.extra.kudosu.entry.empty')}
+            </div>
+          )}
+      </div>
+    );
+  }
+}
