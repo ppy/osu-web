62ded91a6b95871a805b8740cac9611c6ca8d369
nanaya <me@nanaya.pro>
2021-08-20 20:36:16 +0900

    Update import path

--- resources/assets/lib/profile-page/extra-page-props.ts
+++ resources/assets/lib/profile-page/extra-page-props.ts
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import UserExtendedJson from 'interfaces/user-json-extended';
+import UserExtendedJson from 'interfaces/user-extended-json';
 
 export type BeatmapsetSection = 'favouriteBeatmapsets' | 'rankedBeatmapsets' | 'lovedBeatmapsets' | 'pendingBeatmapsets' | 'graveyardBeatmapsets';
 
