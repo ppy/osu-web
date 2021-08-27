e4149011b79ea57f5dfd5f4119e8da1909ea0c60
nanaya <me@nanaya.pro>
2021-08-27 16:52:25 +0900

    Actually convert AchievementBadge to TypeScript

--- resources/assets/lib/profile-page/achievement-badge.tsx
+++ resources/assets/lib/profile-page/achievement-badge.tsx
@@ -1,132 +1,162 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import * as React from 'react'
-import Img2x from 'img2x'
-import { div, img } from 'react-dom-factories'
-import { classWithModifiers } from 'utils/css'
-import { nextVal } from 'utils/seq'
+import Img2x from 'img2x';
+import GameMode from 'interfaces/game-mode';
+import * as React from 'react';
+import StringWithComponent from 'string-with-component';
+import TimeWithTooltip from 'time-with-tooltip';
+import { classWithModifiers, Modifiers } from 'utils/css';
+import { nextVal } from 'utils/seq';
 
-el = React.createElement
+interface AchievementJson {
+  description: string;
+  grouping: string;
+  icon_url: string;
+  id: number;
+  instructions: string | null;
+  mode: GameMode;
+  name: string;
+  ordering: number;
+  slug: string;
+}
 
-export class AchievementBadge extends React.PureComponent
-  constructor: (props) ->
-    super props
+interface UserAchievementJson {
+  achieved_at: string;
+  achievement_id: number;
+}
 
-    @tooltip = React.createRef()
+interface Props {
+  achievement: AchievementJson;
+  modifiers: Modifiers;
+  userAchievement?: UserAchievementJson | null;
+}
 
+export default class AchievementBadge extends React.PureComponent<Props> {
+  private readonly tooltip = React.createRef<HTMLDivElement>();
+  private tooltipId = '';
 
-  render: =>
-    @tooltipId = "#{@props.achievement.slug}-#{nextVal()}"
+  render() {
+    this.tooltipId = `${this.props.achievement.slug}-${nextVal()}`;
 
-    badgeClass = classWithModifiers('badge-achievement', @props.modifiers)
-    tooltipBadgeClass = 'badge-achievement badge-achievement--dynamic-height'
+    const lockedModifier = { locked: this.props.userAchievement == null };
+    const badgeClass = classWithModifiers('badge-achievement', this.props.modifiers, lockedModifier);
+    const tooltipBadgeClass = classWithModifiers('badge-achievement', 'dynamic-height',  lockedModifier);
 
-    if !@props.userAchievement?
-      tooltipBadgeClass += ' badge-achievement--locked'
-      badgeClass += ' badge-achievement--locked'
+    return (
+      <div className={`js-tooltip-achievement ${badgeClass}`}>
+        <Img2x
+          alt={this.props.achievement.name}
+          className='badge-achievement__image'
+          onMouseOver={this.onMouseOver}
+          src={this.props.achievement.icon_url}
+        />
 
-    div
-      className: "js-tooltip-achievement #{badgeClass} #{@props.additionalClasses}",
-      el Img2x,
-        alt: @props.achievement.name
-        className: 'badge-achievement__image'
-        onMouseOver: @onMouseOver
-        src: @props.achievement.icon_url
+        <div className='hidden'>
+          <div
+            ref={this.tooltip}
+            className='js-tooltip-achievement--content tooltip-achievement__main'
+          >
+            <div className='tooltip-achievement__badge'>
+              <div className={tooltipBadgeClass}>
+                <Img2x
+                  alt={this.props.achievement.name}
+                  className='badge-achievement__image'
+                  src={this.props.achievement.icon_url}
+                />
+              </div>
+            </div>
 
-      div
-        className: 'hidden'
-        div
-          className: 'js-tooltip-achievement--content tooltip-achievement__main'
-          ref: @tooltip
-          div
-            className: 'tooltip-achievement__badge'
-            div
-              className: tooltipBadgeClass
-              el Img2x,
-                alt: @props.achievement.name
-                className: 'badge-achievement__image'
-                src: @props.achievement.icon_url
-          div
-            className: 'tooltip-achievement__grouping'
-            @props.achievement.grouping
+            <div className='tooltip-achievement__grouping'>
+              {this.props.achievement.grouping}
+            </div>
 
-          div
-            className: "tooltip-achievement__detail-container #{if @props.achievement.instructions? then 'tooltip-achievement__detail-container--hoverable' else ''}"
-            div
-              className: "tooltip-achievement__detail tooltip-achievement__detail--normal"
-              div
-                className: 'tooltip-achievement__name'
-                @props.achievement.name
-              div
-                className: 'tooltip-achievement__description'
-                dangerouslySetInnerHTML:
-                  __html: @props.achievement.description
-            if @props.achievement.instructions?
-              div
-                className: 'tooltip-achievement__detail tooltip-achievement__detail--hover'
-                div
-                  className: 'tooltip-achievement__instructions'
-                  dangerouslySetInnerHTML:
-                    __html: @props.achievement.instructions
+            <div className={classWithModifiers('tooltip-achievement__detail-container', { hoverable: this.props.achievement.instructions != null })}>
+              <div className='tooltip-achievement__detail tooltip-achievement__detail--normal'>
+                <div className='tooltip-achievement__name'>
+                  {this.props.achievement.name}
+                </div>
+                <div
+                  className='tooltip-achievement__description'
+                  dangerouslySetInnerHTML={{ __html: this.props.achievement.description }}
+                />
+              </div>
+              {this.props.achievement.instructions != null && (
+                <div className='tooltip-achievement__detail tooltip-achievement__detail--hover'>
+                  <div
+                    className='tooltip-achievement__instructions'
+                    dangerouslySetInnerHTML={{ __html: this.props.achievement.instructions }}
+                  />
+                </div>
+              )}
+            </div>
 
-          if @props.userAchievement?
-            div
-              className: 'tooltip-achievement__date'
-              dangerouslySetInnerHTML:
-                __html: osu.trans('users.show.extra.achievements.achieved-on', date: @achievementDateElem())
-          else
-            div
-              className: 'tooltip-achievement__date'
-              osu.trans('users.show.extra.achievements.locked')
+            {this.props.userAchievement != null ? (
+              <div className='tooltip-achievement__date'>
+                <StringWithComponent
+                  mappings={{ date: <TimeWithTooltip dateTime={this.props.userAchievement.achieved_at} /> }}
+                  pattern={osu.trans('users.show.extra.achievements.achieved-on')}
+                />
+              </div>
+            ) : (
+              <div className='tooltip-achievement__date'>
+                {osu.trans('users.show.extra.achievements.locked')}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
+  private readonly onMouseOver = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.persist();
+    const elem = event.currentTarget;
 
-  achievementDateElem: =>
-    ret = document.createElement 'span'
-    ret.classList.add 'js-tooltip-time'
-    ret.title = @props.userAchievement.achieved_at
-    ret.textContent = moment(@props.userAchievement.achieved_at).format 'll'
+    if (elem._tooltip === this.tooltipId) return;
 
-    ret.outerHTML
+    if (this.tooltip.current == null) throw new Error('tooltip content is missing');
 
+    const $content = $(this.tooltip.current).clone();
 
-  onMouseOver: (event) =>
-    event.persist()
-    elem = event.currentTarget
+    if (elem._tooltip != null) {
+      elem._tooltip = this.tooltipId;
+      $(elem).qtip('set', { 'content.text': $content });
+      return;
+    }
 
-    return if elem._loadedTooltipId == @tooltipId
+    elem._tooltip = this.tooltipId;
+    const classes = classWithModifiers('qtip tooltip-achievement', { locked: this.props.userAchievement == null });
 
-    $content = $(@tooltip.current).clone()
+    const options = {
+      content: $content,
+      hide: {
+        delay: 200,
+        fixed: true,
+      },
+      overwrite: false,
+      position: {
+        adjust: {
+          scroll: false,
+        },
+        at: 'top center',
+        my: 'bottom center',
+        viewport: $(window),
+      },
+      show: {
+        delay: 200,
+        event: event.type,
+        ready: true,
+      },
+      style: {
+        classes: `${classes} qtip`,
+        tip: {
+          height: 20,
+          width: 30,
+        },
+      },
+    };
 
-    if elem._loadedTooltipId?
-      elem._loadedTooltipId = @tooltipId
-      $(elem).qtip 'set', 'content.text': $content
-      return
-
-    elem._loadedTooltipId = @tooltipId
-    classes = 'qtip tooltip-achievement'
-    classes += ' tooltip-achievement--locked' if !@props.userAchievement?
-
-    options =
-      overwrite: false
-      content: $content
-      position:
-        my: 'bottom center'
-        at: 'top center'
-        viewport: $(window)
-        adjust:
-          scroll: false
-      show:
-        event: event.type
-        ready: true
-        delay: 200
-      hide:
-        fixed: true
-        delay: 200
-      style:
-        classes: classes
-        tip:
-          width: 30
-          height: 20
-
-    $(elem).qtip options, event
+    $(elem).qtip(options, event);
+  };
+}
