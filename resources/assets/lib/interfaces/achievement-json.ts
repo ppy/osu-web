3b03a993da8b90a853da5bf004910be87f18751e
nanaya <me@nanaya.pro>
2021-08-27 19:41:36 +0900

    Apparently half of the stuff here use relative path

--- resources/assets/lib/interfaces/achievement-json.ts
+++ resources/assets/lib/interfaces/achievement-json.ts
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import GameMode from 'interfaces/game-mode';
+import GameMode from './game-mode';
 
 export default interface AchievementJson {
   description: string;
