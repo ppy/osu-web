a992a8169ffb785243c10e8c94aab89ff627919e
nanaya <me@nanaya.pro>
2021-08-27 17:15:10 +0900

    Split achievement popup and icon to their own component (same file)

--- resources/assets/lib/profile-page/achievement-badge.tsx
+++ resources/assets/lib/profile-page/achievement-badge.tsx
@@ -4,9 +4,10 @@
 import Img2x from 'img2x';
 import GameMode from 'interfaces/game-mode';
 import * as React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
 import StringWithComponent from 'string-with-component';
 import TimeWithTooltip from 'time-with-tooltip';
-import { classWithModifiers, Modifiers } from 'utils/css';
+import { classWithModifiers, mergeModifiers, Modifiers } from 'utils/css';
 import { nextVal } from 'utils/seq';
 
 interface AchievementJson {
@@ -21,6 +22,88 @@ interface AchievementJson {
   slug: string;
 }
 
+type AchievementBadgeIconProps = {
+  achievement: AchievementJson;
+  modifiers: Modifiers;
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+function AchievementBadgeIcon(props: AchievementBadgeIconProps) {
+  const { achievement, modifiers, ...divProps } = props;
+
+  return (
+    <div
+      className={classWithModifiers('badge-achievement', modifiers)}
+      {...divProps}
+    >
+      <Img2x
+        alt={achievement.name}
+        className='badge-achievement__image'
+        src={achievement.icon_url}
+      />
+    </div>
+  );
+}
+
+interface AchievementBadgePopupProps {
+  achievement: AchievementJson;
+  userAchievement?: UserAchievementJson | null;
+}
+
+function AchievementBadgePopup({ achievement, userAchievement }: AchievementBadgePopupProps) {
+  return (
+    <>
+      <div className='tooltip-achievement__main'>
+        <div className='tooltip-achievement__badge'>
+          <AchievementBadgeIcon
+            achievement={achievement}
+            modifiers={{
+              'dynamic-height': true,
+              locked: userAchievement == null,
+            }}
+          />
+        </div>
+
+        <div className='tooltip-achievement__grouping'>
+          {achievement.grouping}
+        </div>
+
+        <div className={classWithModifiers('tooltip-achievement__detail-container', { hoverable: achievement.instructions != null })}>
+          <div className='tooltip-achievement__detail tooltip-achievement__detail--normal'>
+            <div className='tooltip-achievement__name'>
+              {achievement.name}
+            </div>
+            <div
+              className='tooltip-achievement__description'
+              dangerouslySetInnerHTML={{ __html: achievement.description }}
+            />
+          </div>
+          {achievement.instructions != null && (
+            <div className='tooltip-achievement__detail tooltip-achievement__detail--hover'>
+              <div
+                className='tooltip-achievement__instructions'
+                dangerouslySetInnerHTML={{ __html: achievement.instructions }}
+              />
+            </div>
+          )}
+        </div>
+
+        {userAchievement != null ? (
+          <div className='tooltip-achievement__date'>
+            <StringWithComponent
+              mappings={{ date: <TimeWithTooltip dateTime={userAchievement.achieved_at} /> }}
+              pattern={osu.trans('users.show.extra.achievements.achieved-on')}
+            />
+          </div>
+        ) : (
+          <div className='tooltip-achievement__date'>
+            {osu.trans('users.show.extra.achievements.locked')}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
 interface UserAchievementJson {
   achieved_at: string;
   achievement_id: number;
@@ -33,79 +116,17 @@ interface Props {
 }
 
 export default class AchievementBadge extends React.PureComponent<Props> {
-  private readonly tooltip = React.createRef<HTMLDivElement>();
   private tooltipId = '';
 
   render() {
     this.tooltipId = `${this.props.achievement.slug}-${nextVal()}`;
 
-    const lockedModifier = { locked: this.props.userAchievement == null };
-    const badgeClass = classWithModifiers('badge-achievement', this.props.modifiers, lockedModifier);
-    const tooltipBadgeClass = classWithModifiers('badge-achievement', 'dynamic-height',  lockedModifier);
-
     return (
-      <div className={`js-tooltip-achievement ${badgeClass}`}>
-        <Img2x
-          alt={this.props.achievement.name}
-          className='badge-achievement__image'
-          onMouseOver={this.onMouseOver}
-          src={this.props.achievement.icon_url}
-        />
-
-        <div className='hidden'>
-          <div
-            ref={this.tooltip}
-            className='js-tooltip-achievement--content tooltip-achievement__main'
-          >
-            <div className='tooltip-achievement__badge'>
-              <div className={tooltipBadgeClass}>
-                <Img2x
-                  alt={this.props.achievement.name}
-                  className='badge-achievement__image'
-                  src={this.props.achievement.icon_url}
-                />
-              </div>
-            </div>
-
-            <div className='tooltip-achievement__grouping'>
-              {this.props.achievement.grouping}
-            </div>
-
-            <div className={classWithModifiers('tooltip-achievement__detail-container', { hoverable: this.props.achievement.instructions != null })}>
-              <div className='tooltip-achievement__detail tooltip-achievement__detail--normal'>
-                <div className='tooltip-achievement__name'>
-                  {this.props.achievement.name}
-                </div>
-                <div
-                  className='tooltip-achievement__description'
-                  dangerouslySetInnerHTML={{ __html: this.props.achievement.description }}
-                />
-              </div>
-              {this.props.achievement.instructions != null && (
-                <div className='tooltip-achievement__detail tooltip-achievement__detail--hover'>
-                  <div
-                    className='tooltip-achievement__instructions'
-                    dangerouslySetInnerHTML={{ __html: this.props.achievement.instructions }}
-                  />
-                </div>
-              )}
-            </div>
-
-            {this.props.userAchievement != null ? (
-              <div className='tooltip-achievement__date'>
-                <StringWithComponent
-                  mappings={{ date: <TimeWithTooltip dateTime={this.props.userAchievement.achieved_at} /> }}
-                  pattern={osu.trans('users.show.extra.achievements.achieved-on')}
-                />
-              </div>
-            ) : (
-              <div className='tooltip-achievement__date'>
-                {osu.trans('users.show.extra.achievements.locked')}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
+      <AchievementBadgeIcon
+        achievement={this.props.achievement}
+        modifiers={mergeModifiers(this.props.modifiers, { locked: this.props.userAchievement == null })}
+        onMouseOver={this.onMouseOver}
+      />
     );
   }
 
@@ -115,9 +136,12 @@ export default class AchievementBadge extends React.PureComponent<Props> {
 
     if (elem._tooltip === this.tooltipId) return;
 
-    if (this.tooltip.current == null) throw new Error('tooltip content is missing');
-
-    const $content = $(this.tooltip.current).clone();
+    const $content = $(renderToStaticMarkup(
+      <AchievementBadgePopup
+        achievement={this.props.achievement}
+        userAchievement={this.props.userAchievement}
+      />,
+    ));
 
     if (elem._tooltip != null) {
       elem._tooltip = this.tooltipId;
