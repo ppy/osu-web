012bbc1269a5c7b010c5d08bdf5a0c1bc879c776
bakaneko <notbakaneko@users.noreply.github.com>
2021-10-01 16:54:20 +0900

    skip trying to mount ranking country filter on spotlights

--- resources/views/rankings/charts.blade.php
+++ resources/views/rankings/charts.blade.php
@@ -2,7 +2,7 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
-@extends('rankings.index')
+@extends('rankings.index', ['hasFilter' => false])
 
 @section('ranking-header')
     <div
