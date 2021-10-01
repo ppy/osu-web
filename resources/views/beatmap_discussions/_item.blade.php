d519bda043857cab134f56188cf6fdf29f0af0b9
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-01 16:11:36 +0700

    remove underline in beatmap discussion thumbnail

--- resources/views/beatmap_discussions/_item.blade.php
+++ resources/views/beatmap_discussions/_item.blade.php
@@ -16,7 +16,7 @@
     <div class="beatmap-discussion beatmap-discussion--single beatmapset-activities__post-grow{{ $discussion->trashed() ? ' beatmap-discussion--deleted' : ''}}">
         <div class="beatmap-discussion-timestamp__icons-container">
             <div class="beatmap-discussion-timestamp__icons">
-                <a href="{{ route('beatmapsets.discussion', $discussion->beatmapset) }}#/{{ $discussion->getKey() }}">
+                <a class="beatmap-discussion-timestamp__link-plain" href="{{ route('beatmapsets.discussion', $discussion->beatmapset) }}#/{{ $discussion->getKey() }}">
                     <img class='beatmapset-cover'
                         src="{{$discussion->beatmapset->coverURL('list')}}"
                         srcSet="{{$discussion->beatmapset->coverURL('list')}} 1x, {{$discussion->beatmapset->coverURL('list@2x')}} 2x">
