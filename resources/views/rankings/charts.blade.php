95af9d8f49b69654a5b84dd054c50ad431dd3d39
bakaneko <notbakaneko@users.noreply.github.com>
2021-10-07 19:59:48 +0900

    still has friends filter;
    commited too many things from testing alternatives :eyes:

--- resources/views/rankings/charts.blade.php
+++ resources/views/rankings/charts.blade.php
@@ -2,7 +2,7 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
-@extends('rankings.index', ['hasFilter' => false])
+@extends('rankings.index')
 
 @section('ranking-header')
     <div
