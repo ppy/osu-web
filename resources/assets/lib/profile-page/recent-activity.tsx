a45edee668c7d339e5d3716abc641ccdf6ee09d7
bakaneko <notbakaneko@users.noreply.github.com>
2021-10-08 20:41:06 +0900

    convert recent-activities to typescript

--- resources/assets/lib/profile-page/recent-activity.tsx
+++ resources/assets/lib/profile-page/recent-activity.tsx
@@ -1,154 +1,207 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import { snakeCase } from 'lodash'
-import AchievementBadge from 'profile-page/achievement-badge'
-import ExtraHeader from 'profile-page/extra-header'
-import * as React from 'react'
-import { a, div, em, li, p, strong, ul } from 'react-dom-factories'
-import ShowMoreLink from 'show-more-link'
-import StringWithComponent from 'string-with-component'
-import TimeWithTooltip from 'time-with-tooltip'
-el = React.createElement
+import { route } from 'laroute';
+import { snakeCase } from 'lodash';
+import AchievementBadge from 'profile-page/achievement-badge';
+import ExtraHeader from 'profile-page/extra-header';
+import * as React from 'react';
+import ShowMoreLink from 'show-more-link';
+import StringWithComponent from 'string-with-component';
+import TimeWithTooltip from 'time-with-tooltip';
 
-export class RecentActivity extends React.PureComponent
-  link = (url, title) ->
-    a
-      className: 'profile-extra-entries__link'
-      href: url
-      title
+interface Event {
+  created_at: string;
+  id: number;
+  parse_error?: boolean;
+  type: string;
+}
 
+interface Props {
+  name: string;
+  pagination: unknown;
+  recentActivity: Event[]
+  withEdit: boolean;
+}
 
-  render: =>
-    div
-      className: 'page-extra'
-      el ExtraHeader, name: @props.name, withEdit: @props.withEdit
+function link(url: string, title: string) {
+  return <a className='profile-extra-entries__link' href={url}>{title}</a>;
+}
 
-      if @props.recentActivity.length
-        div null,
-          ul
-            className: 'profile-extra-entries'
-            @props.recentActivity.map @_renderEntry
-          div
-            className: 'profile-extra-entries__item'
-            el ShowMoreLink,
-              modifiers: ['profile-page', 't-greyseafoam-dark']
-              event: 'profile:showMore'
-              hasMore: @props.pagination.recentActivity.hasMore
-              loading: @props.pagination.recentActivity.loading
-              data:
-                name: 'recentActivity'
-                url: laroute.route 'users.recent-activity', user: @props.user.id
-      else
-        p className: 'profile-extra-entries', osu.trans('events.empty')
+export default class RecentActivity extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div className='page-extra'>
+        <ExtraHeader name={this.props.name} withEdit={this.props.withEdit} />
+        {this.props.recentActivity.length > 0 ? this.renderEntries() : this.renderEmpty()}
+      </div>
+    );
+  }
 
+  private parseEvent(event: Event) {
+    let badge: JSX.Element | null = null;
+    let mappings: Record<string, React.ReactNode> = {};
 
-  _renderEntry: (event) =>
-    return if event.parse_error
+    switch (event.type) {
+      case 'achievement':
+        badge = (
+          <AchievementBadge
+            achievement={event.achievement}
+            modifiers={['recent-activity']}
+            userAchievement={{
+              achieved_at: event.created_at
+              achievement_id: event.achievement.id
+            }}
+          />
+        );
 
-    switch event.type
-      when 'achievement'
-        badge = el AchievementBadge,
-          modifiers: ['recent-activity']
-          achievement: event.achievement
-          userAchievement:
-            achieved_at: event.created_at
-            achievement_id: event.achievement.id
+        mappings = {
+          achievement: <strong>event.achievement.name</strong>,
+          user: <strong><em>{link(event.user.url, event.user.username)}</em></strong>,
+        };
+        break;
 
-        mappings =
-          user:
-            strong null, em null, link(event.user.url, event.user.username)
-          achievement: strong null, event.achievement.name
+      case 'beatmapPlaycount':
+        mappings = {
+          beatmap: link(event.beatmap.url, event.beatmap.title),
+          count: event.count,
+        };
+        break;
 
-      when 'beatmapPlaycount'
-        mappings =
-          beatmap: link(event.beatmap.url, event.beatmap.title)
-          count: event.count
+      case 'beatmapsetApprove':
+        mappings = {
+          approval: osu.trans(`events.beatmapset_status.${event.approval}`),
+          beatmapset: link(event.beatmapset.url, event.beatmapset.title),
+          user: <strong>{link(event.user.url, event.user.username)}</strong>,
+        };
+        break;
 
-      when 'beatmapsetApprove'
-        mappings =
-          approval: osu.trans "events.beatmapset_status.#{event.approval}"
-          beatmapset: link(event.beatmapset.url, event.beatmapset.title)
-          user: strong null, link(event.user.url, event.user.username)
+      case 'beatmapsetDelete':
+        mappings = {
+          beatmapset: event.beatmapset.title,
+        };
+        break;
 
-      when 'beatmapsetDelete'
-        mappings =
-          beatmapset: event.beatmapset.title
+      case 'beatmapsetRevive':
+        mappings = {
+          beatmapset: link(event.beatmapset.url, event.beatmapset.title),
+          user: <strong>{link(event.user.url, event.user.username)}</strong>,
+        };
+        break;
 
-      when 'beatmapsetRevive'
-        mappings =
-          beatmapset: link(event.beatmapset.url, event.beatmapset.title)
-          user: strong null, link(event.user.url, event.user.username)
+      case 'beatmapsetUpdate':
+        mappings = {
+          beatmapset: <em>{ink(event.beatmapset.url, event.beatmapset.title)}</em>,
+          user: <strong><em>{link(event.user.url, event.user.username)}</em></strong>,
+        };
+        break;
 
-      when 'beatmapsetUpdate'
-        mappings =
-          user: strong null, em null, link(event.user.url, event.user.username)
-          beatmapset: em null, link(event.beatmapset.url, event.beatmapset.title)
+      case 'beatmapsetUpload':
+        mappings = {
+          beatmapset: link(event.beatmapset.url, event.beatmapset.title),
+          user: <strong><em>{link(event.user.url, event.user.username)}</em></strong>,
+        };
+        break;
 
-      when 'beatmapsetUpload'
-        mappings =
-          beatmapset: link(event.beatmapset.url, event.beatmapset.title)
-          user: strong null, em null, link(event.user.url, event.user.username)
+      case 'rank':
+        badge = <div className={`score-rank score-rank--${event.scoreRank}`} />
 
-      when 'medal'
-        # shouldn't exist because the type is overridden to achievement.
-        return
+        mappings = {
+          beatmap: <em>{link(event.beatmap.url, event.beatmap.title)}</em>,
+          mode: osu.trans(`beatmaps.mode.${event.mode}`),
+          rank: event.rank,
+          user: <strong><em>{link(event.user.url, event.user.username)}</em></strong>,
+        };
+        break;
 
-      when 'rank'
-        badge = div
-          className: "score-rank score-rank--#{event.scoreRank}"
+      case 'rankLost':
+        mappings = {
+          beatmap: <em>{link(event.beatmap.url, event.beatmap.title)}</em>,
+          mode: osu.trans(`beatmaps.mode.${event.mode}`),
+          rank: event.rank,
+          user: <strong><em>{link(event.user.url, event.user.username)}</em></strong>,
 
-        mappings =
-          user: strong null, em null, link(event.user.url, event.user.username)
-          rank: event.rank
-          beatmap: em null, link(event.beatmap.url, event.beatmap.title)
-          mode: osu.trans "beatmaps.mode.#{event.mode}"
+        };
+        break;
 
-      when 'rankLost'
-        mappings =
-          user: strong null, em null, link(event.user.url, event.user.username)
-          rank: event.rank
-          beatmap: em null, link(event.beatmap.url, event.beatmap.title)
-          mode: osu.trans "beatmaps.mode.#{event.mode}"
+      case 'userSupportAgain':
+        mappings = {
+          user: <strong>{link(event.user.url, event.user.username)}</strong>,
+        };
+        break;
 
-      when 'userSupportAgain'
-        mappings =
-          user: strong null, link(event.user.url, event.user.username)
+      case 'userSupportFirst':
+        mappings = {
+          user: <strong>{link(event.user.url, event.user.username)}</strong>,
+        };
+        break;
 
-      when 'userSupportFirst'
-        mappings =
-          user: strong null, link(event.user.url, event.user.username)
+      case 'userSupportGift':
+        mappings = {
+          user: <strong>{link(event.user.url, event.user.username)}</strong>,
+        };
+        break;
 
-      when 'userSupportGift'
-        mappings =
-          user: strong null, link(event.user.url, event.user.username)
+      case 'usernameChange':
+        mappings = {
+          previousUsername: <strong>{event.user.previousUsername}</strong>,
+          user: <strong><em>{link(event.user.url, event.user.username)}</em></strong>,
+        };
+        break;
+    }
 
-      when 'usernameChange'
-        mappings =
-          user: strong null, em null, link(event.user.url, event.user.username)
-          previousUsername: strong null, event.user.previousUsername
+    return { badge, mappings };
+  }
 
-      else
-        # unkown event
-        return
+  private renderEmpty() {
+    return <p className='profile-extra-entries'>{osu.trans('events.empty')}</p>;
+  }
 
-    li
-      className: 'profile-extra-entries__item'
-      key: event.id
-      div
-        className: 'profile-extra-entries__detail'
-        div
-          className: 'profile-extra-entries__icon'
-          badge ? null
-        div
-          className: 'profile-extra-entries__text'
-          el StringWithComponent,
-            mappings: mappings ? {}
-            # remove strip tags once translations are updated
-            pattern: osu.trans("events.#{snakeCase(event.type)}").replace(/<[^>]*>/g, '')
+  private renderEntries() {
+    return (
+      <div>
+        <ul className='profile-extra-entries'>
+          {this.props.recentActivity.map(this.renderEntry)}
+        </ul>
+        <div className='profile-extra-entries__item'>
+          <ShowMoreLink
+            data={{
+              name: 'recentActivity',
+              url: route('users.recent-activity', { user: this.props.user.id }),
+            }}
+            event='profile:showMore'
+            hasMore={this.props.pagination.recentActivity.hasMore}
+            loading={this.props.pagination.recentActivity.loading}
+            modifiers={['profile-page', 't-greyseafoam-dark']}
 
-      div
-        className: 'profile-extra-entries__time'
-        el TimeWithTooltip,
-          dateTime: event.created_at
-          relative: true
+          />
+        </div>
+      </div>
+    );
+  }
+
+  private renderEntry = (event: Event) => {
+    if (event.parse_error) return null;
+
+    const { badge, mappings } = this.parseEvent(event);
+
+    return (
+      <li key={event.id} className='profile-extra-entries__item'>
+        <div className='profile-extra-entries__detail'>
+          <div className='profile-extra-entries__icon'>
+            {badge ?? null}
+          </div>
+          <div className='profile-extra-entries__text'>
+            <StringWithComponent
+              mappings={mappings ?? {}}
+              pattern={osu.trans(`events.${snakeCase(event.type)}`).replace(/<[^>]*>/g, '')}
+            />
+          </div>
+        </div>
+        <div className='profile-extra-entries__time'>
+          <TimeWithTooltip dateTime={event.created_at} relative />
+        </div>
+      </li>
+    );
+  }
+}
