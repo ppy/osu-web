1eb62b020c8f830c9635b3c4edbbe316916085f6
nanaya <me@nanaya.pro>
2021-10-11 19:55:14 +0900

    Add return type

--- database/factories/UserAccountHistoryFactory.php
+++ database/factories/UserAccountHistoryFactory.php
@@ -13,7 +13,7 @@ class UserAccountHistoryFactory extends Factory
 {
     protected $model = UserAccountHistory::class;
 
-    public function definition()
+    public function definition(): array
     {
         return [
             'reason' => fn () => $this->faker->bs(),
