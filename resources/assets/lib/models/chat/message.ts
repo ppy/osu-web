f275a62fa9a1e910be0f9328cb455402ff813527
bakaneko <notbakaneko@users.noreply.github.com>
2021-10-19 14:25:58 +0900

    skip persist if previously done

--- resources/assets/lib/models/chat/message.ts
+++ resources/assets/lib/models/chat/message.ts
@@ -53,6 +53,7 @@ export default class Message {
 
   @action
   persist(json: MessageJson) {
+    if (this.persisted) return;
     this.messageId = json.message_id;
     this.timestamp = json.timestamp;
     this.persisted = true;
