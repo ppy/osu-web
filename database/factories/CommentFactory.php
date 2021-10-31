3c76ff83f3acf96092dbed7f5c77e381b304537a
clayton <me@clayton.cc>
2021-10-31 01:15:48 -0700

    Convert Comment factory to class and add support for all commentables

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,25 +3,35 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-$factory->define(App\Models\Comment::class, function (Faker\Generator $faker) {
-    return [
-        'user_id' => function () {
-            return App\Models\User::factory()->create()->user_id;
-        },
-        'message' => function () use ($faker) {
-            return $faker->paragraph();
-        },
-        'commentable_type' => function () {
-            return 'build'; // TODO: add support for more types
-        },
-        'commentable_id' => fn () => App\Models\Build::factory()->create()->getKey(),
-        'created_at' => now(),
-        'updated_at' => now(),
-    ];
-});
+namespace Database\Factories;
 
-$factory->state(App\Models\Comment::class, 'deleted', function () {
-    return [
-        'deleted_at' => now(),
-    ];
-});
+use App\Libraries\MorphMap;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    public function definition(): array
+    {
+        return [
+            'commentable_type' => fn () => $this->faker->randomElement(Comment::COMMENTABLES),
+            'created_at' => fn () => $this->faker->dateTime(),
+            'message' => fn () => $this->faker->paragraph(),
+            'user_id' => User::factory(),
+
+            // depends on commentable_type
+            'commentable_id' => fn (array $attr) => MorphMap::getClass($attr['commentable_type'])::factory(),
+
+            // depends on created_at
+            'updated_at' => fn (array $attr) => $attr['created_at'],
+        ];
+    }
+
+    public function deleted(): static
+    {
+        return $this->state(['deleted_at' => fn ($attr) => $attr['created_at']]);
+    }
+}
