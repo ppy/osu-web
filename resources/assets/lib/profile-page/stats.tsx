6058c76dda651d1dd5761efd5ccc4fd9fbb119e7
nanaya <me@nanaya.pro>
2021-11-09 20:28:55 +0900

    Typescript Stats

--- resources/assets/lib/profile-page/stats.tsx
+++ resources/assets/lib/profile-page/stats.tsx
@@ -1,38 +1,45 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import * as React from 'react'
-import { div, dd, dl, dt } from 'react-dom-factories'
-el = React.createElement
+import UserStatisticsJson from 'interfaces/user-statistics-json';
+import * as React from 'react';
 
+// sorted by display order
+const entryKeys = [
+  'ranked_score',
+  'hit_accuracy',
+  'play_count',
+  'total_score',
+  'total_hits',
+  'maximum_combo',
+  'replays_watched_by_others',
+] as const;
 
-export class Stats extends React.PureComponent
-  entries = [
-    'ranked_score'
-    'hit_accuracy'
-    'play_count'
-    'total_score'
-    'total_hits'
-    'maximum_combo'
-    'replays_watched_by_others'
-  ]
+type EntryKey = typeof entryKeys[number];
 
+interface Props {
+  stats: UserStatisticsJson;
+}
 
-  render: =>
-    div className: 'profile-stats', entries.map(@renderEntry)
+export default class Stats extends React.PureComponent<Props> {
+  render() {
+    return <div className='profile-stats'>{entryKeys.map(this.renderEntry)}</div>;
+  }
 
+  private formatValue(key: EntryKey) {
+    const val = this.props.stats[key];
 
-  renderEntry: (key) =>
-    dl
-      className: 'profile-stats__entry'
-      key: key
-      dt className: 'profile-stats__key', osu.trans("users.show.stats.#{key}")
-      dd className: 'profile-stats__value', @formatValue(key)
+    if (key === 'hit_accuracy') {
+      return `${osu.formatNumber(val, 2)}%`;
+    }
 
+    return osu.formatNumber(val);
+  }
 
-  formatValue: (key) =>
-    val = @props.stats[key]
-
-    switch key
-      when 'hit_accuracy' then "#{osu.formatNumber(val, 2)}%"
-      else osu.formatNumber(val)
+  private readonly renderEntry = (key: EntryKey) => (
+    <dl key={key} className='profile-stats__entry'>
+      <dt className='profile-stats__key'>{osu.trans(`users.show.stats.${key}`)}</dt>
+      <dd className='profile-stats__value'>{this.formatValue(key)}</dd>
+    </dl>
+  );
+}
