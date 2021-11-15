31a997942213df3136052384d94861dafb4a329d
Dan Balasescu <smoogipoo@smgi.me>
2021-11-16 01:10:16 +0900

    Add multiplayer queue modes

--- app/Models/Multiplayer/Room.php
+++ app/Models/Multiplayer/Room.php
@@ -35,6 +35,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property \Carbon\Carbon $starts_at
  * @property \Carbon\Carbon|null $updated_at
  * @property int $user_id
+ * @property string $type
+ * @property string $queue_mode
  */
 class Room extends Model
 {
@@ -55,6 +57,9 @@ class Room extends Model
     const PLAYLIST_TYPE = 'playlists';
     const REALTIME_DEFAULT_TYPE = 'head_to_head';
     const REALTIME_TYPES = ['head_to_head', 'team_versus'];
+
+    const REALTIME_DEFAULT_QUEUE_MODE = 'host_only';
+    const REALTIME_QUEUE_MODES = [ 'host_only', 'free_for_all', 'fair_rotate' ];
 
     protected $casts = [
         'password' => PresentString::class,
@@ -325,6 +330,7 @@ class Room extends Model
             'password',
             'playlist:array',
             'type',
+            'queue_mode',
         ], ['null_missing' => true]);
 
         $this->fill([
@@ -332,6 +338,7 @@ class Room extends Model
             'name' => $params['name'],
             'starts_at' => now(),
             'type' => $params['type'],
+            'queue_mode' => $params['queue_mode'],
             'user_id' => $owner->getKey(),
         ]);
 
@@ -341,6 +348,9 @@ class Room extends Model
         if ($this->isRealtime() || $params['category'] === 'realtime') {
             if (!in_array($this->type, static::REALTIME_TYPES, true)) {
                 $this->type = static::REALTIME_DEFAULT_TYPE;
+            }
+            if (!in_array($this->queue_mode, static::REALTIME_QUEUE_MODES, true)) {
+                $this->queue_mode = static::REALTIME_DEFAULT_QUEUE_MODE;
             }
             // only for realtime rooms for now
             $this->password = $params['password'];
