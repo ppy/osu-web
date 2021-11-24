77952ab512e4900ef97b46efaa7d22f1742008da
nanaya <me@nanaya.pro>
2021-11-24 22:23:28 +0900

    Typescript CoverSelector
    Also mobx.

--- resources/assets/coffee/react/profile-page/header.coffee
+++ resources/assets/coffee/react/profile-page/header.coffee
@@ -1,7 +1,7 @@
 # Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 # See the LICENCE file in the repository root for full licence text.
 
-import { CoverSelector } from './cover-selector'
+import CoverSelector from 'profile-page/cover-selector'
 import { DetailMobile } from './detail-mobile'
 import { GameModeSwitcher } from './game-mode-switcher'
 import * as React from 'react'
--- resources/assets/lib/profile-page/cover-selector.tsx
+++ resources/assets/lib/profile-page/cover-selector.tsx
@@ -1,85 +1,105 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import CoverSelection from 'profile-page/cover-selection'
-import * as React from 'react'
-import { div, p } from 'react-dom-factories'
-import { classWithModifiers } from 'utils/css'
-import { nextVal } from 'utils/seq'
-import CoverUploader from 'profile-page/cover-uploader'
-el = React.createElement
+import UserCoverJson from 'interfaces/user-cover-json';
+import { action, observable, makeObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import * as React from 'react';
+import { classWithModifiers } from 'utils/css';
+import { nextVal } from 'utils/seq';
+import CoverSelection from './cover-selection';
+import CoverUploader from './cover-uploader';
 
-export class CoverSelector extends React.PureComponent
-  constructor: (props) ->
-    super props
+type DropOverlayState = 'hover' | '';
+type DropOverlayVisibility = 'hidden' | '';
 
-    @eventId = "users-show-cover-selector-#{nextVal()}"
-    @dropzoneRef = React.createRef()
-    @uploaderRef = React.createRef()
+const coverIndexes = [...new Array(8)].map((_empty, i) => i.toString());
 
-    @state =
-      dropOverlayState: 'inactive'
-      dropOverlayVisibility: 'hidden'
+interface Props {
+  canUpload: boolean;
+  cover: UserCoverJson;
+}
 
+@observer
+export default class CoverSelector extends React.Component<Props> {
+  @observable private dropOverlayState: DropOverlayState = '';
+  @observable private dropOverlayVisibility: DropOverlayVisibility = 'hidden';
+  private readonly dropzoneRef = React.createRef<HTMLDivElement>();
+  private readonly eventId = `users-show-cover-selector-${nextVal()}`;
+  private readonly uploaderRef = React.createRef<CoverUploader>();
 
-  componentDidMount: =>
-    @_removeListeners()
-    @uploaderRef.current.setup()
-    $.subscribe "dragenterGlobal.#{@eventId}", @_dropOverlayStart
-    $.subscribe "dragendGlobal.#{@eventId}", @_dropOverlayEnd
+  constructor(props: Props) {
+    super(props);
 
-  componentWillUnmount: =>
-    @uploaderRef.current.destroy()
-    @_removeListeners()
+    makeObservable(this);
+  }
 
-  _dropOverlayEnter: =>
-    @setState dropOverlayState: 'hover'
+  componentDidMount() {
+    this.uploaderRef.current?.setup();
+    $.subscribe(`dragenterGlobal.${this.eventId}`, this.dropOverlayStart);
+    $.subscribe(`dragendGlobal.${this.eventId}`, this.dropOverlayEnd);
+  }
 
+  componentWillUnmount() {
+    this.uploaderRef.current?.destroy();
+    $.unsubscribe(`.${this.eventId}`);
+  }
 
-  _dropOverlayLeave: =>
-    @setState dropOverlayState: ''
+  render() {
+    return (
+      <div ref={this.dropzoneRef} className='profile-cover-change-popup'>
+        <div className='profile-cover-change-popup__defaults'>
+          {coverIndexes.map((i) =>
+            (<div key={i} className='profile-cover-change-popup__selection'>
+              <CoverSelection
+                isSelected={this.props.cover.id === i}
+                name={i}
+                thumbUrl={`/images/headers/profile-covers/c${i}t.jpg`}
+                url={`/images/headers/profile-covers/c${i}.jpg`}
+              />
+            </div>),
+          )}
+          <p className='profile-cover-change-popup__selections-info'>
+            {osu.trans('users.show.edit.cover.defaults_info')}
+          </p>
+        </div>
+        <CoverUploader
+          ref={this.uploaderRef}
+          canUpload={this.props.canUpload}
+          cover={this.props.cover}
+          dropzoneRef={this.dropzoneRef}
+        />
+        {this.props.canUpload &&
+          <div
+            className={classWithModifiers('profile-cover-change-popup__drop-overlay', this.dropOverlayState)}
+            data-visibility={this.dropOverlayVisibility}
+            onDragEnter={this.dropOverlayEnter}
+            onDragLeave={this.dropOverlayLeave}
+          >
+            {osu.trans('users.show.edit.cover.upload.dropzone')}
+          </div>
+        }
+      </div>
+    );
+  }
 
+  @action
+  private readonly dropOverlayEnd = () => {
+    this.dropOverlayVisibility = 'hidden';
+  };
 
-  _dropOverlayStart: =>
-    @setState dropOverlayVisibility: ''
+  @action
+  private readonly dropOverlayEnter = () => {
+    this.dropOverlayState = 'hover';
+  };
 
+  @action
+  private readonly dropOverlayLeave = () => {
+    this.dropOverlayState = '';
+  };
 
-  _dropOverlayEnd: =>
-    @setState dropOverlayVisibility: 'hidden'
-
-
-  _removeListeners: ->
-    $.unsubscribe ".#{@eventId}"
-
-
-  render: =>
-    dropOverlayClass = 'profile-cover-change-popup__drop-overlay'
-
-    div
-      className: 'profile-cover-change-popup'
-      ref: @dropzoneRef
-      div className: 'profile-cover-change-popup__defaults',
-        for i in [1..8]
-          i = i.toString()
-          div
-            className: 'profile-cover-change-popup__selection'
-            key: i
-            el CoverSelection,
-              name: i
-              isSelected: @props.cover.id == i
-              url: "/images/headers/profile-covers/c#{i}.jpg"
-              thumbUrl: "/images/headers/profile-covers/c#{i}t.jpg"
-        p className: 'profile-cover-change-popup__selections-info',
-          osu.trans 'users.show.edit.cover.defaults_info'
-      el CoverUploader,
-        cover: @props.cover,
-        canUpload: @props.canUpload
-        dropzoneRef: @dropzoneRef
-        ref: @uploaderRef
-      if @props.canUpload
-        div
-          className: classWithModifiers('profile-cover-change-popup__drop-overlay', [@state.dropOverlayState])
-          'data-visibility': @state.dropOverlayVisibility
-          onDragEnter: @_dropOverlayEnter
-          onDragLeave: @_dropOverlayLeave
-          osu.trans 'users.show.edit.cover.upload.dropzone'
+  @action
+  private readonly dropOverlayStart = () => {
+    this.dropOverlayVisibility = '';
+  };
+}
