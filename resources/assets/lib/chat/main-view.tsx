f99e8e35df20e7e86f475cb1b17f55d766ce8b1b
nanaya <me@nanaya.pro>
2021-12-01 19:57:47 +0900

    Add missing makeObservable calls

--- resources/assets/lib/chat/main-view.tsx
+++ resources/assets/lib/chat/main-view.tsx
@@ -3,7 +3,7 @@
 
 import HeaderV4 from 'header-v4';
 import Img2x from 'img2x';
-import { action } from 'mobx';
+import { action, makeObservable } from 'mobx';
 import { observer, Provider } from 'mobx-react';
 import * as React from 'react';
 import RootDataStore from 'stores/root-data-store';
@@ -17,6 +17,12 @@ interface Props {
 
 @observer
 export default class MainView extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+
+    makeObservable(this);
+  }
+
   @action
   componentDidMount() {
     $('html').addClass('osu-layout--mobile-app');
