5193b925c20ddeed917fa05645eaba6d88091ce1
LavaDesu <me@lava.moe>
2021-12-05 22:32:49 +0700

    Add missing licence headers

--- app/GraphQL/Directives/CostDirective.php
+++ app/GraphQL/Directives/CostDirective.php
@@ -1,5 +1,8 @@
 <?php
 
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
 namespace App\GraphQL\Directives;
 
 use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
