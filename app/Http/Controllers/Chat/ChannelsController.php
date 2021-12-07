35f647abf3fbbdb1a6bbc6bfa236555dadee3196
bakaneko <notbakaneko@users.noreply.github.com>
2021-12-07 14:15:32 +0900

    update docs

--- app/Http/Controllers/Chat/ChannelsController.php
+++ app/Http/Controllers/Chat/ChannelsController.php
@@ -204,6 +204,7 @@ class ChannelsController extends Controller
     /**
      * Create Channel
      *
+     * TODO: description needs fixing.
      * This endpoint creates a new channel if doesn't exist and joins it.
      * Currently only for rejoining existing PM channels which the user has left.
      *
@@ -212,11 +213,14 @@ class ChannelsController extends Controller
      * ### Response Format
      *
      * Returns [ChatChannel](#chatchannel) with `recent_messages` attribute.
-     * Note that if there's no existing PM channel, most of the fields will be blank.
+     * Note that in the case of `PM`s, if there's no existing PM channel, most of the fields will be blank.
      * In that case, [send a message](#create-new-pm) instead to create the channel.
      *
-     * @bodyParam type string required channel type (currently only supports "PM")
-     * @bodyParam target_id integer target user id for type PM
+     * @bodyParam channel object channel details; required if `type` is `ANNOUNCE`.
+     * @bodyParam message string message to send with the announcement; required if `type` is `ANNOUNCE`.
+     * @bodyParam target_id integer target user id; required if `type` is `PM`; ignored, otherwise.
+     * @bodyParam target_ids integer[] target user ids; required if `type` is `PM`; ignored, otherwise.
+     * @bodyParam type string required channel type (currently only supports `PM` and `ANNOUNCE`)
      *
      * @response {
      *   "channel_id": 1,
