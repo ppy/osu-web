fed202b3d65a027f4f4798b29720e27618543bc7
LavaDesu <me@lava.moe>
2021-12-09 00:38:33 +0700

    Add GraphQL tests

---
+++ tests/GraphQL/DirectivesTest.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Tests\GraphQL;
+
+use App\GraphQL\ErrorCodes;
+use App\Models\User;
+use GraphQL\Validator\Rules\QueryComplexity;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Nuwave\Lighthouse\Testing\MocksResolvers;
+use Nuwave\Lighthouse\Testing\UsesTestSchema;
+use Tests\TestCase;
+
+class DirectivesTest extends TestCase
+{
+    use MakesGraphQLRequests;
+    use MocksResolvers;
+    use UsesTestSchema;
+
+    protected $viewer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config()->set('lighthouse.cache.enable', false);
+        $this->setUpTestSchema();
+        $this->viewer = User::factory()->create();
+    }
+
+    public function testCostDirective(): void
+    {
+        config()->set('lighthouse.security.max_query_complexity', 1);
+
+        $this->schema = /** @lang GraphQL */ '
+        enum Animal {
+            dog
+            cat
+        }
+        type Object {
+            str2: String
+            enum: Animal
+        }
+        type Query {
+            str1: String @cost(complexity: 5)
+            obj: Object
+        }
+        ';
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            str1
+        }
+        ')->assertGraphQLErrorMessage(QueryComplexity::maxQueryComplexityErrorMessage(1, 5));
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            obj {
+                str2
+                enum
+            }
+        }
+        ')->assertGraphQLErrorMessage(QueryComplexity::maxQueryComplexityErrorMessage(1, 4));
+    }
+
+    public function testOauthDirective(): void
+    {
+        $this->mockResolver('bar');
+        $this->schema = /** @lang GraphQL */ '
+        type Query {
+            foo: String @oauth @mock
+        }
+        ';
+
+        // Test without auth
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')
+            ->assertJsonCount(1, 'errors')
+            ->assertJsonPath('errors.0.extensions.code', ErrorCodes::AUTH_MISSING_TOKEN[0]);
+
+        // Test with auth
+        $this->actAsScopedUser($this->viewer);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')->assertJsonPath('data.foo', 'bar');
+    }
+
+    public function testRestrictScopes(): void
+    {
+        $this->mockResolver('bar');
+        $this->schema = /** @lang GraphQL */ '
+        type Query {
+            foo: String! @restrict(scopes: ["fakeScope1", "fakeScope2"]) @mock
+        }
+        ';
+
+        // Test without scope
+        $this->actAsScopedUser($this->viewer, ['fakeFakeScope']);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')
+            ->assertJsonCount(1, 'errors')
+            ->assertJsonPath('errors.0.extensions.code', ErrorCodes::AUTH_MISSING_SCOPES[0])
+            ->assertJsonPath('errors.0.extensions.missingScopes', ['fakeScope1', 'fakeScope2']);
+
+        // Test with one scope
+        $this->actAsScopedUser($this->viewer, ['fakeScope1']);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')
+            ->assertJsonCount(1, 'errors')
+            ->assertJsonPath('errors.0.extensions.code', ErrorCodes::AUTH_MISSING_SCOPES[0])
+            ->assertJsonPath('errors.0.extensions.missingScopes', ['fakeScope2']);
+
+        // Test with both scopes
+        $this->actAsScopedUser($this->viewer, ['fakeScope1', 'fakeScope2']);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')->assertJsonPath('data.foo', 'bar');
+    }
+
+    public function testRestrictUser(): void
+    {
+        $user = User::factory()->create();
+        $this->mockResolver($user);
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            user_id: Int! @restrict(isCurrentUser: true)
+        }
+        type Query {
+            user: User! @mock
+        }
+        ';
+
+        // Test as non-user
+        $this->actAsScopedUser($this->viewer);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            user {
+                user_id
+            }
+        }
+        ')
+            ->assertJsonCount(1, 'errors')
+            ->assertJsonPath('errors.0.extensions.code', ErrorCodes::AUTH_OWNER_ONLY[0]);
+
+        // Test as user
+        $this->actAsScopedUser($user);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            user {
+                user_id
+            }
+        }
+        ')->assertJsonPath('data.user.user_id', $user->user_id);
+    }
+
+    public function testRestrictSupporterRequirement(): void
+    {
+        $userNoSupporter = User::factory()->create();
+        $userWithSupporter = User::factory()->create([ 'osu_subscriber' => true ]);
+
+        $this->mockResolver('bar');
+        $this->schema = /** @lang GraphQL */ '
+        type Query {
+            foo: String! @restrict(requiresSupporter: true) @mock
+        }
+        ';
+
+        // Test as user with no supporter
+        $this->actAsScopedUser($userNoSupporter);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')
+            ->assertJsonCount(1, 'errors')
+            ->assertJsonPath('errors.0.extensions.code', ErrorCodes::AUTH_SUPPORTER_REQUIRED[0]);
+
+        // Test as user with supporter
+        $this->actAsScopedUser($userWithSupporter);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ')->assertJsonPath('data.foo', 'bar');
+    }
+}
---
+++ tests/GraphQL/SchemaTest.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Tests\GraphQL;
+
+use App\Models\Beatmap;
+use App\Models\Changelog;
+use App\Models\Group;
+use App\Models\NewsPost;
+use App\Models\User;
+use GraphQL\Type\Introspection;
+use Nuwave\Lighthouse\Testing\ClearsSchemaCache;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Tests\TestCase;
+
+class SchemaTest extends TestCase
+{
+    use ClearsSchemaCache;
+    use MakesGraphQLRequests;
+
+    /** @var \App\Models\User $viewer */
+    protected $viewer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config()->set('lighthouse.cache.enable', false);
+        $this->bootClearsSchemaCache();
+        $this->introspect();
+        $this->viewer = User::factory()->create();
+    }
+
+    /** @return string[] */
+    protected function getLeafsFromType(string $type): array
+    {
+        Introspection::getIntrospectionQuery();
+        $types = $this->introspectType($type);
+        $fields = $types['fields'];
+        $result = [];
+        foreach ($fields as $field) {
+            $type = $field['type'];
+            while (true) {
+                switch ($type['kind']) {
+                    case 'NON_NULL':
+                    case 'LIST':
+                        $type = $type['ofType'];
+                        break 1;
+                    case 'SCALAR':
+                    case 'ENUM':
+                        $result[] = $field['name'];
+                        break 2;
+                    default:
+                        break 2;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function testQueryBeatmap(): void
+    {
+        $this->actAsScopedUser($this->viewer);
+        /** @var \App\Models\Beatmap $beatmap */
+        $beatmap = Beatmap::factory()->create();
+
+        $fields = $this->getLeafsFromType('Beatmap');
+
+        $this->graphQL(/** @lang GraphQL */ '
+        query ($id: Int!) {
+            beatmap(id: $id) {
+                '.implode(' ', $fields).'
+            }
+        }
+        ', [
+            'id' => $beatmap->beatmap_id,
+        ])->assertJsonPath('data.beatmap.id', $beatmap->beatmap_id);
+    }
+
+    public function testQueryUser(): void
+    {
+        $this->actAsScopedUser($this->viewer);
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $fields = $this->getLeafsFromType('User');
+
+        $this->graphQL(/** @lang GraphQL */ '
+        query ($id: Int!) {
+            user(id: $id) {'.implode(' ', $fields).'}
+        }
+        ', [
+            'id' => $user->user_id,
+        ])->assertJsonPath('data.user.id', $user->user_id);
+    }
+
+    public function testQueryMe(): void
+    {
+        $this->actAsScopedUser($this->viewer);
+        $this->graphQL(/** @lang GraphQL */ '
+        query {
+            me {
+                id
+            }
+        }
+        ')->assertJsonPath('data.me.id', $this->viewer->user_id);
+    }
+
+    public function testQueryGroup(): void
+    {
+        /** @var \App\Models\Group $group */
+        $group = factory(Group::class)->create();
+
+        $fields = $this->getLeafsFromType('Group');
+
+        $this->actAsScopedUser($this->viewer);
+        $this->graphQL(/** @lang GraphQL */ '
+        query ($id: Int!) {
+            group(id: $id) {'.implode(' ', $fields).'}
+        }
+        ', [
+            'id' => $group->group_id,
+        ])->assertJsonPath('data.group.id', $group->group_id);
+    }
+
+    public function testQueryChangelog(): void
+    {
+        /** @var \App\Models\Changelog $changelog */
+        $changelog = factory(Changelog::class)->create();
+
+        $fields = $this->getLeafsFromType('Changelog');
+
+        $this->graphQL(/** @lang GraphQL */ '
+        query ($id: Int!) {
+            changelog(id: $id) {'.implode(' ', $fields).'}
+        }
+        ', [
+            'id' => $changelog->changelog_id,
+        ])->assertJsonPath('data.changelog.id', $changelog->changelog_id);
+    }
+
+    public function testQueryNewsPost(): void
+    {
+        /** @var \App\Models\NewsPost $post */
+        $post = NewsPost::factory()->create();
+
+        $fields = $this->getLeafsFromType('NewsPost');
+
+        $this->graphQL(/** @lang GraphQL */ '
+        query ($id: Int!) {
+            newsPost(id: $id) {'.implode(' ', $fields).'}
+        }
+        ', [
+            'id' => $post->id,
+        ])->assertJsonPath('data.newsPost.id', $post->id);
+    }
+}
