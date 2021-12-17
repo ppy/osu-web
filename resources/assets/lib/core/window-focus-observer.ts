1831203aa145331f13a7de60361f41c3f8a54a7a
bakaneko <notbakaneko@users.noreply.github.com>
2021-12-17 14:16:06 +0900

    missing action

--- resources/assets/lib/core/window-focus-observer.ts
+++ resources/assets/lib/core/window-focus-observer.ts
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
 
 export default class WindowFocusObserver {
   @observable hasFocus = document.hasFocus();
@@ -9,6 +9,6 @@ export default class WindowFocusObserver {
   constructor() {
     makeObservable(this);
 
-    $(window).on('blur focus', () => this.hasFocus = document.hasFocus());
+    $(window).on('blur focus', action(() => this.hasFocus = document.hasFocus()));
   }
 }
