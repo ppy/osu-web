379e8bc55441da8d7c0b534b3629308edb3c92a6
LavaDesu <me@lava.moe>
2021-12-19 16:09:05 +0700

    Remove throttle-graphql middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,6 @@ class Kernel extends HttpKernel
         'guest' => Middleware\RedirectIfAuthenticated::class,
         'require-scopes' => Middleware\RequireScopes::class,
         'throttle' => Middleware\ThrottleRequests::class,
-        'throttle-graphql' => \App\GraphQL\Middleware\WrappedThrottleRequests::class,
         'verify-user' => Middleware\VerifyUser::class,
     ];
 }
