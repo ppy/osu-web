ab2cc33373d6ff14da6604a091b1e87d9cbd4b80
clayton <me@clayton.cc>
2022-01-27 12:32:45 -0800

    Get user and group models without helper method in UserGroupsController

--- app/Http/Controllers/InterOp/UserGroupsController.php
+++ app/Http/Controllers/InterOp/UserGroupsController.php
@@ -10,45 +10,31 @@ use App\Models\User;
 
 class UserGroupsController extends Controller
 {
-    public function update()
+    public function update($userId, $groupId)
     {
-        [$user, $group] = $this->getUserAndGroupModels();
-        $playmodes = get_arr(request()->input('playmodes'), 'get_string');
-
-        $user->addToGroup($group, $playmodes);
-
-        return response(null, 204);
-    }
-
-    public function destroy()
-    {
-        [$user, $group] = $this->getUserAndGroupModels();
-
-        $user->removeFromGroup($group);
-
-        return response(null, 204);
-    }
-
-    public function setDefault()
-    {
-        [$user, $group] = $this->getUserAndGroupModels();
-
-        $user->setDefaultGroup($group);
-
-        return response(null, 204);
-    }
-
-    private function getUserAndGroupModels()
-    {
-        $params = array_merge(
-            ['group' => get_int(request()->input('group_id'))],
-            request()->route()->parameters(),
+        User::findOrFail($userId)->addToGroup(
+            app('groups')->byIdOrFail($groupId),
+            get_arr(request()->input('playmodes'), 'get_string'),
         );
-        $group = app('groups')->byIdOrFail($params['group'] ?? null);
 
-        return [
-            User::findOrFail($params['user'] ?? null),
-            $group,
-        ];
+        return response(null, 204);
+    }
+
+    public function destroy($userId, $groupId)
+    {
+        User::findOrFail($userId)->removeFromGroup(
+            app('groups')->byIdOrFail($groupId),
+        );
+
+        return response(null, 204);
+    }
+
+    public function setDefault($userId)
+    {
+        User::findOrFail($userId)->setDefaultGroup(
+            app('groups')->byIdOrFail(request()->input('group_id')),
+        );
+
+        return response(null, 204);
     }
 }
