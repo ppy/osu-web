68d1f9b5f7c894bdea456bf2ea16f22c95813be9
bakaneko <notbakaneko@users.noreply.github.com>
2022-01-28 21:19:53 +0900

    not really accepting options

--- app/Libraries/Markdown/CustomContainerInline/Element.php
+++ app/Libraries/Markdown/CustomContainerInline/Element.php
@@ -10,22 +10,13 @@ use League\CommonMark\Node\Inline\DelimitedInterface;
 
 class Element extends AbstractInline implements DelimitedInterface
 {
-    private string $delimiter;
-
-    public function __construct(string $delimiter = ':')
-    {
-        parent::__construct();
-
-        $this->delimiter = $delimiter;
-    }
-
     public function getOpeningDelimiter(): string
     {
-        return $this->delimiter;
+        return ':';
     }
 
     public function getClosingDelimiter(): string
     {
-        return $this->delimiter;
+        return ':';
     }
 }
