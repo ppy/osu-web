603f05b6ed8df21faa65151494cc49e2cf6e523a
nanaya <me@nanaya.pro>
2022-01-31 20:32:13 +0900

    Fix lint

--- resources/assets/lib/gallery-contest.tsx
+++ resources/assets/lib/gallery-contest.tsx
@@ -1,11 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+import GalleryContestVoteButton from 'components/gallery-contest-vote-button';
+import GalleryContestVoteProgress from 'components/gallery-contest-vote-progress';
 import * as React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { nextVal } from 'utils/seq';
-import GalleryContestVoteButton from 'components/gallery-contest-vote-button';
-import GalleryContestVoteProgress from 'components/gallery-contest-vote-progress';
 
 export default class GalleryContest {
   private eventId: string;
