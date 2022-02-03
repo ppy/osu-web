40d53b2a54125fa392f7b0f7c4ea7498dd563d72
bakaneko <notbakaneko@users.noreply.github.com>
2022-02-03 19:12:44 +0900

    include discussion starter in notification

--- app/Jobs/Notifications/BeatmapsetDiscussionPostNotification.php
+++ app/Jobs/Notifications/BeatmapsetDiscussionPostNotification.php
@@ -46,7 +46,14 @@ abstract class BeatmapsetDiscussionPostNotification extends BroadcastNotificatio
 
     public function getListeningUserIds(): array
     {
-        return $this->beatmapsetDiscussionPost->beatmapset->watches()->pluck('user_id')->all();
+        $userIds = $this->beatmapsetDiscussionPost->beatmapset->watches()->pluck('user_id');
+
+        $discussion = $this->beatmapsetDiscussionPost->beatmapDiscussion;
+        if ($discussion->canBeResolved() && !$discussion->resolved) {
+            $userIds->push($discussion->user_id);
+        }
+
+        return $userIds->all();
     }
 
     public function getNotifiable()
