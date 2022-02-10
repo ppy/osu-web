cafdffd6c8209c0cf56843da34e3020a9376317b
nanaya <me@nanaya.pro>
2022-02-10 16:52:00 +0900

    Css based placeholder hiding

--- resources/views/forum/topics/_post_edit_form.blade.php
+++ resources/views/forum/topics/_post_edit_form.blade.php
@@ -26,7 +26,7 @@
     <div class="{{ $type === 'reply' ? 'js-forum-reply-write' : '' }} bbcode-editor__content">
         @if ($type === 'create')
             <input
-                class="bbcode-editor__input-title js-form-placeholder-hide"
+                class="bbcode-editor__input-title u-focus-hides-placeholder"
                 placeholder="{{ osu_trans("forum.topic.create.placeholder.title") }}"
                 name="title"
             />
