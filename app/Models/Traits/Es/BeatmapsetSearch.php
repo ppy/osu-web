5cce9ee1491f4143b5d66fddcd83f2b90b86e0a7
bakaneko <notbakaneko@users.noreply.github.com>
2022-02-14 20:12:11 +0900

    add missing mapping (and excuse to replace switch)

--- app/Models/Traits/Es/BeatmapsetSearch.php
+++ app/Models/Traits/Es/BeatmapsetSearch.php
@@ -50,7 +50,11 @@ trait BeatmapsetSearch
 
         $values = [];
         foreach ($mappings as $field => $mapping) {
-            $value = $this[$field];
+            $value = match ($field) {
+                'id' => $this->getKey(),
+                default => $this->$field,
+            };
+
             if ($value instanceof Carbon) {
                 $value = $value->toIso8601String();
             }
