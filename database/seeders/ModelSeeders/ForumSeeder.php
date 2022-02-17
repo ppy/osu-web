1de19f13c3c45c082376368271fc3fc8f245aeb9
clayton <me@clayton.cc>
2022-02-17 04:53:38 -0800

    Rewrite ForumSeeder

--- database/seeders/ModelSeeders/ForumSeeder.php
+++ database/seeders/ModelSeeders/ForumSeeder.php
@@ -3,133 +3,109 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+declare(strict_types=1);
+
 namespace Database\Seeders\ModelSeeders;
 
-use App\Models\Beatmapset;
-use App\Models\Forum;
+use App\Models\Forum\Authorize;
+use App\Models\Forum\Forum;
+use App\Models\Forum\Post;
+use App\Models\Forum\Topic;
 use App\Models\User;
-use Exception;
-use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class ForumSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        $faker = Faker::create();
-
-        try {
-            // Create appropriate forum permissions
-            $authOptionIds = [];
-
-            foreach (['f_post', 'f_postcount', 'f_read', 'f_reply'] as $authOption) {
-                $option = new Forum\AuthOption();
-                $option->auth_option = $authOption;
-                $option->save();
-
-                $authOptionIds[] = $option->auth_option_id;
-            }
-
-            $beatmapCount = Beatmapset::count();
-            if ($beatmapCount > 0) {
-                // Create beatmap threads
-                $f = factory(Forum\Forum::class)->states('parent')->create([
-                    'forum_name' => 'Beatmap Threads',
-                    'forum_desc' => 'Beatmap thread info for beatmaps',
-                ]);
-
-                $f2 = $f->subforums()->save(factory(Forum\Forum::class)->states('child')->make([
-                    'parent_id' => 1,
-                    'forum_name' => 'Beatmap Threads',
-                    'forum_desc' => 'Beatmap thread info for beatmaps',
-                ]));
-
-                $bms = Beatmapset::all();
-                foreach ($bms as $set) {
-                    $user = User::find($set->user_id);
-                    $t = $f2->topics()->save(factory(Forum\Topic::class)->make([
-                        'forum_id' => $f2->forum_id,
-                        'topic_first_poster_name' => $user->username,
-                        'topic_poster' => $user->getKey(),
-                        'topic_title' => $set->artist.' - '.$set->title,
-                    ]));
-
-                    $p = $t->posts()->save(factory(Forum\Post::class)->make([
-                        'forum_id' => $f2->forum_id,
-                        'poster_id' => $set->user_id,
-                        'post_username' => $set->creator,
-                        'post_subject' => $set->artist.' - '.$set->title,
-                        'post_text' => '---------------',
-                    ]));
-
-                    $t->refreshCache();
-                    $set->thread_id = $t->topic_id;
-                    $set->save();
-                }
-                $f2->refreshCache();
-            }
-
-            // Create userpage threads
-            $f = factory(Forum\Forum::class)->states('parent')->create([
-                'forum_name' => 'User Pages',
-                'forum_desc' => 'Your user profile pages go here!',
+        // Hardcoded forums
+        Forum::factory()
+            ->closed()
+            ->has(
+                Forum::factory()->state([
+                    'forum_desc' => 'The place to go when you have a problem to report or a question to ask.',
+                    'forum_id' => config('osu.forum.help_forum_id'),
+                    'forum_name' => 'Help',
+                ]),
+                'subforums',
+            )
+            ->has(
+                Forum::factory()->state([
+                    'forum_desc' => 'Suggest what you would like to see in osu!.',
+                    'forum_id' => config('osu.forum.feature_forum_id'),
+                    'forum_name' => 'Feature Requests',
+                ]),
+                'subforums',
+            )
+            ->create([
+                'forum_desc' => 'osu! specific',
+                'forum_name' => 'osu!',
+            ]);
+        Forum::factory()
+            ->closed()
+            ->has(
+                Forum::factory()->state([
+                    'forum_desc' => '',
+                    'forum_name' => 'Management',
+                ]),
+                'subforums',
+            )
+            ->has(
+                Forum::factory()->state([
+                    'forum_desc' => '',
+                    'forum_id' => config('osu.user.user_page_forum_id'),
+                    'forum_name' => 'User Pages',
+                ]),
+                'subforums',
+            )
+            ->create([
+                'forum_desc' => '',
+                'forum_id' => config('osu.forum.admin_forum_id'),
+                'forum_name' => 'Management',
             ]);
 
-            $f->subforums()->save(factory(Forum\Forum::class)->states('child')->make([
-                'forum_id' => config('osu.user.user_page_forum_id'),
-                'parent_id' => $f->forum_id,
-                'forum_name' => 'User Pages',
-            ]));
+        // Other forums
+        Forum::factory()
+            ->closed()
+            ->has(Forum::factory()->count(3), 'subforums')
+            ->create([
+                'forum_desc' => 'Everything else',
+                'forum_name' => 'Other',
+            ]);
 
-            // Create 3 forums
-            factory(Forum\Forum::class, 3)->states('parent')->create()->each(function ($f) {
-                for ($i = 0; $i < 4; $i++) {
-                    // Subforums for each forum.
-                    $f2 = $f->subforums()->save(factory(Forum\Forum::class)->states('child')->make());
-                    // Topics for each subforum
-                    for ($j = 0; $j < 3; $j++) {
-                        $topicUser = User::orderByRaw('RAND()')->first();
-                        $t = $f2->topics()->save(factory(Forum\Topic::class)->make([
-                            'forum_id' => $f2->forum_id,
-                            'topic_first_poster_name' => $topicUser->username,
-                            'topic_poster' => $topicUser->getKey(),
-                        ]));
-                        // Replies to the topic
-                        for ($k = 0; $k < 5; $k++) {
-                            $postUser = User::orderByRaw('RAND()')->first();
-                            $p = $t->posts()->save(factory(Forum\Post::class)->make([
-                                'forum_id' => $f2->forum_id,
-                                'post_username' => $postUser->username,
-                                'poster_id' => $postUser->getKey(),
-                            ]));
-                        }
-                        // Refresh topic cache (updates last post times etc)
-                        $t->refreshCache();
-                    }
-                    // Refresh forum cache
-                    $f2->refreshCache();
-                }
-            });
-
-            foreach (Forum\Forum::all() as $forum) {
-                foreach ($authOptionIds as $optionId) {
-                    $group = new Forum\Authorize();
-
-                    $group->group_id = app('groups')->byIdentifier('default')->getKey();
-                    $group->forum_id = $forum->forum_id;
-                    $group->auth_option_id = $optionId;
-                    $group->auth_setting = 1;
-
-                    $group->save();
-                }
-            }
-        } catch (Exception $ex) {
-            echo $ex->getMessage().PHP_EOL;
+        // Topics and posts
+        $forums = Forum::where('forum_type', 1)
+            ->where('forum_id', '<>', config('osu.user.user_page_forum_id'))
+            ->get();
+        foreach ($forums as $forum) {
+            Topic::factory()
+                ->count(5)
+                ->withPost()
+                ->has(
+                    Post::factory()->count(5)->state([
+                        'poster_id' => fn () => User::inRandomOrder()->first() ?? User::factory(),
+                    ]),
+                    'posts',
+                )
+                ->create([
+                    'forum_id' => $forum,
+                    'topic_poster' => fn () => User::inRandomOrder()->first() ?? User::factory(),
+                ]);
         }
+
+        // Authorization
+        $defaultGroupId = app('groups')->byIdentifier('default')->getKey();
+        foreach (Forum::all() as $forum) {
+            foreach (['post', 'postCount', 'reply'] as $optionState) {
+                Authorize::factory()->$optionState()->create([
+                    'forum_id' => $forum->getKey(),
+                    'group_id' => $defaultGroupId,
+                ]);
+            }
+        }
+
+        // Caches
+        Topic::all()->each(fn (Topic $topic) => $topic->refreshCache());
+        Forum::all()->each(fn (Forum $forum) => $forum->refreshCache());
     }
 }
