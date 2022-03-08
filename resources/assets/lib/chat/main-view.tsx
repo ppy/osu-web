1031fc11ab03d859e3937f962c84175398f421af
bakaneko <notbakaneko@users.noreply.github.com>
2022-03-08 21:51:23 +0900

    separate main view render

--- resources/assets/lib/chat/main-view.tsx
+++ resources/assets/lib/chat/main-view.tsx
@@ -11,6 +11,8 @@ import ConversationList from './conversation-list';
 import ConversationView from './conversation-view';
 import InputBox from './input-box';
 
+const lazerLink = 'https://github.com/ppy/osu/releases';
+
 @observer
 export default class MainView extends React.Component<Record<string, never>> {
   constructor(props: Record<string, never>) {
@@ -32,31 +34,38 @@ export default class MainView extends React.Component<Record<string, never>> {
     });
   }
 
-  render(): React.ReactNode {
-    const lazerLink = 'https://github.com/ppy/osu/releases';
+  render() {
     return (
       <>
         <HeaderV4 theme='chat' />
-        {core.dataStore.channelStore.channels.size > 0 ? (
-          <div className='chat osu-page osu-page--chat'>
-            <div className='chat__sidebar'>
-              <ConversationList />
-            </div>
-            <div className='chat__conversation-area'>
-              <ConversationView />
-              <InputBox />
-            </div>
-          </div>
-        ) : (
-          <div className='chat osu-page osu-page--chat'>
-            <div className='chat__not-active'>
-              <Img2x alt='Art by Badou_Rammsteiner' src='/images/layout/chat/none-yet.png' title='Art by Badou_Rammsteiner' />
-              <div className='chat__title'>{osu.trans('chat.no-conversations.title')}</div>
-              <div className='chat__instructions'>{osu.trans('chat.no-conversations.howto')}</div>
-              <div dangerouslySetInnerHTML={{__html: osu.trans('chat.no-conversations.lazer', {link: lazerLink})}} />
-            </div>
-          </div>
-        )}
+        <div className='chat osu-page osu-page--chat'>
+          {core.dataStore.channelStore.channels.size > 0 ? this.renderPanels() : this.renderEmpty()}
+        </div>
+      </>
+    );
+  }
+
+  private renderEmpty() {
+    return (
+      <div className='chat__not-active'>
+        <Img2x alt='Art by Badou_Rammsteiner' src='/images/layout/chat/none-yet.png' title='Art by Badou_Rammsteiner' />
+        <div className='chat__title'>{osu.trans('chat.no-conversations.title')}</div>
+        <div className='chat__instructions'>{osu.trans('chat.no-conversations.howto')}</div>
+        <div dangerouslySetInnerHTML={{__html: osu.trans('chat.no-conversations.lazer', { link: lazerLink })}} />
+      </div>
+    );
+  }
+
+  private renderPanels() {
+    return (
+      <>
+        <div className='chat__sidebar'>
+          <ConversationList />
+        </div>
+        <div className='chat__conversation-area'>
+          <ConversationView />
+          <InputBox />
+        </div>
       </>
     );
   }
