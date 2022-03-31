a6cfd303745e76f408dab3cb59b418de2c989f74
bakaneko <notbakaneko@users.noreply.github.com>
2022-03-31 16:14:23 +0900

    rename to userIds

--- resources/assets/lib/stores/channel-store.ts
+++ resources/assets/lib/stores/channel-store.ts
@@ -97,7 +97,7 @@ export default class ChannelStore implements DispatchListener {
         continue;
       }
 
-      if (channel.users.includes(userId)) {
+      if (channel.userIds.includes(userId)) {
         return channel;
       }
     }
@@ -256,7 +256,7 @@ export default class ChannelStore implements DispatchListener {
 
     try {
       if (channel.newPmChannel) {
-        const users = channel.users.slice();
+        const users = channel.userIds.slice();
         const userId = users.find((user) => user !== core.currentUserOrFail.id);
 
         if (userId == null) {
