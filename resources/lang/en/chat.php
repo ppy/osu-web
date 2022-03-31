8aa9d6517e18e35488ca3e713f49bc1f71e21eff
bakaneko <notbakaneko@users.noreply.github.com>
2022-03-31 22:23:50 +0900

    add headers to conversation list groupings

--- resources/lang/en/chat.php
+++ resources/lang/en/chat.php
@@ -13,6 +13,18 @@ return [
         'user' => 'You cannot message this user at this time.',
     ],
 
+    'channels' => [
+        'join' => 'join channel',
+        'list' => [
+            'title' => [
+                'ANNOUNCE' => 'Announcements',
+                'GROUP' => 'Groups',
+                'PM' => 'Direct messages',
+                'PUBLIC' => 'Channels',
+            ],
+        ],
+    ],
+
     'not_found' => [
         'message' => 'There\'s nothing here, maybe you left the channel or it doesn\'t exist...',
         'title' => 'channel not found',
