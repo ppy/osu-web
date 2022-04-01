a0417c25437691ac5954fa496556560e6a8c45db
bakaneko <notbakaneko@users.noreply.github.com>
2022-04-01 20:37:19 +0900

    move entire no channel handling to conversation-panel

--- resources/lang/en/chat.php
+++ resources/lang/en/chat.php
@@ -13,7 +13,10 @@ return [
         'user' => 'You cannot message this user at this time.',
     ],
 
-    'not_found' => 'channel not found',
+    'not_found' => [
+        'message' => 'There\'s nothing here, maybe you left the channel or it doesn\'t exist...',
+        'title' => 'channel not found',
+    ],
 
     'input' => [
         'disabled' => 'unable to send message...',
