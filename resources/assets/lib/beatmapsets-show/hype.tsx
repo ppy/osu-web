b440f3d9f7e30f7cd4776374058f54a8235b0c74
nanaya <me@nanaya.pro>
2022-04-05 13:04:51 +0900

    Typescript Hype

--- resources/assets/lib/beatmapsets-show/hype.tsx
+++ resources/assets/lib/beatmapsets-show/hype.tsx
@@ -1,105 +1,138 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import BigButton from 'components/big-button'
-import StringWithComponent from 'components/string-with-component'
-import { route } from 'laroute'
-import * as React from 'react'
-import { a, div, p, span } from 'react-dom-factories'
-el = React.createElement
+import BigButton from 'components/big-button';
+import StringWithComponent from 'components/string-with-component';
+import { BeatmapsetJsonForShow } from 'interfaces/beatmapset-extended-json';
+import { route } from 'laroute';
+import { times } from 'lodash';
+import core from 'osu-core-singleton';
+import * as React from 'react';
+import { classWithModifiers } from 'utils/css';
 
-bn = 'beatmapset-hype'
+const bn = 'beatmapset-hype';
 
-export class Hype extends React.PureComponent
-  render: =>
-    div className: bn,
-      div className: "#{bn}__box #{bn}__box--description",
-        div className: "#{bn}__description-row #{bn}__description-row--status",
-          div className: 'beatmapset-status beatmapset-status--hype',
-            osu.trans("beatmapsets.show.status.#{@props.beatmapset.status}")
-        p className: "#{bn}__description-row #{bn}__description-row--current",
-          osu.trans 'beatmapsets.show.hype.current._',
-            status: osu.trans("beatmapsets.show.hype.current.status.#{@props.beatmapset.status}")
-        if @props.beatmapset.status == 'qualified'
-          p
-            className: "#{bn}__description-row #{bn}__description-row--action"
-            el StringWithComponent,
-              mappings:
-                link: a
-                  href: @reportUrl()
-                  osu.trans 'beatmapsets.show.hype.report.link'
-              pattern:
-                if @userCanDisqualify()
-                  osu.trans('beatmapsets.show.hype.disqualify._')
-                else
-                  osu.trans('beatmapsets.show.hype.report._')
+interface Props {
+  beatmapset: BeatmapsetJsonForShow & { hype: NonNullable<BeatmapsetJsonForShow['hype']> };
+}
 
-        else
-          p
-            className: "#{bn}__description-row #{bn}__description-row--action"
-            dangerouslySetInnerHTML:
-              __html: osu.trans('beatmapsets.show.hype.action')
+export class Hype extends React.PureComponent<Props> {
+  private get hypeUrl() {
+    return `${route('beatmapsets.discussion', {
+      beatmap: '-',
+      beatmapset: this.props.beatmapset.id,
+      filter: 'praises',
+      mode: 'generalAll',
+    })}#new`;
+  }
 
-      div className: "#{bn}__box #{bn}__box--float",
-        div className: "#{bn}__lights-header",
-          span
-            className: "#{bn}__lights-title",
-            osu.trans('beatmaps.hype.section_title')
-          span null,
-            "#{@props.beatmapset.hype.current} / #{@props.beatmapset.hype.required}"
+  private get reportUrl() {
+    return `${route('beatmapsets.discussion', {
+      beatmap: '-',
+      beatmapset: this.props.beatmapset.id,
+      mode: 'generalAll',
+    })}#new`;
+  }
 
-        div className: "#{bn}__lights",
-          for i in _.times(@props.beatmapset.hype.required)
-            state = if i < @props.beatmapset.hype.current then 'on' else 'off'
-            div
-              key: i
-              className: "bar bar--beatmapset-hype bar--beatmapset-#{state}"
+  private get userCanDisqualify() {
+    return core.currentUser != null && (
+      core.currentUser.is_moderator
+      || core.currentUser.is_admin
+      || core.currentUser.is_full_bn
+    );
+  }
 
-        div
-          className: "#{bn}__button"
-          title: @props.beatmapset.current_user_attributes?.can_hype_reason
-          el BigButton,
-            disabled:
-              if @props.currentUser.id?
-                !@props.beatmapset.current_user_attributes.can_hype
-              else
-                false
-            href: "#{route 'beatmapsets.discussion',
-              beatmapset: @props.beatmapset.id
-              beatmap: '-'
-              mode: 'generalAll'
-              filter: 'praises'}#new"
-            icon: 'fas fa-bullhorn'
-            modifiers: 'full'
-            text: osu.trans('beatmaps.hype.button')
+  render() {
+    return (
+      <div className={bn}>
+        <div className={`${bn}__box ${bn}__box--description`}>
+          <div className={`${bn}__description-row ${bn}__description-row--status`}>
+            <div className='beatmapset-status beatmapset-status--hype'>
+              {osu.trans(`beatmapsets.show.status.${this.props.beatmapset.status}`)}
+            </div>
+          </div>
+          <p className={`${bn}__description-row ${bn}__description-row--current`}>
+            {osu.trans('beatmapsets.show.hype.current._', {
+              status: osu.trans(`beatmapsets.show.hype.current.status.${this.props.beatmapset.status}`),
+            })}
+          </p>
+          {this.props.beatmapset.status === 'qualified' ? (
+            <p className={`${bn}__description-row ${bn}__description-row--action`}>
+              <StringWithComponent
+                mappings={{
+                  link: <a href={this.reportUrl}>{osu.trans('beatmapsets.show.hype.report.link')}</a>,
+                }}
+                pattern={this.userCanDisqualify
+                  ? osu.trans('beatmapsets.show.hype.disqualify._')
+                  : osu.trans('beatmapsets.show.hype.report._')
+                }
+              />
+            </p>
+          ) : (
+            <p
+              className={`${bn}__description-row ${bn}__description-row--action`}
+              dangerouslySetInnerHTML={{
+                __html: osu.trans('beatmapsets.show.hype.action'),
+              }}
+            />
+          )}
+        </div>
 
-        div
-          className: "#{bn}__button"
-          @renderReportButton()
+        <div className={`${bn}__box ${bn}__box--float`}>
+          <div className={`${bn}__lights-header`}>
+            <span className={`${bn}__lights-title`}>
+              {osu.trans('beatmaps.hype.section_title')}
+            </span>
+            <span>
+              {`${this.props.beatmapset.hype.current} / ${this.props.beatmapset.hype.required}`}
+            </span>
+          </div>
 
+          <div className={`${bn}__lights`}>
+            {times(this.props.beatmapset.hype.required).map((i) => (
+              <div
+                key={i}
+                className={classWithModifiers('bar', [
+                  'beatmapset-hype',
+                  i < this.props.beatmapset.hype.current ? 'beatmapset-on' : 'beatmapset-off',
+                ])}
+              />
+            ))}
+          </div>
 
-  renderReportButton: =>
-    return unless @props.beatmapset.status == 'qualified'
+          <div
+            className={`${bn}__button`}
+            title={this.props.beatmapset.current_user_attributes?.can_hype_reason}
+          >
+            <BigButton
+              disabled={core.currentUser != null && !this.props.beatmapset.current_user_attributes.can_hype}
+              href={this.hypeUrl}
+              icon='fas fa-bullhorn'
+              modifiers='full'
+              text={osu.trans('beatmaps.hype.button')}
+            />
+          </div>
 
-    if @userCanDisqualify()
-      buttonParams =
-        text: osu.trans 'beatmaps.nominations.disqualify'
-        icon: 'fas fa-thumbs-down'
-    else
-      buttonParams =
-        text: osu.trans('beatmapsets.show.hype.report.button')
-        icon: 'fas fa-exclamation-triangle'
+          <div className={`${bn}__button`}>{this.renderReportButton()}</div>
+        </div>
+      </div>
+    );
+  }
 
-    el BigButton,
-      href: @reportUrl()
-      icon: buttonParams.icon
-      modifiers: 'full'
-      text: buttonParams.text
+  private renderReportButton() {
+    if (this.props.beatmapset.status !== 'qualified') return null;
 
+    const [text, icon] = this.userCanDisqualify
+      ? [osu.trans('beatmaps.nominations.disqualify'), 'fas fa-thumbs-down']
+      : [osu.trans('beatmapsets.show.hype.report.button'), 'fas fa-exclamation-triangle'];
 
-  reportUrl: =>
-    "#{route('beatmapsets.discussion', beatmapset: @props.beatmapset.id, beatmap: '-', mode: 'generalAll')}#new"
-
-
-  userCanDisqualify: =>
-    @props.currentUser? && (@props.currentUser.is_moderator || @props.currentUser.is_admin || @props.currentUser.is_full_bn)
+    return (
+      <BigButton
+        href={this.reportUrl}
+        icon={icon}
+        modifiers='full'
+        text={text}
+      />
+    );
+  }
+}
