0785e809b934cb8c1d1be619dd87da8b70f0ad05
bakaneko <notbakaneko@users.noreply.github.com>
2022-04-13 13:03:05 +0900

    no this needed anywhere

--- resources/assets/lib/chat/conversation-list.tsx
+++ resources/assets/lib/chat/conversation-list.tsx
@@ -7,28 +7,23 @@ import core from 'osu-core-singleton';
 import * as React from 'react';
 import ConversationListItem from './conversation-list-item';
 
-@observer
-export default class ConversationList extends React.Component {
-  render() {
-    return (
-      <div className='chat-conversation-list'>
-        {supportedChannelTypes.map(this.renderChannels)}
+function renderChannels(type: SupportedChannelType) {
+  const channels = core.dataStore.channelStore.groupedChannels[type];
+  if (channels.length === 0) return null;
+
+  return (
+    <React.Fragment key={type}>
+      <div className='chat-conversation-list__header'>
+        {osu.trans(`chat.channels.list.title.${type}`)}
       </div>
-    );
-  }
-
-  private renderChannels(this: void, type: SupportedChannelType) {
-    const channels = core.dataStore.channelStore.groupedChannels[type];
-    if (channels.length === 0) return null;
-
-    return (
-      <React.Fragment key={type}>
-        <div className='chat-conversation-list__header'>
-          {osu.trans(`chat.channels.list.title.${type}`)}
-        </div>
-        {channels.map((channel) => <ConversationListItem key={channel.channelId} channel={channel} />)}
-        <div className='chat-conversation-list-separator' />
-      </React.Fragment>
-    );
-  }
+      {channels.map((channel) => <ConversationListItem key={channel.channelId} channel={channel} />)}
+      <div className='chat-conversation-list-separator' />
+    </React.Fragment>
+  );
 }
+
+export default observer(() => (
+  <div className='chat-conversation-list'>
+    {supportedChannelTypes.map(renderChannels)}
+  </div>
+));
