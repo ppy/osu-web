b5cc2fb8c8f375f2f1aa0349b65cf2444c7b98a0
bakaneko <notbakaneko@users.noreply.github.com>
2022-04-14 16:12:02 +0900

    rename loading functions;
    to better ereflect what they're doing

--- resources/assets/lib/models/chat/channel.ts
+++ resources/assets/lib/models/chat/channel.ts
@@ -182,10 +182,10 @@ export default class Channel {
     if (this.newPmChannel) return;
 
     if (this.type === 'ANNOUNCE' && !this.usersLoaded) {
-      this.refresh();
+      this.loadMetadata();
     }
 
-    this.refreshMessages();
+    this.loadRecentMessages();
   }
 
   @action
@@ -219,18 +219,18 @@ export default class Channel {
   }
 
   @action
-  markAsRead() {
-    this.setLastReadId(this.lastMessageId);
-  }
-
-  @action
-  refresh() {
+  loadMetadata() {
     getChannel(this.channelId).done((json) => {
       runInAction(() => {
         this.updateWithJson(json);
         this.usersLoaded = true;
       });
     });
+  }
+
+  @action
+  markAsRead() {
+    this.setLastReadId(this.lastMessageId);
   }
 
   @action
@@ -264,17 +264,7 @@ export default class Channel {
   }
 
   @action
-  private persistMessage(message: Message, json: MessageJson) {
-    if (json.uuid != null) {
-      this.messagesMap.delete(json.uuid);
-    }
-
-    message.persist(json);
-    this.messagesMap.set(message.messageId, message);
-  }
-
-  @action
-  private async refreshMessages() {
+  private async loadRecentMessages() {
     if (!this.needsRefresh || this.loadingMessages) return;
 
     this.loadingMessages = true;
@@ -309,6 +299,16 @@ export default class Channel {
     } catch {
       runInAction(() => this.loadingMessages = false);
     }
+  }
+
+  @action
+  private persistMessage(message: Message, json: MessageJson) {
+    if (json.uuid != null) {
+      this.messagesMap.delete(json.uuid);
+    }
+
+    message.persist(json);
+    this.messagesMap.set(message.messageId, message);
   }
 
   @action
