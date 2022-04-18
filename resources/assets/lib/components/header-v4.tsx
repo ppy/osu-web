d52743e951a48af96dd4f3d088ed232aca5750fe
nanaya <me@nanaya.pro>
2022-04-18 17:51:14 +0900

    Remove unused prop

--- resources/assets/lib/components/header-v4.tsx
+++ resources/assets/lib/components/header-v4.tsx
@@ -17,7 +17,6 @@ interface Props {
   modifiers?: Modifiers;
   onLinkClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
   theme?: string;
-  titleAppend?: React.ReactNode;
 }
 
 interface RouteSection {
@@ -59,8 +58,6 @@ export default class HeaderV4 extends React.Component<Props> {
               <div className='header-v4__title'>
                 {this.title()}
               </div>
-
-              {this.props.titleAppend}
             </div>
 
             {this.props.contentAppend}
