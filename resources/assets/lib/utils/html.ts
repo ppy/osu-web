eb2171219f7d6eba92bffa4dd17a6ec3edf2e851
nanaya <me@nanaya.pro>
2022-05-16 16:12:26 +0900

    Set channel name on browser title

--- resources/assets/lib/utils/html.ts
+++ resources/assets/lib/utils/html.ts
@@ -94,6 +94,10 @@ export function make2x(url?: string) {
   return url.replace(/(\.[^.]+)$/, '@2x$1');
 }
 
+export function setBrowserTitle(title: string) {
+  document.title = `${title} | osu!`;
+}
+
 export function stripTags(str: string) {
   return str.replace(/<[^>]*>/g, '');
 }
