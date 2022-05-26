1c1a564c36a33b93cd947cdb5afdfa9dd2969028
bakaneko <notbakaneko@users.noreply.github.com>
2022-05-26 12:36:12 +0900

    there's pmTarget for this...

--- resources/assets/lib/stores/channel-store.ts
+++ resources/assets/lib/stores/channel-store.ts
@@ -256,8 +256,7 @@ export default class ChannelStore implements DispatchListener {
 
     try {
       if (channel.newPmChannel) {
-        const users = channel.userIds.slice();
-        const userId = users.find((user) => user !== core.currentUserOrFail.id);
+        const userId = channel.pmTarget;
 
         if (userId == null) {
           console.debug('sendMessage:: userId not found?? this shouldn\'t happen');
