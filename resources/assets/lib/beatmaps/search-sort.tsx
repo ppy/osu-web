965f79fc6a8b49b70f038d4e798d92ec6e84c250
bakaneko <notbakaneko@users.noreply.github.com>
2022-06-01 12:23:19 +0900

    forgot to flip conditional; also remove isEmpty

--- resources/assets/lib/beatmaps/search-sort.tsx
+++ resources/assets/lib/beatmaps/search-sort.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { isEmpty } from 'lodash';
 import { computed, makeObservable } from 'mobx';
 import { observer } from 'mobx-react';
 import core from 'osu-core-singleton';
@@ -31,7 +30,7 @@ export class SearchSort extends React.Component<Props> {
       plays: true,
       ranked: false,
       rating: true,
-      relevance: isEmpty(this.filters.query),
+      relevance: this.filters.query != null,
       title: true,
       updated: false,
     };
