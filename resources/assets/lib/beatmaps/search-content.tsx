41caeb233c027cb377911e2bc4b4f0cff7772f9d
bakaneko <notbakaneko@users.noreply.github.com>
2022-06-03 22:22:55 +0900

    converting search-content to typescript

--- resources/assets/lib/beatmaps/search-content.tsx
+++ resources/assets/lib/beatmaps/search-content.tsx
@@ -1,114 +1,139 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import BeatmapsetCardSizeSelector from 'beatmaps/beatmapset-card-size-selector'
-import VirtualListMeta from 'beatmaps/virtual-list-meta'
-import BeatmapsetPanel, { beatmapsetCardSizes } from 'components/beatmapset-panel'
-import Img2x from 'components/img2x'
-import { route } from 'laroute'
-import { Observer } from 'mobx-react'
-import core from 'osu-core-singleton'
-import * as React from 'react'
-import { a, div, p } from 'react-dom-factories'
-import VirtualList from 'react-virtual-list'
-import { Paginator } from './paginator'
-import { SearchPanel } from './search-panel'
-import { SearchSort } from './search-sort'
+import BeatmapsetCardSizeSelector from 'beatmaps/beatmapset-card-size-selector';
+import VirtualListMeta from 'beatmaps/virtual-list-meta';
+import BeatmapsetPanel, { beatmapsetCardSizes } from 'components/beatmapset-panel';
+import Img2x from 'components/img2x';
+import { route } from 'laroute';
+import { chunk } from 'lodash';
+import { observer } from 'mobx-react';
+import core from 'osu-core-singleton';
+import * as React from 'react';
+import VirtualList from 'react-virtual-list';
+import { classWithModifiers } from 'utils/css';
+import AvailableFilters from './available-filters';
+import { Paginator } from './paginator';
+import { SearchPanel } from './search-panel';
+import { SearchSort } from './search-sort';
 
-el = React.createElement
+// Don't care about the typing of virtual, we're going to replace it.
+interface ListRenderProps {
+  itemHeight: number;
+  virtual: {
+    items: number[][];
+    style: React.CSSProperties;
+  };
+}
 
-ListRender = ({ virtual, itemHeight }) ->
-  div
-    style: virtual.style
-    div
-      className: 'beatmapsets__items'
-      virtual.items.map (row) ->
-        div
-          className: 'beatmapsets__items-row'
-          key: (beatmapsetId for beatmapsetId in row).join('-')
-          for beatmapsetId in row
-            div
-              className: 'beatmapsets__item'
-              key: beatmapsetId
-              el BeatmapsetPanel, beatmapset: core.dataStore.beatmapsetStore.get(beatmapsetId)
+interface Props {
+  availableFilters: AvailableFilters;
+  backToTopAnchor: React.RefObject<HTMLDivElement>;
+}
 
-BeatmapList = VirtualList()(ListRender)
+const ListRender = ({ virtual, itemHeight }: ListRenderProps) => (
+  <div style={virtual.style}>
+    <div className='beatmapsets__items'>
+      {virtual.items.map((row) => (
+        <div key={row.join('-')} className='beatmapsets__items-row'>
+          {row.map((beatmapsetId) => (
+            <div key={beatmapsetId} className='beatmapsets__item'>
+              <BeatmapsetPanel beatmapset={core.dataStore.beatmapsetStore.get(beatmapsetId)} />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
+const BeatmapList = VirtualList()(ListRender);
 
-export class SearchContent extends React.Component
-  constructor: ->
-    @virtualListMeta = new VirtualListMeta
+function renderLinkToSupporterTag(filterText: string) {
+  const url = route('store.products.show', { product: 'supporter-tag' });
+  const link = `<a href="${url}">${osu.trans('beatmaps.listing.search.supporter_filter_quote.link_text')}</a>`;
 
+  return (
+    <p dangerouslySetInnerHTML={{
+      __html: osu.trans('beatmaps.listing.search.supporter_filter_quote._', { filters: filterText, link }),
+    }}/>
+  );
+}
 
-  render: ->
-    el Observer, null, () =>
-      controller = core.beatmapsetSearchController
-      beatmapsetIds = controller.currentBeatmapsetIds
+@observer
+export class SearchContent extends React.Component<Props> {
+  private readonly virtualListMeta = new VirtualListMeta();
 
-      supporterRequiredFilterText = controller.supporterRequiredFilterText
-      listCssClasses = 'beatmapsets'
-      listCssClasses += ' beatmapsets--dimmed' if controller.isBusy
+  private get controller() {
+    return core.beatmapsetSearchController;
+  }
 
-      el React.Fragment, null,
-        el SearchPanel,
-          innerRef: @props.backToTopAnchor
-          firstBeatmapset: core.dataStore.beatmapsetStore.get(beatmapsetIds[0])
-          availableFilters: @props.availableFilters
+  render() {
+    const beatmapsetIds = this.controller.currentBeatmapsetIds;
+    const supporterRequiredFilterText = this.controller.supporterRequiredFilterText
+    const listCssClasses = classWithModifiers('beatmapsets', { dimmed: this.controller.isBusy });
 
-        div className: 'js-sticky-header'
-
-        div
-          className: 'osu-layout__row osu-layout__row--page-compact'
-          div className: listCssClasses,
-            if controller.advancedSearch
-              div className: 'beatmapsets__toolbar',
-                div className: 'beatmapsets__toolbar-item',
-                  el SearchSort, filters: controller.filters
-                div className: 'beatmapsets__toolbar-item',
-                  div className: 'sort hidden-xs', div className: 'sort__items',
-                    beatmapsetCardSizes.map (size) =>
-                      el BeatmapsetCardSizeSelector,
-                        key: size
-                        classElement: 'sort__item'
-                        size: size
-
-            div
-              className: 'beatmapsets__content js-audio--group'
-              if controller.isSupporterMissing
-                div className: 'beatmapsets__empty',
-                  el Img2x,
-                    src: '/images/layout/beatmaps/supporter-required.png'
-                    alt: osu.trans('beatmaps.listing.search.supporter_filter', filters: supporterRequiredFilterText)
-                    title: osu.trans('beatmaps.listing.search.supporter_filter', filters: supporterRequiredFilterText)
-
-                  renderLinkToSupporterTag(supporterRequiredFilterText)
-
-              else
-                if beatmapsetIds.length > 0
-                  el BeatmapList,
-                    items: _.chunk(beatmapsetIds, @virtualListMeta.numberOfColumns)
-                    itemBuffer: 5
-                    itemHeight: @virtualListMeta.itemHeight
-
-                else
-                  div className: 'beatmapsets__empty',
-                    el Img2x,
-                      src: '/images/layout/beatmaps/not-found.png'
-                      alt: osu.trans("beatmaps.listing.search.not-found")
-                      title: osu.trans("beatmaps.listing.search.not-found")
-                    osu.trans("beatmaps.listing.search.not-found-quote")
-
-            if !controller.isSupporterMissing
-              div className: 'beatmapsets__paginator',
-                el Paginator
-
-
-renderLinkToSupporterTag = (filterText) ->
-  url = route('store.products.show', product: 'supporter-tag')
-  link = "<a href=\"#{url}\">#{osu.trans 'beatmaps.listing.search.supporter_filter_quote.link_text'}</a>"
-
-  p
-    dangerouslySetInnerHTML:
-      __html: osu.trans 'beatmaps.listing.search.supporter_filter_quote._',
-        filters: filterText
-        link: link
+    return (
+      <>
+        <SearchPanel
+          availableFilters={this.props.availableFilters}
+          firstBeatmapset={core.dataStore.beatmapsetStore.get(beatmapsetIds[0])}
+          innerRef={this.props.backToTopAnchor}
+        />
+        <div className='js-sticky-header' />
+        <div className='osu-layout__row osu-layout__row--page-compact'>
+          <div className={listCssClasses}>
+            {this.controller.advancedSearch && (
+              <div className='beatmapsets__toolbar'>
+                <div className='beatmapsets__toolbar-item'>
+                  <SearchSort />
+                </div>
+                <div className='beatmapsets__toolbar-item'>
+                  <div className='sort hidden-xs'>
+                    <div className='sort__items'>
+                      {beatmapsetCardSizes.map((size) => (
+                        <BeatmapsetCardSizeSelector key={size} classElement='sort__item' size={size} />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className='beatmapsets__content js-audio--group'>
+              {this.controller.isSupporterMissing ? (
+                <div className='beatmapsets__empty'>
+                  <Img2x
+                    alt={osu.trans('beatmaps.listing.search.supporter_filter', { filters: supporterRequiredFilterText })}
+                    src='/images/layout/beatmaps/supporter-required.png'
+                    title={osu.trans('beatmaps.listing.search.supporter_filter', { filters: supporterRequiredFilterText })}
+                  />
+                  {renderLinkToSupporterTag(supporterRequiredFilterText)}
+                </div>
+              ) : beatmapsetIds.length > 0 ? (
+                <BeatmapList
+                  itemBuffer={5}
+                  itemHeight={this.virtualListMeta.itemHeight}
+                  items={chunk(beatmapsetIds, this.virtualListMeta.numberOfColumns)}
+                />
+              ) : (
+                <div className='beatmapsets__empty'>
+                  <Img2x
+                    alt={osu.trans('beatmaps.listing.search.not-found')}
+                    src='/images/layout/beatmaps/not-found.png'
+                    title={osu.trans('beatmaps.listing.search.not-found')}
+                  />
+                  {osu.trans('beatmaps.listing.search.not-found-quote')}
+                </div>
+              )}
+            </div>
+            {!this.controller.isSupporterMissing && (
+              <div className='beatmapsets__paginator'>
+                <Paginator />
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+    )
+  }
+}
