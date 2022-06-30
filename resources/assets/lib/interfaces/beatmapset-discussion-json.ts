5dd32232d30afc204e9723046819e238cf07be39
bakaneko <notbakaneko@users.noreply.github.com>
2022-06-30 15:24:18 +0900

    update with missing properties

--- resources/assets/lib/interfaces/beatmapset-discussion-json.ts
+++ resources/assets/lib/interfaces/beatmapset-discussion-json.ts
@@ -2,19 +2,51 @@
 // See the LICENCE file in the repository root for full licence text.
 
 import { DiscussionType } from 'beatmap-discussions/discussion-type';
+import BeatmapJson from './beatmap-json';
 import BeatmapsetDiscussionPostJson from './beatmapset-discussion-post-json';
+import BeatmapsetJson from './beatmapset-json';
 
-// TODO: incomplete
-export default interface BeatmapsetDiscussionJson {
+interface BeatmapsetDiscussionAvailableIncludes {
+  beatmap: BeatmapJson;
+  beatmapset: BeatmapsetJson;
+  current_user_attributes: {
+    can_destroy: boolean;
+    can_moderate_kudosu: boolean;
+    can_reopen: boolean;
+    can_resolve: boolean;
+    vote_score: number;
+  };
+  posts: BeatmapsetDiscussionPostJson[];
+  starting_post: BeatmapsetDiscussionPostJson;
+  votes: {
+    down: number;
+    up: number;
+    voters: {
+      down: number[];
+      up: number[];
+    };
+  };
+}
+
+interface BeatmapsetDiscussionDefaultAttributes {
   beatmap_id: number | null;
   beatmapset_id: number;
+  can_be_resolved: boolean;
+  can_grant_kudosu: boolean;
+  created_at: string;
   deleted_at: string | null;
   id: number;
+  kudosu_denied: boolean;
+  last_post_at: string;
   message_type: DiscussionType;
   parent_id: number | null;
   posts: BeatmapsetDiscussionPostJson[];
   resolved: boolean;
   starting_post: BeatmapsetDiscussionPostJson;
   timestamp: number | null;
+  updated_at: string;
   user_id: number;
 }
+
+type BeatmapsetDiscussionJson = BeatmapsetDiscussionDefaultAttributes & Partial<BeatmapsetDiscussionAvailableIncludes>;
+export default BeatmapsetDiscussionJson;
