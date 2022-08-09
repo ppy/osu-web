0e1ae3fcb48616fbdb420968681a285025927062
nanaya <me@nanaya.pro>
2022-08-09 17:13:11 +0900

    Combine tests

--- tests/Models/BeatmapPackUserCompletionTest.php
+++ tests/Models/BeatmapPackUserCompletionTest.php
@@ -3,97 +3,76 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+declare(strict_types=1);
+
 namespace Tests\Models;
 
 use App\Models\Beatmap;
 use App\Models\BeatmapPack;
-use App\Models\Beatmapset;
 use App\Models\Score\Best as ScoreBest;
 use App\Models\User;
 use Tests\TestCase;
 
-class BeatmapPackTest extends TestCase
+class BeatmapPackUserCompletionTest extends TestCase
 {
-    public function testUserCompletionData()
+    /**
+     * @dataProvider dataProviderForTestBasic
+     */
+    public function testBasic(string $userType, ?string $packRuleset, bool $completed): void
     {
-        $beatmapset = Beatmapset::factory()->create();
-        $pack = BeatmapPack::factory()->create(['playmode' => null]);
-        $pack->items()->create(['beatmapset_id' => $beatmapset->getKey()]);
-        $scoreBest = ScoreBest\Taiko::factory()->create();
-        $scoreBest->beatmap->update(['beatmapset_id' => $beatmapset->getKey()]);
-        $user = User::find($scoreBest->user_id);
+        $beatmap = Beatmap::factory()->ranked()->state([
+            'playmode' => Beatmap::MODES['taiko'],
+        ])->create();
+        $pack = BeatmapPack::factory()->create();
+        $pack->items()->create(['beatmapset_id' => $beatmap->beatmapset_id]);
 
-        $pack->refresh();
-
-        // unspecified pack playmode, non-convert score
-        $data = $pack->userCompletionData($user);
-        $this->assertSame(1, count($data['beatmapset_ids']));
-        $this->assertSame(true, $data['completed']);
-
-        // specified pack playmode, non-convert score but different mode
-        $pack->update(['playmode' => Beatmap::MODES['osu']]);
-        $data = $pack->userCompletionData($user);
-        $this->assertSame(0, count($data['beatmapset_ids']));
-        $this->assertSame(false, $data['completed']);
-
-        // specified pack playmode, with match non-convert score mode
-        $pack->update(['playmode' => Beatmap::MODES['taiko']]);
-        $data = $pack->userCompletionData($user);
-        $this->assertSame(1, count($data['beatmapset_ids']));
-        $this->assertSame(true, $data['completed']);
-    }
-
-    public function testUserCompletionDataEmptyUser()
-    {
-        $beatmapset = Beatmapset::factory()->create();
-        $pack = BeatmapPack::factory()->create(['playmode' => null]);
-        $pack->items()->create(['beatmapset_id' => $beatmapset->getKey()]);
-        $scoreBest = ScoreBest\Taiko::factory()->create();
-        $scoreBest->beatmap->update(['beatmapset_id' => $beatmapset->getKey()]);
-        $user = User::factory()->create();
-
-        $pack->refresh();
-
-        foreach ([$user, null] as $u) {
-            $data = $pack->userCompletionData($u);
-            $this->assertSame(0, count($data['beatmapset_ids']));
-            $this->assertSame(false, $data['completed']);
-
-            $pack->update(['playmode' => Beatmap::MODES['osu']]);
-            $data = $pack->userCompletionData($u);
-            $this->assertSame(0, count($data['beatmapset_ids']));
-            $this->assertSame(false, $data['completed']);
-
-            $pack->update(['playmode' => Beatmap::MODES['taiko']]);
-            $data = $pack->userCompletionData($u);
-            $this->assertSame(0, count($data['beatmapset_ids']));
-            $this->assertSame(false, $data['completed']);
+        $scoreUser = User::factory()->create();
+        $scoreClass = ScoreBest\Taiko::class;
+        switch ($userType) {
+            case 'convertOsu':
+                $checkUser = $scoreUser;
+                $scoreClass = ScoreBest\Osu::class;
+                break;
+            case 'default':
+                $checkUser = $scoreUser;
+                break;
+            case 'null':
+                $checkUser = null;
+                break;
+            case 'unrelated':
+                $checkUser = User::factory()->create();
+                break;
         }
-    }
 
-    public function testUserCompletionDataConverts()
-    {
-        $beatmapset = Beatmapset::factory()->create();
-        $pack = BeatmapPack::factory()->create(['playmode' => null]);
-        $pack->items()->create(['beatmapset_id' => $beatmapset->getKey()]);
-        $scoreBest = ScoreBest\Taiko::factory()->create();
-        $scoreBest->beatmap->update([
-            'beatmapset_id' => $beatmapset->getKey(),
-            'playmode' => Beatmap::MODES['osu'],
+        $scoreClass::factory()->create([
+            'beatmap_id' => $beatmap,
+            'user_id' => $scoreUser->getKey(),
         ]);
-        $user = User::find($scoreBest->user_id);
 
+        $rulesetId = $packRuleset === null ? null : Beatmap::MODES[$packRuleset];
+        $pack->update(['playmode' => $rulesetId]);
         $pack->refresh();
 
-        // convert scores are ignored when playmode is null
-        $data = $pack->userCompletionData($user);
-        $this->assertSame(0, count($data['beatmapset_ids']));
-        $this->assertSame(false, $data['completed']);
+        $data = $pack->userCompletionData($checkUser);
+        $this->assertSame($completed ? 1 : 0, count($data['beatmapset_ids']));
+        $this->assertSame($completed, $data['completed']);
+    }
 
-        // convert scores are ok when playmode is specified
-        $pack->update(['playmode' => Beatmap::MODES['taiko']]);
-        $data = $pack->userCompletionData($user);
-        $this->assertSame(1, count($data['beatmapset_ids']));
-        $this->assertSame(true, $data['completed']);
+    public function dataProviderForTestBasic(): array
+    {
+        return [
+            ['convertOsu', 'osu', true],
+            ['convertOsu', 'taiko', false],
+            ['convertOsu', null, false],
+            ['default', 'osu', false],
+            ['default', 'taiko', true],
+            ['default', null, true],
+            ['null', 'osu', false],
+            ['null', 'taiko', false],
+            ['null', null, false],
+            ['unrelated', 'osu', false],
+            ['unrelated', 'taiko', false],
+            ['unrelated', null, false],
+        ];
     }
 }
