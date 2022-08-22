f37d51e8da88d117a4ab8f47fb95e18029dc9365
bakaneko <notbakaneko@users.noreply.github.com>
2022-08-22 22:33:27 +0900

    forgot to move the elements back into the right place

--- resources/assets/lib/beatmap-discussions/new-discussion.tsx
+++ resources/assets/lib/beatmap-discussions/new-discussion.tsx
@@ -347,17 +347,8 @@ export class NewDiscussion extends React.Component<Props> {
     );
   }
 
-  private renderGuest() {
-    if (!(this.props.mode === 'generalAll' && this.props.beatmapset.can_be_hyped)) return null;
-    if (core.currentUser != null) return null;
-    return osu.trans('beatmaps.hype.explanation_guest');
-  }
-
   private renderHype() {
     if (!(this.props.mode === 'generalAll' && this.props.beatmapset.can_be_hyped)) return null;
-    if (core.currentUser == null) {
-      return this.renderGuest();
-    }
 
     return (
       <div className={`${bn}__footer-content js-hype--explanation js-flash-border`}>
@@ -365,23 +356,27 @@ export class NewDiscussion extends React.Component<Props> {
           {osu.trans('beatmaps.hype.title')}
         </div>
         <div className={`${bn}__timestamp-col`}>
-          {this.props.beatmapset.current_user_attributes.can_hype ? osu.trans('beatmaps.hype.explanation') : this.props.beatmapset.current_user_attributes.can_hype_reason}
-          {(this.props.beatmapset.current_user_attributes.can_hype || this.props.beatmapset.current_user_attributes.remaining_hype <= 0) && (
+          {core.currentUser != null ? (
             <>
-              <StringWithComponent
-                mappings={{ remaining: this.props.beatmapset.current_user_attributes.remaining_hype }}
-                pattern={` ${osu.trans('beatmaps.hype.remaining')}`}
-              />
-              {this.props.beatmapset.current_user_attributes.new_hype_time != null && (
-                <StringWithComponent
-                  mappings={{
-                    new_time: <TimeWithTooltip dateTime={this.props.beatmapset.current_user_attributes.new_hype_time} relative />,
-                  }}
-                  pattern={` ${osu.trans('beatmaps.hype.new_time')}`}
-                />
+              {this.props.beatmapset.current_user_attributes.can_hype ? osu.trans('beatmaps.hype.explanation') : this.props.beatmapset.current_user_attributes.can_hype_reason}
+              {(this.props.beatmapset.current_user_attributes.can_hype || this.props.beatmapset.current_user_attributes.remaining_hype <= 0) && (
+                <>
+                  <StringWithComponent
+                    mappings={{ remaining: this.props.beatmapset.current_user_attributes.remaining_hype }}
+                    pattern={` ${osu.trans('beatmaps.hype.remaining')}`}
+                  />
+                  {this.props.beatmapset.current_user_attributes.new_hype_time != null && (
+                    <StringWithComponent
+                      mappings={{
+                        new_time: <TimeWithTooltip dateTime={this.props.beatmapset.current_user_attributes.new_hype_time} relative />,
+                      }}
+                      pattern={` ${osu.trans('beatmaps.hype.new_time')}`}
+                    />
+                  )}
+                </>
               )}
             </>
-          )}
+          ) : osu.trans('beatmaps.hype.explanation_guest')}
         </div>
       </div>
     );
