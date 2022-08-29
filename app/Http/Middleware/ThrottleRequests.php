4abef11842ac48893d26926d8e6fff72707f1f3b
bakaneko <notbakaneko@users.noreply.github.com>
2022-08-29 19:53:38 +0900

    the comment is about the hit() call being moved

--- app/Http/Middleware/ThrottleRequests.php
+++ app/Http/Middleware/ThrottleRequests.php
@@ -25,10 +25,12 @@ class ThrottleRequests extends ThrottleRequestsBase
 
         $response = $next($request);
 
-        // Should still run even if the controller action throws.
         $cost = RequestCost::getCost($request);
 
         foreach ($limits as $limit) {
+            // hit moved to after request is processed to be able to get the cost assigned by the controller action,
+            // in constrast to the original function. This works fine since $next will handle exceptions
+            // thrown by the controller so the rest of the function still runs.
             $this->limiter->hit($limit->key, $limit->decayMinutes * 60, $cost);
 
             $response = $this->addHeaders(
