1cf3c40467f1a74b42a934d9b040a1c8c34eab5a
nanaya <me@nanaya.pro>
2022-09-12 23:26:58 +0900

    Add scope to remove order in a query

--- app/Models/Score/Best/Model.php
+++ app/Models/Score/Best/Model.php
@@ -186,7 +186,9 @@ abstract class Model extends BaseModel implements Traits\ReportableInterface
         return function ($query) {
             $newQuery = clone $query;
             // FIXME: mysql 5.6 compat
-            $newQuery->getQuery()->orders = null;
+            // FIXME: figure out what exactly the above is. Also it's probably
+            // still a good idea to remove the order anyway.
+            $newQuery->unorder();
 
             $scores = $newQuery
                 ->select(['user_id', 'beatmap_id', 'score', 'rank'])
