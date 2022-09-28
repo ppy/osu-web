e26c87f2d3b644c9194494aaf7ea3bce0e73c257
bakaneko <notbakaneko@users.noreply.github.com>
2022-09-28 17:06:19 +0900

    always trigger native scrollIntoView instead

--- resources/assets/lib/beatmap-discussions/editor.tsx
+++ resources/assets/lib/beatmap-discussions/editor.tsx
@@ -12,7 +12,6 @@ import { route } from 'laroute';
 import { filter } from 'lodash';
 import core from 'osu-core-singleton';
 import * as React from 'react';
-import scrollIntoView from 'scroll-into-view-if-needed';
 import { createEditor, Editor as SlateEditor, Element as SlateElement, Node as SlateNode, NodeEntry, Range, Text, Transforms } from 'slate';
 import { withHistory } from 'slate-history';
 import { Editable, ReactEditor, RenderElementProps, RenderLeafProps, Slate, withReact } from 'slate-react';
@@ -418,24 +417,14 @@ export default class Editor extends React.Component<Props, State> {
     this.resetEditorValue();
   };
 
-  // overrides slate Editable's defaultScrollSelectionIntoView with a boundary to stop body from scrolling on input.
-  // https://github.com/ianstormtaylor/slate/blob/ca9e2147c17cc22bb24c098651c12da60d44d8b9/packages/slate-react/src/components/editable.tsx#L1170
+  // Overrides slate Editable's defaultScrollSelectionIntoView and always use browser's version
+  // There's something wrong with slate's range detection in the prior calling function that causes
+  // letters and numbers to be differently.
   scrollSelectionIntoView = (editor: ReactEditor, domRange: DOMRange) => {
-    if (!editor.selection
-      || (editor.selection && Range.isCollapsed(editor.selection))
-    ) {
-      const leafEl = domRange.startContainer.parentElement;
-      if (leafEl != null) {
-        leafEl.getBoundingClientRect = () => domRange.getBoundingClientRect();
-        scrollIntoView(leafEl, {
-          boundary: this.scrollContainerRef.current,
-          scrollMode: 'if-needed',
-        });
-
-        // @ts-expect-error an unorthodox delete D:
-        delete leafEl.getBoundingClientRect;
-      }
-    }
+    domRange.startContainer.parentElement?.scrollIntoView({
+      block: 'nearest',
+      inline: 'nearest' ,
+    });
   };
 
   serialize = () => serializeSlateDocument(this.state.value);
