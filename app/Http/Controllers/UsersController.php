dcbf202414433e076db953408baa9456dac5d78b
bakaneko <notbakaneko@users.noreply.github.com>
2022-10-13 21:46:02 +0900

    cursor would be in getExtraSection...I think?

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -143,7 +143,6 @@ class UsersController extends Controller
     public function extraPages($_id, $page)
     {
         // TODO: counts basically duplicated from UserCompactTransformer
-        // TOOD: switch to cursor pagination?
         switch ($page) {
             case 'beatmaps':
                 return [
