9befee43b884106f57d64b8a6008ca8320b176dd
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-10-25 09:21:45 -0300

    Add license to top of ContestEntriesControllerTest.php

--- tests/Controllers/ContestEntriesControllerTest.php
+++ tests/Controllers/ContestEntriesControllerTest.php
@@ -1,11 +1,13 @@
 <?php
 
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
 namespace Tests\Controllers;
 
 use App\Models\Contest;
 use App\Models\User;
 use Carbon\Carbon;
-use DateTime;
 use Illuminate\Http\Testing\File;
 use Tests\TestCase;
 
