f4f8369ac753a2f7fa54c1f5135c8dcbf478369d
nanaya <me@nanaya.pro>
2022-10-31 22:02:53 +0900

    Use solo scores for profile page recent scores

--- app/Models/Solo/Score.php
+++ app/Models/Solo/Score.php
@@ -106,6 +106,18 @@ class Score extends Model implements Traits\ReportableInterface
         return $query->whereHas('beatmap.beatmapset');
     }
 
+    public function scopeForRuleset(Builder $query, string $ruleset): Builder
+    {
+        return $query->where('ruleset_id', Beatmap::MODES[$ruleset]);
+    }
+
+    public function scopeIncludeFails(Builder $query, bool $includeFails): Builder
+    {
+        return $includeFails
+            ? $query
+            : $query->where('data->passed', true);
+    }
+
     /**
      * This should match the one used in osu-elastic-indexer.
      */
