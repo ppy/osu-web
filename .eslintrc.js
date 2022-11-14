c37fab83fc3a07465fdb3f34d4b339673b7fc5bc
nanaya <me@nanaya.pro>
2022-11-14 18:33:00 +0900

    Readd the actual lint
    I thought it was in the recommended but it's not.

--- .eslintrc.js
+++ .eslintrc.js
@@ -122,6 +122,7 @@ module.exports = {
         ],
         '@typescript-eslint/semi': ['error', 'always'],
         '@typescript-eslint/type-annotation-spacing': 'error',
+        '@typescript-eslint/unbound-method': 'error',
         '@typescript-eslint/unified-signatures': 'error',
         'dot-notation': 'off',
         'no-invalid-this': 'off',
