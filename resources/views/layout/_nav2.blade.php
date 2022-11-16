49d41f51c71237453a06e2ce36609cb6c9b3ab74
nanaya <me@nanaya.pro>
2022-11-16 16:11:21 +0900

    Fix notification popup not correctly hid

--- resources/views/layout/_nav2.blade.php
+++ resources/views/layout/_nav2.blade.php
@@ -112,7 +112,7 @@
                         class="nav-click-popup js-click-menu js-react--notification-widget"
                         data-click-menu-id="nav2-chat-notification-widget"
                         data-visibility="hidden"
-                        data-notification-widget="{{ json_encode(['extraClasses' => 'js-nav2--centered-popup', 'only' => 'channel']) }}"
+                        data-notification-widget="{{ json_encode(['extraClasses' => 'js-nav2--centered-popup hidden', 'only' => 'channel']) }}"
                         data-turbolinks-permanent
                         id="notification-widget-chat"
                     ></div>
@@ -133,7 +133,7 @@
                         class="nav-click-popup js-click-menu js-react--notification-widget"
                         data-click-menu-id="nav2-notification-widget"
                         data-visibility="hidden"
-                        data-notification-widget="{{ json_encode(['extraClasses' => 'js-nav2--centered-popup', 'excludes' => ['channel']]) }}"
+                        data-notification-widget="{{ json_encode(['extraClasses' => 'js-nav2--centered-popup hidden', 'excludes' => ['channel']]) }}"
                         data-turbolinks-permanent
                         id="notification-widget"
                     ></div>
