292101840f1cd5ec953afe4387264656ac29a5db
nanaya <me@nanaya.pro>
2022-11-22 16:53:02 +0900

    Typescript Modal component

--- resources/assets/lib/components/report-form.tsx
+++ resources/assets/lib/components/report-form.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { Modal } from 'components/modal';
+import Modal from 'components/modal';
 import { SelectOptions } from 'components/select-options';
 import { intersectionWith } from 'lodash';
 import { action, computed, makeObservable, observable } from 'mobx';
