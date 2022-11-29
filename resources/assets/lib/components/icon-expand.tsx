c86aec3117e21f745dd6b6906997bdf1602d3ba8
bakaneko <notbakaneko@users.noreply.github.com>
2022-11-29 20:16:34 +0900

    convert to typescript

---
+++ resources/assets/lib/components/icon-expand.tsx
@@ -0,0 +1,44 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import * as React from 'react';
+
+interface IconProps {
+  icon: 'angle-down' | 'angle-up';
+  position: 'bottom' | 'top';
+}
+
+interface Props {
+  expand: boolean;
+  parentClass: string;
+}
+
+function Icon({ position, icon }: IconProps) {
+  return (
+    <span className={`icon-stack__icon icon-stack__icon--${position}`}
+    >
+      <i className={`fas fa-fw fa-${icon}`} />
+    </span>
+  );
+}
+
+export function IconExpand({ expand = true, parentClass }: Props) {
+  return (
+    <span className={`icon-stack ${parentClass}`}>
+      <span className='icon-stack__base'>
+        <i className='fas fa-fw fa-angle-down' />
+      </span>
+      {expand ? (
+        <>
+          <Icon icon='angle-up' position='top' />
+          <Icon icon='angle-down' position='bottom' />
+        </>
+      ) : (
+        <>
+          <Icon icon='angle-down' position='top' />
+          <Icon icon='angle-up' position='bottom' />
+        </>
+      )}
+    </span>
+  );
+}
