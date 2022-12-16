c3a8e13becc407ded6437e88667d18f05d10e27e
Venix <30481900+venix12@users.noreply.github.com>
2022-12-16 16:22:46 +0100

    factory & route test scaffoloding

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
@@ -16,6 +17,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  */
 class Season extends Model
 {
+    use HasFactory;
+
     protected $casts = [
         'concluded' => 'boolean',
     ];
