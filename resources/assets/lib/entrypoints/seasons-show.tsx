76240dc460e3cff3214ae4d89f4956e8bdb7769d
Venix <30481900+venix12@users.noreply.github.com>
2022-12-16 19:55:36 +0100

    simplify entrypoint

--- resources/assets/lib/entrypoints/seasons-show.tsx
+++ resources/assets/lib/entrypoints/seasons-show.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import RoomJson from 'interfaces/room-json';
-import SeasonJson from 'interfaces/season-json';
 import core from 'osu-core-singleton';
 import * as React from 'react';
 import Main from 'seasons-show/main';
@@ -10,18 +8,14 @@ import MultiplayerListStore from 'stores/multiplayer-list-store';
 import { parseJson } from 'utils/json';
 
 core.reactTurbolinks.register('seasons-show', () => {
-  const currentSeason: SeasonJson = parseJson('json-currentSeason');
-  const rooms: RoomJson[] = parseJson('json-rooms');
-  const seasons: SeasonJson[] = parseJson('json-seasons');
-
   const store = new MultiplayerListStore();
   store.updateWithJson(parseJson('json-rooms'));
 
   return (
     <Main
-      currentSeason={currentSeason}
-      rooms={rooms}
-      seasons={seasons}
+      currentSeason={parseJson('json-currentSeason')}
+      rooms={parseJson('json-rooms')}
+      seasons={parseJson('json-seasons')}
       store={store}
     />
   );
