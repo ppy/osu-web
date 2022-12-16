e10c88de22175f5e6865d89cbd751665d5c001aa
Venix <30481900+venix12@users.noreply.github.com>
2022-12-16 16:22:46 +0100

    factory & route test scaffoloding

---
+++ database/factories/SeasonFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Season;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SeasonFactory extends Factory
+{
+    protected $model = Season::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(),
+        ];
+    }
+}
