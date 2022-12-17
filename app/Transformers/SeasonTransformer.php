ecce4611c32231145463baa3fe85ac4abad21e1f
Venix <30481900+venix12@users.noreply.github.com>
2022-12-17 14:27:56 +0100

    strict typing on transformer

--- app/Transformers/SeasonTransformer.php
+++ app/Transformers/SeasonTransformer.php
@@ -3,13 +3,15 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+declare(strict_types=1);
+
 namespace App\Transformers;
 
 use App\Models\Season;
 
 class SeasonTransformer extends TransformerAbstract
 {
-    public function transform(Season $season)
+    public function transform(Season $season): array
     {
         return [
             'end_date' => $season->endDate(),
