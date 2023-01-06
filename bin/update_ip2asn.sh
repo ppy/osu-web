51f022e9d9df025055a1cd6b400c794c79f0e3c4
nanaya <me@nanaya.net>
2023-01-06 21:32:07 +0900

    Add message downloading the database

--- bin/update_ip2asn.sh
+++ bin/update_ip2asn.sh
@@ -3,6 +3,10 @@
 set -e
 set -u
 
+echo 'Updating ip2asn database...'
+
 outfile="$(dirname "${0}")/../database/ip2asn-combined.tsv"
 curl https://iptoasn.com/data/ip2asn-combined.tsv.gz | gzip -d > "${outfile}.new"
 mv "${outfile}.new" "${outfile}"
+
+echo 'Finished updating ip2asn database.'
