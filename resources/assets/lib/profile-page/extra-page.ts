9e3a90e8e004c9a0c1af6ff949b75a36808badbc
bakaneko <notbakaneko@users.noreply.github.com>
2023-01-16 18:04:16 +0900

    send mode when requesting extra-page

--- resources/assets/lib/profile-page/extra-page.ts
+++ resources/assets/lib/profile-page/extra-page.ts
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+import GameMode from 'interfaces/game-mode';
 import { ProfileExtraPage } from 'interfaces/user-extended-json';
 import UserJson from 'interfaces/user-json';
 import { route } from 'laroute';
@@ -18,6 +19,7 @@ export interface PageSectionWithoutCountJson<T> {
   pagination: OffsetPaginationJson;
 }
 
-export default function getPage<T>(user: Pick<UserJson, 'id'>, page: ProfileExtraPage) {
-  return $.ajax(route('users.extra-page', { page, user: user.id })) as JQuery.jqXHR<T>;
+// TODO: how to require mode conditionally based on page?
+export default function getPage<T>(user: Pick<UserJson, 'id'>, page: ProfileExtraPage, mode?: GameMode) {
+  return $.ajax(route('users.extra-page', { mode, page, user: user.id })) as JQuery.jqXHR<T>;
 }
