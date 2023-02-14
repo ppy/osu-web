2445bed37252dc28bde3be8043655299551ae33e
nanaya <me@nanaya.net>
2023-02-14 20:08:49 +0900

    Add 2x image url to profile banner

--- resources/js/interfaces/profile-banner.ts
+++ resources/js/interfaces/profile-banner.ts
@@ -1,8 +1,19 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-export default interface ProfileBannerJson {
+interface ProfileBannerJsonBase {
   id: number;
-  image: string;
   tournament_id: number;
 }
+
+type ProfileBannerJson = ProfileBannerJsonBase & (
+  {
+    image: string;
+    'image@2x': string;
+  } | {
+    image: null;
+    'image@2x': null;
+  }
+);
+
+export default ProfileBannerJson;
