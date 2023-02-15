eda54ea2acb6c8969cd9d7c6ef3b230a145dc41b
bakaneko <notbakaneko@users.noreply.github.com>
2023-02-15 20:57:11 +0900

    render spinner instead of 1px dot

--- resources/js/beatmap-discussions/image-link.tsx
+++ resources/js/beatmap-discussions/image-link.tsx
@@ -1,7 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { action } from 'mobx';
+import { Spinner } from 'components/spinner';
+import { action, makeObservable, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import React from 'react';
 import { ReactMarkdownProps } from 'react-markdown/lib/complex-types';
@@ -9,12 +10,17 @@ import DiscussionsStateContext from './discussions-state-context';
 
 type Props = ReactMarkdownProps & React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
-const placeholder = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-
 @observer
 export default class ImageLink extends React.Component<Props> {
   static contextType = DiscussionsStateContext;
   declare context: React.ContextType<typeof DiscussionsStateContext>;
+  @observable private loaded = false;
+
+  constructor(props: Props) {
+    super(props);
+
+    makeObservable(this);
+  }
 
   @action
   componentDidMount(): void {
@@ -25,17 +31,30 @@ export default class ImageLink extends React.Component<Props> {
 
   render() {
     const url = this.props.src != null ? this.context.mediaUrls.get(this.props.src) : null;
-    // render node mutation when url changes to trigger Layzr
+
     if (url == null) {
       return (
-        <img {...this.props.node.properties} src={placeholder} />
+        <span className='beatmapset-discussion-image-link'>
+          {this.renderSpinner()}
+        </span>
       );
     }
 
+    // TODO: render something else on fail?
     return (
-      <a href={url} rel='nofollow noreferrer' target='_blank'>
-        <img {...this.props.node.properties} data-normal={url} src={placeholder} />
+      <a className='beatmapset-discussion-image-link' href={url} rel='nofollow noreferrer' target='_blank'>
+        {!this.loaded && this.renderSpinner()}
+        <img {...this.props.node.properties} onLoad={this.handleOnLoad} src={url} />
       </a>
     );
+  }
+
+  @action
+  private handleOnLoad = () => {
+    this.loaded = true;
+  };
+
+  private renderSpinner() {
+    return <span className='beatmapset-discussion-image-link__spinner'><Spinner /></span>;
   }
 }
