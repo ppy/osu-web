b75b279ed5d9d0a00856a71fa75a443bd0870894
bakaneko <notbakaneko@users.noreply.github.com>
2023-02-15 19:54:55 +0900

    force up/down arrow navigation to move between lines;
    otherwise it gets stuck on timestamps

--- resources/js/beatmap-discussions/editor.tsx
+++ resources/js/beatmap-discussions/editor.tsx
@@ -230,7 +230,12 @@ export default class Editor extends React.Component<Props, State> {
 
   onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     const nativeEvent = event.nativeEvent;
-    if (isHotkey('mod+b', nativeEvent)) {
+    if (nativeEvent.key === 'ArrowDown') {
+      // TODO: only move to editable lines?
+      Transforms.move(this.slateEditor, { unit: 'line' });
+    } else if (nativeEvent.key === 'ArrowUp') {
+      Transforms.move(this.slateEditor, { distance: -1, unit: 'line' });
+    } else if (isHotkey('mod+b', nativeEvent)) {
       event.preventDefault();
       toggleFormat(this.slateEditor, 'bold');
     } else if (isHotkey('mod+i', nativeEvent)) {
