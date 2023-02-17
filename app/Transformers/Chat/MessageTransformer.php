c1c2ba772fb8d4ce62dffd7698385ec9c92c4102
bakaneko <notbakaneko@users.noreply.github.com>
2023-02-17 16:10:49 +0900

    order keys

--- app/Transformers/Chat/MessageTransformer.php
+++ app/Transformers/Chat/MessageTransformer.php
@@ -23,12 +23,12 @@ class MessageTransformer extends TransformerAbstract
             : ($message->channel->isAnnouncement() ? 'markdown' : 'plain');
 
         $response = [
-            'message_id' => $message->message_id,
-            'sender_id' => $message->user_id,
             'channel_id' => $message->channel_id,
-            'timestamp' => $message->timestamp_json,
             'content' => $message->content,
             'is_action' => $message->is_action, // maybe deprecate?
+            'message_id' => $message->message_id,
+            'sender_id' => $message->user_id,
+            'timestamp' => $message->timestamp_json,
             'type' => $type,
         ];
 
