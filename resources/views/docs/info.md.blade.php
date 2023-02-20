cc7bedbc8b6ea7084f2d75106c7a64b8fe31e1e4
Liam DeVoe <orionldevoe@gmail.com>
2023-02-20 16:39:17 -0500

    add link to ossapi api wrapper

--- resources/views/docs/info.md.blade.php
+++ resources/views/docs/info.md.blade.php
@@ -18,6 +18,12 @@ Use the API for good. Don't overdo it. If in doubt, ask before (ab)using :). _th
 
 Current rate limit is set at an insanely high 1200 requests per minute, with burst capability of up to 200 beyond that. If you require more, you probably fall into the above category of abuse. If you are doing more than 60 requests a minute, you should probably give [peppy](mailto:pe@ppy.sh) a yell.
 
+# Wrappers
+
+There are community maintained wrappers for the api:
+
+- [ossapi](https://github.com/circleguard/ossapi) (python)
+
 # Changelog
 
 For a full list of changes, see the
