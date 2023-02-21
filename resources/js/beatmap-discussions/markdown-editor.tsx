b506db38939ed1b34296548ad4b459710dec7503
bakaneko <notbakaneko@users.noreply.github.com>
2023-02-21 22:15:45 +0900

    wip

--- resources/js/beatmap-discussions/markdown-editor.tsx
+++ resources/js/beatmap-discussions/markdown-editor.tsx
@@ -1,10 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import { makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 import TextareaAutosize from 'react-autosize-textarea/lib';
+import { classWithModifiers } from 'utils/css';
 import DiscussionMessage from './discussion-message';
 import DiscussionMessageLengthCounter from './discussion-message-length-counter';
 
@@ -36,6 +37,18 @@ export default class MarkdownEditor extends React.Component<Props> {
 
     return (
       <div className='markdown-editor'>
+        <div className='page-tabs'>
+          {['write', 'preview'].map((mode) => (
+            <button
+              key={mode}
+              className={classWithModifiers('page-tabs__tab', { active: mode === this.mode })}
+              data-mode={mode}
+              onClick={this.handleClick}
+            >
+              {mode}
+            </button>
+          ))}
+        </div>
         {this.mode === 'write' ? (
           <>
             <TextareaAutosize
@@ -51,4 +64,9 @@ export default class MarkdownEditor extends React.Component<Props> {
       </div>
     );
   }
+
+  @action
+  private handleClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
+    this.mode = event.currentTarget.dataset.mode ?? 'write';
+  };
 }
