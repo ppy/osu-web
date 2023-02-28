cc3dcbaf88b6b8d4797a816c2236585009e5cb21
bakaneko <notbakaneko@users.noreply.github.com>
2023-03-01 08:50:45 +0900

    convert user profile modding discussions component to typescript

--- resources/js/modding-profile/discussions.tsx
+++ resources/js/modding-profile/discussions.tsx
@@ -1,50 +1,67 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import { BeatmapsContext } from 'beatmap-discussions/beatmaps-context'
-import { BeatmapsetsContext } from 'beatmap-discussions/beatmapsets-context'
-import BeatmapsetCover from 'components/beatmapset-cover'
-import { route } from 'laroute'
-import * as React from 'react'
-import { div, h2, a, img } from 'react-dom-factories'
-import { makeUrl } from 'utils/beatmapset-discussion-helper'
-import { trans } from 'utils/lang'
-import { Discussion } from "../beatmap-discussions/discussion"
+import { BeatmapsContext } from 'beatmap-discussions/beatmaps-context';
+import { BeatmapsetsContext } from 'beatmap-discussions/beatmapsets-context';
+import { Discussion } from 'beatmap-discussions/discussion';
+import BeatmapsetCover from 'components/beatmapset-cover';
+import { BeatmapsetDiscussionJsonForBundle } from 'interfaces/beatmapset-discussion-json';
+import UserJson from 'interfaces/user-json';
+import { route } from 'laroute';
+import core from 'osu-core-singleton';
+import React from 'react';
+import { makeUrl } from 'utils/beatmapset-discussion-helper';
+import { trans } from 'utils/lang';
 
-el = React.createElement
 
-export class Discussions extends React.Component
-  render: =>
-    div className: 'page-extra',
-      h2 className: 'title title--page-extra', trans("users.show.extra.discussions.title_longer")
-      div className: 'modding-profile-list',
-        if @props.discussions.length == 0
-          div className: 'modding-profile-list__empty', trans('users.show.extra.none')
-        else
-          el BeatmapsetsContext.Consumer, null, (beatmapsets) =>
-            el BeatmapsContext.Consumer, null, (beatmaps) =>
-              el React.Fragment, null,
-                for discussion in @props.discussions
-                  div
-                    className: 'modding-profile-list__row'
-                    key: discussion.id,
+interface Props {
+  discussions: BeatmapsetDiscussionJsonForBundle[];
+  user: UserJson;
+  users: Partial<Record<number, UserJson>>;
+}
 
-                    a
-                      className: 'modding-profile-list__thumbnail'
-                      href: makeUrl(discussion: discussion),
-
-                      el BeatmapsetCover, beatmapset: beatmapsets[discussion.beatmapset_id], size: 'list'
-
-                    el Discussion,
-                      discussion: discussion
-                      users: @props.users
-                      currentBeatmap: beatmaps[discussion.beatmap_id]
-                      currentUser: currentUser
-                      beatmapset: beatmapsets[discussion.beatmapset_id]
-                      isTimelineVisible: false
-                      showDeleted: true
-                      preview: true
-                a
-                  className: 'modding-profile-list__show-more'
-                  href: route('beatmapsets.discussions.index', {user: @props.user.id}),
-                  trans('users.show.extra.discussions.show_more')
+export class Discussions extends React.Component<Props> {
+  render() {
+    return (
+      <div className='page-extra'>
+        <h2 className='title title--page-extra'>{trans('users.show.extra.discussions.title_longer')}</h2>
+        <div className='modding-profile-list'>
+          {this.props.discussions.length === 0 ? (
+            <div className='modding-profile-list__empty'>{trans('users.show.extra.none')}</div>
+          ) : (
+            <BeatmapsetsContext.Consumer>
+              {(beatmapsets) => (
+                <BeatmapsContext.Consumer>
+                  {(beatmaps) => (
+                    <>
+                      {this.props.discussions.map((discussion) => (
+                        <div key={discussion.id} className='modding-profile-list__row'>
+                          <a className='modding-profile-list__thumbnail' href={makeUrl({ discussion })}>
+                            <BeatmapsetCover beatmapset={beatmapsets[discussion.beatmapset_id]} size='list' />
+                          </a>
+                          <Discussion
+                            beatmapset={beatmapsets[discussion.beatmapset_id]}
+                            currentBeatmap={beatmaps[discussion.beatmap_id]}
+                            currentUser={core.currentUser}
+                            discussion={discussion}
+                            isTimelineVisible={false}
+                            preview
+                            showDeleted
+                            users={this.props.users}
+                          />
+                        </div>
+                      ))}
+                      <a className='modding-profile-list__show-more' href={route('beatmapsets.discussions.index', { user: this.props.user.id })}>
+                        {trans('users.show.extra.discussions.show_more')}
+                      </a>
+                    </>
+                  )}
+                </BeatmapsContext.Consumer>
+              )}
+            </BeatmapsetsContext.Consumer>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
