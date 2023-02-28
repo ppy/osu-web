878bf7c7bd9c525fed1d3f1897fbf6aebbfab31f
nanaya <me@nanaya.net>
2023-02-28 20:45:14 +0900

    Dynamic xml generation

---
+++ resources/views/home/opensearch.blade.php
@@ -0,0 +1,13 @@
+@php
+    $appUrl = config('app.url');
+    $searchUrl = route('search');
+@endphp
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+    <ShortName>osu!</ShortName>
+    <Description>osu! search</Description>
+    <Url method="get" template="{{ $searchUrl }}?query={searchTerms}" type="text/html"/>
+    <Image height="16" width="16" type="image/png">{{ $appUrl }}/images/favicon/favicon-16x16.png</Image>
+    <Image height="32" width="32" type="image/png">{{ $appUrl }}/images/favicon/favicon-32x32.png</Image>
+    <moz:SearchForm>{{ $searchUrl }}</moz:SearchForm>
+    <Url type="application/opensearchdescription+xml" rel="self" template="{{ $appUrl }}/opensearch.xml" />
+</OpenSearchDescription>
