609d2b82b6c8707af8fa6229b87a436f0db4956a
nanaya <me@nanaya.net>
2023-03-02 22:20:56 +0900

    Typescript modding profile stats

--- resources/js/modding-profile/stats.tsx
+++ resources/js/modding-profile/stats.tsx
@@ -1,29 +1,38 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import * as React from 'react'
-import { div, dd, dl, dt } from 'react-dom-factories'
-import { classWithModifiers } from 'utils/css'
-import { formatNumber } from 'utils/html'
-import { trans } from 'utils/lang'
+import UserJson from 'interfaces/user-extended-json';
+import * as React from 'react';
+import { classWithModifiers } from 'utils/css';
+import { formatNumber } from 'utils/html';
+import { trans } from 'utils/lang';
 
-entries = [
-  'ranked_beatmapset_count'
-  'loved_beatmapset_count'
-  'pending_beatmapset_count'
-  'graveyard_beatmapset_count'
-]
+const entries = [
+  'ranked_beatmapset_count',
+  'loved_beatmapset_count',
+  'pending_beatmapset_count',
+  'graveyard_beatmapset_count',
+] as const;
+type UserBeatmapsetCount = typeof entries[number];
 
-export class Stats extends React.PureComponent
-  render: =>
-    div
-      className: classWithModifiers 'profile-stats', 'modding'
-      entries.map(@renderEntry)
+interface Props {
+  // TODO: add actual typing for modding profile user json
+  user: UserJson & Required<Pick<UserJson, UserBeatmapsetCount>>;
+}
 
+export default class Stats extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div className={classWithModifiers('profile-stats', 'modding')}>
+        {entries.map(this.renderEntry)}
+      </div>
+    );
+  }
 
-  renderEntry: (key) =>
-    dl
-      className: 'profile-stats__entry'
-      key: key
-      dt className: 'profile-stats__key', trans("users.show.stats.#{key}")
-      dd className: 'profile-stats__value', formatNumber(@props.user[key])
+  private readonly renderEntry = (key: UserBeatmapsetCount) => (
+    <dl key={key} className='profile-stats__entry'>
+      <dt className='profile-stats__key'>{trans(`users.show.stats.${key}`)}</dt>
+      <dd className='profile-stats__value'>{formatNumber(this.props.user[key])}</dd>
+    </dl>
+  );
+}
