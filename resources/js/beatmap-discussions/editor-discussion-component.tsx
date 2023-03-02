fba882d06c0c5153a4769934e2ee269314caede5
bakaneko <notbakaneko@users.noreply.github.com>
2023-03-02 14:30:25 +0900

    moved to element

--- resources/js/beatmap-discussions/editor-discussion-component.tsx
+++ resources/js/beatmap-discussions/editor-discussion-component.tsx
@@ -31,7 +31,6 @@ interface Cache {
 interface Props extends RenderElementProps {
   beatmaps: BeatmapExtendedJson[];
   beatmapset: BeatmapsetJson;
-  discussionId?: number;
   discussions: Partial<Record<number, BeatmapsetDiscussionJson>>;
   editMode?: boolean;
   element: EmbedElement;
