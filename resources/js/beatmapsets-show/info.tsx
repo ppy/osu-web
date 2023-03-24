066b5dd1da3c90e311fb39a672ada57966ec7890
nanaya <me@nanaya.net>
2023-03-24 11:27:59 +0900

    Overflow tags

--- resources/js/beatmapsets-show/info.tsx
+++ resources/js/beatmapsets-show/info.tsx
@@ -85,17 +85,10 @@ export default class Info extends React.Component<Props> {
   render() {
     const tags = this.controller.beatmapset.tags
       .split(' ')
-      .filter(present)
-      .slice(0, 21);
-
-    const tagsOverload = tags.length === 21;
-
-    if (tagsOverload) {
-      tags.pop();
-    }
+      .filter(present);
 
     return (
-      <div className='beatmapset-info'>
+      <div className='beatmapset-info u-fancy-scrollbar'>
         {this.isEditingDescription &&
           <Modal onClose={this.handleCloseDescriptionEditor}>
             <div className='osu-page'>
@@ -116,16 +109,16 @@ export default class Info extends React.Component<Props> {
           </Modal>
         }
 
-        <div className='beatmapset-info__box beatmapset-info__box--description'>
+        <div className='beatmapset-info__box'>
           {this.withEditDescription && this.renderEditDescriptionButton()}
 
-          <h3 className='beatmapset-info__header'>
-            {trans('beatmapsets.show.info.description')}
-          </h3>
+          <div className='beatmapset-info__row beatmapset-info__row--value-overflow'>
+            <h3 className='beatmapset-info__header'>
+              {trans('beatmapsets.show.info.description')}
+            </h3>
 
-          <div className='beatmapset-info__description-container u-fancy-scrollbar'>
             <div
-              className='beatmapset-info__description'
+              className='beatmapset-info__value-overflow'
               dangerouslySetInnerHTML={{
                 __html: this.controller.beatmapset.description.description ?? '',
               }}
@@ -133,11 +126,11 @@ export default class Info extends React.Component<Props> {
           </div>
         </div>
 
-        <div className='beatmapset-info__box beatmapset-info__box--meta'>
+        <div className='beatmapset-info__box'>
           {this.withEditMetadata && this.renderEditMetadataButton()}
 
           {this.nominators.length > 0 &&
-            <>
+            <div className='beatmapset-info__row'>
               <h3 className='beatmapset-info__header'>
                 {trans('beatmapsets.show.info.nominators')}
               </h3>
@@ -152,11 +145,11 @@ export default class Info extends React.Component<Props> {
                   </React.Fragment>
                 ))}
               </div>
-            </>
+            </div>
           }
 
           {present(this.controller.beatmapset.source) &&
-            <>
+            <div className='beatmapset-info__row'>
               <h3 className='beatmapset-info__header'>
                 {trans('beatmapsets.show.info.source')}
               </h3>
@@ -166,10 +159,10 @@ export default class Info extends React.Component<Props> {
               >
                 {this.controller.beatmapset.source}
               </a>
-            </>
+            </div>
           }
 
-          <div className='beatmapset-info__half-box'>
+          <div className='beatmapset-info__row beatmapset-info__row--half'>
             <div className='beatmapset-info__half-entry'>
               <h3 className='beatmapset-info__header'>
                 {trans('beatmapsets.show.info.genre')}
@@ -196,11 +189,11 @@ export default class Info extends React.Component<Props> {
           </div>
 
           {tags.length > 0 &&
-            <>
+            <div className='beatmapset-info__row beatmapset-info__row--value-overflow'>
               <h3 className='beatmapset-info__header'>
                 {trans('beatmapsets.show.info.tags')}
               </h3>
-              <div>
+              <div className='beatmapset-info__value-overflow'>
                 {tags.map((tag, i) => (
                   <React.Fragment key={`${tag}-${i}`}>
                     <a
@@ -212,9 +205,8 @@ export default class Info extends React.Component<Props> {
                     {' '}
                   </React.Fragment>
                 ))}
-                {tagsOverload && '...'}
               </div>
-            </>
+            </div>
           }
         </div>
 
