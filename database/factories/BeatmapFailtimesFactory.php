2452f70b1697ac86af1b542550b2eb1c5123e71a
nanaya <me@nanaya.net>
2023-03-29 17:24:29 +0900

    Add class based factory for BeatmapFailtimes

--- database/factories/BeatmapFailtimesFactory.php
+++ database/factories/BeatmapFailtimesFactory.php
@@ -2,17 +2,36 @@
 
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
-$factory->define(App\Models\BeatmapFailtimes::class, function (Faker\Generator $faker) {
-    $array = [];
 
-    for ($i = 1; $i <= 100; $i++) {
-        $field = 'p'.strval($i);
-        $array = array_merge($array, [$field => rand(1, 10000)]);
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\BeatmapFailtimes;
+
+class BeatmapFailtimesFactory extends Factory
+{
+    protected $model = BeatmapFailtimes::class;
+
+    public function definition(): array
+    {
+        $array = [];
+
+        for ($i = 1; $i <= 100; $i++) {
+            $field = 'p'.strval($i);
+            $array[$field] = rand(1, 10000);
+        }
+
+        return $array;
     }
 
-    return $array;
-});
+    public function fail(): static
+    {
+        return $this->state(['type' => 'fail']);
+    }
 
-$factory->state(App\Models\BeatmapFailtimes::class, 'fail', ['type' => 'fail']);
-
-$factory->state(App\Models\BeatmapFailtimes::class, 'retry', ['type' => 'exit']);
+    public function retry(): static
+    {
+        return $this->state(['type' => 'exit']);
+    }
+}
