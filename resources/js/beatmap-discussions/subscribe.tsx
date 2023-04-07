a46051293a9703decf250713b677e6f0b9a9e5d8
bakaneko <notbakaneko@users.noreply.github.com>
2023-04-07 20:59:35 +0900

    convert to typescript (and mobx)

--- resources/js/beatmap-discussions/subscribe.tsx
+++ resources/js/beatmap-discussions/subscribe.tsx
@@ -1,50 +1,67 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import BigButton from 'components/big-button'
-import { route } from 'laroute'
-import * as React from 'react'
-import { a, button, div, h1, h2, p } from 'react-dom-factories'
-import { emitError } from 'utils/ajax'
-import { trans } from 'utils/lang'
+import BigButton from 'components/big-button';
+import BeatmapsetJson from 'interfaces/beatmapset-json';
+import { route } from 'laroute';
+import { action, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react';
+import * as React from 'react';
+import { emitError } from 'utils/ajax';
+import { trans } from 'utils/lang';
 
-el = React.createElement
+interface Props {
+  beatmapset: BeatmapsetJson;
+}
 
-export class Subscribe extends React.PureComponent
-  constructor: (props) ->
-    super props
+@observer
+export class Subscribe extends React.Component<Props> {
+  @observable xhr: JQuery.jqXHR | null = null;
 
-    @state = loading: false
+  constructor(props: Props) {
+    super(props);
 
+    makeObservable(this);
+  }
 
-  componentWillUnmount: =>
-    @xhr?.abort()
+  private get isWatching() {
+    return this.props.beatmapset.current_user_attributes?.is_watching ?? false;
+  }
 
+  private get loading() {
+    return this.xhr != null;
+  }
 
-  render: =>
-    el BigButton,
-      disabled: @state.loading
-      icon: if @isWatching() then 'fas fa-eye-slash' else 'fas fa-eye'
-      isBusy: @state.loading
-      modifiers: 'full'
-      props:
-        onClick: @toggleWatch
-      text: trans "common.buttons.watch.to_#{+!@isWatching()}"
+  componentWillUnmount() {
+    this.xhr?.abort();
+  }
 
+  render() {
+    return (
+      <BigButton
+        disabled={this.loading}
+        icon={this.isWatching ? 'fas fa-eye-slash' : 'fas fa-eye'}
+        isBusy={this.loading}
+        modifiers='full'
+        props={{
+          onClick: this.toggleWatch,
+        }}
+        text={trans(`common.buttons.watch.to_${+!this.isWatching}`)}
+      />
+    );
+  }
 
-  isWatching: =>
-    @props.beatmapset.current_user_attributes?.is_watching
+  @action
+  private readonly toggleWatch = () => {
+    this.xhr = $.ajax(route('beatmapsets.watches.update', { watch: this.props.beatmapset.id }), {
+      dataType: 'json',
+      type: this.isWatching ? 'DELETE' : 'PUT',
+    });
 
-
-  toggleWatch: =>
-    @setState loading: true
-
-    @xhr = $.ajax route('beatmapsets.watches.update', watch: @props.beatmapset.id),
-      type: if @isWatching() then 'DELETE' else 'PUT'
-      dataType: 'json'
-    .done (data) =>
-      $.publish 'beatmapsetDiscussions:update', watching: !@isWatching()
-    .fail (xhr) =>
-      emitError() xhr
-    .always =>
-      @setState loading: false
+    this.xhr.done(() => {
+      $.publish('beatmapsetDiscussions:update', { watching: !this.isWatching });
+    })
+      .fail(emitError())
+      .always(action(() => this.xhr = null));
+  };
+}
