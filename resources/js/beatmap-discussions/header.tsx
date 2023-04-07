45ef08e39abde0966ac1860be48df408df895a27
bakaneko <notbakaneko@users.noreply.github.com>
2023-04-07 18:57:00 +0900

    convert to typescript

--- resources/js/beatmap-discussions/header.tsx
+++ resources/js/beatmap-discussions/header.tsx
@@ -1,214 +1,257 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@.ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import headerLinks from 'beatmapsets-show/header-links'
-import BeatmapBasicStats from 'components/beatmap-basic-stats'
-import BeatmapsetBadge from 'components/beatmapset-badge'
-import BeatmapsetCover from 'components/beatmapset-cover'
-import BeatmapsetMapping from 'components/beatmapset-mapping'
-import BigButton from 'components/big-button'
-import HeaderV4 from 'components/header-v4'
-import PlaymodeTabs from 'components/playmode-tabs'
-import StringWithComponent from 'components/string-with-component'
-import UserLink from 'components/user-link'
-import { gameModes } from 'interfaces/game-mode'
-import { route } from 'laroute'
-import { deletedUser } from 'models/user'
-import * as React from 'react'
-import { a, div, h1, h2, p, span } from 'react-dom-factories'
-import { getArtist, getTitle } from 'utils/beatmap-helper'
-import { makeUrl } from 'utils/beatmapset-discussion-helper'
-import { trans } from 'utils/lang'
-import BeatmapList from './beatmap-list'
-import Chart from './chart'
-import { Nominations } from './nominations'
-import { Subscribe } from './subscribe'
-import { UserFilter } from './user-filter'
-import { wikiUrl } from 'utils/url'
+import headerLinks from 'beatmapsets-show/header-links';
+import BeatmapBasicStats from 'components/beatmap-basic-stats';
+import BeatmapsetBadge from 'components/beatmapset-badge';
+import BeatmapsetCover from 'components/beatmapset-cover';
+import BeatmapsetMapping from 'components/beatmapset-mapping';
+import BigButton from 'components/big-button';
+import HeaderV4 from 'components/header-v4';
+import PlaymodeTabs from 'components/playmode-tabs';
+import StringWithComponent from 'components/string-with-component';
+import UserLink from 'components/user-link';
+import BeatmapExtendedJson from 'interfaces/beatmap-extended-json';
+import BeatmapJson from 'interfaces/beatmap-json';
+import BeatmapsetDiscussionJson from 'interfaces/beatmapset-discussion-json';
+import BeatmapsetEventJson from 'interfaces/beatmapset-event-json';
+import BeatmapsetExtendedJson from 'interfaces/beatmapset-extended-json';
+import GameMode, { gameModes } from 'interfaces/game-mode';
+import UserJson from 'interfaces/user-json';
+import { route } from 'laroute';
+import { kebabCase, size, snakeCase } from 'lodash';
+import { deletedUser } from 'models/user';
+import core from 'osu-core-singleton';
+import * as React from 'react';
+import { getArtist, getTitle } from 'utils/beatmap-helper';
+import { makeUrl } from 'utils/beatmapset-discussion-helper';
+import { trans } from 'utils/lang';
+import BeatmapList from './beatmap-list';
+import Chart from './chart';
+import CurrentDiscussions, { Filter } from './current-discussions';
+import { DiscussionPage } from './discussion-mode';
+import { Nominations } from './nominations';
+import { Subscribe } from './subscribe';
+import { UserFilter } from './user-filter';
 
+interface Props {
+  beatmaps: Map<GameMode, BeatmapExtendedJson[]>;
+  beatmapset: BeatmapsetExtendedJson;
+  currentBeatmap: BeatmapExtendedJson;
+  currentDiscussions: CurrentDiscussions;
+  currentFilter: Filter;
+  discussions: Partial<Record<number, BeatmapsetDiscussionJson>>;
+  discussionStarters: UserJson[];
+  events: BeatmapsetEventJson[];
+  mode: DiscussionPage;
+  selectedUserId: number | null;
+  users: Partial<Record<number, UserJson>>;
+}
 
-el = React.createElement
+export class Header extends React.Component<Props> {
+  render() {
+    return (
+      <>
+        <HeaderV4
+          links={headerLinks('discussions', this.props.beatmapset)}
+          linksAppend={(
+            <PlaymodeTabs
+              currentMode={this.props.currentBeatmap.mode}
+              entries={gameModes.map((mode) => ({
+                count: this.props.currentDiscussions.countsByPlaymode[mode],
+                disabled: (this.props.beatmaps.get(mode)?.length ?? 0) === 0,
+                mode,
+              }))}
+              modifiers='beatmapset'
+              onClick={this.onClickMode}
+            />
+          )}
+          theme='beatmapset'
+        />
+        <div className='osu-page'>{this.renderHeaderTop()}</div>
+        <div className='osu-page osu-page--small'>{this.renderHeaderBottom()}</div>
+      </>
+    );
+  }
 
-export class Header extends React.PureComponent
-  render: =>
-    el React.Fragment, null,
-      el HeaderV4,
-        links: headerLinks 'discussions', @props.beatmapset
-        linksAppend: el PlaymodeTabs,
-          currentMode: @props.currentBeatmap.mode
-          entries: gameModes.map (mode) =>
-            counts: @props.currentDiscussions.countsByPlaymode[mode]
-            disabled: @props.beatmaps.get(mode).length == 0
-            mode: mode
-          modifiers: 'beatmapset'
-          onClick: @onClickMode
-        theme: 'beatmapset'
+  private readonly createLink = (beatmap: BeatmapJson) => makeUrl({ beatmap });
 
-      div
-        className: 'osu-page'
-        @headerTop()
+  private readonly getCount = (beatmap: BeatmapExtendedJson) => beatmap.deleted_at == null ? this.props.currentDiscussions.countsByBeatmap[beatmap.id] : undefined;
 
-      div
-        className: 'osu-page osu-page--small'
-        @headerBottom()
+  private onClickMode = (event: React.MouseEvent<HTMLAnchorElement>, mode: GameMode) => {
+    event.preventDefault();
+    $.publish('playmode:set', [{ mode }]);
+  };
 
+  private onSelectBeatmap = (beatmapId: number) => {
+    $.publish('beatmapsetDiscussions:update', {
+      beatmapId,
+      mode: 'timeline',
+    });
+  };
 
-  headerBottom: =>
-    bn = 'beatmap-discussions-header-bottom'
+  private renderHeaderBottom() {
+    const bn = 'beatmap-discussions-header-bottom';
 
-    div className: bn,
-      div className: "#{bn}__content #{bn}__content--details",
-        div className: "#{bn}__details #{bn}__details--full",
-          el BeatmapsetMapping,
-            beatmapset: @props.beatmapset
-            user: @props.users[@props.beatmapset.user_id]
-
-        div className: "#{bn}__details",
-          el Subscribe, beatmapset: @props.beatmapset
-
-        div className: "#{bn}__details",
-          el BigButton,
-            href: route('beatmapsets.show', beatmapset: @props.beatmapset.id)
-            icon: 'fas fa-info'
-            modifiers: 'full'
-            text: trans('beatmaps.discussions.beatmap_information')
-
-      div className: "#{bn}__content #{bn}__content--nomination",
-        el Nominations,
-          beatmapset: @props.beatmapset
-          currentDiscussions: @props.currentDiscussions
-          currentUser: @props.currentUser
-          discussions: @props.discussions
-          events: @props.events
-          users: @props.users
-
-
-  headerTop: =>
-    bn = 'beatmap-discussions-header-top'
-
-    div
-      className: bn
-
-      div
-        className: "#{bn}__content"
-
-        div className: "#{bn}__cover",
-          el BeatmapsetCover,
-            beatmapset: @props.beatmapset
-            modifiers: 'full'
-            size: 'cover'
-
-        div
-          className: "#{bn}__title-container"
-          h1
-            className: "#{bn}__title"
-            a
-              href: route('beatmapsets.show', beatmapset: @props.beatmapset.id)
-              className: 'link link--white link--no-underline'
-              getTitle(@props.beatmapset)
-            el BeatmapsetBadge, beatmapset: @props.beatmapset, type: 'nsfw'
-            el BeatmapsetBadge, beatmapset: @props.beatmapset, type: 'spotlight'
-          h2
-            className: "#{bn}__title #{bn}__title--artist"
-            getArtist(@props.beatmapset)
-            el BeatmapsetBadge, beatmapset: @props.beatmapset, type: 'featured_artist'
-
-        div
-          className: "#{bn}__filters"
-
-          div
-            className: "#{bn}__filter-group"
-            el BeatmapList,
-              beatmaps: @props.beatmaps.get(@props.currentBeatmap.mode)
-              beatmapset: @props.beatmapset
-              currentBeatmap: @props.currentBeatmap
-              createLink: @createLink
-              getCount: @getCount
-              onSelectBeatmap: @onSelectBeatmap
-              users: @props.users
-
-          div
-            className: "#{bn}__filter-group #{bn}__filter-group--stats"
-            el UserFilter,
-              ownerId: @props.beatmapset.user_id
-              selectedUser: if @props.selectedUserId? then @props.users[@props.selectedUserId] else null
-              users: @props.discussionStarters
-
-            div
-              className: "#{bn}__stats"
-              @stats()
-
-        div className: 'u-relative',
-          el Chart,
-            discussions: @props.currentDiscussions.byFilter[@props.currentFilter].timeline
-            duration: @props.currentBeatmap.total_length * 1000
-
-          div className: "#{bn}__beatmap-stats",
-            div className: "#{bn}__guest",
-              if @props.currentBeatmap.user_id != @props.beatmapset.user_id
-                span null,
-                  el StringWithComponent,
-                    mappings:
-                      user: el(UserLink, user: @props.users[@props.currentBeatmap.user_id] ? deletedUser)
-                    pattern: trans('beatmaps.discussions.guest')
-            el BeatmapBasicStats, beatmap: @props.currentBeatmap, beatmapset: @props.beatmapset
+    return (
+      <div className={bn}>
+        <div className={`${bn}__content ${bn}__content--details`}>
+          <div className={`${bn}__details ${bn}__details--full`}>
+            <BeatmapsetMapping
+              beatmapset={this.props.beatmapset}
+              user={this.props.users[this.props.beatmapset.user_id]}
+            />
+          </div>
+          <div className={`${bn}__details`}>
+            <Subscribe beatmapset={this.props.beatmapset} />
+          </div>
+          <div className={`${bn}__details`}>
+            <BigButton
+              href={route('beatmapsets.show', { beatmapset: this.props.beatmapset.id })}
+              icon='fas fa-info'
+              modifiers='full'
+              text={trans('beatmaps.discussions.beatmap_information')}
+            />
+          </div>
+        </div>
+        <div className={`${bn}__content ${bn}__content--nomination`}>
+          <Nominations
+            beatmapset={this.props.beatmapset}
+            currentDiscussions={this.props.currentDiscussions}
+            currentUser={core.currentUser}
+            discussions={this.props.discussions}
+            events={this.props.events}
+            users={this.props.users}
+          />
+        </div>
+      </div>
+    );
+  }
 
 
-  setFilter: (e) =>
-    e.preventDefault()
-    $.publish 'beatmapsetDiscussions:update', filter: e.currentTarget.dataset.type
+  private renderHeaderTop() {
+    const bn = 'beatmap-discussions-header-top';
 
+    return (
+      <div className={bn}>
+        <div className={`${bn}__content`}>
+          <div className={`${bn}__cover`}>
+            <BeatmapsetCover
+              beatmapset={this.props.beatmapset}
+              modifiers='full'
+              size='cover'
+            />
+          </div>
+          <div className={`${bn}__title-container`}>
+            <h1 className={`${bn}__title`}>
+              <a
+                className='link link--white link--no-underline'
+                href={route('beatmapsets.show', { beatmapset: this.props.beatmapset.id })}
+              >
+                {getTitle(this.props.beatmapset)}
+              </a>
+              <BeatmapsetBadge beatmapset={this.props.beatmapset} type='nsfw' />
+              <BeatmapsetBadge beatmapset={this.props.beatmapset} type='spotlight' />
+            </h1>
+            <h2 className={`${bn}__title ${bn}__title--artist`}>
+              {getArtist(this.props.beatmapset)}
+              <BeatmapsetBadge beatmapset={this.props.beatmapset} type='featured_artist' />
+            </h2>
+          </div>
+          <div className={`${bn}__filters`}>
+            <div className={`${bn}__filter-group`}>
+              <BeatmapList
+                beatmaps={this.props.beatmaps.get(this.props.currentBeatmap.mode) ?? []}
+                beatmapset={this.props.beatmapset}
+                createLink={this.createLink}
+                currentBeatmap={this.props.currentBeatmap}
+                getCount={this.getCount}
+                onSelectBeatmap={this.onSelectBeatmap}
+                users={this.props.users}
+              />
+            </div>
+            <div className={`${bn}__filter-group ${bn}__filter-group--stats`}>
+              <UserFilter
+                ownerId={this.props.beatmapset.user_id}
+                selectedUser={this.props.selectedUserId != null ? this.props.users[this.props.selectedUserId] : null}
+                users={this.props.discussionStarters}
+              />
+              <div className={`${bn}__stats`}>{this.renderStats()}</div>
+            </div>
+          </div>
+          <div className='u-relative'>
+            <Chart
+              discussions={this.props.currentDiscussions.byFilter[this.props.currentFilter].timeline}
+              duration={this.props.currentBeatmap.total_length * 1000}
+            />
+            <div className={`${bn}__beatmap-stats`}>
+              <div className={`${bn}__guest`}>
+                {this.props.currentBeatmap.user_id !== this.props.beatmapset.user_id ? (
+                  <span>
+                    <StringWithComponent
+                      mappings={{
+                        user: <UserLink user={this.props.users[this.props.currentBeatmap.user_id] ?? deletedUser} />,
+                      }}
+                      pattern={trans('beatmaps.discussions.guest')}
+                    />
+                  </span>
+                ) : null}
+              </div>
+              <BeatmapBasicStats beatmap={this.props.currentBeatmap} beatmapset={this.props.beatmapset} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-  stats: =>
-    bn = 'counter-box'
+  private renderStats() {
+    return ['mine', 'mapperNotes', 'resolved', 'pending', 'praises', 'deleted', 'total'].map(this.renderType);
+  }
 
-    for type in ['mine', 'mapperNotes', 'resolved', 'pending', 'praises', 'deleted', 'total']
-      continue if type == 'deleted' && !@props.currentUser.is_admin
+  private readonly renderType = (type: Filter) => {
+    if ((type === 'deleted') && !core.currentUser?.is_admin) {
+      return null;
+    }
 
-      topClasses = "#{bn} #{bn}--beatmap-discussions #{bn}--#{_.kebabCase(type)}"
-      topClasses += ' js-active' if @props.mode != 'events' && @props.currentFilter == type
+    const bn = 'counter-box';
 
-      total = 0
-      for own _mode, discussions of @props.currentDiscussions.byFilter[type]
-        total += _.size(discussions)
+    let topClasses = `${bn} ${bn}--beatmap-discussions ${bn}--${kebabCase(type)}`;
+    if (this.props.mode !== 'events' && this.props.currentFilter === type) {
+      topClasses += ' js-active';
+    }
 
-      a
-        key: type
-        href: makeUrl
-          filter: type
-          beatmapsetId: @props.beatmapset.id
-          beatmapId: @props.currentBeatmap.id
-          mode: @props.mode
-        className: topClasses
-        'data-type': type
-        onClick: @setFilter
+    const discussionsByFilter = this.props.currentDiscussions.byFilter[type];
+    const total = Object.values(discussionsByFilter).reduce((acc, discussions) => acc + size(discussions), 0);
 
-        div
-          className: "#{bn}__content"
-          div
-            className: "#{bn}__title"
-            trans("beatmaps.discussions.stats.#{_.snakeCase(type)}")
-          div
-            className: "#{bn}__count"
-            total
+    return (
+      <a
+        key={type}
+        className={topClasses}
+        data-type={type}
+        href={makeUrl({
+          beatmapId: this.props.currentBeatmap.id,
+          beatmapsetId: this.props.beatmapset.id,
+          filter: type,
+          mode: this.props.mode,
+        })}
+        onClick={this.setFilter}
+      >
+        <div className={`${bn}__content`}>
+          <div className={`${bn}__title`}>
+            {trans(`beatmaps.discussions.stats.${snakeCase(type)}`)}
+          </div>
+          <div className={`${bn}__count`}>
+            {total}
+          </div>
+        </div>
+        <div className={`${bn}__line`} />
+      </a>
+    );
+  };
 
-        div className: "#{bn}__line"
-
-
-  createLink: (beatmap) =>
-    makeUrl beatmap: beatmap
-
-
-  getCount: (beatmap) =>
-    if beatmap.deleted_at? then undefined else @props.currentDiscussions.countsByBeatmap[beatmap.id]
-
-
-  onClickMode: (e, mode) =>
-    e.preventDefault()
-    $.publish 'playmode:set', [{ mode }]
-
-
-  onSelectBeatmap: (beatmapId) =>
-    $.publish 'beatmapsetDiscussions:update',
-      beatmapId: beatmapId
-      mode: 'timeline'
+  private readonly setFilter = (event: React.SyntheticEvent<HTMLElement>) => {
+    event.preventDefault();
+    $.publish('beatmapsetDiscussions:update', { filter: event.currentTarget.dataset.type });
+  };
+}
