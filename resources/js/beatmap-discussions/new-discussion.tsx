aabb2cfb27d5c23e23253ede4ef76ba2515e8f36
bakaneko <notbakaneko@users.noreply.github.com>
2023-04-24 16:51:29 +0900

    BeatmapsetWithDiscussionsJson extends from BeatmapsetExtendedJson

--- resources/js/beatmap-discussions/new-discussion.tsx
+++ resources/js/beatmap-discussions/new-discussion.tsx
@@ -10,7 +10,7 @@ import BeatmapExtendedJson from 'interfaces/beatmap-extended-json';
 import BeatmapsetDiscussionJson from 'interfaces/beatmapset-discussion-json';
 import { BeatmapsetDiscussionPostStoreResponseJson } from 'interfaces/beatmapset-discussion-post-responses';
 import BeatmapsetExtendedJson from 'interfaces/beatmapset-extended-json';
-import { BeatmapsetWithDiscussionsJson } from 'interfaces/beatmapset-json';
+import BeatmapsetWithDiscussionsJson from 'interfaces/beatmapset-with-discussions-json';
 import { route } from 'laroute';
 import { action, computed, makeObservable, observable, runInAction } from 'mobx';
 import { observer } from 'mobx-react';
