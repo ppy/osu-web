097698c726710dee581d2c5eb8d762e126681acc
nanaya <me@nanaya.net>
2023-04-24 18:01:59 +0900

    Support wiki link syntax

--- resources/js/chat/message-item.tsx
+++ resources/js/chat/message-item.tsx
@@ -8,7 +8,9 @@ import * as React from 'react';
 import ReactMarkdown from 'react-markdown';
 import autolink from 'remark-plugins/autolink';
 import disableConstructs from 'remark-plugins/disable-constructs';
+import wikiLink from 'remark-wiki-link';
 import { classWithModifiers } from 'utils/css';
+import { wikiUrl } from 'utils/url';
 
 interface Props {
   message: Message;
@@ -58,7 +60,7 @@ export default class MessageItem extends React.Component<Props> {
           'chat-plain': remarkType === 'chatPlain',
         })}
         components={components}
-        remarkPlugins={[autolink, [disableConstructs, { type: remarkType }]]}
+        remarkPlugins={[autolink, [disableConstructs, { type: remarkType }], [wikiLink, { hrefTemplate: wikiUrl }]]}
         unwrapDisallowed
       >
         {this.props.message.content}
