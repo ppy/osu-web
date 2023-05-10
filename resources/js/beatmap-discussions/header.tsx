202d656e1cff7bd2fccea852f2eab2a052f50d4a
bakaneko <notbakaneko@users.noreply.github.com>
2023-05-10 20:53:44 +0900

    ....what

--- resources/js/beatmap-discussions/header.tsx
+++ resources/js/beatmap-discussions/header.tsx
@@ -1,4 +1,4 @@
-// Copyright (c) ppy Pty Ltd <contact@.ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
 import headerLinks from 'beatmapsets-show/header-links';
