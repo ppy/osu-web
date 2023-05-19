3ba4c833b3fdf79373d32fdaf981693ccec51eb1
RockRoller01 <joshuaalexanderkahle@gmail.com>
2023-05-19 05:15:12 +0200

    better format

--- resources/lang/en/beatmaps.php
+++ resources/lang/en/beatmaps.php
@@ -67,24 +67,42 @@ return [
         ],
 
         'message_type' => [
-            'disqualify' => 'Disqualify',
-            'disqualify_title' => 'Post Disqualification',
-            'hype' => 'Hype!',
-            'hype_title' => 'Post Hype!',
-            'mapper_note' => 'Note',
-            'mapper_note_title' => 'Post Note',
-            'nomination_reset' => 'Reset Nomination',
-            'nomination_reset_title' => 'Remove all Nominations',
-            'praise' => 'Praise',
-            'praise_title' => 'Post Praise',
-            'problem' => 'Problem',
-            'problem_title' => 'Post Problem',
-            'problem_warning' => 'Report Problem',
-            'problem_warning_title' => 'Post Problem',
-            'review' => 'Review',
-            'review_title' => 'Post Review',
-            'suggestion' => 'Suggestion',
-            'suggestion_title' => 'Post Suggestion',
+            'disqualify' => [
+                'text' => 'Disqualify',
+                'title' => 'Post Disqualification',
+            ],
+            'hype' => [
+                'text' => 'Hype!',
+                'title' => 'Post Hype!',
+            ],
+            'mapper_note' => [
+                'text' => 'Note',
+                'title' => 'Post Note',
+            ],
+            'nomination_reset' => [
+                'text' => 'Reset Nomination',
+                'title' => 'Remove all Nominations',
+            ],
+            'praise' => [
+                'text' => 'Praise',
+                'title' => 'Post Praise',
+            ],
+            'problem' => [
+                'text' => 'Problem',
+                'title' => 'Post Problem',
+            ],
+            'problem_warning' => [
+                'text' => 'Report Problem',
+                'title' => 'Post Problem',
+            ],
+            'review' => [
+                'text' => 'Review',
+                'title' => 'Post Review',
+            ],
+            'suggestion' => [
+                'text' => 'Suggestion',
+                'title' => 'Post Suggestion',
+            ],
         ],
 
         'mode' => [
