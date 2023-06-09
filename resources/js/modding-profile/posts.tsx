dfeb93121d95afab3d1213365bd0de354ff09140
bakaneko <notbakaneko@users.noreply.github.com>
2023-06-09 18:58:57 +0900

    convert to typescript

--- resources/js/modding-profile/posts.tsx
+++ resources/js/modding-profile/posts.tsx
@@ -1,68 +1,84 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import BeatmapsetCover from 'components/beatmapset-cover'
-import { route } from 'laroute'
-import * as React from 'react'
-import { div, h2, a, img, span } from 'react-dom-factories'
-import { canModeratePosts, makeUrl } from 'utils/beatmapset-discussion-helper'
-import { trans } from 'utils/lang'
-import Post from "../beatmap-discussions/post"
+import BeatmapsetCover from 'components/beatmapset-cover';
+import { BeatmapsetDiscussionMessagePostJson } from 'interfaces/beatmapset-discussion-post-json';
+import UserJson from 'interfaces/user-json';
+import { route } from 'laroute';
+import { deletedUser } from 'models/user';
+import * as React from 'react';
+import { makeUrl } from 'utils/beatmapset-discussion-helper';
+import { classWithModifiers } from 'utils/css';
+import { trans } from 'utils/lang';
+import Post from '../beatmap-discussions/post';
 
-el = React.createElement
+interface Props {
+  posts: BeatmapsetDiscussionMessagePostJson[];
+  user: UserJson;
+  users: Partial<Record<number, UserJson>>;
+}
 
-export class Posts extends React.Component
-  render: =>
-    div className: 'page-extra',
-      h2 className: 'title title--page-extra', trans('users.show.extra.posts.title_longer')
-      div className: 'modding-profile-list',
-        if @props.posts.length == 0
-          div className: 'modding-profile-list__empty', trans('users.show.extra.none')
-        else
-          [
-            for post in @props.posts
-              canModerate = canModeratePosts()
+export class Posts extends React.Component<Props> {
+  render() {
+    return (
+      <div className='page-extra'>
+        <h2 className='title title--page-extra'>{trans('users.show.extra.posts.title_longer')}</h2>
+        <div className='modding-profile-list'>
+          {this.props.posts.length === 0 ? (
+            <div className='modding-profile-list__empty'>{trans('users.show.extra.none')}</div>
+          ) : (
+            <>
+              {this.props.posts.map(this.renderPost)}
+              <a
+                className='modding-profile-list__show-more'
+                href={route('users.modding.posts', { user: this.props.user.id })}
+              >
+                {trans('users.show.extra.posts.show_more')}
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
 
-              discussionClasses = 'beatmap-discussion beatmap-discussion--preview beatmap-discussion--modding-profile'
+  private renderPost = (post: BeatmapsetDiscussionMessagePostJson) => {
+    const discussionClasses = classWithModifiers('beatmap-discussion', ['preview', 'modding-profile'], { deleted: post.deleted_at != null });
 
-              if post.deleted_at?
-                discussionClasses += ' beatmap-discussion--deleted'
+    const lastEditor = post.last_editor_id != null ? this.props.users[post.last_editor_id] ?? deletedUser.toJson() : null;
 
-              div
-                key: post.id
-                className: 'modding-profile-list__row',
-
-                a
-                  className: 'modding-profile-list__thumbnail'
-                  href: makeUrl(discussion: post.beatmap_discussion),
-
-                  el BeatmapsetCover,
-                    beatmapset: post.beatmap_discussion.beatmapset
-                    size: 'list'
-
-                div className: "modding-profile-list__timestamp hidden-xs",
-                  div className: "beatmap-discussion-timestamp",
-                    div className: "beatmap-discussion-timestamp__icons-container",
-                      span
-                        className: 'fas fa-reply'
-                        title: trans 'common.buttons.reply'
-
-                div className: discussionClasses,
-                  div className: "beatmap-discussion__discussion",
-                    el Post,
-                      key: post.id
-                      beatmapset: post.beatmap_discussion.beatmapset
-                      discussion: post.beatmap_discussion
-                      post: post
-                      type: 'reply'
-                      users: @props.users
-                      user: @props.users[post.user_id]
-                      read: true
-                      readonly: true
-                      lastEditor: @props.users[post.last_editor_id] ? @props.users[null] if post.last_editor_id?
-            a
-              key: 'show-more'
-              className: 'modding-profile-list__show-more'
-              href: route('users.modding.posts', {user: @props.user.id}),
-              trans('users.show.extra.posts.show_more')
-          ]
+    return (
+      <div key={post.id} className='modding-profile-list__row'>
+        <a className='modding-profile-list__thumbnail' href={makeUrl({ discussion: post.beatmap_discussion })}>
+          <BeatmapsetCover
+            beatmapset={post.beatmap_discussion?.beatmapset}
+            size='list'
+          />
+        </a>
+        <div className='modding-profile-list__timestamp hidden-xs'>
+          <div className='beatmap-discussion-timestamp'>
+            <div className='beatmap-discussion-timestamp__icons-container'>
+              <span className='fas fa-reply' title={trans('common.buttons.reply')} />
+            </div>
+          </div>
+        </div>
+        <div className={discussionClasses}>
+          <div className='beatmap-discussion__discussion'>
+            <Post
+              key={post.id}
+              beatmapset={post.beatmap_discussion.beatmapset}
+              discussion={post.beatmap_discussion}
+              lastEditor={lastEditor}
+              post={post}
+              read
+              readonly
+              type='reply'
+              user={this.props.users[post.user_id] ?? deletedUser.toJson()}
+              users={this.props.users}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
