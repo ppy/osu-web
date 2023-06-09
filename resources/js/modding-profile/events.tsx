515cef5b7d5b0dddefeaf95f2255548eda4c4a27
bakaneko <notbakaneko@users.noreply.github.com>
2023-06-09 14:54:28 +0900

    convert to typescript

--- resources/js/modding-profile/events.tsx
+++ resources/js/modding-profile/events.tsx
@@ -1,29 +1,44 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import BeatmapsetEvents from 'components/beatmapset-events'
-import { route } from 'laroute'
-import * as React from 'react'
-import { div, h2, a, img } from 'react-dom-factories'
-import { trans } from 'utils/lang'
+import BeatmapsetEvents from 'components/beatmapset-events';
+import BeatmapsetEventJson from 'interfaces/beatmapset-event-json';
+import UserJson from 'interfaces/user-json';
+import { route } from 'laroute';
+import * as React from 'react';
+import { trans } from 'utils/lang';
 
-el = React.createElement
+interface Props {
+  events: BeatmapsetEventJson[];
+  user: UserJson;
+  users: Partial<Record<string, UserJson>>;
+}
 
-export class Events extends React.Component
-  render: =>
-    div className: 'page-extra',
-      h2 className: 'title title--page-extra', trans('users.show.extra.events.title_longer')
-      div className: 'modding-profile-list',
-        if @props.events.length == 0
-          div className: 'modding-profile-list__empty', trans('users.show.extra.none')
-        else
-          el React.Fragment, null,
-            el BeatmapsetEvents,
-              events: @props.events
-              mode: 'profile'
-              users: @props.users
-
-            a
-              className: 'modding-profile-list__show-more'
-              href: route('beatmapsets.events.index', user: @props.user.id),
-              trans('users.show.extra.events.show_more')
+export class Events extends React.Component<Props> {
+  render() {
+    return (
+      <div className='page-extra'>
+        <h2 className='title title--page-extra'>{trans('users.show.extra.events.title_longer')}</h2>
+        <div className='modding-profile-list'>
+          {this.props.events.length === 0 ? (
+            <div className='modding-profile-list__empty'>{trans('users.show.extra.none')}</div>
+          ) : (
+            <>
+              <BeatmapsetEvents
+                events={this.props.events}
+                mode='profile'
+                users={this.props.users}
+              />
+              <a
+                className='modding-profile-list__show-more'
+                href={route('beatmapsets.events.index', { user: this.props.user.id })}
+              >
+                {trans('users.show.extra.events.show_more')}
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
