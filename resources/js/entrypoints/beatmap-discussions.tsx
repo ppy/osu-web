d8e8696d97b4ea212b1cc9587ebe82ecf60147b8
bakaneko <notbakaneko@users.noreply.github.com>
2023-06-13 21:52:42 +0900

    convert entrypoint to typescript

--- resources/js/entrypoints/beatmap-discussions.tsx
+++ resources/js/entrypoints/beatmap-discussions.tsx
@@ -1,12 +1,14 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import core from 'osu-core-singleton'
-import { createElement } from 'react'
-import { parseJson } from 'utils/json'
-import { Main } from 'beatmap-discussions/main'
+import Main from 'beatmap-discussions/main';
+import core from 'osu-core-singleton';
+import React from 'react';
+import { parseJson } from 'utils/json';
 
-core.reactTurbolinks.register 'beatmap-discussions', (container) ->
-  createElement Main,
-    initial: parseJson 'json-beatmapset-discussion'
-    container: container
+core.reactTurbolinks.register('beatmap-discussions', (container: HTMLElement) => (
+  <Main
+    container={container}
+    initial={parseJson('json-beatmapset-discussion')}
+  />
+));
