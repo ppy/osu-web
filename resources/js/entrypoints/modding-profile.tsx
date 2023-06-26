b8921e17ecb7355bbd600f2036d7550ed52a16c1
bakaneko <notbakaneko@users.noreply.github.com>
2023-06-26 21:51:28 +0900

    convert to typescript

--- resources/js/entrypoints/modding-profile.tsx
+++ resources/js/entrypoints/modding-profile.tsx
@@ -1,22 +1,24 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import core from 'osu-core-singleton'
-import { createElement } from 'react'
-import { parseJson } from 'utils/json'
-import { Main } from 'modding-profile/main'
+import { Main } from 'modding-profile/main';
+import core from 'osu-core-singleton';
+import React from 'react';
+import { parseJson } from 'utils/json';
 
-core.reactTurbolinks.register 'modding-profile', (container) ->
-  createElement Main,
-    beatmaps: parseJson('json-beatmaps')
-    beatmapsets: parseJson('json-beatmapsets')
-    container: container
-    discussions: parseJson('json-discussions')
-    events: parseJson('json-events')
-    extras: parseJson('json-extras')
-    perPage: parseJson('json-perPage')
-    posts: parseJson('json-posts')
-    reviewsConfig: parseJson('json-reviewsConfig')
-    user: parseJson('json-user')
-    users: parseJson('json-users')
-    votes: parseJson('json-votes')
+core.reactTurbolinks.register('modding-profile', (container) => (
+  <Main
+    beatmaps={parseJson('json-beatmaps')}
+    beatmapsets={parseJson('json-beatmapsets')}
+    container={container}
+    discussions={parseJson('json-discussions')}
+    events={parseJson('json-events')}
+    extras={parseJson('json-extras')}
+    perPage={parseJson('json-perPage')}
+    posts={parseJson('json-posts')}
+    reviewsConfig={parseJson('json-reviewsConfig')}
+    user={parseJson('json-user')}
+    users={parseJson('json-users')}
+    votes={parseJson('json-votes')}
+  />
+));
