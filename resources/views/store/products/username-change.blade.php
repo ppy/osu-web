18092a841b20a1bdd5969479ab65ef21f909e56f
bakaneko <notbakaneko@users.noreply.github.com>
2023-07-03 22:14:58 +0900

    unneeded nested div

--- resources/views/store/products/username-change.blade.php
+++ resources/views/store/products/username-change.blade.php
@@ -8,18 +8,16 @@
 <div class="js-username-change username-change">
     <div style="background-image: url('{{ Auth::user()->user_avatar }}');" class="avatar avatar--centered"></div>
     <div>
-        <div>
-            <input type="hidden" name="item[product_id]" value="{{ $product->product_id }}" />
-            <input type="hidden" name="item[quantity]" class="js-store-item-quantity" value="1" />
-            <input type="hidden" id="username-form-price" name="item[cost]" value="0" />
-            {!! Form::label('username', 'New Username') !!}
-            {!! Form::text('item[extra_info]', '', [
-                'id' => 'username',
-                'class' => 'js-username-change-input form-text form-text--username-change',
-                'placeholder' => 'Requested Username',
-                'autocomplete' => 'off',
-            ]) !!}
-        </div>
+        <input type="hidden" name="item[product_id]" value="{{ $product->product_id }}" />
+        <input type="hidden" name="item[quantity]" class="js-store-item-quantity" value="1" />
+        <input type="hidden" id="username-form-price" name="item[cost]" value="0" />
+        {!! Form::label('username', 'New Username') !!}
+        {!! Form::text('item[extra_info]', '', [
+            'id' => 'username',
+            'class' => 'js-username-change-input form-text form-text--username-change',
+            'placeholder' => 'Requested Username',
+            'autocomplete' => 'off',
+        ]) !!}
         <em class="store-text store-text--emphasis">
             <div id="username-check-status">{{ osu_trans('store.username_change.check') }}</div>
         </em>
