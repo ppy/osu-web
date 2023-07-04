7414f7571f25ea499631c23d76ba305c34209a76
nanaya <me@nanaya.net>
2023-07-04 19:13:44 +0900

    Add missing licence

--- resources/js/core/browser-title-with-notification-count.ts
+++ resources/js/core/browser-title-with-notification-count.ts
@@ -1,3 +1,6 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
 import { action, autorun, makeObservable, observable, runInAction } from 'mobx';
 import OsuCore from 'osu-core';
 import { formatNumber } from 'utils/html';
