0a196455aac363be3a9d98523231b2ca7a2bdc16
bakaneko <notbakaneko@users.noreply.github.com>
2023-07-06 18:26:58 +0900

    wip

--- resources/js/beatmap-discussions/discussions-state.ts
+++ resources/js/beatmap-discussions/discussions-state.ts
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-import BeatmapsetDiscussionJson from 'interfaces/beatmapset-discussion-json';
+import { BeatmapsetDiscussionJsonForShow } from 'interfaces/beatmapset-discussion-json';
 import BeatmapsetWithDiscussionsJson from 'interfaces/beatmapset-with-discussions-json';
 import GameMode from 'interfaces/game-mode';
 import { isEmpty, keyBy, maxBy } from 'lodash';
@@ -9,32 +9,28 @@ import { action, computed, makeObservable, observable, toJS } from 'mobx';
 import { deletedUser } from 'models/user';
 import moment from 'moment';
 import core from 'osu-core-singleton';
-import { findDefault, group } from 'utils/beatmap-helper';
-import { parseUrl } from 'utils/beatmapset-discussion-helper';
+import { findDefault, group, sortWithMode } from 'utils/beatmap-helper';
+import { makeUrl, parseUrl } from 'utils/beatmapset-discussion-helper';
 import { switchNever } from 'utils/switch-never';
-import { Filter } from './current-discussions';
-import DiscussionMode, { DiscussionPage } from './discussion-mode';
+import { Filter, filters } from './current-discussions';
+import DiscussionMode, { DiscussionPage, isDiscussionPage } from './discussion-mode';
 
 type DiscussionsAlias = BeatmapsetWithDiscussionsJson['discussions'];
 
 export interface UpdateOptions {
-  beatmapId: number;
   beatmapset: BeatmapsetWithDiscussionsJson;
-  filter: Filter;
-  mode: DiscussionPage;
-  modeIf: DiscussionPage;
-  playmode: GameMode;
-  selectedUserId: number;
   watching: boolean;
 }
 
-export function filterDiscusionsByMode(discussions: DiscussionsAlias, mode: DiscussionMode, beatmapId: number) {
-  console.log(mode);
+// FIXME this doesn't make it so the modes with optional beatmapId can pass a beatmapId that gets ignored.
+function filterDiscusionsByMode(discussions: DiscussionsAlias, mode: 'general' | 'timeline', beatmapId: number): DiscussionsAlias;
+function filterDiscusionsByMode(discussions: DiscussionsAlias, mode: 'generalAll' | 'reviews'): DiscussionsAlias;
+function filterDiscusionsByMode(discussions: DiscussionsAlias, mode: DiscussionMode, beatmapId?: number) {
   switch (mode) {
     case 'general':
       return discussions.filter((discussion) => discussion.beatmap_id === beatmapId);
     case 'generalAll':
-      return discussions.filter((discussion) => discussion.beatmap_id == null);
+      return discussions.filter((discussion) => discussion.beatmap_id == null && discussion.message_type !== 'review');
     case 'reviews':
       return discussions.filter((discussion) => discussion.message_type === 'review');
     case 'timeline':
@@ -45,42 +41,13 @@ export function filterDiscusionsByMode(discussions: DiscussionsAlias, mode: Disc
   }
 }
 
-export function filterDiscussionsByFilter(discussions: DiscussionsAlias, filter: Filter) {
-  console.log(filter);
-  switch (filter) {
-    case 'deleted':
-      return discussions.filter((discussion) => discussion.deleted_at != null);
-    case 'hype':
-      return discussions.filter((discussion) => discussion.message_type === 'hype');
-    case 'mapperNotes':
-      return discussions.filter((discussion) => discussion.message_type === 'mapper_note');
-    case 'mine': {
-      const userId = core.currentUserOrFail.id;
-      return discussions.filter((discussion) => discussion.user_id === userId);
-    }
-    case 'pending':
-      // TODO:
-      // pending reviews
-      // if (discussion.parent_id != null) {
-      //   const parentDiscussion = discussions[discussion.parent_id];
-      //   if (parentDiscussion != null && parentDiscussion.message_type == 'review') return true;
-      // }
-      return discussions.filter((discussion) => discussion.can_be_resolved && !discussion.resolved);
-    case 'praises':
-      return discussions.filter((discussion) => discussion.message_type === 'praise' || discussion.message_type === 'hype');
-    case 'resolved':
-      return discussions.filter((discussion) => discussion.can_be_resolved && discussion.resolved);
-    case 'total':
-      return discussions;
-    default:
-      switchNever(filter);
-      throw new Error('missing valid filter');
-  }
+function isFilter(value: unknown): value is Filter {
+  return (filters as readonly unknown[]).includes(value);
 }
 
 export default class DiscussionsState {
   @observable currentBeatmapId: number;
-  @observable currentFilter: Filter = 'total';
+  @observable currentFilter: Filter = 'total'; // TODO: filter should always be total when page is events (also no highlight)
   @observable currentMode: DiscussionPage = 'general';
   @observable discussionCollapsed = new Map<number, boolean>();
   @observable discussionDefaultCollapsed = false;
@@ -91,6 +58,9 @@ export default class DiscussionsState {
   @observable readPostIds = new Set<number>();
   @observable selectedUserId: number | null = null;
   @observable showDeleted = true;
+
+  private previousFilter: Filter = 'total';
+  private previousPage: DiscussionPage = 'general';
 
   @computed
   get beatmaps() {
@@ -113,11 +83,84 @@ export default class DiscussionsState {
   }
 
   @computed
+  get currentBeatmapDiscussions() {
+    return this.discussionsByBeatmap(this.currentBeatmapId);
+  }
+
+  @computed
+  get currentBeatmapDiscussionsCurrentModeWithFilter() {
+    if (this.currentMode === 'events') return [];
+    return this.currentDiscussions[this.currentMode];
+  }
+
+  @computed
+  get currentDiscussions() {
+    const discussions = this.currentDiscussionsGroupedByFilter[this.currentFilter];)
+
+    return {
+      general: filterDiscusionsByMode(discussions, 'general', this.currentBeatmapId),
+      generalAll: filterDiscusionsByMode(discussions, 'generalAll'),
+      reviews: filterDiscusionsByMode(discussions, 'reviews'),
+      timeline: filterDiscusionsByMode(discussions, 'timeline', this.currentBeatmapId),
+    };
+  }
+
+  @computed
+  get currentDiscussionsGroupedByFilter() {
+    const groups: Record<Filter, DiscussionsAlias> = {
+      deleted: [],
+      hype: [],
+      mapperNotes: [],
+      mine: [],
+      pending: [],
+      praises: [],
+      resolved: [],
+      total: [],
+    };
+
+    for (const filter of filters) {
+      groups[filter] = this.filterDiscussionsByFilter(this.currentBeatmapDiscussions, filter);
+    }
+
+    return groups;
+  }
+
+  @computed
   get discussions() {
     // skipped discussions
     // - not privileged (deleted discussion)
     // - deleted beatmap
-    return keyBy(this.beatmapset.discussions.filter((discussion) => !isEmpty(discussion)), 'id') as Partial<Record<number, BeatmapsetWithDiscussionsJson['discussions'][number]>>;
+
+    // TODO need some typing to handle the not for show variant
+    // null part of the key so we can use .get(null)
+    const map = new Map<number | null | undefined, BeatmapsetDiscussionJsonForShow>();
+
+    for (const discussion of this.beatmapset.discussions) {
+      if (!isEmpty(discussion)) {
+        map.set(discussion.id, discussion);
+      }
+    }
+
+    return map;
+  }
+
+  @computed
+  get discussionsCountByPlaymode() {
+    const counts: Record<GameMode, number> = {
+      fruits: 0,
+      mania: 0,
+      osu: 0,
+      taiko: 0,
+    };
+
+    for (const discussion of this.nonNullDiscussions) {
+      const mode = discussion.beatmap?.mode;
+      if (mode != null) {
+        counts[mode]++;
+      }
+    }
+
+    return counts;
   }
 
   @computed
@@ -135,6 +178,13 @@ export default class DiscussionsState {
   }
 
   @computed
+  get hasCurrentUserHyped() {
+    const currentUser = core.currentUser; // core.currentUser check below doesn't make the inferrence that it's not nullable after the check.
+    const discussions = filterDiscusionsByMode(this.currentDiscussionsGroupedByFilter.hype, 'generalAll');
+    return currentUser != null && discussions.some((discussion) => discussion?.user_id === currentUser.id);
+  }
+
+  @computed
   get lastUpdate() {
     const maxLastUpdate = Math.max(
       +this.beatmapset.last_updated,
@@ -143,6 +193,17 @@ export default class DiscussionsState {
     );
 
     return maxLastUpdate != null ? moment(maxLastUpdate).unix() : null;
+  }
+
+  get selectedUser() {
+    return this.selectedUserId != null ? this.users[this.selectedUserId] : null;
+  }
+
+  get sortedBeatmaps() {
+    // TODO
+    // filter to only include beatmaps from the current discussion's beatmapset (for the modding profile page)
+    // const beatmaps = filter(this.props.beatmaps, this.isCurrentBeatmap);
+    return sortWithMode(Object.values(this.beatmaps));
   }
 
   @computed
@@ -156,13 +217,12 @@ export default class DiscussionsState {
 
   @computed
   get nonNullDiscussions() {
-    console.log('nonNullDiscussions');
-    return Object.values(this.discussions).filter((discussion) => discussion != null) as BeatmapsetDiscussionJson[];
+    return [...this.discussions.values()].filter((discussion) => discussion != null) as DiscussionsAlias;
   }
 
   @computed
   get presentDiscussions() {
-    return this.nonNullDiscussions.filter((discussion) => discussion.deleted_at == null);
+    return this.nonNullDiscussions.filter((discussion) => discussion.deleted_at == null) as DiscussionsAlias;
   }
 
   @computed
@@ -241,16 +301,61 @@ export default class DiscussionsState {
     makeObservable(this);
   }
 
+  @action
+  changeDiscussionPage(page?: string) {
+    if (!isDiscussionPage(page)) return;
+
+    const url = makeUrl({
+      beatmap: this.currentBeatmap,
+      filter: this.currentFilter,
+      mode: page,
+      user: this.selectedUserId ?? undefined,
+    });
+
+    if (page === 'events') {
+      // record page and filter when switching to events
+      this.previousPage = this.currentMode;
+      this.previousFilter = this.currentFilter;
+    } else if (this.currentFilter !== this.previousFilter) {
+      // restore previous filter when switching away from events
+      this.currentFilter = this.previousFilter;
+    }
+
+    this.currentMode = page;
+    Turbolinks.controller.advanceHistory(url);
+  }
+
+  @action
+  changeFilter(filter: unknown) {
+    if (!isFilter(filter)) return;
+
+    // restore previous page when selecting a filter.
+    if (this.currentMode === 'events') {
+      this.currentMode = this.previousPage;
+    }
+
+    this.currentFilter = filter;
+  }
+
+  @action
+  changeGameMode(mode: GameMode) {
+    const beatmap = findDefault({ items: this.groupedBeatmaps.get(mode) });
+    if (beatmap != null) {
+      this.currentBeatmapId = beatmap.id;
+    }
+  }
+
   discussionsByBeatmap(beatmapId: number) {
-    return computed(() => this.presentDiscussions.filter((discussion) => (discussion.beatmap_id == null || discussion.beatmap_id === beatmapId)));
+    return this.presentDiscussions.filter((discussion) => (discussion.beatmap_id == null || discussion.beatmap_id === beatmapId)) as DiscussionsAlias;
   }
 
-  discussionsByFilter(filter: Filter, mode: DiscussionMode, beatmapId: number) {
-    return computed(() => filterDiscussionsByFilter(this.discussionsByMode(mode, beatmapId), filter)).get();
-  }
-
-  discussionsByMode(mode: DiscussionMode, beatmapId: number) {
-    return computed(() => filterDiscusionsByMode(this.nonNullDiscussions, mode, beatmapId)).get();
+  @action
+  markAsRead(ids: number | number[]) {
+    if (Array.isArray(ids)) {
+      ids.forEach((id) => this.readPostIds.add(id));
+    } else {
+      this.readPostIds.add(ids);
+    }
   }
 
   toJsonString() {
@@ -269,15 +374,8 @@ export default class DiscussionsState {
   update(options: Partial<UpdateOptions>) {
     const {
       beatmapset,
-      filter,
-      mode,
-      modeIf, // TODO: remove?
-      playmode,
-      selectedUserId,
       watching,
     } = options;
-
-    let { beatmapId } = options;
 
     if (beatmapset != null) {
       this.beatmapset = beatmapset;
@@ -286,51 +384,47 @@ export default class DiscussionsState {
     if (watching != null) {
       this.beatmapset.current_user_attributes.is_watching = watching;
     }
+  }
 
-    if (playmode != null) {
-      const beatmap = findDefault({ items: this.groupedBeatmaps.get(playmode) });
-      beatmapId = beatmap?.id;
-    }
-
-    if (beatmapId != null && beatmapId !== this.currentBeatmap.id) {
-      this.currentBeatmapId = beatmapId;
-    }
-
-    if (filter != null) {
-      // TODO: this
-      // if (this.currentMode === 'events') {
-      //   this.currentMode = this.lastMode ?? defaultMode(this.currentBeatmapId);
-      // }
-
-      if (filter !== this.currentFilter) {
-        this.currentFilter = filter;
+  private filterDiscussionsByFilter(discussions: DiscussionsAlias, filter: Filter) {
+    switch (filter) {
+      case 'deleted':
+        return discussions.filter((discussion) => discussion.deleted_at != null);
+      case 'hype':
+        return discussions.filter((discussion) => discussion.message_type === 'hype');
+      case 'mapperNotes':
+        return discussions.filter((discussion) => discussion.message_type === 'mapper_note');
+      case 'mine': {
+        const userId = core.currentUserOrFail.id;
+        return discussions.filter((discussion) => discussion.user_id === userId);
       }
-    }
+      case 'pending': {
+        const reviewsWithPending = new Set<BeatmapsetDiscussionJsonForShow>();
 
-    if (mode != null && mode !== this.currentMode) {
-      // TODO: all this
-      // if (modeIf == null || modeIf === this.currentMode) {
-      //   this.currentMode = mode;
-      // }
+        const filteredDiscussions = discussions.filter((discussion) => {
+          if (!discussion.can_be_resolved || discussion.resolved) return false;
 
-      // // switching to events:
-      // // - record last filter, to be restored when setMode is called
-      // // - record last mode, to be restored when setFilter is called
-      // // - set filter to total
-      // if (mode === 'events') {
-      //   this.lastMode = this.currentMode;
-      //   this.lastFilter = this.currentFilter;
-      //   this.currentFilter = 'total';
-      // } else if (this.currentMode === 'events') {
-      //   // switching from events:
-      //   // - restore whatever last filter set or default to total
-      //   this.currentFilter = this.lastFilter ?? 'total';
-      // }
-    }
+          if (discussion.parent_id != null) {
+            const parentDiscussion = this.discussions.get(discussion.parent_id);
+            if (parentDiscussion != null && parentDiscussion.message_type === 'review') {
+              reviewsWithPending.add(parentDiscussion);
+            }
+          }
 
-    // need to setState if null
-    if (selectedUserId !== undefined) {
-      this.selectedUserId = selectedUserId;
+          return true;
+        });
+
+        return [...filteredDiscussions, ...reviewsWithPending.values()];
+      }
+      case 'praises':
+        return discussions.filter((discussion) => discussion.message_type === 'praise' || discussion.message_type === 'hype');
+      case 'resolved':
+        return discussions.filter((discussion) => discussion.can_be_resolved && discussion.resolved);
+      case 'total':
+        return discussions;
+      default:
+        switchNever(filter);
+        throw new Error('missing valid filter');
     }
   }
 }
