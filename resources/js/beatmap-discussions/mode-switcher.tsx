aa4aa65e96673c6acd3be2f8c4c910a4ddc76a8b
bakaneko <notbakaneko@users.noreply.github.com>
2023-07-07 17:33:28 +0900

    ModeSwitcher uses DiscussionsState

--- resources/js/beatmap-discussions/mode-switcher.tsx
+++ resources/js/beatmap-discussions/mode-switcher.tsx
@@ -2,29 +2,38 @@
 // See the LICENCE file in the repository root for full licence text.
 
 import StringWithComponent from 'components/string-with-component';
-import BeatmapJson from 'interfaces/beatmap-json';
-import BeatmapsetJson from 'interfaces/beatmapset-json';
-import { snakeCase, size } from 'lodash';
+import { snakeCase } from 'lodash';
+import { action } from 'mobx';
+import { observer } from 'mobx-react';
 import * as React from 'react';
 import { makeUrl } from 'utils/beatmapset-discussion-helper';
 import { classWithModifiers } from 'utils/css';
 import { trans } from 'utils/lang';
-import CurrentDiscussions, { Filter } from './current-discussions';
 import { DiscussionPage, discussionPages } from './discussion-mode';
+import DiscussionsState from './discussions-state';
 
 interface Props {
-  beatmapset: BeatmapsetJson;
-  currentBeatmap: BeatmapJson;
-  currentDiscussions: CurrentDiscussions;
-  currentFilter: Filter;
+  discussionsState: DiscussionsState;
   innerRef: React.RefObject<HTMLDivElement>;
-  mode: DiscussionPage;
 }
 
 const selectedClassName = 'page-mode-link--is-active';
 
-export class ModeSwitcher extends React.PureComponent<Props> {
+@observer
+export class ModeSwitcher extends React.Component<Props> {
   private scrollerRef = React.createRef<HTMLUListElement>();
+
+  private get beatmapset() {
+    return this.props.discussionsState.beatmapset;
+  }
+
+  private get currentBeatmap() {
+    return this.props.discussionsState.currentBeatmap;
+  }
+
+  private get currentMode() {
+    return this.props.discussionsState.currentMode;
+  }
 
   componentDidMount() {
     this.scrollModeSwitcher();
@@ -52,11 +61,11 @@ export class ModeSwitcher extends React.PureComponent<Props> {
   private renderMode = (mode: DiscussionPage) => (
     <li key={mode} className='page-mode__item'>
       <a
-        className={classWithModifiers('page-mode-link', { 'is-active': this.props.mode === mode })}
+        className={classWithModifiers('page-mode-link', { 'is-active': this.currentMode === mode })}
         data-mode={mode}
         href={makeUrl({
-          beatmapId: this.props.currentBeatmap.id,
-          beatmapsetId: this.props.beatmapset.id,
+          beatmapId: this.currentBeatmap.id,
+          beatmapsetId: this.beatmapset.id,
           mode,
         })}
         onClick={this.switch}
@@ -65,7 +74,7 @@ export class ModeSwitcher extends React.PureComponent<Props> {
           {this.renderModeText(mode)}
           {mode !== 'events' && (
             <span className='page-mode-link__badge'>
-              {size(this.props.currentDiscussions.byFilter[this.props.currentFilter][mode])}
+              {this.props.discussionsState.currentDiscussions[mode].length}
             </span>
           )}
           <span className='page-mode-link__stripe' />
@@ -77,7 +86,7 @@ export class ModeSwitcher extends React.PureComponent<Props> {
   private renderModeText(mode: DiscussionPage) {
     if (mode === 'general' || mode === 'generalAll') {
       const text = mode === 'general'
-        ? this.props.currentBeatmap.version
+        ? this.currentBeatmap.version
         : trans('beatmaps.discussions.mode.scopes.generalAll');
 
       return (
@@ -100,9 +109,10 @@ export class ModeSwitcher extends React.PureComponent<Props> {
     $(this.scrollerRef.current).scrollTo(`.${selectedClassName}`, 0, { over: { left: -1 } });
   }
 
+  @action
   private readonly switch = (e: React.SyntheticEvent<HTMLAnchorElement>) => {
     e.preventDefault();
 
-    $.publish('beatmapsetDiscussions:update', { mode: e.currentTarget.dataset.mode });
+    this.props.discussionsState.changeDiscussionPage(e.currentTarget.dataset.mode);
   };
 }
