c5a3674e95fe486de28ab9e74473c95e03486a85
bakaneko <notbakaneko@users.noreply.github.com>
2023-07-07 18:55:03 +0900

    convert to typescript

--- resources/js/entrypoints/beatmap-discussions-history.tsx
+++ resources/js/entrypoints/beatmap-discussions-history.tsx
@@ -1,20 +1,34 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import core from 'osu-core-singleton'
-import { createElement } from 'react'
-import { parseJson } from 'utils/json'
-import { Main } from 'beatmap-discussions-history/main'
+import { Main } from 'beatmap-discussions-history/main';
+import BeatmapExtendedJson from 'interfaces/beatmap-extended-json';
+import { BeatmapsetDiscussionJsonForBundle } from 'interfaces/beatmapset-discussion-json';
+import BeatmapsetExtendedJson from 'interfaces/beatmapset-extended-json';
+import UserJson from 'interfaces/user-json';
+import core from 'osu-core-singleton';
+import React from 'react';
+import { parseJson } from 'utils/json';
 
-core.reactTurbolinks.register 'beatmap-discussions-history', (container) ->
-  bundle = parseJson 'json-index'
+interface BeatmapsetDiscussionsBundleJson {
+  beatmaps: BeatmapExtendedJson[];
+  beatmapsets: BeatmapsetExtendedJson[];
+  discussions: BeatmapsetDiscussionJsonForBundle[];
+  included_discussions: BeatmapsetDiscussionJsonForBundle[];
+  users: UserJson[];
+}
 
-  # TODO: rename props to match
-  createElement Main,
-    beatmapsets: bundle.beatmapsets
-    discussions: bundle.discussions
-    users: bundle.users
-    relatedBeatmaps: bundle.beatmaps
-    relatedDiscussions: bundle.included_discussions
-    reviewsConfig: bundle.reviews_config
-    container: container
+core.reactTurbolinks.register('beatmap-discussions-history', () => {
+  const bundle = parseJson<BeatmapsetDiscussionsBundleJson>('json-index');
+
+  // TODO: rename props to match
+  return (
+    <Main
+      beatmapsets={bundle.beatmapsets}
+      discussions={bundle.discussions}
+      relatedBeatmaps={bundle.beatmaps}
+      relatedDiscussions={bundle.included_discussions}
+      users={bundle.users}
+    />
+  );
+});
