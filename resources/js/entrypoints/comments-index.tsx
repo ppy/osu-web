d04ff2ccd4acdd67aae331e642c4082eeeef6afa
nanaya <me@nanaya.net>
2023-07-12 19:36:39 +0900

    Typescript comments index/show entrypoints

--- resources/js/entrypoints/comments-index.tsx
+++ resources/js/entrypoints/comments-index.tsx
@@ -1,9 +1,10 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import CommentsIndex from 'comments-index'
-import core from 'osu-core-singleton'
-import { createElement } from 'react'
+import CommentsIndex from 'comments-index';
+import core from 'osu-core-singleton';
+import * as React from 'react';
 
-core.reactTurbolinks.register 'comments-index', ->
-  createElement(CommentsIndex, controllerStateSelector: '#json-index')
+core.reactTurbolinks.register('comments-index', () => (
+  <CommentsIndex controllerStateSelector='#json-index' />
+));
--- resources/js/entrypoints/comments-show.tsx
+++ resources/js/entrypoints/comments-show.tsx
@@ -1,9 +1,10 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import CommentsShow from 'comments-show'
-import core from 'osu-core-singleton'
-import { createElement } from 'react'
+import CommentsShow from 'comments-show';
+import core from 'osu-core-singleton';
+import * as React from 'react';
 
-core.reactTurbolinks.register 'comments-show', ->
-  createElement(CommentsShow, controllerStateSelector: '#json-show')
+core.reactTurbolinks.register('comments-show', () => (
+  <CommentsShow controllerStateSelector='#json-show' />
+));
