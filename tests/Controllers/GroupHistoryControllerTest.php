f149f3756e45fd968b5dcbd446173624ee77df69
clayton <me@clayton.cc>
2023-07-28 13:49:06 -0700

    Add test for skipping user_group_events query

--- tests/Controllers/GroupHistoryControllerTest.php
+++ tests/Controllers/GroupHistoryControllerTest.php
@@ -9,6 +9,7 @@ namespace Tests\Controllers;
 
 use App\Models\User;
 use App\Models\UserGroupEvent;
+use Illuminate\Database\Events\QueryExecuted;
 use Tests\TestCase;
 
 class GroupHistoryControllerTest extends TestCase
@@ -54,6 +55,25 @@ class GroupHistoryControllerTest extends TestCase
 
         $responseEventIds = collect($response['events'])->pluck('id');
         $this->assertContains($event->getKey(), $responseEventIds);
+    }
+
+    public function testIndexNonexistentGroupSkipsQuery(): void
+    {
+        $queryCount = 0;
+        $this->getConnection()->listen(function (QueryExecuted $event) use (&$queryCount) {
+            if (strpos($event->sql, (new UserGroupEvent())->getTable()) !== false) {
+                $queryCount++;
+            }
+        });
+
+        $response = $this
+            ->getJson(route('group-history.index', ['group' => 'nonexistent group']))
+            ->assertOk()
+            ->json();
+
+        $this->assertSame($queryCount, 0);
+        $this->assertNull($response['cursor_string']);
+        $this->assertEmpty($response['events']);
     }
 
     public function textIndexListsEvents(): void
