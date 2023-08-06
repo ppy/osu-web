a597bf91d46f744bdb14526a6c386e10e83a0651
Venix <30481900+venix12@users.noreply.github.com>
2023-08-06 23:37:52 +0200

    remove unused import

--- resources/js/models/contest-entry.ts
+++ resources/js/models/contest-entry.ts
@@ -3,7 +3,7 @@
 
 import ContestEntryJson from 'interfaces/contest-entry-json';
 import ContestJudgeVoteJson from 'interfaces/contest-judge-vote-json';
-import { action, computed, makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
 
 export class ContestEntry {
   @observable contest_id: number;
