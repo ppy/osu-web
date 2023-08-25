adfb65b71f6a715f50b5ee3e37557426c1309fe0
nanaya <me@nanaya.net>
2023-08-25 16:33:46 +0900

    Fix parameter for scores show in browser test

--- tests/Browser/SanityTest.php
+++ tests/Browser/SanityTest.php
@@ -430,6 +430,10 @@ class SanityTest extends DuskTestCase
             'changelog.show' => [
                 'changelog' => self::$scaffolding['build']->version,
             ],
+            'scores.show' => [
+                'rulesetOrScore' => static::$scaffolding['score']->getMode(),
+                'score' => static::$scaffolding['score']->getKey(),
+            ],
             'legal' => [
                 'locale' => 'en',
                 'path' => 'Terms',
