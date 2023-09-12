8b5c07869364068793796e298b050c4c4246107d
clayton <me@clayton.cc>
2023-09-12 15:46:51 -0700

    Correctly type GithubUserJson

--- resources/js/interfaces/github-user-json.ts
+++ resources/js/interfaces/github-user-json.ts
@@ -1,15 +1,24 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
-// Note: `github_url`, `github_username`, and `id` are null when this model is
-//       created for use in legacy changelog entries. Typings don't reflect this
-//       because changelogs are only CoffeeScript for now.
-export default interface GithubUserJson {
+interface CommonProps {
   display_name: string;
+}
+
+interface GithubProps {
   github_url: string;
   github_username: string;
   id: number;
-  osu_username: string | null;
-  user_id: number | null;
-  user_url: string | null;
 }
+
+interface OsuProps {
+  osu_username: string;
+  user_id: number;
+  user_url: string;
+}
+
+type GithubUserJson = CommonProps & GithubProps & (OsuProps | { [P in keyof OsuProps]: null });
+type GithubUserJsonLegacy = CommonProps & { [P in keyof GithubProps]: null } & OsuProps;
+
+export default GithubUserJson;
+export type GithubUserJsonForChangelog = GithubUserJson | GithubUserJsonLegacy;
