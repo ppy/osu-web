84fd5d13e3f37cfff9fbe5ed14bea9db719ef230
bakaneko <notbakaneko@users.noreply.github.com>
2023-09-21 16:08:12 +0900

    ...haha

--- app/Models/User.php
+++ app/Models/User.php
@@ -1342,7 +1342,7 @@ class User extends Model implements AfterCommit, AuthenticatableContract, HasLoc
 
     public function statisticsMania7k()
     {
-        return $this->hasOne(UserStatistics\Mania4k::class);
+        return $this->hasOne(UserStatistics\Mania7k::class);
     }
 
     public function statisticsTaiko()
