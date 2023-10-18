4e4820b5830657fac76e4bc90fb3e3406a03196b
bakaneko <notbakaneko@users.noreply.github.com>
2023-10-18 21:37:13 +0900

    also shouldn't be there

--- routes/web.php
+++ routes/web.php
@@ -498,7 +498,7 @@ Route::group(['as' => 'api.', 'prefix' => 'api', 'middleware' => ['api', Throttl
 
         // Beatmapsets
         //   GET /api/v2/beatmapsets/search/:filters
-        Route::get('beatmapsets/search/{filters?}', 'BeatmapsetsController@search');
+        Route::get('beatmapsets/search', 'BeatmapsetsController@search');
         //   GET /api/v2/beatmapsets/lookup
         Route::get('beatmapsets/lookup', 'BeatmapsetsController@lookup');
         //   GET /api/v2/beatmapsets/:beatmapset_id
