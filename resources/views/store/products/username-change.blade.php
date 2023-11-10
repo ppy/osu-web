6817bfa8edf15adf87ad84f37e4780b685c28553
nanaya <me@nanaya.net>
2023-11-10 22:16:40 +0900

    Add more translation for username change

--- resources/views/store/products/username-change.blade.php
+++ resources/views/store/products/username-change.blade.php
@@ -14,18 +14,18 @@
         <input type="hidden" name="item[product_id]" value="{{ $product->getKey() }}" />
         <input type="hidden" name="item[quantity]" class="js-store-item-quantity" value="1" />
         <input type="hidden" id="username-form-price" name="item[cost]" value="0" />
-        <label for="username">New Username</label>
+        <label for="username">{{ osu_trans('store.username_change.label') }}</label>
         <input
             autocomplete="off"
             class="js-username-change-input form-text form-text--username-change"
             id="username"
             name="item[extra_info]"
-            placeholder="Requested Username"
+            placeholder="{{ osu_trans('store.username_change.placeholder') }}"
         />
         <em class="store-text store-text--emphasis">
             <div id="username-check-status">{{ osu_trans('store.username_change.check') }}</div>
         </em>
-        <div>Your current username is "{{ $user->username }}".</div>
+        <div>{{ osu_trans('store.username_change.current', ['username' => $user->username]) }}</div>
     </div>
     <p class="store-text store-text--price" id="username-check-price"></p>
 </div>
