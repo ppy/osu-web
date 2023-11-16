ff92baf6d9404bc4383ae0d1f6dbe20f48788f44
bakaneko <notbakaneko@users.noreply.github.com>
2023-11-16 19:09:51 +0900

    get array index with bounds check helper for mobx

--- resources/js/beatmap-discussions/user-filter.tsx
+++ resources/js/beatmap-discussions/user-filter.tsx
@@ -9,6 +9,7 @@ import { action, computed, makeObservable } from 'mobx';
 import { observer } from 'mobx-react';
 import { usernameSortAscending } from 'models/user';
 import * as React from 'react';
+import { arrayGet } from 'utils/array';
 import { makeUrl, parseUrl } from 'utils/beatmapset-discussion-helper';
 import { groupColour } from 'utils/css';
 import { trans } from 'utils/lang';
@@ -95,10 +96,8 @@ export class UserFilter extends React.Component<Props> {
 
   private getGroup(option: Option) {
     if (this.isOwner(option)) return mapperGroup;
-    if (!option.groups) return null;
 
-    if (option.groups == null || option.groups.length === 0) return null;
-    return option.groups[0];
+    return arrayGet(option.groups, 0);
   }
 
   @action
