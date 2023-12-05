31b92c1645a183873bd57a72e56e4cd49ffaec15
Venix <30481900+venix12@users.noreply.github.com>
2023-12-05 14:07:56 +0100

    add ``ContestJudge`` to ``alwaysCheck`` in authorize

--- app/Libraries/OsuAuthorize.php
+++ app/Libraries/OsuAuthorize.php
@@ -44,6 +44,7 @@ class OsuAuthorize
         static $set;
 
         $set ??= new Ds\Set([
+            'ContestJudge',
             'IsOwnClient',
             'IsNotOAuth',
             'IsSpecialScope',
