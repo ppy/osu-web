8cfe59505ab42947c829207efd1fa961b3dd0a15
Venix <30481900+venix12@users.noreply.github.com>
2023-12-11 02:52:24 +0100

    css classes naming clean-up

--- resources/js/contest-judge-results/vote.tsx
+++ resources/js/contest-judge-results/vote.tsx
@@ -18,16 +18,16 @@ export default function Vote(props: Props) {
   return (
     <div>
       {props.vote.user != null && (
-        <div className='contest-judge-results__user'>
-          <UserLink className='contest-judge-results__avatar' user={props.vote.user}>
+        <div className='contest-judge-results-vote__user'>
+          <UserLink className='contest-judge-results-vote__avatar' user={props.vote.user}>
             <UserAvatar modifiers='full-circle' user={props.vote.user} />
           </UserLink>
 
-          <UserLink className='contest-judge-results__username' user={props.vote.user} />
+          <UserLink className='contest-judge-results-vote__username' user={props.vote.user} />
         </div>
       )}
 
-      <div className='contest-judge-results__total-score'>
+      <div className='contest-judge-results-vote__total-score'>
         <ValueDisplay
           label={trans('contest.judge_results.score')}
           modifiers={['score']}
@@ -35,24 +35,24 @@ export default function Vote(props: Props) {
         />
       </div>
 
-      <div className='contest-judge-categories'>
+      <div className='contest-judge-results-scores'>
         {props.vote.scores?.map((score) => (
-          <div key={score.id} className='contest-judge-categories__row'>
-            <div className='contest-judge-categories__col'>
+          <div key={score.id} className='contest-judge-results-scores__row'>
+            <div className='contest-judge-results-scores__col'>
               <div className='contest-judge-entry__description-icon' title={score.category?.description}>
                 <i className='fas fa-question-circle' />
               </div>
 
               <span className='u-ellipsis-overflow'>{score.category?.name}</span>
             </div>
-            <div className='contest-judge-categories__col contest-judge-categories__col--score'>
+            <div className='contest-judge-results-scores__col contest-judge-results-scores__col--score'>
               {score.value}/{score.category?.max_value}
             </div>
           </div>
         ))}
       </div>
 
-      {props.vote.comment != null && <div className='contest-judge-results__comment'>{props.vote.comment}</div>}
+      {props.vote.comment != null && <div className='contest-judge-results-vote__comment'>{props.vote.comment}</div>}
     </div>
   );
 }
