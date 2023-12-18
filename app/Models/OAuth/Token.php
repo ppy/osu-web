67cbfd1ae3fa96300bd62e6150c67c6bb7cff67f
nanaya <me@nanaya.net>
2023-12-18 21:15:40 +0900

    Note on default verified value

--- app/Models/OAuth/Token.php
+++ app/Models/OAuth/Token.php
@@ -234,6 +234,8 @@ class Token extends PassportToken implements SessionVerificationInterface
 
     private function setVerifiedState(): void
     {
+        // client credential doesn't have user attached and auth code is
+        // already verified during grant process
         $this->verified ??= $this->user === null || !$this->client->password_client;
     }
 }
