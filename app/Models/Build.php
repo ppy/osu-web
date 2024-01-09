c7d54f11f3fcb9bc9b7c711dcb8b1e39c53ca637
tsunyoku <tsunyoku@gmail.com>
2024-01-09 19:25:52 +0000

    ensure lazer users are not double-counted

--- app/Models/Build.php
+++ app/Models/Build.php
@@ -248,6 +248,11 @@ class Build extends Model implements Commentable
 
     public static function onlineUsers()
     {
-        return static::where('allow_bancho', 1)->sum('users');
+        // Lazer builds uses "fake" release rows to track platform-specific builds
+        // In order to not double-count users, we must use the "main" releases which will never have a hash.
+        return static
+            ::where('allow_bancho', 1)
+            ->whereNull('hash')
+            ->sum('users');
     }
 }
