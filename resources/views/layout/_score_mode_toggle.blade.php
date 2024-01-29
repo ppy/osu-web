fc6cec66f90aa524b87370d38d3238231e365c19
nanaya <me@nanaya.net>
2024-01-29 22:43:02 +0900

    Update icon for lazer mode

--- resources/views/layout/_score_mode_toggle.blade.php
+++ resources/views/layout/_score_mode_toggle.blade.php
@@ -3,15 +3,15 @@
     See the LICENCE file in the repository root for full licence text.
 --}}
 @php
-    $legacyScoreOnlyValue = App\Libraries\Search\ScoreSearchParams::showLegacyForUser(Auth::user());
-    $icon = $legacyScoreOnlyValue
+    $legacyScoreMode ??= App\Libraries\Search\ScoreSearchParams::showLegacyForUser($currentUser) === true;
+    $icon = $legacyScoreMode
         ? 'far fa-square'
         : 'fas fa-check-square';
 @endphp
 <button
     class="{{ $class }}"
     type="button"
-    data-url="{{ route('account.options', ['user_profile_customization[legacy_score_only]' => !$legacyScoreOnlyValue]) }}"
+    data-url="{{ route('account.options', ['user_profile_customization[legacy_score_only]' => !$legacyScoreMode]) }}"
     data-method="PUT"
     data-remote="1"
     data-reload-on-success="1"
