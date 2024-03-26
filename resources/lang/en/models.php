c35505ddb509562ddccfd7d42cb315eca6af0bca
nanaya <me@nanaya.net>
2024-03-26 18:46:10 +0900

    Show and translate model not found message

---
+++ resources/lang/en/models.php
@@ -0,0 +1,14 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+return [
+    'not_found' => "specified :model (:ids) couldn't be found",
+    'not_found_no_ids' => "specified :model couldn't be found",
+
+    'name' => [
+        'App\Models\Beatmap' => 'beatmap difficulty',
+        'App\Models\Beatmapset' => 'beatmap',
+    ],
+];
