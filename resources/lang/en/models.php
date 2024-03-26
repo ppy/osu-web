d7efd20c7d537ed9f335876f332f74222b0caf4b
nanaya <me@nanaya.net>
2024-03-26 19:20:45 +0900

    Actual sentence

--- resources/lang/en/models.php
+++ resources/lang/en/models.php
@@ -4,8 +4,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 return [
-    'not_found' => "specified :model (:ids) couldn't be found",
-    'not_found_no_ids' => "specified :model couldn't be found",
+    'not_found' => "Specified :model (:ids) couldn't be found.",
+    'not_found_no_ids' => "Specified :model couldn't be found.",
 
     'name' => [
         'App\Models\Beatmap' => 'beatmap difficulty',
