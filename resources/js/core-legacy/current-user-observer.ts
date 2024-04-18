99dc38c339253a3fa6dfb88ab20f7ff1b7b08785
bakaneko <notbakaneko@users.noreply.github.com>
2024-04-18 15:14:20 +0900

    convert to typescript

--- resources/js/core-legacy/current-user-observer.ts
+++ resources/js/core-legacy/current-user-observer.ts
@@ -1,55 +1,66 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import { pull } from 'lodash'
-import { reaction } from 'mobx'
-import core from 'osu-core-singleton'
-import { urlPresence } from 'utils/css'
+import CurrentUserJson from 'interfaces/current-user-json';
+import { pull } from 'lodash';
+import { reaction, toJS } from 'mobx';
+import core from 'osu-core-singleton';
+import { urlPresence } from 'utils/css';
 
-export default class CurrentUserObserver
-  constructor: ->
-    @covers = document.getElementsByClassName('js-current-user-cover')
-    @avatars = document.getElementsByClassName('js-current-user-avatar')
+export default class CurrentUserObserver {
+  private readonly avatars = document.getElementsByClassName('js-current-user-avatar');
+  private readonly covers = document.getElementsByClassName('js-current-user-cover');
 
-    $.subscribe 'user:update', @setData
-    $(document).on 'turbolinks:load', @reinit
-    $.subscribe 'user:followUserMapping:update', @updateFollowUserMapping
+  constructor() {
+    $.subscribe('user:update', this.setData);
+    $(document).on('turbolinks:load', this.reinit);
+    $.subscribe('user:followUserMapping:update', this.updateFollowUserMapping);
 
-    # one time setup to monitor cover url variable. No disposer because nothing destroys this object.
-    $ =>
-      reaction((=> core.currentUser?.cover.url), @setCovers)
+    // one time setup to monitor cover url variable. No disposer because nothing destroys this object.
+    $(() => reaction(() => core.currentUser?.cover.url, this.setCovers));
+  }
 
+  private readonly reinit = () => {
+    this.setAvatars();
+  };
 
-  reinit: =>
-    @setAvatars()
+  private setAvatars() {
+    const bgImage = urlPresence(core.currentUser?.avatar_url) ?? '';
+    for (const el of this.avatars) {
+      if (el instanceof HTMLElement) {
+        el.style.backgroundImage = bgImage;
+      }
+    }
+  }
 
+  private readonly setCovers = () => {
+    const bgImage = urlPresence(core.currentUser?.cover.url) ?? '';
+    for (const el of this.covers) {
+      if (el instanceof HTMLElement) {
+        el.style.backgroundImage = bgImage;
+      }
+    }
+  };
 
-  setAvatars: (elements) =>
-    elements ?= @avatars
+  private readonly setData = (_event: unknown, data: CurrentUserJson) => {
+    window.currentUser = data;
+    this.reinit();
+  };
 
-    bgImage = urlPresence(currentUser.avatar_url) if currentUser.id?
-    for el in elements
-      el.style.backgroundImage = bgImage
+  private readonly updateFollowUserMapping = (_event: unknown, data: { following: boolean; userId: number }) => {
+    const currentUser = core.currentUser;
+    if (currentUser == null) return;
 
+    if (data.following) {
+      currentUser.follow_user_mapping.push(data.userId);
+    } else {
+      pull(currentUser.follow_user_mapping, data.userId);
+    }
 
-  setCovers: =>
-    bgImage = urlPresence(core.currentUser.cover.url) if core.currentUser?
-    for el in @covers
-      el.style.backgroundImage = bgImage
+    if (window.currentUser.id != null) {
+      window.currentUser.follow_user_mapping = toJS(currentUser.follow_user_mapping);
+    }
 
-
-  setData: (_e, data) =>
-    window.currentUser = data
-
-    @reinit()
-
-
-  updateFollowUserMapping: (_e, data) =>
-    if data.following
-      currentUser.follow_user_mapping.push(data.userId)
-    else
-      pull(currentUser.follow_user_mapping, data.userId)
-
-    core.currentUser.follow_user_mapping = currentUser.follow_user_mapping
-
-    $.publish 'user:followUserMapping:refresh'
+    $.publish('user:followUserMapping:refresh');
+  };
+}
