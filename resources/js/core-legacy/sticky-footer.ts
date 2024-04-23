5c5150ef1a9bd50772c186fe52bf0ddb2d81edca
bakaneko <notbakaneko@users.noreply.github.com>
2024-04-23 18:26:55 +0900

    convert to typescript;
    also remove unused function

--- resources/js/core-legacy/sticky-footer.ts
+++ resources/js/core-legacy/sticky-footer.ts
@@ -1,46 +1,55 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-# How to use:
-# 1. create a marker on when it should be fixed, with class including
-#    'js-sticky-footer' and data attribute 'data-sticky-footer-target'
-# 2. subscribe to 'stickyFooter' event
-# 3. in the function, check if second parameter (first one is unused event
-#    object) is the correct target
-# 4. stick if matches, unstick otherwise
-export default class StickyFooter
-  constructor: ->
-    @stickMarker = document.getElementsByClassName('js-sticky-footer')
-    @permanentFixedFooter = document.getElementsByClassName('js-permanent-fixed-footer')
-    @throttledStickOrUnstick = _.throttle @stickOrUnstick, 100
+import { throttle } from 'lodash';
 
-    $(window).on 'scroll resize', @stickOrUnstick
-    $.subscribe 'stickyFooter:check', @throttledStickOrUnstick
-    $(document).on 'turbolinks:load', @throttledStickOrUnstick
+// How to use:
+// 1. create a marker on when it should be fixed, with class including
+//    'js-sticky-footer' and data attribute 'data-sticky-footer-target'
+// 2. subscribe to 'stickyFooter' event
+// 3. in the function, check if second parameter (first one is unused event
+//    object) is the correct target
+// 4. stick if matches, unstick otherwise
+export default class StickyFooter {
+  private readonly permanentFixedFooter = document.getElementsByClassName('js-permanent-fixed-footer');
+  private readonly stickMarker = document.getElementsByClassName('js-sticky-footer');
+  private readonly throttledStickOrUnstick;
 
+  constructor() {
+    this.throttledStickOrUnstick = throttle(this.stickOrUnstick, 100);
 
-  stickOrUnstick: =>
-    return if @stickMarker.length == 0
+    $(window).on('scroll resize', this.stickOrUnstick);
+    $.subscribe('stickyFooter:check', this.throttledStickOrUnstick);
+    $(document).on('turbolinks:load', this.throttledStickOrUnstick);
+  }
 
-    bottom = window.innerHeight - @permanentFixedFooter[0].offsetHeight
+  markerDisable(el: HTMLElement) {
+    el.dataset.stickyFooterDisabled = '1';
+  }
 
-    for marker in @stickMarker
-      continue if marker.getAttribute('data-sticky-footer-disabled') == '1'
+  markerEnable(el: HTMLElement) {
+    el.dataset.stickyFooterDisabled = '';
+  }
 
-      if marker.getBoundingClientRect().top >= bottom
-        $.publish 'stickyFooter', marker.getAttribute('data-sticky-footer-target')
-        return
+  private readonly stickOrUnstick = () => {
+    if (this.stickMarker.length === 0) return;
 
-    $.publish 'stickyFooter'
+    const footer = this.permanentFixedFooter[0];
+    if (!(footer instanceof HTMLElement)) return;
 
+    const bottom = window.innerHeight - footer.offsetHeight;
 
-  markerCheckEnabled: (el) ->
-    el.getAttribute('data-sticky-footer-disabled') == '1'
+    for (const marker of this.stickMarker) {
+      if (marker instanceof HTMLElement) {
+        if (marker.dataset.stickyFooterDisabled === '1') continue;
 
+        if (marker.getBoundingClientRect().top >= bottom) {
+          $.publish('stickyFooter', marker.dataset.stickyFooterTarget);
+          return;
+        }
+      }
+    }
 
-  markerDisable: (el) ->
-    el.setAttribute('data-sticky-footer-disabled', '1')
-
-
-  markerEnable: (el) ->
-    el.setAttribute('data-sticky-footer-disabled', '')
+    $.publish('stickyFooter');
+  };
+}
