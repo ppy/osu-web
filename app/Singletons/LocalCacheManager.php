fd352ea95f11ff22865a68d13e577bfc6d28f352
nanaya <me@nanaya.net>
2024-04-25 18:10:31 +0900

    Configurable cache expiry

--- app/Singletons/LocalCacheManager.php
+++ app/Singletons/LocalCacheManager.php
@@ -22,7 +22,7 @@ class LocalCacheManager
     {
         $currentTime = time();
         // Always reset cache after 1 minute.
-        if ($currentTime - $this->resetTime < 60) {
+        if ($currentTime - $this->resetTime < $GLOBALS['cfg']['osu']['octane']['local_cache_expire_second']) {
             $this->resetTicker++;
 
             if ($this->resetTicker < $GLOBALS['cfg']['osu']['octane']['local_cache_reset_requests']) {
