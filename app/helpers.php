624d55495fcd4e2ec0aebb116d16e982c30d2782
Hiviexd <rayenattia@gmail.com>
2024-07-03 17:28:20 +0100

    add HSL to HEX helper

--- app/helpers.php
+++ app/helpers.php
@@ -414,6 +414,73 @@ function get_valid_locale($requestedLocale)
     }
 }
 
+function hsl_to_hex($h, $s, $l)
+{
+    $r = $l;
+    $g = $l;
+    $b = $l;
+
+    $v = ($l <= 0.5) ? ($l * (1.0 + $s)) : ($l + $s - $l * $s);
+
+    if ($v > 0) {
+        $m;
+        $sv;
+        $sextant;
+        $fract;
+        $vsf;
+        $mid1;
+        $mid2;
+
+        $m = $l + $l - $v;
+        $sv = ($v - $m) / $v;
+        $h *= 6.0;
+        $sextant = floor($h);
+        $fract = $h - $sextant;
+        $vsf = $v * $sv * $fract;
+        $mid1 = $m + $vsf;
+        $mid2 = $v - $vsf;
+
+        switch ($sextant) {
+            case 0:
+                $r = $v;
+                $g = $mid1;
+                $b = $m;
+                break;
+            case 1:
+                $r = $mid2;
+                $g = $v;
+                $b = $m;
+                break;
+            case 2:
+                $r = $m;
+                $g = $v;
+                $b = $mid1;
+                break;
+            case 3:
+                $r = $m;
+                $g = $mid2;
+                $b = $v;
+                break;
+            case 4:
+                $r = $mid1;
+                $g = $m;
+                $b = $v;
+                break;
+            case 5:
+                $r = $v;
+                $g = $m;
+                $b = $mid2;
+                break;
+        }
+    }
+
+    $r = round($r * 255);
+    $g = round($g * 255);
+    $b = round($b * 255);
+
+    return sprintf('#%02x%02x%02x', $r, $g, $b);
+}
+
 function html_entity_decode_better($string)
 {
     // ENT_HTML5 to handle more named entities (&apos;, etc?).
