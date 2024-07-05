4da9b86c77cfb723ccc215a89e9677aecdb0d31d
Hiviexd <rayenattia@gmail.com>
2024-07-05 12:44:40 +0100

    make function actually work + apply suggestions

--- app/helpers.php
+++ app/helpers.php
@@ -416,67 +416,25 @@ function get_valid_locale($requestedLocale)
 
 function hsl_to_hex($h, $s, $l)
 {
-    $r = $l;
-    $g = $l;
-    $b = $l;
+    $s /= 100;
+    $l /= 100;
 
-    $v = $l <= 0.5 ? $l * (1.0 + $s) : $l + $s - $l * $s;
+    $c = (1 - abs(2 * $l - 1)) * $s;
+    $x = $c * (1 - abs(fmod(($h / 60), 2) - 1));
+    $m = $l - ($c / 2);
 
-    if ($v > 0) {
-        $m;
-        $sv;
-        $sextant;
-        $fract;
-        $vsf;
-        $mid1;
-        $mid2;
+    [$r, $g, $b] = match(true) {
+        $h < 60  => [$c, $x, 0],
+        $h < 120 => [$x, $c, 0],
+        $h < 180 => [0, $c, $x],
+        $h < 240 => [0, $x, $c],
+        $h < 300 => [$x, 0, $c],
+        default  => [$c, 0, $x]
+    };
 
-        $m = $l + $l - $v;
-        $sv = ($v - $m) / $v;
-        $h *= 6.0;
-        $sextant = floor($h);
-        $fract = $h - $sextant;
-        $vsf = $v * $sv * $fract;
-        $mid1 = $m + $vsf;
-        $mid2 = $v - $vsf;
-
-        switch ($sextant) {
-            case 0:
-                $r = $v;
-                $g = $mid1;
-                $b = $m;
-                break;
-            case 1:
-                $r = $mid2;
-                $g = $v;
-                $b = $m;
-                break;
-            case 2:
-                $r = $m;
-                $g = $v;
-                $b = $mid1;
-                break;
-            case 3:
-                $r = $m;
-                $g = $mid2;
-                $b = $v;
-                break;
-            case 4:
-                $r = $mid1;
-                $g = $m;
-                $b = $v;
-                break;
-            case 5:
-                $r = $v;
-                $g = $m;
-                $b = $mid2;
-                break;
-        }
-    }
-
-    $r = round($r * 255);
-    $g = round($g * 255);
-    $b = round($b * 255);
+    $r = round(($r + $m) * 255);
+    $g = round(($g + $m) * 255);
+    $b = round(($b + $m) * 255);
 
     return sprintf('#%02x%02x%02x', $r, $g, $b);
 }
