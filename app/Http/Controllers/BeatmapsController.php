c6291153c466a67618039bc6f1590a8c9fcb2595
bakaneko <notbakaneko@users.noreply.github.com>
2024-07-30 17:14:09 +0900

    eagerload for userScore

--- app/Http/Controllers/BeatmapsController.php
+++ app/Http/Controllers/BeatmapsController.php
@@ -23,7 +23,7 @@ use App\Transformers\ScoreTransformer;
 class BeatmapsController extends Controller
 {
     const DEFAULT_API_INCLUDES = ['beatmapset.ratings', 'failtimes', 'max_combo'];
-    const DEFAULT_SCORE_INCLUDES = ['user', 'user.country', 'user.cover'];
+    const DEFAULT_SCORE_INCLUDES = ['user', 'user.country', 'user.cover', 'beatmap.owners'];
 
     public function __construct()
     {
