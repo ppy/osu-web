204f645b4dec11e6c15fe18cc3a34cb6a50cff8d
nanaya <me@nanaya.net>
2024-08-16 00:44:01 +0900

    Fix permission
    (mine ended up owned by root for some reason)

--- docker/development/entrypoint.sh
+++ docker/development/entrypoint.sh
@@ -23,6 +23,7 @@ if [ "$uid" != 0 ]; then
 fi
 
 usermod -d /app/.docker osuweb > /dev/null
+chown -f "${uid}:${gid}" .docker/js-build/assets .docker/js-build/builds
 
 # helper functions
 _rexec() {
