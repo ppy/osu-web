90a8e2eea207f644af4d0cafc8cf69b3b20c57dd
bakaneko <notbakaneko@users.noreply.github.com>
2024-08-29 12:42:48 +0900

    and the log message

--- bin/run_dusk.sh
+++ bin/run_dusk.sh
@@ -7,7 +7,7 @@ if ! pgrep chromedriver > /dev/null; then
     # wait for the driver to be ready
     printf "Waiting for chromedriver to start..."
     chromedriver_tries=0
-    while ! grep -qF "ChromeDriver was started successfully." "$chromedriver_log"; do
+    while ! grep -qF "ChromeDriver was started successfully on port 9515." "$chromedriver_log"; do
         printf .
         sleep 1
         chromedriver_tries=$(($chromedriver_tries + 1))
