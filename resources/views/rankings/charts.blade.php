2652f712042c9b8dbcb49e5dbcf6e0ad9493222b
bakaneko <notbakaneko@users.noreply.github.com>
2024-09-11 17:20:35 +0900

    modifiers do nothing useful when react hasn't rendered

--- resources/views/rankings/charts.blade.php
+++ resources/views/rankings/charts.blade.php
@@ -7,7 +7,7 @@
 @section('ranking-header')
     <div class="osu-page osu-page--ranking-info">
         <div class="js-react--basic-select-options">
-            <div class="select-options select-options--basic">
+            <div class="select-options">
                 <div class="select-options__select">
                     <span class="select-options__option">
                         {{ $spotlight->name }}
