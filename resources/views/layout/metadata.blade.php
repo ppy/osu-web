be11bc17e0942ea5b3f8b71e9a44afc4f6acd529
nanaya <me@nanaya.net>
2024-10-15 16:10:16 +0900

    Use origin check for guests

--- resources/views/layout/metadata.blade.php
+++ resources/views/layout/metadata.blade.php
@@ -47,7 +47,7 @@
 @endif
 
 <meta name="csrf-param" content="_token">
-<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="csrf-token" content="{{ $currentUser === null ? '' : csrf_token() }}">
 
 <meta name="turbolinks-cache-control" content="no-preview">
 
