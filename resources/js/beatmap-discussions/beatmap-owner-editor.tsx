d2638102ba6674e28db312cb4c37489c5ac7d725
bakaneko <notbakaneko@users.noreply.github.com>
2024-11-18 19:36:04 +0900

    it's turbo now

--- resources/js/beatmap-discussions/beatmap-owner-editor.tsx
+++ resources/js/beatmap-discussions/beatmap-owner-editor.tsx
@@ -54,7 +54,7 @@ export default class BeatmapOwnerEditor extends React.Component<Props> {
   }
 
   componentDidUpdate() {
-    document.addEventListener('turbolinks:before-visit', this.handleBeforeVisit);
+    document.addEventListener('turbo:before-visit', this.handleBeforeVisit);
 
     if (this.shouldFocusInputOnNextRender) {
       this.shouldFocusInputOnNextRender = false;
@@ -64,7 +64,7 @@ export default class BeatmapOwnerEditor extends React.Component<Props> {
 
   componentWillUnmount() {
     this.updateOwnerXhr?.abort();
-    document.removeEventListener('turbolinks:before-visit', this.handleBeforeVisit);
+    document.removeEventListener('turbo:before-visit', this.handleBeforeVisit);
   }
 
   render() {
