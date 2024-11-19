4c31a32e95dd199735e30bfd1e2e15cf478e6e19
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-11-19 15:10:12 +0100

    Fix missing licence header

--- database/migrations/2024_11_19_130731_create_beatmap_versioning_tables.php
+++ database/migrations/2024_11_19_130731_create_beatmap_versioning_tables.php
@@ -1,5 +1,7 @@
 <?php
 
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
