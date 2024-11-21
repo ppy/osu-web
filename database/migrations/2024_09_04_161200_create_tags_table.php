3ac3111fc7ae268d66a5865b28c446eedca0d297
tsunyoku <tsunyoku@gmail.com>
2024-11-21 13:46:13 +0000

    add unique constraint to `name` column of `tags`

--- database/migrations/2024_09_04_161200_create_tags_table.php
+++ database/migrations/2024_09_04_161200_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('description');
             $table->timestamps();
         });
