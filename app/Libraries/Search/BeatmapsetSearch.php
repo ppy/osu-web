8e0612441715165c283021f679d69aa750a70cfc
bakaneko <notbakaneko@users.noreply.github.com>
2024-12-25 17:41:59 +0900

    search nested tags

--- app/Libraries/Search/BeatmapsetSearch.php
+++ app/Libraries/Search/BeatmapsetSearch.php
@@ -60,6 +60,12 @@ class BeatmapsetSearch extends RecordSearch
                     ->should(['term' => ['_id' => ['value' => $this->params->queryString, 'boost' => 100]]])
                     ->should(QueryHelper::queryString($this->params->queryString, $partialMatchFields, 'or', 1 / count($terms)))
                     ->should(QueryHelper::queryString($this->params->queryString, [], 'and'))
+                    ->should([
+                        'nested' => [
+                            'path' => 'beatmaps',
+                            'query' => QueryHelper::queryString($this->params->queryString, ['beatmaps.top_tags'], 'or', 0.5 / count($terms)),
+                        ]
+                    ])
             );
         }
 
