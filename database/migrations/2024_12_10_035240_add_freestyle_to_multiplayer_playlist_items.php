4699f50af05ab3908e9f33022eee0a6dc9e85ed9
Dan Balasescu <smoogipoo@smgi.me>
2025-01-08 19:01:00 +0900

    Require strict types + add license header

--- database/migrations/2024_12_10_035240_add_freestyle_to_multiplayer_playlist_items.php
+++ database/migrations/2024_12_10_035240_add_freestyle_to_multiplayer_playlist_items.php
@@ -1,5 +1,10 @@
 <?php
 
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
