0638d90ce62f60f4b7beae5d4d851d7fc1ee196b
Venix <30481900+venix12@users.noreply.github.com>
2025-01-25 13:36:35 +0100

    use proper table name for drop

--- database/migrations/2025_01_22_190453_create_user_season_score_aggregates_table.php
+++ database/migrations/2025_01_22_190453_create_user_season_score_aggregates_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_season_scores');
+        Schema::dropIfExists('user_season_score_aggregates');
     }
 };
