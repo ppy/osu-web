abd17d007cb828e1693019f7c719c0df2252439d
agatemosu <agaatem@outlook.com>
2025-02-08 20:37:44 +0100

    Convert changelog chart to TypeScript

--- resources/js/charts/changelog-chart.ts
+++ resources/js/charts/changelog-chart.ts
@@ -1,245 +1,309 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import { fadeIn, fadeOut } from 'utils/fade'
-import { formatNumber } from 'utils/html'
-import { parseJson } from 'utils/json'
+import * as d3 from 'd3';
+import { first, groupBy, kebabCase, last, map, sumBy } from 'lodash';
+import moment from 'moment';
+import { fadeIn, fadeOut } from 'utils/fade';
+import { formatNumber } from 'utils/html';
+import { parseJson } from 'utils/json';
 
-export default class ChangelogChart
-  constructor: (area) ->
-    @options =
-      scales:
-        x: d3.scaleTime()
-        y: d3.scaleLinear()
-        class: d3.scaleOrdinal()
+interface BuildHistory {
+  created_at: string;
+  label: string | null;
+  normalized: number;
+  user_count: number;
+}
 
-    @area = d3.select area
-    @area.classed 'changelog-chart', true
+interface Config {
+  build_history: BuildHistory[];
+  order: (string | null)[];
+  stream_name: string | null;
+}
 
-    @svg = @area
-      .append 'svg'
+type DataObj = {
+  created_at: string;
+  date: Date;
+  date_formatted: string;
+} & Record<string, BuildHistory>;
 
-    @svgWrapper = @svg
-      .append 'g'
+type Datum = d3.SeriesPoint<DataObj>;
 
-    @areaFunction = d3.area()
-      .curve d3.curveMonotoneX
-      .x (d) => @options.scales.x d.data.date
-      .y1 (d) => @options.scales.y d[1]
-      .y0 (d, i) => @options.scales.y d[0]
+export default class ChangelogChart {
+  private _autoHideTooltip?: number;
+  private readonly area;
+  private readonly areaFunction;
+  private config!: Config;
+  private data!: d3.Series<DataObj, string>[];
+  private hasData!: boolean | null;
+  private height!: number;
+  private readonly hoverArea;
+  private readonly options;
+  private readonly svg;
+  private readonly svgWrapper;
+  private readonly tooltip;
+  private readonly tooltipArea;
+  private readonly tooltipContainer;
+  private readonly tooltipDate;
+  private readonly tooltipLine;
+  private readonly tooltipName;
+  private readonly tooltipUserCount;
+  private width!: number;
+  private x?: Date;
+  private y?: number;
 
-    @hoverArea = @svg.append 'rect'
-      .classed 'changelog-chart__hover-area', true
-      .on 'mouseout', @hideTooltip
-      .on 'mousemove', @moveTooltip
+  constructor(area: HTMLElement) {
+    this.options = {
+      scales: {
+        class: d3.scaleOrdinal<string>(),
+        x: d3.scaleTime(),
+        y: d3.scaleLinear(),
+      },
+    };
 
-    @tooltipArea = @area.append 'div'
-      .classed 'changelog-chart__tooltip-area', true
+    this.area = d3.select(area);
+    this.area.classed('changelog-chart', true);
 
-    @tooltipContainer = @tooltipArea.append 'div'
-      .classed 'changelog-chart__tooltip-container', true
-      .attr 'data-visibility', 'hidden'
+    this.svg = this.area.append('svg');
+    this.svgWrapper = this.svg.append('g');
 
-    @tooltipContainer.append 'div'
-      .classed 'changelog-chart__tooltip-line', true
+    this.areaFunction = d3
+      .area<Datum>()
+      .curve(d3.curveMonotoneX)
+      .x((d) => this.options.scales.x(d.data.date))
+      .y1((d) => this.options.scales.y(d[1]))
+      .y0((d) => this.options.scales.y(d[0]));
 
-    @tooltip = @tooltipContainer.append 'div'
-      .classed 'changelog-chart__tooltip', true
+    this.hoverArea = this.svg
+      .append('rect')
+      .classed('changelog-chart__hover-area', true)
+      .on('mouseout', this.hideTooltip)
+      .on('mousemove', this.moveTooltip);
 
-    @tooltipName = @tooltip.append 'div'
+    this.tooltipArea = this.area
+      .append('div')
+      .classed('changelog-chart__tooltip-area', true);
 
-    @tooltipUserCount = @tooltip.append 'div'
-      .classed 'changelog-chart__text changelog-chart__text--user-count', true
+    this.tooltipContainer = this.tooltipArea
+      .append('div')
+      .classed('changelog-chart__tooltip-container', true)
+      .attr('data-visibility', 'hidden');
 
-    @tooltipDate = @tooltip.append 'div'
-      .classed 'changelog-chart__text changelog-chart__text--date', true
+    this.tooltipContainer
+      .append('div')
+      .classed('changelog-chart__tooltip-line', true);
 
-    @tooltipContainer.append 'div'
-      .classed 'changelog-chart__tooltip-line', true
+    this.tooltip = this.tooltipContainer
+      .append('div')
+      .classed('changelog-chart__tooltip', true);
 
-    @tooltipLine = @tooltipContainer.selectAll '.changelog-chart__tooltip-line'
+    this.tooltipName = this.tooltip.append('div');
 
+    this.tooltipUserCount = this.tooltip
+      .append('div')
+      .classed('changelog-chart__text changelog-chart__text--user-count', true);
 
-  loadData: ->
-    @config = parseJson 'json-chart-config'
+    this.tooltipDate = this.tooltip
+      .append('div')
+      .classed('changelog-chart__text changelog-chart__text--date', true);
 
-    {data, hasData} = @normalizeData @config.build_history
+    this.tooltipContainer
+      .append('div')
+      .classed('changelog-chart__tooltip-line', true);
 
-    stack = d3.stack()
-      .keys @config.order
-      .value (d, val) ->
-        if d[val]? then d[val].normalized else 0
+    this.tooltipLine = this.tooltipContainer.selectAll(
+      '.changelog-chart__tooltip-line',
+    );
+  }
 
-    @data = stack data
+  loadData() {
+    this.config = parseJson('json-chart-config');
 
-    @hasData = @config.build_history? &&
-      @config.build_history.length > 0 &&
-      hasData
+    const { data, hasData } = this.normalizeData(this.config.build_history);
 
+    const stack = d3
+      .stack<DataObj, string>()
+      .keys(this.config.order.map(String))
+      .value((d, val) => (d[val] != null ? d[val].normalized : 0));
 
-    @resize()
+    this.data = stack(data);
+    this.hasData = this.config.build_history.length > 0 && hasData;
 
+    this.resize();
+  }
 
-  setDimensions: ->
-    areaDims = @area.node().getBoundingClientRect()
+  resize() {
+    this.area.classed('hidden', !this.hasData);
 
-    @width = areaDims.width
-    @height = areaDims.height
+    if (!this.hasData) return;
 
+    this.setDimensions();
+    this.setScalesRange();
+    this.setSvgSize();
+    this.setHoverAreaSize();
+    this.drawLines();
+    this.positionTooltip();
+  }
 
-  setSvgSize: ->
-    @svg
-      .attr 'width', @width
-      .attr 'height', @height
-
-
-  setHoverAreaSize: ->
-    @hoverArea
-      .attr 'width', @width
-      .attr 'height', @height
-
-
-  setScalesRange: ->
-    @options.scales.x
-      .range [0, @width]
-      .domain [_.first(@data[0])?.data.date, _.last(@data[0])?.data.date]
-
-    @options.scales.y
-      .range [0, @height]
-      .domain [0, 1]
-
-    @options.scales.class
-      .range _.map @config.order, (d, i) =>
-        # rotate over available build ids (0-6) when the amount of builds
-        # exceeds the available amount of colors
-        if @config.stream_name? then "#{@config.stream_name}-build-#{i % 7}" else _.kebabCase d
-      .domain @config.order
-
-
-  drawLines: ->
-    @svgWrapper
-      .selectAll 'g'
-      .data @data
+  private drawLines() {
+    this.svgWrapper
+      .selectAll('g')
+      .data(this.data)
       .enter()
-      .append 'path'
-      .attr 'class', (d) => "changelog-chart__area changelog-chart__area--#{@options.scales.class d.key}"
-      .attr 'd', @areaFunction
+      .append('path')
+      .attr('class', (d) => `changelog-chart__area changelog-chart__area--${this.options.scales.class(d.key)}`)
+      .attr('d', this.areaFunction);
+  }
 
+  private readonly hideTooltip = () => fadeOut(this.tooltipContainer.node());
 
-  showTooltip: =>
-    fadeIn @tooltipContainer.node()
+  private readonly moveTooltip = (event: Event) => {
+    const mousePos = d3.pointer(event);
+    this.x = this.options.scales.x.invert(mousePos[0]);
+    this.y = mousePos[1] / this.height;
 
+    this.showTooltip();
 
-  hideTooltip: =>
-    fadeOut @tooltipContainer.node()
+    clearTimeout(this._autoHideTooltip);
+    this._autoHideTooltip = setTimeout(this.hideTooltip, 3000);
 
+    this.positionTooltip();
+  };
 
-  moveTooltip: (event) =>
-    mousePos = d3.pointer event
-    @x = @options.scales.x.invert mousePos[0]
-    @y = mousePos[1] / @height
+  private normalizeData(rawData: BuildHistory[]) {
+    // normalize the user count values
+    // and parse data into a form digestible by d3.stack()
 
-    @showTooltip()
+    let resetLabel: string | null = null;
+    let hasData: boolean | null = null;
 
-    Timeout.clear @_autoHideTooltip
-    @_autoHideTooltip = Timeout.set 3000, @hideTooltip
+    const sortedData = groupBy(rawData, 'created_at');
 
-    @positionTooltip()
+    const data = map(sortedData, (values, timestamp) => {
+      let sum = sumBy(values, 'user_count');
 
+      if (sum === 0) {
+        let fakedVal = resetLabel != null
+          ? values.find((val) => val.label === resetLabel)
+          : null;
 
-  positionTooltip: =>
-    x = @x
-    y = @y
+        if (fakedVal == null) {
+          fakedVal = last(values);
+          resetLabel = fakedVal?.label ?? null;
+        }
 
-    return unless x?
+        if (fakedVal) fakedVal.user_count = 1;
+        sum = 1;
+      } else {
+        hasData ??= true;
+      }
 
-    pos = d3.bisector((d) -> d.data.date).left @data[0], x
+      // parse date stored in strings to JS Date object for use by
+      // d3 domains, and format it into a string shown on the tooltip
+      const m = moment(values[0].created_at);
 
-    return unless pos?
+      const obj = {
+        created_at: timestamp,
+        date: m.toDate(),
+        date_formatted: m.format('YYYY/MM/DD'),
+      } as DataObj;
 
-    for el, i in @data
-      if y <= el[pos][1] && el[pos].data[el.key]?
-        dataRow = i
-        currentLabel = el.key
-        labelModifier = @options.scales.class currentLabel
-        break
+      for (const val of values) {
+        val.normalized = val.user_count / sum;
+        obj[String(val.label)] = val;
+      }
 
-    coord = @options.scales.x x
+      return obj;
+    });
 
-    @tooltipName
-      .attr 'class', "changelog-chart__text changelog-chart__text--name changelog-chart__text--#{labelModifier}"
-      .text currentLabel
-    @tooltipUserCount.text formatNumber(@data[dataRow][pos].data[currentLabel].user_count)
-    @tooltipDate.text @data[dataRow][pos].data.date_formatted
+    return { data, hasData };
+  }
 
-    tooltipWidth = @tooltip.node().getBoundingClientRect().width
-    tooltipXBase = coord - (tooltipWidth / 2)
+  private positionTooltip() {
+    const { x, y } = this;
 
-    # shift the toltip container when near to the left/right edge
-    # of the chart, so that the tooltip doesn't extend outside of ito
-    tooltipX =
-      if tooltipXBase < 0
-        0
-      else if tooltipXBase + tooltipWidth > @width
-        @width - tooltipWidth
-      else
-        tooltipXBase
+    if (x == null || y == null) return;
 
-    @tooltip
-      .style 'transform', "translateX(#{tooltipX}px)"
+    const pos = d3
+      .bisector<Datum, Date>((d) => d.data.date)
+      .left(this.data[0], x);
 
-    @tooltipLine
-      .style 'transform', "translateX(#{coord}px)"
+    let dataRow = 0;
+    let currentLabel = '';
+    let labelModifier = '';
 
+    for (let i = 0; i < this.data.length; i++) {
+      const el = this.data[i];
+      if (y <= el[pos][1] && el[pos].data[el.key] != null) {
+        dataRow = i;
+        currentLabel = el.key;
+        labelModifier = this.options.scales.class(currentLabel);
+        break;
+      }
+    }
 
-  resize: =>
-    @area.classed 'hidden', !@hasData
+    const coord = this.options.scales.x(x);
 
-    return if !@hasData
+    this.tooltipName
+      .attr(
+        'class',
+        `changelog-chart__text changelog-chart__text--name changelog-chart__text--${labelModifier}`,
+      )
+      .text(currentLabel);
+    this.tooltipUserCount.text(
+      formatNumber(this.data[dataRow][pos].data[currentLabel].user_count),
+    );
+    this.tooltipDate.text(this.data[dataRow][pos].data.date_formatted);
 
-    @setDimensions()
-    @setScalesRange()
-    @setSvgSize()
-    @setHoverAreaSize()
-    @drawLines()
-    @positionTooltip()
+    const tooltipWidth = this.tooltip.node()?.getBoundingClientRect().width ?? 0;
+    const tooltipXBase = coord - tooltipWidth / 2;
 
+    // shift the tooltip container when near to the left/right edge
+    // of the chart, so that the tooltip doesn't extend outside of it
+    const tooltipX = tooltipXBase < 0
+      ? 0
+      : tooltipXBase + tooltipWidth > this.width
+        ? this.width - tooltipWidth
+        : tooltipXBase;
 
+    this.tooltip.style('transform', `translateX(${tooltipX}px)`);
+    this.tooltipLine.style('transform', `translateX(${coord}px)`);
+  }
 
-  normalizeData: (rawData) ->
-    # normalize the user count values
-    # and parse data into a form digestible by d3.stack()
+  private setDimensions() {
+    const areaDims = this.area.node()?.getBoundingClientRect();
+    if (!areaDims) return;
 
-    resetLabel = null
-    hasData = null
+    this.width = areaDims.width;
+    this.height = areaDims.height;
+  }
 
-    data =
-      for own timestamp, values of _.groupBy rawData, 'created_at'
-        sum = _.sumBy values, 'user_count'
+  private setHoverAreaSize() {
+    this.hoverArea.attr('width', this.width).attr('height', this.height);
+  }
 
-        if sum == 0
-          fakedVal = _.find(values, label: resetLabel) if resetLabel?
-          unless fakedVal?
-            fakedVal = _.last(values)
-            resetLabel = fakedVal.label
-          fakedVal.user_count = 1
-          sum = 1
-        else
-          hasData ?= true
+  private setScalesRange() {
+    this.options.scales.x.range([0, this.width]).domain([
+      first(this.data[0])?.data.date ?? 0,
+      last(this.data[0])?.data.date ?? 0,
+    ]);
 
-        # parse date stored in strings to JS Date object for use by
-        # d3 domains, and format it into a string shown on the tooltip
-        m = moment values[0].created_at
+    this.options.scales.y.range([0, this.height]).domain([0, 1]);
 
-        obj =
-          created_at: timestamp
-          date: m.toDate()
-          date_formatted: m.format 'YYYY/MM/DD'
+    this.options.scales.class.range(
+      this.config.order.map((d, i) =>
+        // rotate over available build ids (0-6) when the amount of builds
+        // exceeds the available amount of colors
+        this.config.stream_name != null
+          ? `${this.config.stream_name}-build-${i % 7}`
+          : kebabCase(d ?? ''),
+      ),
+    ).domain(this.config.order.map(String));
+  }
 
-        for val in values
-          val.normalized = val.user_count / sum
-          obj[val.label] = val
+  private setSvgSize() {
+    this.svg.attr('width', this.width).attr('height', this.height);
+  }
 
-        obj
-
-    {data, hasData}
+  private readonly showTooltip = () => fadeIn(this.tooltipContainer.node());
+}
--- resources/js/core/changelog-chart-loader.ts
+++ resources/js/core/changelog-chart-loader.ts
@@ -1,31 +1,35 @@
-# Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
-# See the LICENCE file in the repository root for full licence text.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
 
-import ChangelogChart from 'charts/changelog-chart'
+import ChangelogChart from 'charts/changelog-chart';
 
-export default class ChangelogChartLoader
-  constructor: ->
-    document.addEventListener 'turbo:load', @initialize
-    document.addEventListener 'turbo:before-cache', @reset
+export default class ChangelogChartLoader {
+  private chart: ChangelogChart | null = null;
 
+  constructor() {
+    document.addEventListener('turbo:load', this.initialize);
+    document.addEventListener('turbo:before-cache', this.reset);
+  }
 
-  initialize: =>
-    container = document.querySelector('.js-changelog-chart')
+  readonly initialize = () => {
+    const container = document.querySelector('.js-changelog-chart');
 
-    return unless container?
+    if (!(container instanceof HTMLElement)) return;
 
-    # reset existing chart
-    container.innerHTML = ''
+    // reset existing chart
+    container.innerHTML = '';
 
-    @chart = new ChangelogChart container
-    @chart.loadData()
-    window.addEventListener 'resize', @resize
+    this.chart = new ChangelogChart(container);
+    this.chart.loadData();
+    window.addEventListener('resize', this.resize);
+  };
 
+  private readonly reset = () => {
+    this.chart = null;
+    window.removeEventListener('resize', this.resize);
+  };
 
-  reset: =>
-    @chart = null
-    window.removeEventListener 'resize', @resize
-
-
-  resize: =>
-    @chart.resize()
+  private readonly resize = () => {
+    this.chart?.resize();
+  };
+}
