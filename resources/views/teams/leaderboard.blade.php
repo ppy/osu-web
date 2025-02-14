76d4781e816d1d591136c5893ef40beda9a7f4e4
nanaya <me@nanaya.net>
2025-02-14 18:26:01 +0900

    Remove leftover stuff copied from other view

--- resources/views/teams/leaderboard.blade.php
+++ resources/views/teams/leaderboard.blade.php
@@ -2,28 +2,13 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
-@php
-    use App\Transformers\UserCompactTransformer;
-
-    $userTransformer = new UserCompactTransformer();
-    $toJson = fn ($users) => json_collection($users, $userTransformer, UserCompactTransformer::CARD_INCLUDES);
-    $teamMembers = array_map($toJson, $team->members->mapToGroups(fn ($member) => [
-        $member->user_id === $team->leader_id ? 'leader' : 'member' => $member->userOrDeleted(),
-    ])->all());
-    $teamMembers['member'] ??= [];
-    $teamMembers['leader'] ??= $toJson([$team->members()->make(['user_id' => $team->leader_id])->userOrDeleted()]);
-    $headerUrl = $team->header()->url();
-
-    $currentUser = Auth::user();
-@endphp
-
 @extends('master', [
     'titlePrepend' => $team->name,
 ])
 
 @section('content')
     @component('layout._page_header_v4', ['params' => [
-        'backgroundImage' => $headerUrl,
+        'backgroundImage' => $team->header()->url(),
         'links' => App\Http\Controllers\TeamsController::pageLinks('leaderboard', $team),
         'theme' => 'team',
     ]])
