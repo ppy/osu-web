536a5fe2eca5d1642516e9da548812ca137ad074
nanaya <me@nanaya.net>
2025-02-19 00:47:40 +0900

    Move to general model attribute namespace

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -124,14 +124,14 @@
                             </div>
                             @if (present($team->url))
                                 <div class="team-info-entry">
-                                    <div class="team-info-entry__title">{{ osu_trans('teams.edit.settings.url') }}</div>
+                                    <div class="team-info-entry__title">{{ osu_trans('model_validation.team.attributes.url') }}</div>
                                     <div class="team-info-entry__value u-ellipsis-overflow">
                                         <a href="{{ $team->url }}">{{ $team->url }}</a>
                                     </div>
                                 </div>
                             @endif
                             <div class="team-info-entry">
-                                <div class="team-info-entry__title">{{ osu_trans('teams.edit.settings.default_ruleset') }}</div>
+                                <div class="team-info-entry__title">{{ osu_trans('model_validation.team.attributes.default_ruleset_id') }}</div>
                                 <div class="team-info-entry__value">
                                     @php
                                         $rulesetName = App\Models\Beatmap::modeStr($team->default_ruleset_id);
@@ -141,7 +141,7 @@
                                 </div>
                             </div>
                             <div class="team-info-entry">
-                                <div class="team-info-entry__title">{{ osu_trans('teams.edit.settings.application') }}</div>
+                                <div class="team-info-entry__title">{{ osu_trans('model_validation.team.attributes.is_open') }}</div>
                                 <div class="team-info-entry__value">
                                     {{ osu_trans('teams.edit.settings.application_state.state_'.(string) $team->is_open) }}
                                 </div>
