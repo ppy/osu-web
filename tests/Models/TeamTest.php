403220d34a6931c282171686b79e9b2f69e44db8
nanaya <me@nanaya.net>
2025-02-20 19:03:17 +0900

    Delete pending applications when deleting team

--- tests/Models/TeamTest.php
+++ tests/Models/TeamTest.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Tests\Models;
 
 use App\Models\Team;
+use App\Models\TeamApplication;
 use App\Models\TeamMember;
 use App\Models\User;
 use Tests\TestCase;
@@ -26,10 +27,12 @@ class TeamTest extends TestCase
     {
         $team = Team::factory()->create();
         $team->members()->create(['user_id' => User::factory()->create()->getKey()]);
+        $team->applications()->create(['user_id' => User::factory()->create()->getKey()]);
         $otherTeam = Team::factory()->create();
         $otherTeam->members()->create(['user_id' => User::factory()->create()->getKey()]);
 
         $this->expectCountChange(fn () => Team::count(), -1);
+        $this->expectCountChange(fn () => TeamApplication::count(), -1);
         $this->expectCountChange(fn () => TeamMember::count(), -2);
         $this->expectCountChange(fn () => $otherTeam->members()->count(), 0);
 
