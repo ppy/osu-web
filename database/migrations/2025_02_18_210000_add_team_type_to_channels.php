791d8d0e75815fb83011df1c458097126ed78f8b
nanaya <me@nanaya.net>
2025-02-20 13:52:50 +0900

    Copied old code a bit too much

--- database/migrations/2025_02_18_210000_add_team_type_to_channels.php
+++ database/migrations/2025_02_18_210000_add_team_type_to_channels.php
@@ -3,9 +3,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 
-class AddTeamTypeToChannels extends Migration
+return new class extends Migration
 {
     public function up(): void
     {
@@ -36,4 +38,4 @@ class AddTeamTypeToChannels extends Migration
             'ANNOUNCE'
         ) NOT NULL DEFAULT 'TEMPORARY'");
     }
-}
+};
