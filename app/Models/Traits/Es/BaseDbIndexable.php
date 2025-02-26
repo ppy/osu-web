0bf051f26dd8aae6387f4ffd6b735130a30d1152
bakaneko <notbakaneko@users.noreply.github.com>
2025-02-26 15:33:26 +0900

    update to spread operator

--- app/Models/Traits/Es/BaseDbIndexable.php
+++ app/Models/Traits/Es/BaseDbIndexable.php
@@ -83,12 +83,13 @@ trait BaseDbIndexable
 
     public function esDeleteDocument(array $options = [])
     {
-        $document = array_merge([
+        $document = [
             'index' => static::esIndexName(),
             'routing' => $this->esRouting(),
             'id' => $this->getEsId(),
             'client' => ['ignore' => 404],
-        ], $options);
+            ...$options,
+        ];
 
         return Es::getClient()->delete($document);
     }
@@ -99,12 +100,13 @@ trait BaseDbIndexable
             return $this->esDeleteDocument($options);
         }
 
-        $document = array_merge([
+        $document = [
             'index' => static::esIndexName(),
             'routing' => $this->esRouting(),
             'id' => $this->getEsId(),
             'body' => $this->toEsJson(),
-        ], $options);
+            ...$options,
+        ];
 
         return Es::getClient()->index($document);
     }
