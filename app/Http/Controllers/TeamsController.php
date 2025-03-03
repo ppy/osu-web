761e0019963708b15d4004962b7bdedeff711a7b
nanaya <me@nanaya.net>
2025-03-03 11:23:35 +0900

    Fix team channel update not being rolled back on error

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -145,12 +145,13 @@ class TeamsController extends Controller
         $user = \Auth::user();
         $team = (new Team([...$params, 'leader_id' => $user->getKey()]));
         try {
-            \DB::transaction(function () use ($team, $user) {
-                $channel = $team->createChannel();
-                $team->saveOrExplode();
-                $team->members()->create(['user_id' => $user->getKey()]);
-                $channel->addUser($user);
-            });
+            \DB::transaction(fn () =>
+                \DB::connection('mysql-chat')->transaction(function () use ($team, $user) {
+                    $channel = $team->createChannel();
+                    $team->saveOrExplode();
+                    $team->members()->create(['user_id' => $user->getKey()]);
+                    $channel->addUser($user);
+                }));
         } catch (ModelNotSavedException) {
             return ext_view('teams.create', compact('team'), status: 422);
         }
