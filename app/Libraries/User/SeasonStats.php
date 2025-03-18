ba67dde1aeba734e9ef7ff4ac52f0cab44e35e00
Venix <30481900+venix12@users.noreply.github.com>
2025-03-18 09:46:16 +0100

    move stats calculation to its own function

--- app/Libraries/User/SeasonStats.php
+++ app/Libraries/User/SeasonStats.php
@@ -14,42 +14,46 @@ use App\Transformers\SeasonTransformer;
 
 class SeasonStats
 {
+    public function calculate(User $user, Season $season): ?array {
+        $score = $season->userScores()
+            ->whereBelongsTo($user)
+            ->first();
+
+        if ($score === null) {
+            return null;
+        }
+
+        $rank = $season->userScores()
+            ->where('user_id', '<>', $score->user_id)
+            ->where('total_score', '>=', $score->total_score)
+            ->count() + 1;
+
+        foreach ($season->divisionsWithMaxRanks() as $division) {
+            if ($rank <= $division['max_rank']) {
+                $userDivision = $division['division'];
+                break;
+            }
+        }
+
+        if (!isset($userDivision)) {
+            return null;
+        }
+
+        return [
+            'division' => json_item($userDivision, new SeasonDivisionTransformer()),
+            'rank' => $rank,
+            'season' => json_item($season, new SeasonTransformer()),
+            'total_score' => $score->total_score,
+        ];
+    }
+
     public static function get(User $user, Season $season): ?array
     {
         return \Cache::remember(
             static::seasonCacheKey($user->getKey(), $season->getKey()),
             600,
             function () use ($season, $user) {
-                $score = $season->userScores()
-                    ->whereBelongsTo($user)
-                    ->first();
-
-                if ($score === null) {
-                    return null;
-                }
-
-                $rank = $season->userScores()
-                    ->where('user_id', '<>', $score->user_id)
-                    ->where('total_score', '>=', $score->total_score)
-                    ->count() + 1;
-
-                foreach ($season->divisionsWithMaxRanks() as $division) {
-                    if ($rank <= $division['max_rank']) {
-                        $userDivision = $division['division'];
-                        break;
-                    }
-                }
-
-                if (!isset($userDivision)) {
-                    return null;
-                }
-
-                return [
-                    'division' => json_item($userDivision, new SeasonDivisionTransformer()),
-                    'rank' => $rank,
-                    'season' => json_item($season, new SeasonTransformer()),
-                    'total_score' => $score->total_score,
-                ];
+                return $this->calculate($user, $season);
             }
         );
     }
