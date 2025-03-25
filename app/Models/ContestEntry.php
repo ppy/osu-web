c88982d760945014f5efac4d55fd7951ae9adaa5
bakaneko <notbakaneko@users.noreply.github.com>
2025-03-25 20:11:57 +0900

    lint

--- app/Models/ContestEntry.php
+++ app/Models/ContestEntry.php
@@ -5,7 +5,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasMany;
