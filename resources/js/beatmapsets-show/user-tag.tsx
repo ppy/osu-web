c8c2791ca60a548aa2805b9d8ed7736aae51a0e4
bakaneko <notbakaneko@users.noreply.github.com>
2025-04-16 17:06:34 +0900

    new user tag style

--- resources/js/beatmapsets-show/user-tag.tsx
+++ resources/js/beatmapsets-show/user-tag.tsx
@@ -13,13 +13,31 @@ interface Props {
 
 @observer
 export default class UserTag extends React.PureComponent<Props> {
+  private readonly category;
+  private readonly name;
+
+  private get url() {
+    return route('beatmapsets.index', { q: makeSearchQueryOption('tag', this.props.tag.name) });
+  }
+
+  constructor(props: Props) {
+    super(props);
+
+    const split = props.tag.name.split('/');
+    this.category = split[0];
+    this.name = split[1];
+  }
+
   render() {
     return (
       <a
-        className='beatmapset-info__link'
-        href={route('beatmapsets.index', { q: makeSearchQueryOption('tag', this.props.tag.name) })}
+        className='user-tag'
+        href={this.url}
+        title={this.props.tag.description}
       >
-        {this.props.tag.name}
+        <span className='user-tag__item user-tag__item--category'>{this.category}</span>
+        <span className='user-tag__item user-tag__item--name'>{this.name}</span>
+        <span className='user-tag__item user-tag__item--count'>{this.props.tag.count}</span>
       </a>
     );
   }
