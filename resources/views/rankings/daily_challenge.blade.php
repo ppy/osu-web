fadf2a540018a101ca48120ff0f526d282bfaf68
nanaya <me@nanaya.net>
2025-06-16 19:41:43 +0900

    Simplify blade template for select options component

--- resources/views/rankings/daily_challenge.blade.php
+++ resources/views/rankings/daily_challenge.blade.php
@@ -13,23 +13,11 @@
 @endphp
 @section('ranking-header')
     <div class="osu-page osu-page--ranking-info">
-        <div class="js-react--basic-select-options">
-            <div class="select-options">
-                <div class="select-options__select">
-                    <span class="select-options__option">
-                        {{ $currentRoomOption['text'] }}
-                    </span>
-                </div>
-            </div>
-        </div>
-
-        <script id="json-basic-select-options" type="application/json">
-            {!! json_encode([
-                'currentItem' => $currentRoomOption,
-                'items' => $roomOptions,
-                'type' => 'daily_challenge',
-            ]) !!}
-        </script>
+        @include('objects._basic_select_options', ['selectOptions' => [
+            'currentItem' => $currentRoomOption,
+            'items' => $roomOptions,
+            'type' => 'daily_challenge',
+        ]])
 
         <div class="grid-items grid-items--ranking-info-bar">
             <div class="counter-box counter-box--ranking">
