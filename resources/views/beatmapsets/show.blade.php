d24f25b3e5ad3bd7259ff167d7c50a6d52a8c216
Hivie <me@hivie.tn>
2025-07-17 01:41:11 +0100

    move admin button from footer to admin menu

--- resources/views/beatmapsets/show.blade.php
+++ resources/views/beatmapsets/show.blade.php
@@ -4,14 +4,9 @@
 --}}
 @php
     $currentUser ??= Auth::user();
-    $extraFooterLinks = $currentUser?->isAdmin()
-        ? [
-            osu_trans('common.buttons.admin') => route('admin.beatmapsets.show', $beatmapset->getKey()),
-        ] : [];
 @endphp
 @extends('master', [
     'titlePrepend' => "{$beatmapset->getDisplayArtist($currentUser)} - {$beatmapset->getDisplayTitle($currentUser)}",
-    'extraFooterLinks' => $extraFooterLinks,
 ])
 
 @section('content')
@@ -23,6 +18,19 @@
                 <span class="admin-menu__button-icon fas fa-tools"></span>
             </button>
             <div class="admin-menu__menu js-menu" data-menu-id="admin-beatmapset" data-visibility="hidden">
+                @if ($currentUser?->isAdmin() ?? false)
+                    <a class="admin-menu-item" href="{{ route('admin.beatmapsets.show', $beatmapset->getKey()) }}" target="_blank">
+                        <span class="admin-menu-item__content">
+                            <span class="admin-menu-item__label admin-menu-item__label--icon">
+                                <span class="fas fa-cogs"></span>
+                            </span>
+
+                            <span class="admin-menu-item__label admin-menu-item__label--text">
+                                {{ osu_trans('beatmapsets.show.admin.page') }}
+                            </span>
+                        </span>
+                    </a>
+                @endif
                 <a class="admin-menu-item" href="{{ $beatmapset->coverURL('raw') }}" target="_blank">
                     <span class="admin-menu-item__content">
                         <span class="admin-menu-item__label admin-menu-item__label--icon">
