b781400baba9de9f0e792264091adb0ab26a4df0
bakaneko <notbakaneko@users.noreply.github.com>
2025-07-25 20:20:05 +0900

    set mapping when setting options

--- app/Libraries/Search/BeatmapsetSearchOptions.php
+++ app/Libraries/Search/BeatmapsetSearchOptions.php
@@ -9,6 +9,31 @@ namespace App\Libraries\Search;
 
 class BeatmapsetSearchOptions
 {
+    private const OPTION_MAP = [
+        'ar' => 'ar',
+        'artist' => 'artist',
+        'bpm' => 'bpm',
+        'circles' => 'countNormal',
+        'created' => 'created',
+        'creator' => 'creator',
+        'cs' => 'cs',
+        'difficulty' => 'difficulty',
+        'dr' => 'drain',
+        'favourites' => 'favouriteCount',
+        'featured_artist' => 'featuredArtist',
+        'keys' => 'keys',
+        'length' => 'totalLength',
+        'od' => 'accuracy',
+        'ranked' => 'ranked',
+        'sliders' => 'countSlider',
+        'source' => 'source',
+        'stars' => 'difficultyRating',
+        'status' => 'statusRange',
+        'tag' => 'tags',
+        'title' => 'title',
+        'updated' => 'updated',
+    ];
+
     public ?array $accuracy = null;
     public ?array $ar = null;
     public ?string $artist = null;
@@ -31,4 +56,13 @@ class BeatmapsetSearchOptions
     public ?string $title = null;
     public ?array $totalLength = null;
     public ?string $updated = null;
+
+    public function set(string $key, $value): void
+    {
+        $propName = self::OPTION_MAP[$key] ?? null;
+
+        if ($propName !== null) {
+            $this->$propName = $value;
+        }
+    }
 }
