110d05d1aee834bb241b66841918bd1f081d4e5c
bakaneko <notbakaneko@users.noreply.github.com>
2025-07-29 19:24:51 +0900

    reorder functions

--- tests/Models/OAuth/GroupPermissionTest.php
+++ tests/Models/OAuth/GroupPermissionTest.php
@@ -7,40 +7,11 @@ namespace Tests\Models\OAuth;
 
 use App\Models\OAuth\Client;
 use App\Models\User;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class GroupPermissionTest extends TestCase
 {
-    /**
-     * TODO: maybe an exclusion list of when groups are allowed with token instead...
-     *
-     * @dataProvider groupsDataProvider
-     *
-     * @return void
-     */
-    public function testGroupWithOAuth(string $group, string $method, bool $inGroup)
-    {
-        $user = User::factory()->withGroup($group)->create();
-        $client = Client::factory()->create(['user_id' => $user]);
-        $token = $this->createToken($user, ['*'], $client);
-        $this->actAsUserWithToken($token);
-
-        $this->assertSame($inGroup, auth()->user()->$method());
-    }
-
-    /**
-     * @dataProvider groupsDataProvider
-     *
-     * @return void
-     */
-    public function testGroupWithoutOAuth(string $group, string $method)
-    {
-        $user = User::factory()->withGroup($group)->create();
-        $this->actAsUser($user);
-
-        $this->assertTrue(auth()->user()->$method());
-    }
-
     public static function groupsDataProvider()
     {
         return [
@@ -54,5 +25,28 @@ class GroupPermissionTest extends TestCase
             ['loved', 'isProjectLoved', false],
             ['nat', 'isNAT', false],
         ];
+    }
+
+    /**
+     * TODO: maybe an exclusion list of when groups are allowed with token instead...
+     */
+    #[DataProvider('groupsDataProvider')]
+    public function testGroupWithOAuth(string $group, string $method, bool $inGroup)
+    {
+        $user = User::factory()->withGroup($group)->create();
+        $client = Client::factory()->create(['user_id' => $user]);
+        $token = $this->createToken($user, ['*'], $client);
+        $this->actAsUserWithToken($token);
+
+        $this->assertSame($inGroup, auth()->user()->$method());
+    }
+
+    #[DataProvider('groupsDataProvider')]
+    public function testGroupWithoutOAuth(string $group, string $method)
+    {
+        $user = User::factory()->withGroup($group)->create();
+        $this->actAsUser($user);
+
+        $this->assertTrue(auth()->user()->$method());
     }
 }
