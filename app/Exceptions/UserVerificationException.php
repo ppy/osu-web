e822eb4b68d0bfd44dd8acce0bd833f30636e750
nanaya <me@nanaya.net>
2025-07-31 21:00:36 +0900

    Clean up verification exception class

--- app/Exceptions/UserVerificationException.php
+++ app/Exceptions/UserVerificationException.php
@@ -3,31 +3,25 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
 
+declare(strict_types=1);
+
 namespace App\Exceptions;
 
 use Exception;
 
 class UserVerificationException extends Exception
 {
-    private $reasonKey;
-    private $shouldReissue;
-
-    public function __construct(string $reasonKey, bool $shouldReissue)
+    public function __construct(private string $reasonKey, private bool $shouldReissue)
     {
-        $this->reasonKey = $reasonKey;
-        $this->shouldReissue = $shouldReissue;
-
-        $message = osu_trans("user_verification.errors.{$reasonKey}");
-
-        parent::__construct($message);
+        parent::__construct(osu_trans("user_verification.errors.{$reasonKey}"));
     }
 
-    public function reasonKey()
+    public function reasonKey(): string
     {
         return $this->reasonKey;
     }
 
-    public function shouldReissue()
+    public function shouldReissue(): bool
     {
         return $this->shouldReissue;
     }
