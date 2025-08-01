f21ea5b94cd7e94b970ce546dfa6828aaafb557c
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-08-01 14:03:43 +0200

    Add failing tests

--- tests/Models/Multiplayer/RoomTest.php
+++ tests/Models/Multiplayer/RoomTest.php
@@ -236,6 +236,48 @@ class RoomTest extends TestCase
         $this->assertSame('good word', $room->name);
     }
 
+    public function testRoomDifficultyRangeDoesNotIncludeExpiredItems_WhenFetchingRaw() {
+        $room = Room::factory()->create();
+
+        $firstBeatmap = Beatmap::factory()->create(['difficultyrating' => 1]);
+        $secondBeatmap = Beatmap::factory()->create(['difficultyrating' => 3]);
+        $thirdBeatmap = Beatmap::factory()->create(['difficultyrating' => 5]);
+        $fourthBeatmap = Beatmap::factory()->create(['difficultyrating' => 7]);
+
+        PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $firstBeatmap->getKey(), 'expired' => true]);
+        PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $secondBeatmap->getKey(), 'expired' => false]);
+        PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $thirdBeatmap->getKey(), 'expired' => false]);
+        PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $fourthBeatmap->getKey(), 'expired' => true]);
+
+        $difficultyRange = $room->difficultyRange();
+        $this->assertSame(3.0, $difficultyRange['min']);
+        $this->assertSame(5.0, $difficultyRange['max']);
+    }
+
+    public function testRoomDifficultyRangeDoesNotIncludeExpiredItems_WhenNotFetchingRaw() {
+        $room = Room::factory()->create();
+
+        $firstBeatmap = Beatmap::factory()->create(['difficultyrating' => 1]);
+        $secondBeatmap = Beatmap::factory()->create(['difficultyrating' => 3]);
+        $thirdBeatmap = Beatmap::factory()->create(['difficultyrating' => 5]);
+        $fourthBeatmap = Beatmap::factory()->create(['difficultyrating' => 7]);
+
+        $firstItem = PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $firstBeatmap->getKey(), 'expired' => true]);
+        $firstItem->setRelation('beatmap', $firstBeatmap);
+        $secondItem = PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $secondBeatmap->getKey(), 'expired' => false]);
+        $secondItem->setRelation('beatmap', $secondBeatmap);
+        $thirdItem = PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $thirdBeatmap->getKey(), 'expired' => false]);
+        $thirdItem->setRelation('beatmap', $thirdBeatmap);
+        $fourthItem = PlaylistItem::factory()->create(['room_id' => $room, 'beatmap_id' => $fourthBeatmap->getKey(), 'expired' => true]);
+        $fourthItem->setRelation('beatmap', $fourthBeatmap);
+
+        $room->setRelation('playlist', collect([$firstItem, $secondItem, $thirdItem, $fourthItem]));
+
+        $difficultyRange = $room->difficultyRange();
+        $this->assertSame(3.0, $difficultyRange['min']);
+        $this->assertSame(5.0, $difficultyRange['max']);
+    }
+
     public static function startGameDurationDataProvider()
     {
         static $dayMinutes = 1440;
