63e09fdbac62282215859b0fbcc61309d558e26a
Dan Balasescu <smoogipoo@smgi.me>
2025-08-04 21:25:23 +0900

    Allow matchmaking rooms with large playlist

--- app/Models/Multiplayer/Room.php
+++ app/Models/Multiplayer/Room.php
@@ -77,6 +77,7 @@ class Room extends Model
     ];
 
     const PLAYLIST_TYPE = 'playlists';
+    const MATCHMAKING_TYPE = 'matchmaking';
     const REALTIME_DEFAULT_TYPE = 'head_to_head';
     const REALTIME_TYPES = ['head_to_head', 'team_versus', 'matchmaking'];
 
@@ -681,7 +682,9 @@ class Room extends Model
         $playlistItemsCount = count($playlistItems);
 
         if ($this->isRealtime() && $playlistItemsCount !== 1) {
-            throw new InvariantException('realtime room must have exactly one playlist item');
+            if ($this->type !== static::MATCHMAKING_TYPE) {
+                throw new InvariantException('realtime room must have exactly one playlist item');
+            }
         }
 
         if ($playlistItemsCount < 1) {
