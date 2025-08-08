cebda2683427c1843de479afae97057f7657e95d
bakaneko <notbakaneko@users.noreply.github.com>
2025-08-08 23:27:16 +0900

    apply exclusion to correct level

--- app/Libraries/Search/BeatmapsetSearch.php
+++ app/Libraries/Search/BeatmapsetSearch.php
@@ -271,7 +271,7 @@ class BeatmapsetSearch extends RecordSearch
 
         if ($this->excludes->featuredArtist !== null) {
             $trackIds = ArtistTrack::where('artist_id', $this->excludes->featuredArtist)->pluck('id');
-            $this->nestedMustNot->should(['terms' => ['track_id' => $trackIds]]);
+            $this->query->mustNot(['terms' => ['track_id' => $trackIds]]);
         }
     }
 
@@ -572,7 +572,7 @@ class BeatmapsetSearch extends RecordSearch
         if ($include) {
             $this->query->must($subQuery);
         } else {
-            $this->nestedMustNot->should($subQuery);
+            $this->query->mustNot($subQuery);
         }
     }
 
