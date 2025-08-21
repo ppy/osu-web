e2e66ae1a6cd91f6c071b418f2546701125c5727
bakaneko <notbakaneko@users.noreply.github.com>
2025-08-21 20:06:49 +0900

    offset beatmap years

--- tests/Libraries/Search/BeatmapsetSearch/RankedFilterTest.php
+++ tests/Libraries/Search/BeatmapsetSearch/RankedFilterTest.php
@@ -8,27 +8,30 @@ declare(strict_types=1);
 namespace Tests\Libraries\Search\BeatmapsetSearch;
 
 use App\Models\Beatmapset;
-use DateTime;
-use DateTimeInterface;
+use Carbon\CarbonImmutable;
 
+// Includes created and updated date tests.
 class RankedFilterTest extends TestCase
 {
     public static function dataProvider(): array
     {
-        // Includes created and updated date tests.
         static $keys = ['created', 'ranked', 'updated'];
 
         $data = [];
-        foreach ($keys as $key) {
+        foreach ($keys as $index => $key) {
             // BeatmapsetQueryParserTest will handle the other date formats
-            $data[] = [['q' => "{$key}=2024"], [2, 3, 4]];
-            $data[] = [['q' => "{$key}=2024-02"], [2, 3]];
-            $data[] = [['q' => "{$key}=2024-02-28"], [3]];
-            $data[] = [['q' => "{$key}=2024"], [2, 3, 4]];
-            $data[] = [['q' => "{$key}>2023"], [2, 3, 4]];
-            $data[] = [['q' => "{$key}>=2023"], [1, 2, 3, 4]];
-            $data[] = [['q' => "{$key}<2023"], [0]];
-            $data[] = [['q' => "{$key}<=2023"], [0, 1]];
+            $year = 2023 + $index;
+
+            $data[] = [['q' => "{$key}={$year}"], [2, 3, 4]];
+            $data[] = [['q' => "{$key}={$year}-02"], [2, 3]];
+            $data[] = [['q' => "{$key}={$year}-02-28"], [3]];
+            $data[] = [['q' => "{$key}={$year}"], [2, 3, 4]];
+
+            $year = 2022 + $index;
+            $data[] = [['q' => "{$key}>{$year}"], [2, 3, 4]];
+            $data[] = [['q' => "{$key}>={$year}"], [1, 2, 3, 4]];
+            $data[] = [['q' => "{$key}<{$year}"], [0]];
+            $data[] = [['q' => "{$key}<={$year}"], [0, 1]];
         }
 
         return $data;
@@ -38,16 +41,17 @@ class RankedFilterTest extends TestCase
     {
         static::withDbAccess(function () {
             $factory = Beatmapset::factory()->withBeatmaps();
-            $helper = fn (DateTimeInterface $date) => $factory->ranked($date)->state([
-                'last_update' => $date,
-                'submit_date' => $date,
+            $helper = fn (CarbonImmutable $date) => $factory->ranked($date)->state([
+                'last_update' => $date->addYearNoOverflow(1),
+                'submit_date' => $date->addYearNoOverflow(-1),
             ]);
+
             static::$beatmapsets = [
-                $helper(new DateTime('2022-02-25'))->state(['approved' => Beatmapset::STATES['approved']])->create(),
-                $helper(new DateTime('2023-02-26'))->state(['approved' => Beatmapset::STATES['loved']])->create(),
-                $helper(new DateTime('2024-02-27'))->create(),
-                $helper(new DateTime('2024-02-28'))->create(),
-                $helper(new DateTime('2024-03-05'))->create(),
+                $helper(new CarbonImmutable('2022-02-25'))->state(['approved' => Beatmapset::STATES['approved']])->create(),
+                $helper(new CarbonImmutable('2023-02-26'))->state(['approved' => Beatmapset::STATES['loved']])->create(),
+                $helper(new CarbonImmutable('2024-02-27'))->create(),
+                $helper(new CarbonImmutable('2024-02-28'))->create(),
+                $helper(new CarbonImmutable('2024-03-05'))->create(),
             ];
         });
 
