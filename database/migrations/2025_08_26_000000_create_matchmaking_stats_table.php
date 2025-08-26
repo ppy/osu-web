e06ee14fe5e1e76eefae2244ce6506e49d227b43
Dan Balasescu <smoogipoo@smgi.me>
2025-08-26 12:25:32 +0900

    Add `matchmaking_stats` table (#2)

---
+++ database/migrations/2025_08_26_000000_create_matchmaking_stats_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matchmaking_stats', function (Blueprint $table) {
+            $table->unsignedMediumInteger('user_id')->nullable(false);
+            $table->unsignedMediumInteger('first_placements')->default(0);
+
+            $table->primary('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matchmaking_stats');
+    }
+};
