16ea6a05db1f6613aedd713aa011e70f621a979f
bakaneko <notbakaneko@users.noreply.github.com>
2025-08-27 12:21:25 +0900

    tokenising on non-whitespace was a bad idea. Also fix spelling

--- app/Libraries/Elasticsearch/QueryHelper.php
+++ app/Libraries/Elasticsearch/QueryHelper.php
@@ -27,26 +27,58 @@ class QueryHelper
         throw new Exception('$clause should be associative array or Queryable.');
     }
 
-    public static function tokenize(string $query): array
+    /**
+     * Tokenises and parses a search query into include and exclude portions.
+     * Multiple whitespaces are not preserved and treated as a single whitespace.
+     */
+    public static function tokenise(string $query): array
     {
+        $query = preg_replace('/[\s\pZ\pC]+/u', ' ', $query);
         $parts = [
             'exclude' => [],
             'include' => [],
         ];
 
         $mode = 'include';
+        $phrase = '';
+        $quoteOpened = false;
+        $quoteClosed = false;
+
         $token = strtok($query, ' ');
         while ($token !== false) {
-            $mode = str_starts_with($token, '-') ? 'exclude' : 'include';
-            $word = ltrim($token, '-');
-            if (str_starts_with($word, '"')) {
-                $token = strtok('"');
-                $parts[$mode][] = $word.' '.$token.'"';
-            } else {
-                $parts[$mode][] = $word;
+            $word = $token;
+            if ($phrase === '') {
+                $mode = str_starts_with($token, '-') ? 'exclude' : 'include';
+                $word = ltrim($token, '-');
+            }
+
+            if ($quoteOpened) {
+                $phrase .= ' ';
+            }
+
+            $phrase .= $word;
+
+            if (!$quoteOpened) {
+                $quoteOpened = str_starts_with($word, '"');
+            }
+            if ($quoteOpened) {
+                $quoteClosed = $phrase !== '"' && str_ends_with($word, '"');
+            }
+
+            // don't push if still looking for closing quote
+            if (!($quoteOpened && !$quoteClosed)) {
+                $parts[$mode][] = $phrase;
+                $phrase = '';
+                $quoteOpened = false;
+                $quoteClosed = false;
             }
 
             $token = strtok(' ');
+        }
+
+        // push remainder if missing closing quote.
+        if ($phrase !== '') {
+            $parts[$mode][] = $phrase;
         }
 
         return $parts;
