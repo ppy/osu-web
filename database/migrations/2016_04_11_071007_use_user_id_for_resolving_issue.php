d0ba55e40c7cb2b77d473eab3ac1695ad8ca845d
nanaya <me@nanaya.net>
2025-09-04 22:31:50 +0900

    Add strict to migration files and create better default

--- database/migrations/2016_04_11_071007_use_user_id_for_resolving_issue.php
+++ database/migrations/2016_04_11_071007_use_user_id_for_resolving_issue.php
@@ -2,6 +2,9 @@
 
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
 // See the LICENCE file in the repository root for full licence text.
+
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 
 class UseUserIdForResolvingIssue extends Migration
