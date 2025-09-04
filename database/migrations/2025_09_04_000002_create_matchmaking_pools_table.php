2e61107c8c4349400d096cbd6bb94ccc827c5b4f
Dan Balasescu <smoogipoo@smgi.me>
2025-09-04 12:42:45 +0900

    Add matchmaking pool table migrations (#4)

---
+++ database/migrations/2025_09_04_000002_create_matchmaking_pools_table.php
@@ -0,0 +1,35 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matchmaking_pools', function (Blueprint $table) {
+            $table->increments('id');
+            $table->smallInteger('ruleset_id');
+            $table->string('name');
+            $table->boolean('active');
+            $table->timestamps();
+
+            $table->index(['ruleset_id', 'active']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matchmaking_pools');
+    }
+};
