00164151b65d3a39c5a673ad360229227ddc0d5f
bakaneko <notbakaneko@users.noreply.github.com>
2025-09-08 15:57:50 +0900

    Revert search score ordering tests. Testing for an exact order is currently too flakey.
    Keep the updated query string logic.
    negative boost should be safe to test... maybe

--- tests/Libraries/Search/BeatmapsetSearch/TitleFilterTest.php
+++ tests/Libraries/Search/BeatmapsetSearch/TitleFilterTest.php
@@ -14,17 +14,17 @@ class TitleFilterTest extends TestCase
     public static function dataProvider(): array
     {
         return [
-            [['q' => 'best'], [0, 4, 1, 3, 2], true],
-            [['q' => 'best beatmap'], [1, 3, 2, 0, 4], true],
-            [['q' => '"best beatmap"'], [1, 3, 2], true],
+            [['q' => 'best'], [0, 1, 2, 3, 4]],
+            [['q' => 'best beatmap'], [0, 1, 2, 3, 4]],
+            [['q' => '"best beatmap"'], [1, 2, 3]],
             [['q' => '-best'], []],
             [['q' => '-best -beatmap'], []],
-            [['q' => '-"best beatmap"'], [4, 0], true],
+            [['q' => '-"best beatmap"'], [0, 4]],
 
-            [['q' => 'title=best'], [0, 1, 3, 2], true],
-            [['q' => 'title="best beatmap"'], [1, 3, 2], true],
-            [['q' => 'title="the beatmap"'], [2, 3], true],
-            [['q' => 'title=""best beatmap""'], [1, 3, 2], true],
+            [['q' => 'title=best'], [0, 1, 2, 3]],
+            [['q' => 'title="best beatmap"'], [1, 2, 3]],
+            [['q' => 'title="the beatmap"'], [1, 2]],
+            [['q' => 'title=""best beatmap""'], [1, 2, 3]],
             [['q' => 'title=""the beatmap""'], []],
         ];
     }
@@ -32,13 +32,12 @@ class TitleFilterTest extends TestCase
     public static function setUpBeforeClass(): void
     {
         static::withDbAccess(function () {
-            $factory = Beatmapset::factory()->consistent()->ranked()->withBeatmaps();
+            $factory = Beatmapset::factory()->ranked()->withBeatmaps();
             static::$beatmapsets = [
                 $factory->create(['title' => 'best']),
-                $factory->create(['title' => 'best beatmap']),
                 $factory->create(['title' => 'the best beatmap']),
-                $factory->create(['title' => 'the best beatmap', 'title_unicode' => 'ダ best beatmap']), // scores slightly better in prefix matching.
-                // this will score better in the 'best' test due to the term having lower frequency in the artist field.
+                $factory->create(['title' => 'the best beatmap', 'title_unicode' => 'ダ best beatmap']),
+                $factory->create(['title' => 'best beatmap']),
                 $factory->create(['artist' => 'the best artist']),
             ];
         });
