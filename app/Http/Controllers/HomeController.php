7bb661413c71289a95292a7d2cc92fe2387902c3
Joseph Madamba <madamba.joehu@outlook.com>
2025-09-16 20:11:58 -0700

    Update android requirement to 9+ for osu!(lazer)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,7 +61,7 @@ class HomeController extends Controller
     public function getDownload()
     {
         $lazerPlatformNames = [
-            'android' => osu_trans('home.download.os_version_or_later', ['os_version' => 'Android 5']),
+            'android' => osu_trans('home.download.os_version_or_later', ['os_version' => 'Android 9']),
             'ios' => osu_trans('home.download.os_version_or_later', ['os_version' => 'iOS 13.4']),
             'linux_x64' => 'Linux (x64)',
             'macos_as' => osu_trans('home.download.os_version_or_later', ['os_version' => 'macOS 10.15']).' (Apple Silicon)',
