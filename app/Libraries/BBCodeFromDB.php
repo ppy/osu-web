4ecb567cf999cfa51ca6e5bf18f4de2836d36949
kama <steamnitz@gmail.com>
2025-10-02 12:02:34 +0200

    Update BBCodeFromDB.php

--- app/Libraries/BBCodeFromDB.php
+++ app/Libraries/BBCodeFromDB.php
@@ -85,7 +85,7 @@ class BBCodeFromDB
 
 	public function parseCentre($text)
 	{
-		$text = str_replace("[centre:{$this->uid}]", "<div class='bbcode--centre'>", $text);
+		$text = str_replace("[centre:{$this->uid}]", "<div data-align='center'>", $text);
 		$text = str_replace("[/centre:{$this->uid}]", '</div>', $text);
 
 		return $text;
@@ -93,7 +93,7 @@ class BBCodeFromDB
 
 	public function parseRight($text)
 	{
-		$text = str_replace("[right:{$this->uid}]", "<div class='bbcode--right'>", $text);
+		$text = str_replace("[right:{$this->uid}]", "<div data-align='right'>", $text);
 		$text = str_replace("[/right:{$this->uid}]", '</div>', $text);
 
 	return $text;
