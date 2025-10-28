c426ff38918dae76517e1d1f326f27ca1f2e213d
bakaneko <notbakaneko@users.noreply.github.com>
2025-10-28 21:19:39 +0900

    update mobiledetect and add extras that are relevant

---
+++ app/Libraries/Agent.php
@@ -0,0 +1,37 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+declare(strict_types=1);
+
+namespace App\Libraries;
+
+use Detection\MobileDetect;
+
+class Agent extends MobileDetect
+{
+    private static array $additionalOperatingSystems = [
+        'Windows' => 'Windows|Windows NT [VER]',
+        'OS X' => 'Mac OS X|OS X [VER]',
+        'Linux' => 'Linux',
+    ];
+
+    #[\Override]
+    public function getRules(): array
+    {
+        static $rules;
+
+        if (!isset($rules)) {
+            $rules = [
+                ...static::$browsers,
+                ...static::$operatingSystems,
+                ...static::$phoneDevices,
+                ...static::$tabletDevices,
+                ...static::$additionalOperatingSystems,
+            ];
+        }
+
+        return $rules;
+    }
+}
