dd9edfa19030f23a88ee194889ba7c4fcb63ce88
Dan Balasescu <smoogipoo@smgi.me>
2025-11-04 16:54:00 +0900

    Add various indexes

--- database/migrations/2025_11_04_071640_add_rating_to_matchmaking_user_stats.php
+++ database/migrations/2025_11_04_071640_add_rating_to_matchmaking_user_stats.php
@@ -15,12 +15,20 @@ return new class extends Migration
     {
         Schema::table('matchmaking_user_stats', function (Blueprint $table) {
             $table->integer('rating')->after('total_points')->default(1500);
+
+            $table->index(['first_placements']);
+            $table->index(['total_points']);
+            $table->index(['rating']);
         });
     }
 
     public function down(): void
     {
         Schema::table('matchmaking_user_stats', function (Blueprint $table) {
+            $table->dropIndex(['first_placements']);
+            $table->dropIndex(['total_points']);
+            $table->dropIndex(['rating']);
+
             $table->dropColumn('rating');
         });
     }
