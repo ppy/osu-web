b0e1c3eee97f7c213e4160e1e8e607c47017b2ce
Dan Balasescu <smoogipoo@smgi.me>
2025-11-12 16:38:40 +0900

    Add `matchmaking_room_events` table

---
+++ database/migrations/2025_11_12_073551_create_matchmaking_room_events.php
@@ -0,0 +1,31 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('matchmaking_room_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('room_id');
+            $table->string('event_type');
+            $table->bigInteger('playlist_item_id')->nullable();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->json('event_detail')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('matchmaking_room_events');
+    }
+};
