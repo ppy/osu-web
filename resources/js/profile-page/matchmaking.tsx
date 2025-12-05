c8964f475c7e66ee71ae8f5a475a97f79cd238f2
nanaya <me@nanaya.net>
2025-12-06 03:19:49 +0900

    Hide the quickplay badge if there's no play at all

--- resources/js/profile-page/matchmaking.tsx
+++ resources/js/profile-page/matchmaking.tsx
@@ -14,6 +14,9 @@ interface Props {
 @observer
 export default class Matchmaking extends React.Component<Props> {
   render() {
+    if (this.props.allStats.length === 0) {
+      return null;
+    }
     let highestRank: null | number = null;
     for (const stats of this.props.allStats) {
       const rank = stats.rank;
