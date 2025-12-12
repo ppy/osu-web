fd96be15f66e93504001ac9d1a860fb6c23b7ea2
ThePooN <thepoon@osufr.live>
2025-12-12 18:43:29 +0100

    ✨ Add php.ini overrides, fix max upload size

--- config/octane.php
+++ config/octane.php
@@ -127,13 +127,6 @@ return [
     'flush' => [
     ],
 
-    'swoole' => [
-        'options' => [
-            // default of 10mb is too low for beatmap contest uploads
-            'package_max_length' => 32 * 1024 * 1024,
-        ],
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Octane Cache Table
