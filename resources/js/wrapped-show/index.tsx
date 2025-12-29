39755746ff630cc524db07b7f80d23f6240f3e16
nanaya <me@nanaya.net>
2025-12-29 21:51:37 +0900

    Remove confusing stats

--- resources/js/wrapped-show/index.tsx
+++ resources/js/wrapped-show/index.tsx
@@ -626,7 +626,6 @@ export default class WrappedShow extends React.Component<WrappedData> {
             value={`top ${formatNumber(summary.scores.playcount.top_percent, 2, { style: 'percent' })}`}
           />
           <WrappedStat modifiers='fancy' title='Total playcount' value={summary.scores.playcount.playcount} />
-          <WrappedStat modifiers='fancy' round title='Total pp' value={summary.scores.pp} />
           <WrappedStat modifiers='fancy' title='Highest score' value={summary.scores.score} />
           <WrappedStat modifiers='fancy' percent title='Average accuracy' value={summary.scores.acc} />
           <WrappedStat modifiers='fancy' title='Highest combo' value={summary.scores.combo} />
