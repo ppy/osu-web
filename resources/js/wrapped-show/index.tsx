995886203d7c9dd8348a1720d648eeab4a612d42
bakaneko <notbakaneko@users.noreply.github.com>
2025-12-29 21:38:34 +0900

    remove space before pp

--- resources/js/wrapped-show/index.tsx
+++ resources/js/wrapped-show/index.tsx
@@ -100,7 +100,7 @@ function TopPlay(props: { beatmap?: BeatmapForWrappedJson; play: TopPlay }) {
           {beatmapset != null ? getTitle(beatmapset) : trans('beatmapsets.cover.deleted')}
         </div>
         <div className='wrapped__summary-list-item-value'>
-          {formatNumber(Math.round(props.play.pp))} pp
+          {formatNumber(Math.round(props.play.pp))}pp
         </div>
       </div>
     </a>
