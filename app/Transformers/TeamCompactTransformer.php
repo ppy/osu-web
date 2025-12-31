6be57730f8469ee0251ab848d97b03692f001317
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2025-12-31 01:58:23 +0100

    add missing license and strict_types

--- app/Transformers/TeamCompactTransformer.php
+++ app/Transformers/TeamCompactTransformer.php
@@ -1,5 +1,10 @@
 <?php
 
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+declare(strict_types=1);
+
 namespace App\Transformers;
 
 use App\Models\Team;
