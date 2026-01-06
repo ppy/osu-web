e103232ce180c9a94626e35426b30855392b950f
nanaya <me@nanaya.net>
2026-01-06 18:17:54 +0900

    Always overwrite if current best score is 0

--- app/Models/UserSummary.php
+++ app/Models/UserSummary.php
@@ -346,7 +346,7 @@ class UserSummary extends Model
                 $ret['pp_avg'] += $pp;
                 $ppScoreCount++;
             }
-            if ($score->total_score > $ret['score_best']) {
+            if ($score->total_score > $ret['score_best'] || $ret['score_best'] === 0) {
                 $ret['score_best'] = $score->total_score;
                 $ret['score_best_beatmap_id'] = $score->beatmap_id;
             }
