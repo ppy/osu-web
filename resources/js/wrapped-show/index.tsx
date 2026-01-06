7031725479c33ed3d538397262ad7ab6cc999c63
nanaya <me@nanaya.net>
2026-01-06 18:36:10 +0900

    Fix switcher on mobile

--- resources/js/wrapped-show/index.tsx
+++ resources/js/wrapped-show/index.tsx
@@ -702,6 +702,7 @@ export default class WrappedShow extends React.Component<WrappedData> {
         className={classWithModifiers('wrapped__switcher-item', { active: index === this.selectedIndex })}
         data-index={index}
         onClick={this.handleSwitcherOnClick}
+        onTouchEnd={this.handleSwitcherOnClick}
         title={pageTitles[page]}
       >
         <img src={this.backgroundForSwitcher(page, 0)} />
