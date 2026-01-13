041aa4611826c38e21312be93c823a7d83077c73
Hivie <me@hivie.tn>
2026-01-13 12:18:11 +0100

    update typing

--- resources/js/beatmapsets-show/info.tsx
+++ resources/js/beatmapsets-show/info.tsx
@@ -229,7 +229,7 @@ export default class Info extends React.Component<Props> {
               </div>
             }
 
-            {this.controller.beatmapset.pack_tags && this.controller.beatmapset.pack_tags.length > 0 &&
+            {this.controller.beatmapset.pack_tags.length > 0 &&
               <div className='beatmapset-info__row'>
                 <h3 className='beatmapset-info__header beatmapset-info__header--sticky'>
                   {trans('beatmapsets.show.info.pack_tags')}
