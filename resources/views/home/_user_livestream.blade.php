7aee99b5df85bc6a399c3accb1f16bb42ca36880
Hivie <me@hivie.tn>
2026-01-29 17:23:11 +0100

    remove extra div

--- resources/views/home/_user_livestream.blade.php
+++ resources/views/home/_user_livestream.blade.php
@@ -2,11 +2,9 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
-<div class="user-home-livestream">
-    <div
-        id="home-livestream-{{ time() }}-{{ rand() }}"
-        class="js-twitch-player user-home-livestream__player"
-        data-channel="{{ $stream->data['user_name'] }}"
-        data-autoplay="false"
-    ></div>
-</div>
+<div
+    id="home-livestream-{{ time() }}-{{ rand() }}"
+    class="js-twitch-player user-home-livestream"
+    data-channel="{{ $stream->data['user_name'] }}"
+    data-autoplay="false"
+></div>
