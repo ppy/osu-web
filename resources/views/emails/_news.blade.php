291662759a9bd1d1560e32544220927a10f366d2
bakaneko <notbakaneko@users.noreply.github.com>
2026-01-30 20:59:17 +0900

    move news updates to separate section in mail

---
+++ resources/views/emails/_news.blade.php
@@ -0,0 +1,12 @@
+{{--
+    Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+    See the LICENCE file in the repository root for full licence text.
+--}}
+# {!! osu_trans('notifications.mail.news_updates') !!}
+
+@foreach ($news as $details)
+## {!! $details['title'] !!}
+{!! $details['link'] !!}
+
+@endforeach
+---
