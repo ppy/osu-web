ec35ab8df3a80f3e830d7423a36bd5528e3bfbf9
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2026-02-15 14:10:55 +0100

    Remove more dead code

--- resources/js/beatmaps/user-tag-picker-button.tsx
+++ resources/js/beatmaps/user-tag-picker-button.tsx
@@ -3,20 +3,16 @@
 
 import PopupMenu from 'components/popup-menu';
 import { observer } from 'mobx-react';
-import core from 'osu-core-singleton';
 import React, { useCallback } from 'react';
-import { classWithModifiers } from 'utils/css';
 import { trans } from 'utils/lang';
 import UserTagPicker from './user-tag-picker';
-
-const controller = core.beatmapTagPickerController;
 
 const UserTagPickerButton = observer(() => {
   const renderButton = useCallback((children: React.ReactNode, ref: React.RefObject<HTMLDivElement>, toggle: (event: React.MouseEvent<HTMLElement>) => void) => (
     <>
       <div
         ref={ref}
-        className={classWithModifiers('beatmapsets-search__icon', { active: controller.showPicker, tags: true })}
+        className='beatmapsets-search__icon beatmapsets-search__icon--tags'
         onClick={toggle}
         title={trans('beatmaps.listing.search.tag_picker.tooltip')}
       >
