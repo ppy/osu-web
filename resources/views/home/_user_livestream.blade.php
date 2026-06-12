b1825967efd5caec7a17e8ab56e7e62f9ce2b924
Hivie <me@hivie.tn>
2026-06-12 05:20:26 +0100

    replace homepage livestream embed with static preview

--- resources/views/home/_user_livestream.blade.php
+++ resources/views/home/_user_livestream.blade.php
@@ -2,9 +2,23 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
-<div
-    id="home-livestream-{{ time() }}-{{ rand() }}"
-    class="js-twitch-player user-home-livestream"
-    data-channel="{{ $stream->data['user_name'] }}"
-    data-autoplay="false"
-></div>
+<div class="user-home-livestream">
+    <a class="user-home-livestream__content" href="{{ $stream->url() }}" target="_blank">
+        <div
+            class="user-home-livestream__image"
+            style="background-image: url('{{ $stream->preview(640, 360) }}');"
+        ></div>
+
+        <p class="user-home-livestream__text user-home-livestream__text--title">
+            {{ $stream->data['title'] }} b
+        </p>
+
+        <p class="user-home-livestream__text user-home-livestream__text--name">
+            {{ $stream->data['user_name'] }}
+        </p>
+
+        <p class="user-home-livestream__text user-home-livestream__text--detail">
+            {{ $stream->data['viewer_count'] }} <i class="fas fa-eye"></i>
+        </p>
+    </a>
+</div>
