8c48e82d18f13aaf7908b31f317ea54c8238173e
Hivie <me@hivie.tn>
2026-06-15 10:53:33 +0100

    cleanup styling

--- resources/views/home/_user_livestream.blade.php
+++ resources/views/home/_user_livestream.blade.php
@@ -2,25 +2,23 @@
     Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
     See the LICENCE file in the repository root for full licence text.
 --}}
-<div class="user-home-livestream">
-    <a
-        class="user-home-livestream__content"
-        href="{{ $stream->url() }}"
-        style="background-image: url('{{ $stream->preview(640, 360) }}');"
-        target="_blank"
-    >
-        <div class="user-home-livestream__info">
-            <h3 class="user-home-livestream__text user-home-livestream__text--name">
-                {{ $stream->data['user_name'] }}
-            </h3>
+<a
+    class="user-home-livestream"
+    href="{{ $stream->url() }}"
+    style="background-image: url('{{ $stream->preview(640, 360) }}');"
+    target="_blank"
+>
+    <div class="user-home-livestream__info">
+        <h3 class="user-home-livestream__text user-home-livestream__text--name">
+            {{ $stream->data['user_name'] }}
+        </h3>
 
-            <p class="user-home-livestream__text user-home-livestream__text--detail">
-                {{ $stream->data['title'] }}
-            </p>
+        <p class="user-home-livestream__text">
+            {{ $stream->data['title'] }}
+        </p>
 
-            <p class="user-home-livestream__text user-home-livestream__text--detail">
-                {{ $stream->data['viewer_count'] }} <i class="fas fa-eye"></i>
-            </p>
-        </div>
-    </a>
-</div>
+        <p class="user-home-livestream__text">
+            {{ $stream->data['viewer_count'] }} <i class="fas fa-eye"></i>
+        </p>
+    </div>
+</a>
