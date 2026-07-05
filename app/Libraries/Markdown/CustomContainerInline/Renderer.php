c58526d21b8da0190b9dd029bc6870ed2a11e479
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2026-07-05 23:28:39 +0700

    add createFlagElement function

--- app/Libraries/Markdown/CustomContainerInline/Renderer.php
+++ app/Libraries/Markdown/CustomContainerInline/Renderer.php
@@ -5,6 +5,7 @@
 
 namespace App\Libraries\Markdown\CustomContainerInline;
 
+use Dflydev\DotAccessData\DataInterface;
 use League\CommonMark\Node\Node;
 use League\CommonMark\Renderer\ChildNodeRendererInterface;
 use League\CommonMark\Renderer\NodeRendererInterface;
@@ -12,28 +13,41 @@ use League\CommonMark\Util\HtmlElement;
 
 class Renderer implements NodeRendererInterface
 {
+    private DataInterface $attrs;
+    private ChildNodeRendererInterface $childRenderer;
+    private Node $node;
+
     public function render(Node $node, ChildNodeRendererInterface $childRenderer)
     {
         Element::assertInstanceOf($node);
 
-        $attrs = $node->data->getData('attributes');
+        $this->node = $node;
+        $this->childRenderer = $childRenderer;
+        $this->attrs = $this->node->data->getData('attributes');
 
-        $code = presence($attrs->get('flag', null));
+        $code = presence($this->attrs->get('flag', null));
         if ($code !== null) {
-            $attrs->remove('flag');
-            $attrs->set('class', 'flag-country flag-country--flat flag-country--wiki');
-            $attrs->set('style', "background-image: url('".flag_url($code)."')");
-
-            $country = app('countries')->byCode($code);
-            if ($country !== null) {
-                $attrs->set('title', $country->name);
-            }
+            return $this->createFlagElement($code);
         }
 
         return new HtmlElement(
             'span',
-            $attrs->export(),
-            $childRenderer->renderNodes($node->children()),
+            $this->attrs->export(),
+            $this->childRenderer->renderNodes($this->node->children()),
         );
+    }
+
+    private function createFlagElement(string $code)
+    {
+        $this->attrs->remove('flag');
+        $this->attrs->set('class', 'flag-country flag-country--flat flag-country--wiki');
+        $this->attrs->set('style', "background-image: url('".flag_url($code)."')");
+
+        $country = app('countries')->byCode($code);
+        if ($country !== null) {
+            $this->attrs->set('title', $country->name);
+        }
+
+        return new HtmlElement('span', $this->attrs->export(), '');
     }
 }
