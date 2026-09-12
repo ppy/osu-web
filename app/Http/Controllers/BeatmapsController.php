ff6e65a02afc225fe20fc985f3438ed89ed9c1e3
Dean Herbert <pe@ppy.sh>
2026-09-12 17:04:23 +0900

    Add toggle for returning leaderboard counts

--- app/Http/Controllers/BeatmapsController.php
+++ app/Http/Controllers/BeatmapsController.php
@@ -89,7 +89,9 @@ class BeatmapsController extends Controller
 
         $isApi = is_api_request();
 
-        if ($isApi) {
+        $totalsEnabled = $GLOBALS['cfg']['osu']['scores']['leaderboard_totals_enabled'];
+
+        if ($isApi && $totalsEnabled) {
             $results['score_count'] = UserRank::getCount($esFetch->baseParams);
         }
 
