3e3c8aaa07452881c7a5107b11bcb573f0bb1922
bakaneko <notbakaneko@users.noreply.github.com>
2026-09-17 19:32:51 +0900

    migrate BeatmapList to using SelectOptions

--- resources/js/beatmap-discussions/beatmap-list.tsx
+++ resources/js/beatmap-discussions/beatmap-list.tsx
@@ -2,140 +2,68 @@
 // See the LICENCE file in the repository root for full licence text.
 
 import BeatmapListItem from 'components/beatmap-list-item';
-import BeatmapExtendedJson from 'interfaces/beatmap-extended-json';
-import UserJson from 'interfaces/user-json';
-import { action, autorun, computed, makeObservable, observable } from 'mobx';
-import { disposeOnUnmount, observer } from 'mobx-react';
+import SelectOptions from 'components/select-options';
+import { action, computed, makeObservable } from 'mobx';
+import { observer } from 'mobx-react';
 import * as React from 'react';
 import { makeUrl } from 'utils/beatmapset-discussion-helper';
-import { blackoutToggle } from 'utils/blackout';
-import { classWithModifiers } from 'utils/css';
-import { formatNumber } from 'utils/html';
-import { nextVal } from 'utils/seq';
+import { getInt } from 'utils/math';
 import DiscussionsState from './discussions-state';
 
 interface Props {
   discussionsState: DiscussionsState;
-  users: Map<number | null | undefined, UserJson>;
 }
 
 @observer
-export default class BeatmapList extends React.Component<Props> {
-  private readonly eventId = `beatmapset-discussions-show-beatmap-list-${nextVal()}`;
-  @observable private showingSelector = false;
-
+export default class BeatmapList extends React.PureComponent<Props> {
   @computed
-  private get beatmaps() {
-    return this.props.discussionsState.groupedBeatmaps.get(this.props.discussionsState.currentBeatmap.mode) ?? [];
+  private get options() {
+    const beatmaps = this.props.discussionsState.groupedBeatmaps.get(this.props.discussionsState.currentBeatmap.mode) ?? [];
+
+    return beatmaps.map((beatmap) => {
+      const count = this.props.discussionsState.unresolvedDiscussionCounts.byBeatmap[beatmap.id];
+      return {
+        href: makeUrl({ beatmap, filter: this.props.discussionsState.currentFilter }),
+        id: beatmap.id,
+        text: (
+          <BeatmapListItem
+            beatmap={beatmap}
+            beatmapUrl={makeUrl({ beatmap, filter: this.props.discussionsState.currentFilter })}
+            beatmapset={this.props.discussionsState.beatmapset}
+            count={count}
+            showNonGuestOwner={false}
+            showOwners
+          />
+        ),
+      };
+    });
   }
 
   constructor(props: Props) {
     super(props);
-
     makeObservable(this);
-    disposeOnUnmount(this, autorun(() => {
-      blackoutToggle(this, this.showingSelector);
-    }));
-  }
-
-  componentDidMount() {
-    $(document).on(`click.${this.eventId}`, this.onDocumentClick);
-    $(document).on(`turbo:before-cache.${this.eventId}`, this.handleBeforeCache);
-  }
-
-  componentWillUnmount() {
-    $(document).off(`.${this.eventId}`);
-    blackoutToggle(this, false);
   }
 
   render() {
     return (
-      <div className={classWithModifiers('beatmap-list', { selecting: this.showingSelector })}>
-        <div className='beatmap-list__body'>
-          <a
-            className='beatmap-list__item beatmap-list__item--selected beatmap-list__item--large js-beatmap-list-selector'
-            href={makeUrl({ beatmap: this.props.discussionsState.currentBeatmap })}
-            onClick={this.toggleSelector}
-          >
-            <BeatmapListItem beatmap={this.props.discussionsState.currentBeatmap} modifiers='large' showOwners={false} />
-            <div className='beatmap-list__item-selector-button'>
-              <span className='fas fa-chevron-down' />
-            </div>
-          </a>
-
-          <div className='beatmap-list__selector-container'>
-            <div className='beatmap-list__selector'>
-              {this.beatmaps.map(this.beatmapListItem)}
-            </div>
-          </div>
-        </div>
-      </div>
+      <SelectOptions
+        href={makeUrl({ beatmap: this.props.discussionsState.currentBeatmap })}
+        modifiers='beatmap-list'
+        onSelect={this.handleSelect}
+        options={this.options}
+        selected={this.props.discussionsState.currentBeatmapId}
+      >
+        <BeatmapListItem beatmap={this.props.discussionsState.currentBeatmap} modifiers='large' showOwners={false} />
+      </SelectOptions>
     );
   }
 
-  private readonly beatmapListItem = (beatmap: BeatmapExtendedJson) => {
-    const count = this.props.discussionsState.unresolvedDiscussionCounts.byBeatmap[beatmap.id];
-
-    return (
-      <div
-        key={beatmap.id}
-        className={classWithModifiers('beatmap-list__item', { current: beatmap.id === this.props.discussionsState.currentBeatmap.id })}
-        data-id={beatmap.id}
-        onClick={this.selectBeatmap}
-      >
-        <BeatmapListItem
-          beatmap={beatmap}
-          beatmapUrl={makeUrl({ beatmap, filter: this.props.discussionsState.currentFilter })}
-          beatmapset={this.props.discussionsState.beatmapset}
-          showNonGuestOwner={false}
-          showOwners
-        />
-        {count != null &&
-          <div className='beatmap-list__item-count'>
-            {formatNumber(count)}
-          </div>
-        }
-      </div>
-    );
-  };
-
   @action
-  private readonly handleBeforeCache = () => {
-    this.setShowingSelector(false);
-  };
-
-  @action
-  private readonly onDocumentClick = (e: JQuery.ClickEvent) => {
-    if (e.button !== 0) return;
-
-    if ($(e.target).closest('.js-beatmap-list-selector').length) {
-      return;
-    }
-
-    this.setShowingSelector(false);
-  };
-
-  @action
-  private readonly selectBeatmap = (e: React.MouseEvent<HTMLElement>) => {
-    if (e.button !== 0) return;
-    e.preventDefault();
-
-    const beatmapId = parseInt(e.currentTarget.dataset.id ?? '', 10);
+  private readonly handleSelect = (id?: string) => {
+    const beatmapId = getInt(id);
+    if (beatmapId == null) return;
 
     this.props.discussionsState.currentBeatmapId = beatmapId;
     this.props.discussionsState.changeDiscussionPage('timeline');
-  };
-
-  @action
-  private setShowingSelector(state: boolean) {
-    this.showingSelector = state;
-  }
-
-  @action
-  private readonly toggleSelector = (e: React.MouseEvent<HTMLElement>) => {
-    if (e.button !== 0) return;
-    e.preventDefault();
-
-    this.setShowingSelector(!this.showingSelector);
   };
 }
